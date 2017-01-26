a21e1eb541182253c7199211ba702b02df5213c9
Richard W.M. Jones <rjones@redhat.com>
2017-01-26 08:09:22 -0500

    RHEL 5: Define alternative macros for htobe32 etc.
    These macros and the header <endian.h> are not available on RHEL 5.
    
    These definitions are taken from Fedora 25's glibc.

--- src/internal.h
+++ src/internal.h
@@ -55,6 +55,42 @@
 #define SOCK_CLOEXEC 0
 #endif
 
+#ifndef htobe32
+#include <byteswap.h>
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe16(x) __bswap_16 (x)
+#  define htole16(x) (x)
+#  define be16toh(x) __bswap_16 (x)
+#  define le16toh(x) (x)
+
+#  define htobe32(x) __bswap_32 (x)
+#  define htole32(x) (x)
+#  define be32toh(x) __bswap_32 (x)
+#  define le32toh(x) (x)
+
+#  define htobe64(x) __bswap_64 (x)
+#  define htole64(x) (x)
+#  define be64toh(x) __bswap_64 (x)
+#  define le64toh(x) (x)
+
+# else
+#  define htobe16(x) (x)
+#  define htole16(x) __bswap_16 (x)
+#  define be16toh(x) (x)
+#  define le16toh(x) __bswap_16 (x)
+
+#  define htobe32(x) (x)
+#  define htole32(x) __bswap_32 (x)
+#  define be32toh(x) (x)
+#  define le32toh(x) __bswap_32 (x)
+
+#  define htobe64(x) (x)
+#  define htole64(x) __bswap_64 (x)
+#  define be64toh(x) (x)
+#  define le64toh(x) __bswap_64 (x)
+# endif
+#endif
+
 /* main.c */
 extern const char *exportname;
 extern const char *ipaddr;
