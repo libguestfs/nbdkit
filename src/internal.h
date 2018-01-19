libguestfs/nbdkit

commit 876d715e17a59bd25df53be34b942afd96e52da9

Refactor plugin_* functions into a backend struct.
Introduce the concept of a backend.  Currently the only type of
backend is a plugin, and there can only be one of them.  Instead of
calling functions like ‘plugin_pwrite’ you call the backend method
‘backend->pwrite (backend, ...)’.

The change is largely mechanical.  I was able to remove ‘assert (dl)’
statements throughout since we can now prove they will never be
called.

Note this does not lift the restriction of one plugin per server, and
it can *never* do that because plugins can use global variables.
Message-Id: <20180117205356.8699-3-rjones@redhat.com>

src/internal.h
@@ -35,6 +35,7 @@
 #define NBDKIT_INTERNAL_H
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdarg.h>
 #include <sys/socket.h>
 #include <pthread.h>
@@ -91,6 +92,11 @@
 # endif
 #endif
 
+#define container_of(ptr, type, member) ({                       \
+      const typeof (((type *) 0)->member) *__mptr = (ptr);       \
+      (type *) ((char *) __mptr - offsetof(type, member));       \
+    })
+
 /* main.c */
 extern const char *exportname;
 extern const char *ipaddr;
@@ -107,6 +113,8 @@ extern int threads;
 
 extern volatile int quit;
 extern int quit_fd;
+
+extern struct backend *backend;
 
 /* cleanup.c */
 extern void cleanup_free (void *ptr);
@@ -142,28 +150,31 @@ extern int crypto_negotiate_tls (struct connection *conn, int sockin, int sockou
 #define debug nbdkit_debug
 
 /* plugins.c */
-extern void plugin_register (const char *_filename, void *_dl, struct nbdkit_plugin *(*plugin_init) (void));
-extern void plugin_cleanup (void);
-extern int plugin_thread_model (void);
-extern const char *plugin_name (void);
-extern void plugin_usage (void);
-extern const char *plugin_version (void);
-extern void plugin_dump_fields (void);
-extern void plugin_config (const char *key, const char *value);
-extern void plugin_config_complete (void);
-extern int plugin_errno_is_preserved (void);
-extern int plugin_open (struct connection *conn, int readonly);
-extern void plugin_close (struct connection *conn);
-extern int64_t plugin_get_size (struct connection *conn);
-extern int plugin_can_write (struct connection *conn);
-extern int plugin_can_flush (struct connection *conn);
-extern int plugin_is_rotational (struct connection *conn);
-extern int plugin_can_trim (struct connection *conn);
-extern int plugin_pread (struct connection *conn, void *buf, uint32_t count, uint64_t offset);
-extern int plugin_pwrite (struct connection *conn, void *buf, uint32_t count, uint64_t offset);
-extern int plugin_flush (struct connection *conn);
-extern int plugin_trim (struct connection *conn, uint32_t count, uint64_t offset);
-extern int plugin_zero (struct connection *conn, uint32_t count, uint64_t offset, int may_trim);
+struct backend {
+  void (*free) (struct backend *);
+  int (*thread_model) (struct backend *);
+  const char *(*name) (struct backend *);
+  void (*usage) (struct backend *);
+  const char *(*version) (struct backend *);
+  void (*dump_fields) (struct backend *);
+  void (*config) (struct backend *, const char *key, const char *value);
+  void (*config_complete) (struct backend *);
+  int (*errno_is_preserved) (struct backend *);
+  int (*open) (struct backend *, struct connection *conn, int readonly);
+  void (*close) (struct backend *, struct connection *conn);
+  int64_t (*get_size) (struct backend *, struct connection *conn);
+  int (*can_write) (struct backend *, struct connection *conn);
+  int (*can_flush) (struct backend *, struct connection *conn);
+  int (*is_rotational) (struct backend *, struct connection *conn);
+  int (*can_trim) (struct backend *, struct connection *conn);
+  int (*pread) (struct backend *, struct connection *conn, void *buf, uint32_t count, uint64_t offset);
+  int (*pwrite) (struct backend *, struct connection *conn, void *buf, uint32_t count, uint64_t offset);
+  int (*flush) (struct backend *, struct connection *conn);
+  int (*trim) (struct backend *, struct connection *conn, uint32_t count, uint64_t offset);
+  int (*zero) (struct backend *, struct connection *conn, uint32_t count, uint64_t offset, int may_trim);
+};
+
+extern struct backend *plugin_register (const char *_filename, void *_dl, struct nbdkit_plugin *(*plugin_init) (void));
 
 /* locks.c */
 extern void lock_connection (void);

src/locks.c
@@ -45,7 +45,7 @@ static pthread_rwlock_t unload_prevention_lock = PTHREAD_RWLOCK_INITIALIZER;
 void
 lock_connection (void)
 {
-  int thread_model = plugin_thread_model ();
+  int thread_model = backend->thread_model (backend);
 
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS) {
     debug ("acquire connection lock");
@@ -56,7 +56,7 @@ lock_connection (void)
 void
 unlock_connection (void)
 {
-  int thread_model = plugin_thread_model ();
+  int thread_model = backend->thread_model (backend);
 
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS) {
     debug ("release connection lock");
@@ -67,7 +67,7 @@ unlock_connection (void)
 void
 lock_request (struct connection *conn)
 {
-  int thread_model = plugin_thread_model ();
+  int thread_model = backend->thread_model (backend);
 
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS) {
     debug ("acquire global request lock");
@@ -86,7 +86,7 @@ lock_request (struct connection *conn)
 void
 unlock_request (struct connection *conn)
 {
-  int thread_model = plugin_thread_model ();
+  int thread_model = backend->thread_model (backend);
 
   debug ("release unload prevention lock");
   pthread_rwlock_unlock (&unload_prevention_lock);

src/main.c
@@ -64,7 +64,7 @@
 
 static int is_short_name (const char *);
 static char *make_random_fifo (void);
-static void open_plugin_so (const char *filename, int short_name);
+static struct backend *open_plugin_so (const char *filename, int short_name);
 static void start_serving (void);
 static void set_up_signals (void);
 static void run_command (void);
@@ -102,6 +102,9 @@ unsigned int socket_activation  /* $LISTEN_FDS and $LISTEN_PID set */;
 volatile int quit;
 int quit_fd;
 static int write_quit_fd;
+
+/* The currently loaded plugin. */
+struct backend *backend;
 
 static char *random_fifo_dir = NULL;
 static char *random_fifo = NULL;
@@ -493,12 +496,12 @@ main (int argc, char *argv[])
     }
   }
 
-  open_plugin_so (filename, short_name);
+  backend = open_plugin_so (filename, short_name);
 
   if (help) {
     usage ();
     printf ("\n%s:\n\n", filename);
-    plugin_usage ();
+    backend->usage (backend);
     exit (EXIT_SUCCESS);
   }
 
@@ -506,8 +509,8 @@ main (int argc, char *argv[])
     const char *v;
 
     display_version ();
-    printf ("%s", plugin_name ());
-    if ((v = plugin_version ()) != NULL)
+    printf ("%s", backend->name (backend));
+    if ((v = backend->version (backend)) != NULL)
       printf (" %s", v);
     printf ("\n");
     exit (EXIT_SUCCESS);
@@ -518,7 +521,7 @@ main (int argc, char *argv[])
    * we assume it is 'script=...'.
    */
   if (optind < argc && (p = strchr (argv[optind], '=')) == NULL) {
-    plugin_config ("script", argv[optind]);
+    backend->config (backend, "script", argv[optind]);
     ++optind;
   }
 
@@ -528,14 +531,14 @@ main (int argc, char *argv[])
    * script=... parameter (and do not wait for config_complete).
    */
   if (dump_plugin) {
-    plugin_dump_fields ();
+    backend->dump_fields (backend);
     exit (EXIT_SUCCESS);
   }
 
   while (optind < argc) {
     if ((p = strchr (argv[optind], '=')) != NULL) {
       *p = '\0';
-      plugin_config (argv[optind], p+1);
+      backend->config (backend, argv[optind], p+1);
       ++optind;
     }
     else {
@@ -546,11 +549,12 @@ main (int argc, char *argv[])
     }
   }
 
-  plugin_config_complete ();
+  backend->config_complete (backend);
 
   start_serving ();
 
-  plugin_cleanup ();
+  backend->free (backend);
+  backend = NULL;
 
   free (unixsocket);
   free (pidfile);
@@ -609,9 +613,10 @@ make_random_fifo (void)
   return unixsocket;
 }
 
-static void
+static struct backend *
 open_plugin_so (const char *name, int short_name)
 {
+  struct backend *ret;
   char *filename = (char *) name;
   int free_filename = 0;
   void *dl;
@@ -647,10 +652,12 @@ open_plugin_so (const char *name, int short_name)
   }
 
   /* Register the plugin. */
-  plugin_register (filename, dl, plugin_init);
+  ret = plugin_register (filename, dl, plugin_init);
 
   if (free_filename)
     free (filename);
+
+  return ret;
 }
 
 static void

src/connections.c
@@ -211,16 +211,17 @@ _handle_single_connection (int sockin, int sockout)
   int nworkers = threads ? threads : DEFAULT_PARALLEL_REQUESTS;
   pthread_t *workers = NULL;
 
-  if (plugin_thread_model () < NBDKIT_THREAD_MODEL_PARALLEL || nworkers == 1)
+  if (backend->thread_model (backend) < NBDKIT_THREAD_MODEL_PARALLEL ||
+      nworkers == 1)
     nworkers = 0;
   conn = new_connection (sockin, sockout, nworkers);
   if (!conn)
     goto done;
 
-  if (plugin_open (conn, readonly) == -1)
+  if (backend->open (backend, conn, readonly) == -1)
     goto done;
 
-  threadlocal_set_name (plugin_name ());
+  threadlocal_set_name (backend->name (backend));
 
   /* Handshake. */
   if (negotiate_handshake (conn) == -1)
@@ -251,7 +252,8 @@ _handle_single_connection (int sockin, int sockout)
         set_status (conn, -1);
         goto wait;
       }
-      if (asprintf (&worker->name, "%s.%d", plugin_name (), nworkers) < 0) {
+      if (asprintf (&worker->name,
+                    "%s.%d", backend->name (backend), nworkers) < 0) {
         perror ("asprintf");
         set_status (conn, -1);
         free (worker);
@@ -340,7 +342,7 @@ free_connection (struct connection *conn)
    */
   if (!quit) {
     if (conn->handle)
-      plugin_close (conn);
+      backend->close (backend, conn);
   }
 
   free (conn);
@@ -352,7 +354,7 @@ compute_eflags (struct connection *conn, uint16_t *flags)
   uint16_t eflags = NBD_FLAG_HAS_FLAGS;
   int fl;
 
-  fl = plugin_can_write (conn);
+  fl = backend->can_write (backend, conn);
   if (fl == -1)
     return -1;
   if (readonly || !fl) {
@@ -363,7 +365,7 @@ compute_eflags (struct connection *conn, uint16_t *flags)
     eflags |= NBD_FLAG_SEND_WRITE_ZEROES;
   }
 
-  fl = plugin_can_flush (conn);
+  fl = backend->can_flush (backend, conn);
   if (fl == -1)
     return -1;
   if (fl) {
@@ -371,7 +373,7 @@ compute_eflags (struct connection *conn, uint16_t *flags)
     conn->can_flush = 1;
   }
 
-  fl = plugin_is_rotational (conn);
+  fl = backend->is_rotational (backend, conn);
   if (fl == -1)
     return -1;
   if (fl) {
@@ -379,7 +381,7 @@ compute_eflags (struct connection *conn, uint16_t *flags)
     conn->is_rotational = 1;
   }
 
-  fl = plugin_can_trim (conn);
+  fl = backend->can_trim (backend, conn);
   if (fl == -1)
     return -1;
   if (fl) {
@@ -407,7 +409,7 @@ _negotiate_handshake_oldstyle (struct connection *conn)
     return -1;
   }
 
-  r = plugin_get_size (conn);
+  r = backend->get_size (backend, conn);
   if (r == -1)
     return -1;
   if (r < 0) {
@@ -703,7 +705,7 @@ _negotiate_handshake_newstyle (struct connection *conn)
     return -1;
 
   /* Finish the newstyle handshake. */
-  r = plugin_get_size (conn);
+  r = backend->get_size (backend, conn);
   if (r == -1)
     return -1;
   if (r < 0) {
@@ -848,7 +850,7 @@ get_error (struct connection *conn)
 {
   int ret = threadlocal_get_error ();
 
-  if (!ret && plugin_errno_is_preserved ())
+  if (!ret && backend->errno_is_preserved (backend))
     ret = errno;
   return ret ? ret : EIO;
 }
@@ -881,28 +883,28 @@ handle_request (struct connection *conn,
 
   switch (cmd) {
   case NBD_CMD_READ:
-    if (plugin_pread (conn, buf, count, offset) == -1)
+    if (backend->pread (backend, conn, buf, count, offset) == -1)
       return get_error (conn);
     break;
 
   case NBD_CMD_WRITE:
-    if (plugin_pwrite (conn, buf, count, offset) == -1)
+    if (backend->pwrite (backend, conn, buf, count, offset) == -1)
       return get_error (conn);
     break;
 
   case NBD_CMD_FLUSH:
-    if (plugin_flush (conn) == -1)
+    if (backend->flush (backend, conn) == -1)
       return get_error (conn);
     break;
 
   case NBD_CMD_TRIM:
-    if (plugin_trim (conn, count, offset) == -1)
+    if (backend->trim (backend, conn, count, offset) == -1)
       return get_error (conn);
     break;
 
   case NBD_CMD_WRITE_ZEROES:
-    if (plugin_zero (conn, count, offset,
-                     !(flags & NBD_CMD_FLAG_NO_HOLE)) == -1)
+    if (backend->zero (backend, conn, count, offset,
+                       !(flags & NBD_CMD_FLAG_NO_HOLE)) == -1)
       return get_error (conn);
     break;
 
@@ -910,7 +912,7 @@ handle_request (struct connection *conn,
     abort ();
   }
 
-  if (flush_after_command && plugin_flush (conn) == -1)
+  if (flush_after_command && backend->flush (backend, conn) == -1)
     return get_error (conn);
 
   return 0;

commit b22fff03d933bc3b25eaf1d40f45dcd063568bd6

locks: Cache the plugin thread model.
Commit 876d715e17a59bd25df53be34b942afd96e52da9
("Refactor plugin_* functions into a backend struct.") causes hidden
crashes in the test suite (which unfortunately don't cause tests to
fail).

These all come from handle_single_connection where we may lock the
connection, run a single connection, free the backend, and then try to
unlock the connection.  Unfortunately the unlock operation has to
check the thread model again which fails because the backend has gone
away:

  Program terminated with signal SIGSEGV, Segmentation fault.
  #0  0x00000000004070ce in unlock_connection () at locks.c:59
  59	  int thread_model = backend->thread_model (backend);
  [Current thread is 1 (Thread 0x7fab43243700 (LWP 6676))]
  (gdb) bt
  #0  0x00000000004070ce in unlock_connection () at locks.c:59
  #1  0x00000000004061b1 in handle_single_connection (sockin=<optimized out>,
      sockout=<optimized out>) at connections.c:317
  #2  0x00000000004087d3 in start_thread (datav=0x229fd00) at sockets.c:262
  #3  0x00007fab44e525e1 in start_thread () from /lib64/libpthread.so.0
  #4  0x00007fab44b8717f in clone () from /lib64/libc.so.6

Since the thread model cannot be changed after a plugin is loaded,
just cache it in src/locks.c after the backend is created.

Note that I don't believe commit 876d715e17a59bd25df53be34b942afd96e52da9
caused this breakage.  I think it only worked by accident before
because it would still have been possible that
handle_single_connection was reading the plugin._thread_model field
after dlclose.

src/internal.h
@@ -181,6 +181,7 @@ struct backend {
 extern struct backend *plugin_register (const char *_filename, void *_dl, struct nbdkit_plugin *(*plugin_init) (void));
 
 /* locks.c */
+extern void lock_init_thread_model (void);
 extern void lock_connection (void);
 extern void unlock_connection (void);
 extern void lock_request (struct connection *conn);

src/locks.c
@@ -38,15 +38,24 @@
 
 #include "internal.h"
 
+/* Note that the plugin's thread model cannot change after being
+ * loaded, so caching it here is safe.
+ */
+static int thread_model;
+
 static pthread_mutex_t connection_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t all_requests_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_rwlock_t unload_prevention_lock = PTHREAD_RWLOCK_INITIALIZER;
 
 void
+lock_init_thread_model (void)
+{
+  thread_model = backend->thread_model (backend);
+}
+
+void
 lock_connection (void)
 {
-  int thread_model = backend->thread_model (backend);
-
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS) {
     debug ("acquire connection lock");
     pthread_mutex_lock (&connection_lock);
@@ -56,8 +65,6 @@ lock_connection (void)
 void
 unlock_connection (void)
 {
-  int thread_model = backend->thread_model (backend);
-
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS) {
     debug ("release connection lock");
     pthread_mutex_unlock (&connection_lock);
@@ -67,8 +74,6 @@ unlock_connection (void)
 void
 lock_request (struct connection *conn)
 {
-  int thread_model = backend->thread_model (backend);
-
   if (thread_model <= NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS) {
     debug ("acquire global request lock");
     pthread_mutex_lock (&all_requests_lock);
@@ -86,8 +91,6 @@ lock_request (struct connection *conn)
 void
 unlock_request (struct connection *conn)
 {
-  int thread_model = backend->thread_model (backend);
-
   debug ("release unload prevention lock");
   pthread_rwlock_unlock (&unload_prevention_lock);
 

src/main.c
@@ -497,6 +497,7 @@ main (int argc, char *argv[])
   }
 
   backend = open_plugin_so (filename, short_name);
+  lock_init_thread_model ();
 
   if (help) {
     usage ();