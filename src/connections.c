eff5acb6bae15754342dc4a4146410c126161e79
Richard W.M. Jones <rjones@redhat.com>
2018-08-01 13:44:09 +0100

    connections: Handle asynch exit causing backend to become NULL.
    If there is an asynchronous exit while running
    _handle_single_connection (eg. because of a signal or exit being
    called from a thread) backend can become NULL at just about any time.
    Make this function more robust and add a comment.
    
    One example seen when running the tests:
    
    $1 = (struct backend *) 0x0
    (gdb) t a a bt
    
    Thread 3 (Thread 0x7f143906dc80 (LWP 26858)):
        p=0x1256fe0, have_lock=<optimized out>) at malloc.c:4281
        listp=0x7f143a882738 <__exit_funcs>,
        run_list_atexit=run_list_atexit@entry=true, run_dtors=run_dtors@entry=true)
        at exit.c:108
        argv=0x7ffe58414148, init=<optimized out>, fini=<optimized out>,
        rtld_fini=<optimized out>, stack_end=0x7ffe58414138)
        at ../csu/libc-start.c:308
    
    Thread 2 (Thread 0x7f1433fff700 (LWP 26872)):
        at ../sysdeps/unix/sysv/linux/read.c:26
    
    Thread 1 (Thread 0x7f1438e68700 (LWP 26860)):
        sockin=<optimized out>) at connections.c:317
        at connections.c:335
        at pthread_create.c:474
        at ../sysdeps/unix/sysv/linux/x86_64/clone.S:95
    
    (cherry picked from commit 99f264bb09a52a20badd2a658c2b88c7334ae64c)

--- src/connections.c
+++ src/connections.c
@@ -229,6 +229,7 @@ connection_worker (void *data)
 static int
 _handle_single_connection (int sockin, int sockout)
 {
+  const char *plugin_name;
   int ret = -1, r;
   struct connection *conn;
   int nworkers = threads ? threads : DEFAULT_PARALLEL_REQUESTS;
@@ -247,7 +248,14 @@ _handle_single_connection (int sockin, int sockout)
   if (r == -1)
     goto done;
 
-  threadlocal_set_name (backend->plugin_name (backend));
+  /* NB: because of an asynchronous exit backend can be set to NULL at
+   * just about any time.
+   */
+  if (backend)
+    plugin_name = backend->plugin_name (backend);
+  else
+    plugin_name = "(unknown)";
+  threadlocal_set_name (plugin_name);
 
   /* Prepare (for filters), called just after open. */
   lock_request (conn);
@@ -288,8 +296,7 @@ _handle_single_connection (int sockin, int sockout)
         set_status (conn, -1);
         goto wait;
       }
-      if (asprintf (&worker->name,
-                    "%s.%d", backend->plugin_name (backend), nworkers) < 0) {
+      if (asprintf (&worker->name, "%s.%d", plugin_name, nworkers) < 0) {
         perror ("asprintf");
         set_status (conn, -1);
         free (worker);
