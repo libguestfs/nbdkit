fa1d66c1bd08766949a57651826253188f6c27af
Richard W.M. Jones <rjones@redhat.com>
2018-12-11 22:33:19 +0000

    Add "full" plugin to complement the null, zero plugins.
    It returns ENOSPC for every write request.  This is analogous to the
    /dev/full device in Unix, but of course different: /dev/full is a char
    device, but this is more like a thin-provisioned virtual disk which
    has run out of space on the host.
    
    You can implement this already using nbdkit-error-filter, but the
    command line to do it is very long (see the man page).
    
    This plugin has an interesting negative effect on libguestfs, because
    it causes the underlying qemu to pause, causing libguestfs to hang.

---
+++ plugins/full/full.c
@@ -0,0 +1,157 @@
+/* nbdkit
+ * Copyright (C) 2017-2018 Red Hat Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#define NBDKIT_API_VERSION 2
+
+#include <nbdkit-plugin.h>
+
+/* The size of disk in bytes (initialized by size=<SIZE> parameter). */
+static int64_t size = -1;
+
+static int
+full_config (const char *key, const char *value)
+{
+  int64_t r;
+
+  if (strcmp (key, "size") == 0) {
+    r = nbdkit_parse_size (value);
+    if (r == -1)
+      return -1;
+    size = r;
+  }
+  else {
+    nbdkit_error ("unknown parameter '%s'", key);
+    return -1;
+  }
+
+  return 0;
+}
+
+static int
+full_config_complete (void)
+{
+  if (size == -1) {
+    nbdkit_error ("size parameter is required");
+    return -1;
+  }
+
+  return 0;
+}
+
+#define full_config_help \
+  "size=<SIZE>  (required) Size of the backing disk"
+
+/* Create the per-connection handle. */
+static void *
+full_open (int readonly)
+{
+  static int handle;
+
+  return &handle;
+}
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+/* Get the disk size. */
+static int64_t
+full_get_size (void *handle)
+{
+  return size;
+}
+
+/* Read data. */
+static int
+full_pread (void *handle, void *buf, uint32_t count, uint64_t offset,
+            uint32_t flags)
+{
+  memset (buf, 0, count);
+  return 0;
+}
+
+/* Write data. */
+static int
+full_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
+             uint32_t flags)
+{
+  errno = ENOSPC;
+  return -1;
+}
+
+/* Write zeros. */
+static int
+full_zero (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
+{
+  errno = ENOSPC;
+  return -1;
+}
+
+/* Trim. */
+static int
+full_trim (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
+{
+  errno = ENOSPC;
+  return -1;
+}
+
+/* Note that we don't need to handle flush: If there has been previous
+ * write then we have already returned an error.  If there have been
+ * no previous writes then flush can be ignored.
+ */
+
+static struct nbdkit_plugin plugin = {
+  .name              = "full",
+  .version           = PACKAGE_VERSION,
+  .config            = full_config,
+  .config_complete   = full_config_complete,
+  .config_help       = full_config_help,
+  .open              = full_open,
+  .get_size          = full_get_size,
+  .pread             = full_pread,
+  .pwrite            = full_pwrite,
+  .zero              = full_zero,
+  .trim              = full_trim,
+  /* In this plugin, errno is preserved properly along error return
+   * paths from failed system calls.
+   */
+  .errno_is_preserved = 1,
+};
+
+NBDKIT_REGISTER_PLUGIN(plugin)
---
+++ tests/test-full.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018 Red Hat Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test the full plugin.  Note that this plugin causes libguestfs to
+# hang so we have to use low level tools (qemu-io).
+
+source ./functions.sh
+set -e
+
+files="full.pid full.sock full.out"
+rm -f $files
+cleanup_fn rm -f $files
+
+# Test that qemu-io works
+if ! qemu-io --help >/dev/null; then
+    echo "$0: missing or broken qemu-io"
+    exit 77
+fi
+
+# Run nbdkit with the full plugin.
+start_nbdkit -P full.pid -U full.sock full size=1M
+
+# All reads should succeed.
+qemu-io -f raw 'nbd+unix://?socket=full.sock' \
+        -c 'r -v 0 512' \
+        -c 'r -v 512 512' \
+        -c 'r -v 1048064 512'
+
+# All writes should fail with the ENOSPC error.
+! LANG=C qemu-io -f raw 'nbd+unix://?socket=full.sock' \
+        -c 'w -P 1 0 512' \
+        -c 'w -P 2 1048064 512' >& full.out
+grep "No space left on device" full.out
