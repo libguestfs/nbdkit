4a4634309a08fe75acb63e8cf1950de8ec09e001
Richard W.M. Jones <rjones@redhat.com>
2018-12-29 11:39:11 +0000

    tests: Add more tests of the error filter.

---
+++ tests/test-error10.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018 Red Hat Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -e
+set -x
+
+# Test that qemu-io works
+if ! qemu-io --help >/dev/null; then
+    echo "$0: missing or broken qemu-io"
+    exit 77
+fi
+
+files="error10.sock error10.pid"
+rm -f $files
+cleanup_fn rm -f $files
+
+# Run nbdkit with the error filter.
+start_nbdkit -P error10.pid -U error10.sock \
+             --filter=error \
+             pattern size=1G error-rate=10%
+
+# The error rate is 10% so about every 1 in 10 operations should fail.
+errors=0
+for i in {1..100}; do
+    qemu-io -r -f raw 'nbd+unix://?socket=error10.sock' \
+            -c "r 0 512" || ((++errors))
+done
+
+echo errors=$errors
+# Unfortunately there's no "right answer" here, and in some rare cases
+# the test will fail XXX.
+if [ $errors -lt 3 ] || [ $errors -gt 20 ]; then
+    echo "$0: unexpected error rate"
+    exit 1
+fi
