617b64f0376d3fbef976f5ce869a469160df9efe
Richard W.M. Jones <rjones@redhat.com>
2019-02-07 22:28:19 +0000

    Add support for writing plugins in Rust.

---
+++ plugins/rust/examples/ramdisk.rs
@@ -0,0 +1,119 @@
+// nbdkit
+// Copyright (C) 2019 Red Hat Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Red Hat nor the names of its contributors may be
+// used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+extern crate nbdkit;
+
+#[macro_use]
+extern crate lazy_static;
+
+use libc::*;
+use std::ptr;
+use std::os::raw::c_int;
+use std::sync::Mutex;
+
+use nbdkit::*;
+use nbdkit::ThreadModel::*;
+
+// The RAM disk.
+lazy_static! {
+    static ref DISK: Mutex<Vec<u8>> = Mutex::new (vec![0; 100 * 1024 * 1024]);
+}
+
+struct Handle {
+    // Box::new doesn't allocate anything unless we put some dummy
+    // fields here.  In a real implementation you would put per-handle
+    // data here as required.
+    _not_used: i32,
+}
+
+extern fn ramdisk_open (_readonly: c_int) -> *mut c_void {
+    let h = Handle {_not_used: 0};
+    let h = Box::new(h);
+    return Box::into_raw(h) as *mut c_void;
+}
+
+extern fn ramdisk_close (h: *mut c_void) {
+    let h = unsafe { Box::from_raw(h as *mut Handle) };
+    drop (h);
+}
+
+extern fn ramdisk_get_size (_h: *mut c_void) -> int64_t {
+    return DISK.lock().unwrap().capacity() as int64_t;
+}
+
+extern fn ramdisk_pread (_h: *mut c_void, buf: *mut c_char, count: uint32_t,
+                         offset: uint64_t, _flags: uint32_t) -> c_int {
+    let offset = offset as usize;
+    let count = count as usize;
+    let disk = DISK.lock().unwrap();
+    unsafe {
+        ptr::copy_nonoverlapping (&disk[offset], buf as *mut u8, count);
+    }
+    return 0;
+}
+
+extern fn ramdisk_pwrite (_h: *mut c_void, buf: *const c_char, count: uint32_t,
+                          offset: uint64_t, _flags: uint32_t) -> c_int {
+    let offset = offset as usize;
+    let count = count as usize;
+    let mut disk = DISK.lock().unwrap();
+    unsafe {
+        ptr::copy_nonoverlapping (buf as *const u8, &mut disk[offset], count);
+    }
+    return 0;
+}
+
+// Every plugin must define a public, C-compatible plugin_init
+// function which returns a pointer to a Plugin struct.
+#[no_mangle]
+pub extern fn plugin_init () -> *const Plugin {
+    // Plugin name.
+    // https://github.com/rust-lang/rfcs/issues/400
+    let name = "ramdisk\0" as *const str as *const [c_char] as *const c_char;
+
+    // Create a mutable plugin, setting the 5 required fields.
+    let mut plugin = Plugin::new (
+        Parallel,
+        name,
+        ramdisk_open,
+        ramdisk_get_size,
+        ramdisk_pread
+    );
+    // Update any other fields as required.
+    plugin.close = Some (ramdisk_close);
+    plugin.pwrite = Some (ramdisk_pwrite);
+
+    // Return the pointer.
+    let plugin = Box::new(plugin);
+    // XXX Memory leak.
+    return Box::into_raw(plugin);
+}
---
+++ plugins/rust/src/lib.rs
@@ -0,0 +1,155 @@
+// nbdkit
+// Copyright (C) 2019 Red Hat Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of Red Hat nor the names of its contributors may be
+// used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+extern crate libc;
+
+use libc::*;
+use std::os::raw::c_int;
+use std::mem;
+
+// This struct describes the plugin ABI which your plugin_init()
+// function must return.
+#[repr(C)]
+pub struct Plugin {
+    _struct_size: uint64_t,
+    _api_version: c_int,
+    _thread_model: c_int,
+
+    pub name: *const c_char,
+    pub longname: *const c_char,
+    pub version: *const c_char,
+    pub description: *const c_char,
+
+    pub load: Option<extern fn ()>,
+    pub unload: Option<extern fn ()>,
+
+    pub config: Option<extern fn (*const c_char, *const c_char)>,
+    pub config_complete: Option<extern fn () -> c_int>,
+    pub config_help: *const c_char,
+
+    pub open: extern fn (c_int) -> *mut c_void,
+    pub close: Option<extern fn (*mut c_void)>,
+
+    pub get_size: extern fn (*mut c_void) -> int64_t,
+
+    pub can_write: Option<extern fn (*mut c_void) -> c_int>,
+    pub can_flush: Option<extern fn (*mut c_void) -> c_int>,
+    pub is_rotational: Option<extern fn (*mut c_void) -> c_int>,
+    pub can_trim: Option<extern fn (*mut c_void) -> c_int>,
+
+    // Slots for old v1 API functions.
+    _pread_old: Option<extern fn ()>,
+    _pwrite_old: Option<extern fn ()>,
+    _flush_old: Option<extern fn ()>,
+    _trim_old: Option<extern fn ()>,
+    _zero_old: Option<extern fn ()>,
+
+    errno_is_preserved: c_int,
+
+    pub dump_plugin: Option<extern fn ()>,
+
+    pub can_zero: Option<extern fn (*mut c_void) -> c_int>,
+    pub can_fua: Option<extern fn (*mut c_void) -> c_int>,
+
+    pub pread: extern fn (h: *mut c_void, buf: *mut c_char, count: uint32_t,
+                          offset: uint64_t,
+                          flags: uint32_t) -> c_int,
+    pub pwrite: Option<extern fn (h: *mut c_void, buf: *const c_char,
+                                  count: uint32_t, offset: uint64_t,
+                                  flags: uint32_t) -> c_int>,
+    pub flush: Option<extern fn (h: *mut c_void, flags: uint32_t) -> c_int>,
+    pub trim: Option<extern fn (h: *mut c_void,
+                                count: uint32_t, offset: uint64_t,
+                                flags: uint32_t) -> c_int>,
+    pub zero: Option<extern fn (h: *mut c_void,
+                                count: uint32_t, offset: uint64_t,
+                                flags: uint32_t) -> c_int>,
+
+    pub magic_config_key: *const c_char,
+
+    pub can_multi_conn: Option<extern fn (h: *mut c_void) -> c_int>,
+}
+
+pub enum ThreadModel {
+    SerializeConnections = 0,
+    SerializeAllRequests = 1,
+    SerializeRequests = 2,
+    Parallel = 3,
+}
+
+impl Plugin {
+    pub fn new (thread_model: ThreadModel,
+                name: *const c_char,
+                open: extern fn (c_int) -> *mut c_void,
+                get_size: extern fn (*mut c_void) -> int64_t,
+                pread: extern fn (h: *mut c_void, buf: *mut c_char,
+                                  count: uint32_t, offset: uint64_t,
+                                  flags: uint32_t) -> c_int) -> Plugin {
+        Plugin {
+            _struct_size: mem::size_of::<Plugin>() as uint64_t,
+            _api_version: 2,
+            _thread_model: thread_model as c_int,
+            name: name,
+            longname: std::ptr::null(),
+            version: std::ptr::null(),
+            description: std::ptr::null(),
+            load: None,
+            unload: None,
+            config: None,
+            config_complete: None,
+            config_help: std::ptr::null(),
+            open: open,
+            close: None,
+            get_size: get_size,
+            can_write: None,
+            can_flush: None,
+            is_rotational: None,
+            can_trim: None,
+            _pread_old: None,
+            _pwrite_old: None,
+            _flush_old: None,
+            _trim_old: None,
+            _zero_old: None,
+            errno_is_preserved: 0,
+            dump_plugin: None,
+            can_zero: None,
+            can_fua: None,
+            pread: pread,
+            pwrite: None,
+            flush: None,
+            trim: None,
+            zero: None,
+            magic_config_key: std::ptr::null(),
+            can_multi_conn: None,
+        }
+    }
+}
