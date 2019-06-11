e007f066008407efac5bd888cdcbd09a4deb7052
Martin Kletzander <mkletzan@redhat.com>
2019-06-11 10:49:48 +0200

    Introduce cacheextents filter
    This filter caches the last result of the extents() call and offers a nice
    speed-up for clients that only support req_one=1 in combination with plugins
    like vddk, which has no overhead for returning information for multiple extents
    in one call, but that call is very time-consuming.
    
    Quick test showed that on a fast connection and a sparsely allocated 16G disk
    with a OS installed `qemu-img map` runs 16s instead of 33s (out of which it
    takes 5s to the first extents request).  For 100G disk with no data on it, that
    is one hole extent spanning the whole disk (where there is no space for
    improvement) this does not add any noticeable overhead.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
    Message-Id: <ce08f27c1bb7cc2c6c590ce017f23c16a8ded43b.1560242961.git.mkletzan@redhat.com>
    [eblake: minor tweaks]
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ filters/cacheextents/cacheextents.c
@@ -0,0 +1,203 @@
+/* nbdkit
+ * Copyright (C) 2019 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <inttypes.h>
+
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "cleanup.h"
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+/* This lock protects the global state. */
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+/* Cached extents from the last extents () call and its start and end for the
+ * sake of simplicity.
+ */
+struct nbdkit_extents *cache_extents;
+static uint64_t cache_start;
+static uint64_t cache_end;
+
+static void
+cacheextents_unload (void)
+{
+  nbdkit_extents_free (cache_extents);
+}
+
+static int
+cacheextents_add (struct nbdkit_extents *extents, int *err)
+{
+  size_t i = 0;
+
+  for (i = 0; i < nbdkit_extents_count (cache_extents); i++) {
+    struct nbdkit_extent ex = nbdkit_get_extent (cache_extents, i);
+    if (nbdkit_add_extent (extents, ex.offset, ex.length, ex.type) == -1) {
+      *err = errno;
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static int
+cacheextents_fill (struct nbdkit_extents *extents, int *err)
+{
+  size_t i = 0;
+  size_t count = nbdkit_extents_count (extents);
+  struct nbdkit_extent first = nbdkit_get_extent (extents, 0);
+  struct nbdkit_extent last = nbdkit_get_extent (extents, count - 1);
+
+  nbdkit_extents_free (cache_extents);
+  cache_start = first.offset;
+  cache_end = last.offset + last.length;
+  cache_extents = nbdkit_extents_new (cache_start, cache_end);
+
+  if (!cache_extents)
+    return -1;
+
+  for (i = 0; i < count; i++) {
+    struct nbdkit_extent ex = nbdkit_get_extent (extents, i);
+    nbdkit_debug ("cacheextents: updating cache with:"
+                  " offset=%" PRIu64
+                  " length=%" PRIu64
+                  " type=%x",
+                  ex.offset, ex.length, ex.type);
+    if (nbdkit_add_extent (cache_extents, ex.offset, ex.length, ex.type) == -1) {
+      *err = errno;
+      nbdkit_extents_free (cache_extents);
+      cache_extents = NULL;
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static int
+cacheextents_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
+                      void *handle, uint32_t count, uint64_t offset, uint32_t flags,
+                      struct nbdkit_extents *extents,
+                      int *err)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+
+  nbdkit_debug ("cacheextents:"
+                " cache_start=%" PRIu64
+                " cache_end=%" PRIu64
+                " cache_extents=%p",
+                cache_start, cache_end, cache_extents);
+
+  if (cache_extents &&
+      offset >= cache_start && offset < cache_end) {
+    nbdkit_debug ("cacheextents: returning from cache");
+    return cacheextents_add (extents, err);
+  }
+
+  nbdkit_debug ("cacheextents: cache miss");
+  /* Clear REQ_ONE to ask the plugin for as much information as it is
+   * willing to return (the plugin may still truncate if it is too
+   * costly to provide everything).
+   */
+  flags &= ~(NBDKIT_FLAG_REQ_ONE);
+  if (next_ops->extents (nxdata, count, offset, flags, extents, err) == -1)
+    return -1;
+
+  return cacheextents_fill (extents, err);
+}
+
+/* Any changes to the data needs to clean the cache.
+ *
+ * Similar to the readahead filter this could be more intelligent, but
+ * there would be very little benefit.
+ */
+
+static void
+kill_cacheextents (void)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+  nbdkit_extents_free (cache_extents);
+  cache_extents = NULL;
+}
+
+static int
+cacheextents_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
+                     void *handle,
+                     const void *buf, uint32_t count, uint64_t offset,
+                     uint32_t flags, int *err)
+{
+  kill_cacheextents ();
+  return next_ops->pwrite (nxdata, buf, count, offset, flags, err);
+}
+
+static int
+cacheextents_trim (struct nbdkit_next_ops *next_ops, void *nxdata,
+                   void *handle,
+                   uint32_t count, uint64_t offset, uint32_t flags,
+                   int *err)
+{
+  kill_cacheextents ();
+  return next_ops->trim (nxdata, count, offset, flags, err);
+}
+
+static int
+cacheextents_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
+                   void *handle,
+                   uint32_t count, uint64_t offset, uint32_t flags,
+                   int *err)
+{
+  kill_cacheextents ();
+  return next_ops->zero (nxdata, count, offset, flags, err);
+}
+
+static struct nbdkit_filter filter = {
+  .name              = "cacheextents",
+  .longname          = "nbdkit cacheextents filter",
+  .version           = PACKAGE_VERSION,
+  .unload            = cacheextents_unload,
+  .pwrite            = cacheextents_pwrite,
+  .trim              = cacheextents_trim,
+  .zero              = cacheextents_zero,
+  .extents           = cacheextents_extents,
+};
+
+NBDKIT_REGISTER_FILTER (filter)
---
+++ tests/test-cacheextents.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2019 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -x
+set -e
+
+requires grep --version
+requires qemu-img --version
+requires qemu-io --version
+
+sock="$(mktemp -u)"
+sockurl="nbd+unix:///?socket=$sock"
+pidfile="test-cacheextents.pid"
+accessfile="test-cacheextents-access.log"
+accessfile_full="$PWD/test-cacheextents-access.log"
+files="$pidfile $sock"
+rm -f $files $accessfile
+cleanup_fn rm -f $files
+
+# Intentionally using EOF rather than 'EOF' so that we can pass in the
+# $accessfile_full
+start_nbdkit \
+    -P $pidfile \
+    -U $sock \
+    --filter=cacheextents \
+    sh - <<EOF
+echo "Call: \$@" >>$accessfile_full
+size=4M
+block_size=\$((1024*1024))
+case "\$1" in
+  get_size) echo \$size ;;
+  can_extents) ;;
+  extents)
+    echo "extents request: \$@" >>$accessfile_full
+    offset=\$((\$4 / \$block_size))
+    count=\$((\$3 / \$block_size))
+    length=\$((\$offset + \$count))
+    for i in \$(seq \$offset \$length); do
+      echo \${i}M \$block_size \$((i%4)) >>$accessfile_full
+      echo \${i}M \$block_size \$((i%4))
+    done
+    ;;
+  pread) dd if=/dev/zero count=\$3 iflag=count_bytes ;;
+  can_write) ;;
+  pwrite) dd of=/dev/null ;;
+  can_trim) ;;
+  trim) ;;
+  can_zero) ;;
+  zero) ;;
+  *) exit 2 ;;
+esac
+EOF
+
+
+test_me() {
+    num_accesses=$1
+    shift
+
+    qemu-io -f raw "$@" "$sockurl"
+    test "$(grep -c "^extents request: " $accessfile)" -eq "$num_accesses"
+    ret=$?
+    rm -f "$accessfile"
+    return $ret
+}
+
+# First one causes caching, the rest should be returned from cache.
+test_me 1 -c 'map' -c 'map' -c 'map'
+# First one is still cached from last time, discard should kill the cache, then
+# one request should go through.
+test_me 1 -c 'map' -c 'discard 0 1' -c 'map'
+# Same as above, only this time the cache is killed before all the operations as
+# well.  This is used from now on to clear the cache as it seems nicer and
+# faster than running new nbdkit for each test.
+test_me 2 -c 'discard 0 1' -c 'map' -c 'discard 0 1' -c 'map'
+# Write should kill the cache as well.
+test_me 2 -c 'discard 0 1' -c 'map' -c 'write 0 1' -c 'map'
+# Alloc should use cached data from map
+test_me 1 -c 'discard 0 1' -c 'map' -c 'alloc 0'
+# Read should not kill the cache
+test_me 1 -c 'discard 0 1' -c 'map' -c 'read 0 1' -c 'map' -c 'alloc 0'
