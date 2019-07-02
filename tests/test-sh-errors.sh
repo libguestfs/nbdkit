c707cb66480a8e0b36f81ff3e4d97995077e7f53
Eric Blake <eblake@redhat.com>
2019-07-02 17:41:30 -0500

    sh: Test error parsing
    As promised in the previous patch.  Tested on glibc; we'll probably
    have to tweak things for other libc spellings of strerror(), but I at
    least set up the test to make it easy to do so.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ tests/test-sh-errors.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2019 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -e
+set -x
+
+requires qemu-io --version
+
+out="test-sh-errors.out"
+expected="test-sh-errors.expected"
+files="$out $expected"
+rm -f $files
+cleanup_fn rm -f $files
+
+# do_test 'egrep pattern'
+# (Use of an egrep pattern makes it easy to whitelist other localized error
+# outputs that may be system-dependent)
+do_test ()
+{
+    : > $out
+    nbdkit -v -U - sh - --run 'qemu-io -r -f raw -c "r 0 1" $nbd &&
+      echo qemu-io unexpectedly passed >> '$out'; :' >> $out
+    if grep "qemu-io unexpectedly passed" $out ||
+       ! egrep "$1" $out; then
+        echo "$0: output did not match expected data"
+        echo "expected: $1"
+        echo "output:"
+        cat $out
+        exit 1
+    fi
+}
+
+# All caps, no trailing whitespace, ENOMEM
+do_test 'Cannot allocate memory' <<'EOF'
+case "$1" in
+    get_size) echo 64K ;;
+    pread) printf ENOMEM >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
+
+# Lower case, trailing newline, EPERM
+do_test 'Operation not permitted' <<'EOF'
+case "$1" in
+    get_size) echo 64K ;;
+    pread) echo eperm >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
+
+# Trailing single-line message, ENOSPC
+do_test 'No space left on device' <<'EOF'
+case "$1" in
+    get_size) echo 64K ;;
+    pread) echo ENOSPC custom message does not reach client >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
+
+# Multi-line message, ESHUTDOWN
+do_test 'Cannot send after transport endpoint shutdown' <<'EOF'
+case "$1" in
+    get_size) echo 1M ;;
+    pread) printf 'ESHUTDOWN\nline one\nline two\n' >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
+
+# Any errno that can't be sent over NBD is flattened to EIO
+do_test 'Input/output error' <<'EOF'
+case "$1" in
+    get_size) echo 1M ;;
+    pread) echo EPIPE >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
+
+# EINVALID is not a real errno name, and should not be confused with EINVAL
+do_test 'Input/output error' <<'EOF'
+case "$1" in
+    get_size) echo 1M ;;
+    pread) echo EINVALID >&2; exit 1 ;;
+    *) exit 2 ;;
+esac
+EOF
