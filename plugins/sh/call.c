libguestfs/nbdkit

commit 262787dc863cfffaace493ac8a348e2e12e6eef3

Add shell script plugin, for writing plugins in shell or scripting languages.

plugins/sh/nbdkit-sh-plugin.pod
@@ -1,0 +1,229 @@
+=head1 NAME
+
+nbdkit-sh-plugin - nbdkit shell, script or executable plugin
+
+=head1 SYNOPSIS
+
+ nbdkit sh /path/to/script [arguments...]
+
+=head1 DESCRIPTION
+
+C<nbdkit-sh-plugin> allows you to write plugins for L<nbdkit(1)> using
+arbitrary scripting languages, including shells like L<bash(1)>,
+L<dash(1)>, L<csh(1)>, L<zsh(1)> etc., other scripting environments,
+or any executable.  Note if you want to use an established scripting
+language like Perl or Python, then nbdkit has specific plugins to
+handle those languages and those will be more efficient (see
+L<nbdkit(1)> for a complete list).
+
+Broadly speaking, nbdkit shell plugins work like C ones, so you should
+read L<nbdkit-plugin(3)> first.
+
+=head2 USING AN NBDKIT SH PLUGIN
+
+Assuming you have a shell script which is an nbdkit plugin, you run it
+like this:
+
+ nbdkit sh /path/to/script
+
+You may have to add further C<key=value> arguments to the command
+line.
+
+=head1 WRITING AN NBDKIT SH PLUGIN
+
+For an example plugin written in Bash, see:
+L<https://github.com/libguestfs/nbdkit/blob/master/plugins/sh/example.sh>
+
+=head2 PROGRAMMING MODEL
+
+This plugin has a simple programming model: For every plugin method
+that needs to be called, the external script is invoked with
+parameters describing the method and its arguments.  The first
+parameter is always the method name.  For example:
+
+ /path/to/script config <key> <value>
+ /path/to/script pread <handle> <count> <offset>
+
+A fresh script is invoked for each method call (ie. scripts are
+stateless), so if the script needs to store state it has to store it
+somewhere in the filesystem in a format and location which is left up
+to the author of the script.
+
+=head2 EXIT CODES
+
+The script should exit with specific exit codes:
+
+=over 4
+
+=item S<0>
+
+The method was executed successfully.
+
+=item 1 and 8-127
+
+There was an error.  The script may print on stderr an errno and a
+message, for example:
+
+ ENOSPC Out of space
+
+If the script doesn't print anything or the output cannot be parsed
+then nbdkit assumes error C<EIO>.
+
+=item S<2>
+
+The requested method is not supported by the script.
+
+=item S<3>
+
+For methods which return booleans, this code indicates false.
+
+=item 4, 5, 6, 7
+
+These exit codes are reserved for future use.
+
+=back
+
+=head2 HANDLES
+
+Handles are arbitrary strings, but it is best to limit them to short
+alphanumeric strings.
+
+=head2 METHODS
+
+This just documents the arguments to the script corresponding to each
+plugin method, and any way that they differ from the C callbacks.  In
+all other respects they work the same way as the C callbacks, so you
+should go and read L<nbdkit-plugin(3)>.
+
+=over 4
+
+=item C<dump_plugin>
+
+ /path/to/script dump_plugin
+
+=item C<config>
+
+ /path/to/script config <key> <value>
+
+=item C<config_complete>
+
+ /path/to/script config_complete
+
+=item C<open>
+
+ /path/to/script open <readonly>
+
+The C<readonly> parameter will be C<true> or C<false>.
+
+On success this should print the handle (any string) on stdout and
+exit with code C<0>.
+
+=item C<close>
+
+ /path/to/script close <handle>
+
+=item C<get_size>
+
+ /path/to/script get_size <handle>
+
+The script should print the size of the disk image on stdout.  You can
+print the size in bytes, or use any format understood by
+C<nbdkit_parse_size> such as C<1M> (see
+L<nbdkit-plugin(3)/PARSING SIZE PARAMETERS>).
+
+=item C<can_write>
+
+=item C<can_flush>
+
+=item C<can_trim>
+
+Unlike in other languages, you B<must> provide the C<can_*> methods
+otherwise they are assumed to all return false and your C<pwrite>,
+C<flush> and C<trim> methods will never be called.  The reason for
+this is obscure: In other languages we can detect if (eg) a C<pwrite>
+method is defined and synthesize an appropriate response if no actual
+C<can_write> method is defined.  However detecting if methods are
+present without running them is not possible with this plugin.
+
+ /path/to/script can_write <handle>
+ /path/to/script can_flush <handle>
+ /path/to/script can_trim <handle>
+
+The script should exit with code C<0> for true or code C<3> for false.
+
+=item C<is_rotational>
+
+ /path/to/script is_rotational <handle>
+
+The script should exit with code C<0> for true or code C<3> for false.
+
+=item C<pread>
+
+ /path/to/script pread <handle> <count> <offset>
+
+The script should print the requested binary data on stdout.  Exactly
+C<count> bytes must be printed.
+
+=item C<pwrite>
+
+ /path/to/script pwrite <handle> <count> <offset>
+
+The script should read the binary data to be written from stdin.
+
+Unlike in other languages, if you provide a C<pwrite> method you
+B<must> also provide a C<can_pwrite> method which exits with code C<0>
+(true).
+
+=item C<flush>
+
+ /path/to/script flush <handle>
+
+Unlike in other languages, if you provide a C<flush> method you
+B<must> also provide a C<can_flush> method which exits with code C<0>
+(true).
+
+=item C<trim>
+
+ /path/to/script trim <handle> <count> <offset>
+
+Unlike in other languages, if you provide a C<trim> method you B<must>
+also provide a C<can_trim> method which exits with code C<0> (true).
+
+=item C<zero>
+
+ /path/to/script zero <handle> <count> <offset> <may_trim>
+
+C<may_trim> will be either C<true> or C<false>.
+
+=back
+
+=head2 MISSING CALLBACKS
+
+=over 4
+
+=item Missing: C<load>, C<unload>, C<name>, C<version>, C<longname>,
+C<description>, C<config_help>, C<can_zero>, C<can_fua>
+
+These are not yet supported.
+
+=back
+
+=head2 THREADS
+
+The thread model for scripts currently cannot be set from this plugin.
+It is hard-coded in the C part to
+C<NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS>.  This may change or be
+settable in future.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2018 Red Hat Inc.

.gitignore
@@ -86,6 +86,8 @@ Makefile.in
 /tests/test-python
 /tests/test-random
 /tests/test-ruby
+/tests/test-shell
+/tests/test-shell.img
 /tests/test-socket-activation
 /tests/test-split
 /tests/test-streaming

README
@@ -15,7 +15,8 @@ The key features are:
  * Well-documented, simple plugin API with a stable ABI guarantee.
    Lets you export “unconventional” block devices easily.
 
- * You can write plugins in C, Lua, Perl, Python, OCaml, Ruby or Tcl.
+ * You can write plugins in C, Lua, Perl, Python, OCaml, Ruby, shell
+   script or Tcl.
 
  * Filters can be stacked in front of plugins to transform the output.
 

configure.ac
@@ -576,6 +576,7 @@ AC_CONFIG_FILES([Makefile
                  plugins/python/Makefile
                  plugins/random/Makefile
                  plugins/ruby/Makefile
+                 plugins/sh/Makefile
                  plugins/split/Makefile
                  plugins/streaming/Makefile
                  plugins/tar/Makefile

docs/nbdkit-plugin.pod
@@ -66,6 +66,7 @@ L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>,
 L<nbdkit-ruby-plugin(3)>,
+L<nbdkit-sh-plugin(3)>,
 L<nbdkit-tcl-plugin(3)>.
 
 =head1 C<#define NBDKIT_API_VERSION 2>
@@ -883,8 +884,9 @@ which defines C<$(NBDKIT_PLUGINDIR)> in automake-generated Makefiles.
 
 =head1 WRITING PLUGINS IN OTHER PROGRAMMING LANGUAGES
 
-You can also write nbdkit plugins in Lua, OCaml, Perl, Python, Ruby or Tcl.
-Other programming languages may be offered in future.
+You can also write nbdkit plugins in Lua, OCaml, Perl, Python, Ruby,
+shell script or Tcl.  Other programming languages may be offered in
+future.
 
 For more information see:
 L<nbdkit-lua-plugin(3)>,
@@ -892,6 +894,7 @@ L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>,
 L<nbdkit-ruby-plugin(3)>,
+L<nbdkit-sh-plugin(3)>,
 L<nbdkit-tcl-plugin(3)>.
 
 Plugins written in scripting languages may also be installed in

docs/nbdkit.pod
@@ -514,6 +514,7 @@ L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>,
 L<nbdkit-ruby-plugin(3)>,
+L<nbdkit-sh-plugin(3)>,
 L<nbdkit-tcl-plugin(3)>.
 
 =head2 NBD clients

plugins/sh/Makefile.am
@@ -1,5 +1,5 @@
 # nbdkit
-# Copyright (C) 2013-2018 Red Hat Inc.
+# Copyright (C) 2018 Red Hat Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,51 +30,33 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-plugins = \
-	curl \
-	data \
-	example1 \
-	example2 \
-	example3 \
-	example4 \
-	ext2 \
-	file \
-	guestfs \
-	gzip \
-	libvirt \
-	lua \
-	memory \
-	nbd \
-	null \
-	ocaml \
-	pattern \
-	perl \
-	python \
-	random \
-	ruby \
-	sh \
-	split \
-	streaming \
-	tar \
-	tcl \
-	vddk \
-	xz \
-	zero
+include $(top_srcdir)/common-rules.mk
 
-filters = \
-	blocksize \
-	cache \
-	cow \
-	delay \
-	error \
-	fua \
-	log \
-	nozero \
-	offset \
-	partition \
-	truncate
+EXTRA_DIST = \
+	nbdkit-sh-plugin.pod \
+	example.sh
 
-plugindir = $(libdir)/nbdkit/plugins
-filterdir = $(libdir)/nbdkit/filters
+plugin_LTLIBRARIES = nbdkit-sh-plugin.la
 
-CLEANFILES = *~ *.cmi *.cmx *.cmxa *.so
+nbdkit_sh_plugin_la_SOURCES = \
+	sh.c \
+	$(top_srcdir)/include/nbdkit-plugin.h
+
+nbdkit_sh_plugin_la_CPPFLAGS = \
+	-I$(top_srcdir)/include
+nbdkit_sh_plugin_la_CFLAGS = \
+	$(WARNINGS_CFLAGS)
+nbdkit_sh_plugin_la_LDFLAGS = \
+	-module -avoid-version -shared
+
+if HAVE_POD
+
+man_MANS = nbdkit-sh-plugin.3
+CLEANFILES += $(man_MANS)
+
+nbdkit-sh-plugin.3: nbdkit-sh-plugin.pod
+	$(PODWRAPPER) --section=3 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

plugins/sh/example.sh
@@ -1,0 +1,106 @@
+#!/usr/bin/env bash
+#
+# Example Bash plugin.
+#
+# This example can be freely used for any purpose.
+#
+# Run it from the build directory like this:
+#
+#   ./nbdkit -f -v sh ./plugins/sh/example.sh file=disk.img
+#
+# Or run it after installing nbdkit like this:
+#
+#   nbdkit -f -v sh ./plugins/sh/example.sh file=disk.img
+#
+# The -f -v arguments are optional.  They cause the server to stay in
+# the foreground and print debugging, which is useful when testing.
+#
+# You can connect to the server using guestfish or qemu, eg:
+#
+#   guestfish --format=raw -a nbd://localhost
+#   ><fs> run
+#   ><fs> list-filesystems
+#   ><fs> mount /dev/sda1 /
+
+# It's highly insecure to save state in a known directory under /tmp
+# but this is just an example.  Don't do this in real code!
+state=/tmp/nbdkit-example-sh
+mkdir -p $state
+f=$state/file
+
+case "$1" in
+    dump_plugin)
+        # This is called from: nbdkit sh example.sh --dump-plugin
+        echo "example_sh=1"
+        ;;
+
+    config)
+        # We expect a file=... parameter pointing to the file to serve.
+        if [ "$2" = "file" ]; then
+            if [ ! -r "$3" ]; then
+                echo "file $3 does not exist or is not readable" >&2
+                exit 1
+            fi
+            ln -sf `realpath "$3"` $f
+        else
+            echo "unknown parameter $2=$3" >&2
+            exit 1
+        fi
+        ;;
+
+    config_complete)
+        # Check the file parameter was passed.
+        if [ ! -L $f ]; then
+            echo "file parameter missing" >&2
+            exit 1
+        fi
+        ;;
+
+    open)
+        # Open a new client connection.
+        # We can return any handle here.
+        echo handle
+        ;;
+
+    get_size)
+        stat -L -c '%s' $f || exit 1
+        ;;
+
+    pread)
+        dd iflag=skip_bytes,count_bytes skip=$4 count=$3 if=$f || \
+            exit 1
+        ;;
+
+    pwrite)
+        dd oflag=seek_bytes conv=notrunc seek=$4 of=$f || exit 1
+        ;;
+
+    can_write)
+        # If we provide a pwrite method, we must provide this method
+        # (and similarly for flush and trim).  See nbdkit-sh-plugin(3)
+        # for details.  This will exit 0 (below) which means true.
+        # Use ‘exit 3’ if false.
+        ;;
+
+    trim)
+        fallocate -p -o $4 -l $3 -n $f
+        ;;
+    can_trim)
+        # We can trim if the fallocate command exists.
+        fallocate --help >/dev/null 2>&1 || exit 3
+        ;;
+
+    zero)
+        fallocate -z -o $4 -l $3 -n $f
+        ;;
+    can_zero)
+        # We can zero efficiently if the fallocate command exists.
+        fallocate --help >/dev/null 2>&1 || exit 3
+        ;;
+
+    *)
+        # Unknown methods must exit with code 2.
+        exit 2
+esac
+
+exit 0

commit 0528ebde97cedd2ab2c1fefe2bc383a2306ecd47

sh: Parse a larger number of error messages
In order to trigger a non-EIO failure, a script had to output
_exactly_ 'EINVAL ' or similar, with one trailing space and no other
message, because we forgot to limit the length of the string
comparison. Fix things to tolerate an error name without a message, as
well as to be case-insensitive.

Also, parse EOVERFLOW (missed in commit 6f8c8084).

The next patch will add a test to avoid regressions.

Not done here but perhaps worth considering: if the first portion of
the scripts output resembles a numeric value, we could see if that
matches a known errno value.

Fixes: 262787dc86
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/sh/call.c
@@ -42,6 +42,7 @@
 #include <poll.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #include <nbdkit-plugin.h>
 
@@ -267,32 +268,36 @@ static void
 handle_script_error (char *ebuf, size_t len)
 {
   int err;
-  size_t skip;
+  size_t skip = 0;
   char *p;
 
-  if (strcmp (ebuf, "EPERM ") == 0) {
+  if (strncasecmp (ebuf, "EPERM", 5) == 0) {
     err = EPERM;
+    skip = 5;
+  }
+  else if (strncasecmp (ebuf, "EIO", 3) == 0) {
+    err = EIO;
+    skip = 3;
+  }
+  else if (strncasecmp (ebuf, "ENOMEM", 6) == 0) {
+    err = ENOMEM;
     skip = 6;
   }
-  else if (strcmp (ebuf, "EIO ") == 0) {
-    err = EIO;
-    skip = 4;
-  }
-  else if (strcmp (ebuf, "ENOMEM ") == 0) {
-    err = ENOMEM;
-    skip = 7;
-  }
-  else if (strcmp (ebuf, "EINVAL ") == 0) {
+  else if (strncasecmp (ebuf, "EINVAL", 6) == 0) {
     err = EINVAL;
-    skip = 7;
+    skip = 6;
   }
-  else if (strcmp (ebuf, "ENOSPC ") == 0) {
+  else if (strncasecmp (ebuf, "ENOSPC", 6) == 0) {
     err = ENOSPC;
-    skip = 7;
+    skip = 6;
   }
-  else if (strcmp (ebuf, "ESHUTDOWN ") == 0) {
+  else if (strncasecmp (ebuf, "EOVERFLOW", 9) == 0) {
+    err = EOVERFLOW;
+    skip = 9;
+  }
+  else if (strncasecmp (ebuf, "ESHUTDOWN", 9) == 0) {
     err = ESHUTDOWN;
-    skip = 10;
+    skip = 9;
   }
   else {
     /* Default to EIO. */
@@ -300,19 +305,30 @@ handle_script_error (char *ebuf, size_t len)
     skip = 0;
   }
 
+  if (skip && ebuf[skip]) {
+    if (!isspace ((unsigned char) ebuf[skip])) {
+      /* Treat 'EINVALID' as EIO, not EINVAL */
+      err = EIO;
+      skip = 0;
+    }
+    else
+      do
+        skip++;
+      while (isspace ((unsigned char) ebuf[skip]));
+  }
+
   while (len > 0 && ebuf[len-1] == '\n')
     ebuf[--len] = '\0';
 
   if (len > 0) {
-    p = strchr (ebuf, '\n');
+    p = strchr (ebuf + skip, '\n');
     if (p) {
       /* More than one line, so write the whole message to debug ... */
       nbdkit_debug ("%s: %s", script, ebuf);
       /* ... but truncate it for the error message below. */
       *p = '\0';
     }
-    if (strlen (ebuf) >= skip)
-      ebuf += skip;
+    ebuf += skip;
     nbdkit_error ("%s: %s", script, ebuf);
   }
   else

plugins/sh/nbdkit-sh-plugin.pod
@@ -85,8 +85,8 @@ The method was executed successfully.
 
 =item 1 and 8-127
 
-There was an error.  The script may print on stderr an errno and a
-message, for example:
+There was an error.  The script may print on stderr an errno name,
+optionally followed by whitespace and a message, for example:
 
  ENOSPC Out of space
 