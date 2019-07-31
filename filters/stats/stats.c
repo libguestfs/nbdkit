libguestfs/nbdkit

commit fa0585f1281f1087ff6798791e0edd33cebd1c07

Add stats filter.

filters/stats/stats.c
@@ -1,0 +1,268 @@
+/* nbdkit
+ * Copyright (C) 2019 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "tvdiff.h"
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+static char *filename;
+static bool append;
+static FILE *fp;
+static struct timeval start_t;
+
+/* This lock protects all the stats. */
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static uint64_t pread_ops, pread_bytes;
+static uint64_t pwrite_ops, pwrite_bytes;
+static uint64_t trim_ops, trim_bytes;
+static uint64_t zero_ops, zero_bytes;
+static uint64_t extents_ops, extents_bytes;
+
+static inline double
+calc_bps (uint64_t bytes, int64_t usecs)
+{
+  return 8.0 * bytes / usecs * 1000000.;
+}
+
+static inline void
+print_stats (int64_t usecs)
+{
+  fprintf (fp, "elapsed time: %g s\n", usecs / 1000000.);
+
+  if (pread_ops > 0)
+    fprintf (fp, "read: %" PRIu64 " ops, %" PRIu64 " bytes, %g bits/s\n",
+             pread_ops, pread_bytes, calc_bps (pread_bytes, usecs));
+  if (pwrite_ops > 0)
+    fprintf (fp, "write: %" PRIu64 " ops, %" PRIu64 " bytes, %g bits/s\n",
+             pwrite_ops, pwrite_bytes, calc_bps (pwrite_bytes, usecs));
+  if (trim_ops > 0)
+    fprintf (fp, "trim: %" PRIu64 " ops, %" PRIu64 " bytes, %g bits/s\n",
+             trim_ops, trim_bytes, calc_bps (trim_bytes, usecs));
+  if (zero_ops > 0)
+    fprintf (fp, "zero: %" PRIu64 " ops, %" PRIu64 " bytes, %g bits/s\n",
+             zero_ops, zero_bytes, calc_bps (zero_bytes, usecs));
+  if (extents_ops > 0)
+    fprintf (fp, "extents: %" PRIu64 " ops, %" PRIu64 " bytes, %g bits/s\n",
+             extents_ops, extents_bytes, calc_bps (extents_bytes, usecs));
+
+  fflush (fp);
+}
+
+static void
+stats_unload (void)
+{
+  struct timeval now;
+  int64_t usecs;
+
+  gettimeofday (&now, NULL);
+  usecs = tvdiff_usec (&start_t, &now);
+  if (fp && usecs > 0) {
+    pthread_mutex_lock (&lock);
+    print_stats (usecs);
+    pthread_mutex_unlock (&lock);
+  }
+
+  if (fp)
+    fclose (fp);
+  free (filename);
+}
+
+static int
+stats_config (nbdkit_next_config *next, void *nxdata,
+              const char *key, const char *value)
+{
+  int r;
+
+  if (strcmp (key, "statsfile") == 0) {
+    free (filename);
+    filename = nbdkit_absolute_path (value);
+    if (filename == NULL)
+      return -1;
+    return 0;
+  }
+  else if (strcmp (value, "statsappend") == 0) {
+    r = nbdkit_parse_bool (value);
+    if (r == -1)
+      return -1;
+    append = r;
+    return 0;
+  }
+
+  return next (nxdata, key, value);
+}
+
+static int
+stats_config_complete (nbdkit_next_config_complete *next, void *nxdata)
+{
+  if (filename == NULL) {
+    nbdkit_error ("stats filter requires statsfile parameter");
+    return -1;
+  }
+
+  fp = fopen (filename, append ? "a" : "w");
+  if (fp == NULL) {
+    nbdkit_error ("%s: %m", filename);
+    return -1;
+  }
+
+  gettimeofday (&start_t, NULL);
+
+  return next (nxdata);
+}
+
+/* Read. */
+static int
+stats_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
+             void *handle, void *buf, uint32_t count, uint64_t offset,
+             uint32_t flags, int *err)
+{
+  int r;
+
+  r = next_ops->pread (nxdata, buf, count, offset, flags, err);
+  if (r == 0) {
+    pthread_mutex_lock (&lock);
+    pread_ops++;
+    pread_bytes += count;
+    pthread_mutex_unlock (&lock);
+  }
+  return r;
+}
+
+/* Write. */
+static int
+stats_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
+              void *handle,
+              const void *buf, uint32_t count, uint64_t offset,
+              uint32_t flags, int *err)
+{
+  int r;
+
+  r = next_ops->pwrite (nxdata, buf, count, offset, flags, err);
+  if (r == 0) {
+    pthread_mutex_lock (&lock);
+    pwrite_ops++;
+    pwrite_bytes += count;
+    pthread_mutex_unlock (&lock);
+  }
+  return r;
+}
+
+/* Trim. */
+static int
+stats_trim (struct nbdkit_next_ops *next_ops, void *nxdata,
+            void *handle,
+            uint32_t count, uint64_t offset, uint32_t flags,
+            int *err)
+{
+  int r;
+
+  r = next_ops->trim (nxdata, count, offset, flags, err);
+  if (r == 0) {
+    pthread_mutex_lock (&lock);
+    trim_ops++;
+    trim_bytes += count;
+    pthread_mutex_unlock (&lock);
+  }
+  return r;
+}
+
+/* Zero. */
+static int
+stats_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
+            void *handle,
+            uint32_t count, uint64_t offset, uint32_t flags,
+            int *err)
+{
+  int r;
+
+  r = next_ops->zero (nxdata, count, offset, flags, err);
+  if (r == 0) {
+    pthread_mutex_lock (&lock);
+    zero_ops++;
+    zero_bytes += count;
+    pthread_mutex_unlock (&lock);
+  }
+  return r;
+}
+
+/* Extents. */
+static int
+stats_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
+               void *handle,
+               uint32_t count, uint64_t offset, uint32_t flags,
+               struct nbdkit_extents *extents, int *err)
+{
+  int r;
+
+  r = next_ops->extents (nxdata, count, offset, flags, extents, err);
+  if (r == 0) {
+    pthread_mutex_lock (&lock);
+    extents_ops++;
+    /* XXX There's a case for trying to determine how long the extents
+     * will be that are returned to the client, given the flags and
+     * the complex rules in the protocol.
+     */
+    extents_bytes += count;
+    pthread_mutex_unlock (&lock);
+  }
+  return r;
+}
+
+static struct nbdkit_filter filter = {
+  .name              = "stats",
+  .longname          = "nbdkit stats filter",
+  .version           = PACKAGE_VERSION,
+  .unload            = stats_unload,
+  .config            = stats_config,
+  .config_complete   = stats_config_complete,
+  .pread             = stats_pread,
+  .pwrite            = stats_pwrite,
+  .trim              = stats_trim,
+  .zero              = stats_zero,
+  .extents           = stats_extents,
+};
+
+NBDKIT_REGISTER_FILTER(filter)

configure.ac
@@ -836,6 +836,7 @@ filters="\
         partition \
         rate \
         readahead \
+        stats \
         truncate \
         xz \
         "
@@ -910,6 +911,7 @@ AC_CONFIG_FILES([Makefile
                  filters/partition/Makefile
                  filters/rate/Makefile
                  filters/readahead/Makefile
+                 filters/stats/Makefile
                  filters/truncate/Makefile
                  filters/xz/Makefile
                  fuzzing/Makefile

filters/log/nbdkit-log-filter.pod
@@ -65,7 +65,8 @@ successful read is:
 L<nbdkit(1)>,
 L<nbdkit-file-plugin(1)>,
 L<nbdkit-cow-filter(1)>,
-L<nbdkit-filter(3)>.
+L<nbdkit-filter(3)>,
+L<nbdkit-stats-filter(1)>.
 
 =head1 AUTHORS
 

filters/stats/Makefile.am
@@ -1,0 +1,61 @@
+# nbdkit
+# Copyright (C) 2019 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-stats-filter.pod
+
+filter_LTLIBRARIES = nbdkit-stats-filter.la
+
+nbdkit_stats_filter_la_SOURCES = \
+	stats.c \
+	$(top_srcdir)/include/nbdkit-filter.h
+
+nbdkit_stats_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include
+nbdkit_stats_filter_la_CFLAGS = \
+	$(WARNINGS_CFLAGS)
+nbdkit_stats_filter_la_LDFLAGS = \
+	-module -avoid-version -shared \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms
+
+if HAVE_POD
+
+man_MANS = nbdkit-stats-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-stats-filter.1: nbdkit-stats-filter.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

filters/stats/nbdkit-stats-filter.pod
@@ -1,0 +1,61 @@
+=head1 NAME
+
+nbdkit-stats-filter - display statistics about operations
+
+=head1 SYNOPSIS
+
+ nbdkit --filter=stats PLUGIN statsfile=FILE [statsappend=true]
+
+=head1 DESCRIPTION
+
+C<nbdkit-stats-filter> is a filter that displays statistics about NBD
+operations, such as the number of bytes read and written.  Statistics
+are written to a file once when nbdkit exits.
+
+=head1 EXAMPLE
+
+In this example we run L<guestfish(1)> over nbdkit to create an ext4
+filesystem on a RAM disk, and use the stats filter to display the
+number of read, write and trim operations involved:
+
+ $ nbdkit -U - --filter=stats memory size=1G statsfile=/dev/stderr \
+          --run '
+       guestfish add-drive "" protocol:nbd server:unix:$unixsocket \
+                              discard:enable format:raw : \
+                 run : \
+                 mkfs ext4 /dev/sda
+ '
+ elapsed time: 1.1248 s
+ read: 219 ops, 4964864 bytes, 3.53119e+07 bits/s
+ write: 78 ops, 34230272 bytes, 2.43458e+08 bits/s
+ trim: 33 ops, 1073741824 bytes, 7.63683e+09 bits/s
+
+=head1 PARAMETERS
+
+=over 4
+
+=item B<statsfile=>FILE
+
+The file where we write the stats.
+
+This parameter is required.
+
+=item B<statsappend=true>
+
+If set then we append to the file instead of replacing it.
+
+=back
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-filter(3)>,
+L<nbdkit-log-filter(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2019 Red Hat Inc.

commit e13cc5d0b40df8f4882f630e2025ba378593490d

stats: Document config parameters
Make 'nbdkit --help --filter=stats null' give more details about the
stats filter.  Missed with the introduction of the filter.

Fixes: fa0585f1
Signed-off-by: Eric Blake <eblake@redhat.com>

filters/stats/stats.c
@@ -155,6 +155,10 @@ stats_config_complete (nbdkit_next_config_complete *next, void *nxdata)
   return next (nxdata);
 }
 
+#define stats_config_help \
+  "statsfile=<FILE>    (required) The file to place the log in.\n" \
+  "statsappend=<BOOL>  True to append to the log (default false).\n"
+
 /* Read. */
 static int
 stats_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
@@ -273,6 +277,7 @@ static struct nbdkit_filter filter = {
   .unload            = stats_unload,
   .config            = stats_config,
   .config_complete   = stats_config_complete,
+  .config_help       = stats_config_help,
   .pread             = stats_pread,
   .pwrite            = stats_pwrite,
   .trim              = stats_trim,