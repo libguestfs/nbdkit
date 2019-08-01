ca06572b307ca101fc0ebdd97d59b94237b56900
Eric Blake <eblake@redhat.com>
2019-08-01 16:56:22 -0500

    plugins: Match docs for .errno_is_preserved
    Ever since commit 69ae137f, the docs claimed that we check
    .errno_is_preserved == 1, but the code has checked for != 0.  It's
    easy enough to add new fields if we need new semantics, so there is no
    reason to break any existing plugin that was setting the field to 2;
    so change the docs to match reality, but also change --dump-plugin to
    canonicalize its output.
    
    Furthermore, the mention of .errno_is_preserved in the docs was rather
    hidden; a new section will make it easier to add future knobs that
    likewise affect the plugin as a whole and not an individual
    connection.
    
    Fixes: 69ae137f
    Signed-off-by: Eric Blake <eblake@redhat.com>
    (cherry picked from commit 1f5b642613edd7d84017b9a6068e6db3afd286e6)

--- server/plugins.c
+++ server/plugins.c
@@ -166,7 +166,7 @@ plugin_dump_fields (struct backend *b)
     break;
   }
   printf ("\n");
-  printf ("errno_is_preserved=%d\n", p->plugin.errno_is_preserved);
+  printf ("errno_is_preserved=%d\n", !!p->plugin.errno_is_preserved);
   if (p->plugin.magic_config_key)
     printf ("magic_config_key=%s\n", p->plugin.magic_config_key);
 
@@ -464,7 +464,7 @@ get_error (struct backend_plugin *p)
 {
   int ret = threadlocal_get_error ();
 
-  if (!ret && p->plugin.errno_is_preserved)
+  if (!ret && p->plugin.errno_is_preserved != 0)
     ret = errno;
   return ret ? ret : EIO;
 }
