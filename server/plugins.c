libguestfs/nbdkit

commit 69ae137f78bc7fb443f865d85d9363300ed04a70

Define .errno_is_preserved constant instead of .errno_is_reliable callback.
The callback doesn't make much sense: Could the value change
per-connection?  Unlikely.  This is a property of the plugin as a
whole.

I changed the name to "errno_is_preserved", because it's not about the
reliability of errno, but about whether errno is preserved across
calls.

docs/nbdkit-plugin.pod
@@ -147,10 +147,16 @@ C<nbdkit_error> to report an error message; additionally, if the
 callback is involved in serving data, the plugin should call
 C<nbdkit_set_error> to influence the error code that will be sent to
 the client.  These two functions can be called in either order.  Then,
-the callback should return the appropriate error indication, eg. NULL
-or -1.  If the call to C<nbdkit_set_error> is omitted while serving
-data, then the C<.errno_is_reliable> callback controls whether to fall
-back to the value of C<errno> or a hard-coded C<EIO>.
+the callback should return the appropriate error indication,
+eg. C<NULL> or C<-1>.
+
+If the call to C<nbdkit_set_error> is omitted while serving data, then
+the global variable C<errno> may be used.  For plugins which have
+C<.errno_is_preserved == 1> the core code will use C<errno>.  In
+plugins written in non-C languages, we usually cannot trust that
+C<errno> will not be overwritten when returning from that language to
+C.  In that case, either the plugin must call C<nbdkit_set_error> or
+hard-coded C<EIO> is used.
 
 C<nbdkit_error> has the following prototype and works like
 L<printf(3)>:
@@ -385,29 +391,6 @@ error message and return C<-1>.
 
 This callback is not required.  If omitted, then we return true iff a
 C<.trim> callback has been defined.
-
-=head2 C<.errno_is_reliable>
-
- int errno_is_reliable (void *handle);
-
-This is called during the option negotiation phase to find out what
-errors to reflect to the client.  An error explicitly reported to
-C<nbdkit_set_error> takes precedence, but plugins written in C can
-often get by with implicitly using the value stored in C<errno>, when
-this callback returns true. On the other hand, a plugin written in any
-other language generally cannot control the value of C<errno> after
-the glue code of the language binding wrappers; returning false forces
-the use of C<EIO> as the fallback error rather than using an
-unreliable value in C<errno>.  This callback is not needed in the
-bindings of other languages.  The results of this callback do not
-matter if all data serving callbacks correctly call
-C<nbdkit_set_error>.
-
-If there is an error, C<.errno_is_reliable> should call C<nbdkit_error>
-with an error message and return C<-1>.
-
-This callback is not required.  If omitted, then we return true for
-a plugin written in C, and false for plugins in other languages.
 
 =head2 C<.pread>
 

src/plugins.c
@@ -236,6 +236,7 @@ plugin_dump_fields (void)
     break;
   }
   printf ("\n");
+  printf ("errno_is_preserved=%d\n", plugin.errno_is_preserved);
 
 #define HAS(field) if (plugin.field) printf ("has_%s=1\n", #field)
   HAS (longname);
@@ -350,6 +351,14 @@ plugin_unlock_request (struct connection *conn)
 }
 
 int
+plugin_errno_is_preserved (void)
+{
+  assert (dl);
+
+  return plugin.errno_is_preserved;
+}
+
+int
 plugin_open (struct connection *conn, int readonly)
 {
   void *handle;
@@ -392,21 +401,6 @@ plugin_get_size (struct connection *conn)
   debug ("get_size");
 
   return plugin.get_size (conn->handle);
-}
-
-int
-plugin_errno_is_reliable (struct connection *conn)
-{
-  assert (dl);
-  assert (conn->handle);
-
-  debug ("errno_is_reliable");
-
-  if (plugin.errno_is_reliable)
-    return plugin.errno_is_reliable (conn->handle);
-
-  /* Default to 1, for backwards compatibility (correct for C plugins) */
-  return 1;
 }
 
 int
@@ -548,7 +542,7 @@ plugin_zero (struct connection *conn,
     result = plugin.zero (conn->handle, count, offset, may_trim);
     if (result == -1) {
       err = tls_get_error ();
-      if (!err && conn->errno_is_reliable)
+      if (!err && plugin_errno_is_preserved ())
         err = errno;
     }
     if (result == 0 || err != EOPNOTSUPP)

include/nbdkit-plugin.h
@@ -92,7 +92,7 @@ struct nbdkit_plugin {
   int (*trim) (void *handle, uint32_t count, uint64_t offset);
   int (*zero) (void *handle, uint32_t count, uint64_t offset, int may_trim);
 
-  int (*errno_is_reliable) (void *handle);
+  int errno_is_preserved;
 
   /* int (*set_exportname) (void *handle, const char *exportname); */
 };

plugins/ocaml/nbdkit-ocaml-plugin.pod
@@ -89,17 +89,6 @@ handle struct in your plugin:
    printf "handle ID = %d\n" handle.h_id;
    (* ... *)
 
-=head2 MISSING CALLBACKS
-
-=over 4
-
-=item Missing: C<errno_is_reliable>
-
-This is not needed because the process of gluing OCaml code into C cannot
-reliably use C<errno>.
-
-=back
-
 =head2 THREADS
 
 The first parameter of C<NBDKit.register_plugin> is the thread model,

plugins/ocaml/ocaml.c
@@ -499,16 +499,6 @@ SET(pwrite)
 SET(flush)
 SET(trim)
 
-/* We can't guarantee that errno is stable across language binding
- * glue code, so this callback is implemented in C only, and not
- * exposed in OCaml.
- */
-static int
-plugin_ocaml_errno_is_reliable (void *handle)
-{
-  return 0;
-}
-
 /* We can't directly use NBDKIT_REGISTER_PLUGIN(). */
 struct nbdkit_plugin *
 plugin_init (void)
@@ -517,6 +507,5 @@ plugin_init (void)
     fprintf (stderr, "error: OCaml code did not call NBDKit.register_plugin\n");
     exit (EXIT_FAILURE);
   }
-  plugin.errno_is_reliable = plugin_ocaml_errno_is_reliable;
   return &plugin;
 }

plugins/perl/nbdkit-perl-plugin.pod
@@ -327,12 +327,6 @@ C<Nbdkit::set_error(POSIX::EOPNOTSUPP)>.
 These are not needed because you can just use regular Perl C<BEGIN>
 and C<END> constructs.
 
-=item Missing: C<errno_is_reliable>
-
-This is not needed because the process of gluing Perl code into C cannot
-reliably use C<errno>.  Instead, call C<Nbdkit::set_error> when reporting
-a failure.
-
 =item Missing: C<name>, C<version>, C<longname>, C<description>, C<config_help>
 
 These are not yet supported.

plugins/perl/perl.c
@@ -647,16 +647,6 @@ perl_trim (void *handle, uint32_t count, uint64_t offset)
   return 0;
 }
 
-/* We can't guarantee that errno is stable across language binding
- * glue code, so this callback is implemented in C only, and not
- * exposed in Perl.
- */
-static int
-perl_errno_is_reliable (void *handle)
-{
-  return 0;
-}
-
 #define perl_config_help \
   "script=<FILENAME>     (required) The Perl plugin to run.\n" \
   "[other arguments may be used by the plugin that you load]"
@@ -688,8 +678,6 @@ static struct nbdkit_plugin plugin = {
   .flush             = perl_flush,
   .trim              = perl_trim,
   .zero              = perl_zero,
-
-  .errno_is_reliable = perl_errno_is_reliable,
 };
 
 NBDKIT_REGISTER_PLUGIN(plugin)

plugins/python/nbdkit-python-plugin.pod
@@ -246,12 +246,6 @@ use C<nbdkit.set_error(errno.EOPNOTSUPP)>.
 These are not needed because you can just use ordinary Python
 constructs.
 
-=item Missing: C<errno_is_reliable>
-
-This is not needed because the process of gluing Python code into C cannot
-reliably use C<errno>.  Instead, call C<nbdkit.set_error> when reporting
-a failure.
-
 =item Missing: C<name>, C<version>, C<longname>, C<description>, C<config_help>
 
 These are not yet supported.

commit 1f5b642613edd7d84017b9a6068e6db3afd286e6

plugins: Match docs for .errno_is_preserved
Ever since commit 69ae137f, the docs claimed that we check
.errno_is_preserved == 1, but the code has checked for != 0.  It's
easy enough to add new fields if we need new semantics, so there is no
reason to break any existing plugin that was setting the field to 2;
so change the docs to match reality, but also change --dump-plugin to
canonicalize its output.

Furthermore, the mention of .errno_is_preserved in the docs was rather
hidden; a new section will make it easier to add future knobs that
likewise affect the plugin as a whole and not an individual
connection.

Fixes: 69ae137f
Signed-off-by: Eric Blake <eblake@redhat.com>

docs/nbdkit-plugin.pod
@@ -271,7 +271,7 @@ eg. C<NULL> or C<-1>.
 
 If the call to C<nbdkit_set_error> is omitted while serving data, then
 the global variable C<errno> may be used.  For plugins which have
-C<.errno_is_preserved == 1> the core code will use C<errno>.  In
+C<.errno_is_preserved != 0> the core code will use C<errno>.  In
 plugins written in non-C languages, we usually cannot trust that
 C<errno> will not be overwritten when returning from that language to
 C.  In that case, either the plugin must call C<nbdkit_set_error> or
@@ -914,6 +914,22 @@ with a specific caching semantic.
 If there is an error, C<.cache> should call C<nbdkit_error> with an
 error message, and C<nbdkit_set_error> to record an appropriate error
 (unless C<errno> is sufficient), then return C<-1>.
+
+=head1 OTHER FIELDS
+
+The plugin struct also contains an integer field used as a
+boolean in C code, but unlikely to be exposed in other language
+bindings:
+
+=over 4
+
+=item C<.errno_is_preserved>
+
+This defaults to 0; if non-zero, nbdkit can reliably use the value of
+C<errno> when a callback reports failure, rather than the plugin
+having to call C<nbdkit_set_error>.
+
+=back
 
 =head1 THREADS
 

server/plugins.c
@@ -176,7 +176,7 @@ plugin_dump_fields (struct backend *b)
     break;
   }
   printf ("\n");
-  printf ("errno_is_preserved=%d\n", p->plugin.errno_is_preserved);
+  printf ("errno_is_preserved=%d\n", !!p->plugin.errno_is_preserved);
   if (p->plugin.magic_config_key)
     printf ("magic_config_key=%s\n", p->plugin.magic_config_key);
 
@@ -492,7 +492,7 @@ get_error (struct backend_plugin *p)
 {
   int ret = threadlocal_get_error ();
 
-  if (!ret && p->plugin.errno_is_preserved)
+  if (!ret && p->plugin.errno_is_preserved != 0)
     ret = errno;
   return ret ? ret : EIO;
 }