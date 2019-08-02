libguestfs/nbdkit

commit 9911a9d39a3c4c5d80d92767d5f10f17b00ec0db

plugins: Add .thread_model callback
Plugins cannot get rid of #define THREAD_MODEL (doing so would break
API/ABI compatibility); however, we CAN add an optional callback for
allowing a runtime reduction of the thread model. This can be
especially useful for language bindings, as in the next patch, where
the C glue code for the language has to cater to the maximum
parallelism possible for that language, but where an individual plugin
may still want to be stricter.

Conversely, it is intentional that the .thread_model callback can
request to be more parallel than THREAD_MODEL, and such requests are
silently ignored. This allows us to release language bindings that
expose only the callback form in other languages, while still defining
THREAD_MODEL to match the restrictions of the glue code; if we can
later figure out how to make the glue code more parallel, the script
plugin can automatically benefit from that when run against newer
nbdkit.

Signed-off-by: Eric Blake <eblake@redhat.com>

docs/nbdkit-plugin.pod
@@ -133,6 +133,12 @@ has been passed to the plugin.
 Both are called after loading the plugin but before any connections
 are accepted.
 
+=item C<.thread_model>
+
+C<.thread_model> is called once after all configuration information
+has been passed to the plugin, and before any connections are
+accepted.
+
 =item C<.open>
 
 A new client has connected.
@@ -465,6 +471,19 @@ what already appears in C<.description>.
 
 If the plugin doesn't take any config parameters you should probably
 omit this.
+
+=head2 C<.thread_model>
+
+ int thread_model (void)
+
+This optional callback is called after all the configuration has been
+passed to the plugin.  It can be used to force a stricter thread model
+based on configuration, compared to C<THREAD_MODEL>.  See L</THREADS>
+below for details.  Attempts to request a looser (more parallel) model
+are silently ignored.
+
+If there is an error, C<.thread_model> should call C<nbdkit_error>
+with an error message and return C<-1>.
 
 =head2 C<.open>
 
@@ -898,9 +917,14 @@ error message, and C<nbdkit_set_error> to record an appropriate error
 
 =head1 THREADS
 
-Each nbdkit plugin must declare its thread safety model by defining
-the C<THREAD_MODEL> macro.  (This macro is used by
-C<NBDKIT_REGISTER_PLUGIN>).
+Each nbdkit plugin must declare its maximum thread safety model by
+defining the C<THREAD_MODEL> macro.  (This macro is used by
+C<NBDKIT_REGISTER_PLUGIN>).  Additionally, a plugin may implement the
+C<.thread_model> callback, called right after C<.config_complete> to
+make a runtime decision on which thread model to use.  The nbdkit
+server chooses the most restrictive model between the plugin's
+C<THREAD_MODEL>, the C<.thread_model> if present, and any restrictions
+requested by filters.
 
 The possible settings for C<THREAD_MODEL> are defined below.
 
@@ -935,7 +959,8 @@ parallel.  However only one request will happen per handle at a time
 =item C<#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL>
 
 Multiple handles can be open and multiple data requests can happen in
-parallel (even on the same handle).
+parallel (even on the same handle).  The server may reorder replies,
+answering a later request before an earlier one.
 
 All the libraries you use must be thread-safe and reentrant.  You may
 also need to provide mutexes for fields in your connection handle.

server/plugins.c
@@ -87,8 +87,18 @@ static int
 plugin_thread_model (struct backend *b)
 {
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
+  int thread_model = p->plugin._thread_model;
+  int r;
 
-  return p->plugin._thread_model;
+  if (p->plugin.thread_model) {
+    r = p->plugin.thread_model ();
+    if (r == -1)
+      exit (EXIT_FAILURE);
+    if (r < thread_model)
+      thread_model = r;
+  }
+
+  return thread_model;
 }
 
 static const char *

include/nbdkit-plugin.h
@@ -130,6 +130,8 @@ struct nbdkit_plugin {
                   struct nbdkit_extents *extents);
   int (*can_cache) (void *handle);
   int (*cache) (void *handle, uint32_t count, uint64_t offset, uint32_t flags);
+
+  int (*thread_model) (void);
 };
 
 extern void nbdkit_set_error (int err);

commit f12371c8acd658f502b0765d211686d372a31b22

plugins: Expose more thread_model details in --dump-plugin
When we added .thread_model to plugins, we forgot to expose
has_thread_model in the --dump-plugin output.  What's more, it is
useful to determine if the plugin itself is aware of tighter
restrictions imposed by dynamic choices, by outputting both
'max_thread_model' (what the plugin was compiled as) and
'thread_model' (the resulting model based on runtime knowledge).

Note that this means that a plugin's .thread_model can be called prior
to .config_complete, so the sh plugin must be prepared for a missing
script.  In fact, exposing additional information during --dump-plugin
will make it easier for a future patch to make sh actually allow
PARALLEL, and still have introspection on which model will actually be
used.

Fixes: 9911a9d3
Signed-off-by: Eric Blake <eblake@redhat.com>

docs/nbdkit-plugin.pod
@@ -128,16 +128,19 @@ is called once just after the plugin is loaded into memory.
 
 C<.config> is called zero or more times during command line parsing.
 C<.config_complete> is called once after all configuration information
-has been passed to the plugin.
+has been passed to the plugin (but not during C<nbdkit --dump-plugin>).
 
 Both are called after loading the plugin but before any connections
 are accepted.
 
 =item C<.thread_model>
 
-C<.thread_model> is called once after all configuration information
-has been passed to the plugin, and before any connections are
-accepted.
+In normal operation, C<.thread_model> is called once after
+C<.config_complete> has validated all configuration information, and
+before any connections are accepted.  However, during C<nbdkit
+--dump-plugin>, it is called after any C<.config> calls but without
+C<.config_complete> (so a plugin which determines the results from a
+script must be prepared for a missing script).
 
 =item C<.open>
 

plugins/sh/nbdkit-sh-plugin.pod
@@ -191,7 +191,8 @@ C<"serialize_requests">, or C<"parallel">.
 This method is I<not> required; if omitted, then the plugin will be
 executed under the default sh thread model (currently
 C<"serialize_all_requests">, which implies this callback only makes a
-difference with an output of C<"serialize_connections">).  If an error
+difference with an output of C<"serialize_connections">; look for
+'max_thread_model' in C<nbdkit --dump-plugin sh>).  If an error
 occurs, the script should output an error message and exit with status
 C<1>; unrecognized output is ignored.
 

plugins/sh/sh.c
@@ -273,6 +273,9 @@ sh_thread_model (void)
   size_t slen;
   int r;
 
+  if (!script)
+    return THREAD_MODEL;
+
   switch (call_read (&s, &slen, args)) {
   case OK:
     if (slen > 0 && s[slen-1] == '\n')

server/plugins.c
@@ -140,6 +140,33 @@ plugin_version (struct backend *b)
   return p->plugin.version;
 }
 
+/* Map thread model to a string */
+static void
+plugin_dump_thread_model (const char *prefix, int model)
+{
+  flockfile (stdout);
+  printf ("%s=", prefix);
+  switch (model) {
+  case NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS:
+    printf ("serialize_connections");
+    break;
+  case NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS:
+    printf ("serialize_all_requests");
+    break;
+  case NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS:
+    printf ("serialize_requests");
+    break;
+  case NBDKIT_THREAD_MODEL_PARALLEL:
+    printf ("parallel");
+    break;
+  default:
+    printf ("%d # unknown thread model!", model);
+    break;
+  }
+  printf ("\n");
+  funlockfile (stdout);
+}
+
 /* This implements the --dump-plugin option. */
 static void
 plugin_dump_fields (struct backend *b)
@@ -157,25 +184,8 @@ plugin_dump_fields (struct backend *b)
 
   printf ("api_version=%d\n", p->plugin._api_version);
   printf ("struct_size=%" PRIu64 "\n", p->plugin._struct_size);
-  printf ("thread_model=");
-  switch (p->plugin._thread_model) {
-  case NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS:
-    printf ("serialize_connections");
-    break;
-  case NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS:
-    printf ("serialize_all_requests");
-    break;
-  case NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS:
-    printf ("serialize_requests");
-    break;
-  case NBDKIT_THREAD_MODEL_PARALLEL:
-    printf ("parallel");
-    break;
-  default:
-    printf ("%d # unknown thread model!", p->plugin._thread_model);
-    break;
-  }
-  printf ("\n");
+  plugin_dump_thread_model ("max_thread_model", p->plugin._thread_model);
+  plugin_dump_thread_model ("thread_model", backend->thread_model (backend));
   printf ("errno_is_preserved=%d\n", !!p->plugin.errno_is_preserved);
   if (p->plugin.magic_config_key)
     printf ("magic_config_key=%s\n", p->plugin.magic_config_key);
@@ -213,6 +223,7 @@ plugin_dump_fields (struct backend *b)
   HAS (extents);
   HAS (can_cache);
   HAS (cache);
+  HAS (thread_model);
 #undef HAS
 
   /* Custom fields. */

tests/test-dump-plugin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbdkit
-# Copyright (C) 2014 Red Hat Inc.
+# Copyright (C) 2014-2019 Red Hat Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -68,3 +68,32 @@ do_test ()
     esac
 }
 foreach_plugin do_test
+
+# Test that --dump-plugin can be used to introspect a resulting dynamic
+# thread model.
+out=$({
+    # sh does not yet support parallel
+    nbdkit --dump-plugin sh
+    # Here, the script further reduces things
+    nbdkit --dump-plugin sh - <<\EOF
+case $1 in
+    get_size) echo 1M ;;
+    thread_model) echo serialize_connections ;;
+    *) exit 2 ;;
+esac
+EOF
+    # Here, the filter further reduces things
+    nbdkit --dump-plugin --filter=noparallel sh serialize=connections
+} | grep thread_model)
+exp="max_thread_model=serialize_all_requests
+thread_model=serialize_all_requests
+has_thread_model=1
+max_thread_model=serialize_all_requests
+thread_model=serialize_connections
+has_thread_model=1
+max_thread_model=serialize_all_requests
+thread_model=serialize_connections
+has_thread_model=1"
+if [ "$out" != "$exp" ]; then
+    echo "thread_model mismatch"; exit 1
+fi