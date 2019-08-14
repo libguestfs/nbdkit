libguestfs/nbdkit

commit 08b61ba6c18c07f78f3e9f0d407c0fb856da603f

build: Silence git warning under podwrapper
If the current git checkout has configured an orderfile for saner
reading of diffs, this configuration bleeds through to the use of git
within podwrapper for annoying warnings like:

perl /home/eblake/libnbd/podwrapper.pl --section=3 --man nbd_create.3 \
    --html ../html/nbd_create.3.html \
    nbd_create.pod
fatal: failed to read orderfile 'scripts/git.orderfile': No such file or directory

Although the warnings have no impact on the build, we might as well
silence them (since the use of the orderfile is worthwhile).

Signed-off-by: Eric Blake <eblake@redhat.com>

include/nbdkit-plugin.h
@@ -132,6 +132,7 @@ struct nbdkit_plugin {
   int (*cache) (void *handle, uint32_t count, uint64_t offset, uint32_t flags);
 
   int (*thread_model) (void);
+  int (*can_fast_zero) (void *handle);
 };
 
 extern void nbdkit_set_error (int err);

podwrapper.pl.in
@@ -213,7 +213,7 @@ my $date;
 my $filename = "$abs_top_srcdir/.git";
 if (!$date && -d $filename) {
     local $ENV{GIT_DIR} = $filename;
-    $_ = `git show -s --format=%ci`;
+    $_ = `git show -O/dev/null -s --format=%ci`;
     $date = $1 if /^(\d+-\d+-\d+)\s/;
 }
 if (!$date) {

commit a57632e1f6aa7838e49cc3db3dfb45279f2e7d62

include: Undo accidental commit
Fast zero support is coming soon, but it was a mistake to include it
in an unrelated patch.

Fixes: 08b61ba6
Signed-off-by: Eric Blake <eblake@redhat.com>

include/nbdkit-plugin.h
@@ -132,7 +132,6 @@ struct nbdkit_plugin {
   int (*cache) (void *handle, uint32_t count, uint64_t offset, uint32_t flags);
 
   int (*thread_model) (void);
-  int (*can_fast_zero) (void *handle);
 };
 
 extern void nbdkit_set_error (int err);