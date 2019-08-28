02f0b179fdd8919b7018bf222e8272144a44de51
Richard W.M. Jones <rjones@redhat.com>
2019-08-28 14:36:00 +0100

    tests: Fix test-cache-max-size.sh.
    This test fails often on slow systems.  It's quite easy to make it
    fail on S/390 machines.  Even on x86-64 it occasionally fails when run
    under valgrind.  The error is shown below.
    
    The failure is caused when the process has more than 11 file
    descriptors.  The file descriptor we are looking for in the example
    below should be fd 3:
    
      lrwx------. 1 rjones rjones 64 Aug 28 14:08 3 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache/SVuOVG (deleted)
    
    However because of a faulty regexp in the test combined with having >=
    11 file descriptors and a slow machine we actually matched the wrong
    file:
    
      lrwx------. 1 rjones rjones 64 Aug 28 14:08 10 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache-max-size.img
    
    It also happens that the file plugin closes this file on the
    connection close path which we are running after the qemu-io client
    finishes.  So if the timing is wrong the file gets deleted before we
    can do the stat on this (wrong) file causing the failure that is seen.
    
    In any case since we are matching the completely wrong file, fix the
    regexp.
    
    ----------
    
    + fddir=/proc/2403/fd
    + ls -l /proc/2403/fd
    nbdkit: file.8: debug: exiting worker thread file.8
    nbdkit: file.6: debug: exiting worker thread file.6
    nbdkit: file.7: debug: exiting worker thread file.7
    nbdkit: file.9: debug: exiting worker thread file.9
    nbdkit: file.10: debug: exiting worker thread file.10
    nbdkit: file.11: debug: exiting worker thread file.11
    nbdkit: file.12: debug: exiting worker thread file.12
    total 0
    lr-x------. 1 rjones rjones 64 Aug 28 14:08 0 -> /dev/null
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 1 -> /dev/null
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 10 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache-max-size.img
    lr-x------. 1 rjones rjones 64 Aug 28 14:08 1024 -> /home/rjones/d/nbdkit/server/nbdkit
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 1025 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache/valgrind_proc_2294_cmdline_4435fcc9 (deleted)
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 1026 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache/valgrind_proc_2294_auxv_4435fcc9 (deleted)
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 1027 -> /home/rjones/d/nbdkit/tests/test-cache-max-size.sh.log
    lr-x------. 1 rjones rjones 64 Aug 28 14:08 1028 -> pipe:[78252230]
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 1029 -> pipe:[78252230]
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 2 -> /home/rjones/d/nbdkit/tests/test-cache-max-size.sh.log
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 3 -> /home/rjones/d/nbdkit/tests/cache-max-size.d/cache/SVuOVG (deleted)
    lr-x------. 1 rjones rjones 64 Aug 28 14:08 4 -> pipe:[78248572]
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 5 -> pipe:[78248572]
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 6 -> socket:[78248573]
    lrwx------. 1 rjones rjones 64 Aug 28 14:08 7 -> socket:[78253136]
    lr-x------. 1 rjones rjones 64 Aug 28 14:08 8 -> pipe:[78252236]
    l-wx------. 1 rjones rjones 64 Aug 28 14:08 9 -> pipe:[78252236]
    nbdkit: file.13: debug: exiting worker thread file.13
    nbdkit: file.14: debug: exiting worker thread file.14
    nbdkit: file.15: debug: exiting worker thread file.15
    ++ head -1
    ++ ls -l /proc/2403/fd
    nbdkit: file.0: debug: exiting worker thread file.0
    nbdkit: file.1: debug: exiting worker thread file.1
    nbdkit: file.4: debug: exiting worker thread file.4
    nbdkit: file.3: debug: exiting worker thread file.3
    nbdkit: file.2: debug: exiting worker thread file.2
    nbdkit: file.5: debug: exiting worker thread file.5
    ++ grep cache-max-size.d/cache
    ++ awk '{print $9}'
    nbdkit: file[1]: debug: cache: finalize
    + fd=/proc/2403/fd/10
    + stat -L /proc/2403/fd/10
    nbdkit: file[1]: debug: cache: close
    nbdkit: file[1]: debug: close
    stat: cannot stat '/proc/2403/fd/10': No such file or directory
    
    (cherry picked from commit 7ff24962dfbefdfa1e7f63e0438548a5e0a3e901)

--- tests/test-cache-max-size.sh
+++ tests/test-cache-max-size.sh
@@ -75,7 +75,7 @@ qemu-io -f raw "nbd+unix://?socket=$sock" \
 # Get the /proc link to the cache file, and the size of it in bytes.
 fddir="/proc/$( cat $d/cache-max-size.pid )/fd"
 ls -l $fddir ||:
-fd="$fddir/$( ls -l $fddir | grep $TMPDIR | head -1 | awk '{print $9}' )"
+fd="$fddir/$( ls -l $fddir | grep $TMPDIR/ | head -1 | awk '{print $9}' )"
 stat -L $fd
 size=$(( $(stat -L -c '%b * %B' $fd) ))
 
