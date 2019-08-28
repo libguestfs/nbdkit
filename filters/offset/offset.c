libguestfs/nbdkit

commit 1a5e2d9c01dcb46b0af38a16040900301a75b3ac

offset: Implement mapping of extents.
Allows you to safely use nbdkit-offset-filter on top of a plugin
supporting extents.

filters/offset/offset.c
@@ -37,6 +37,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <assert.h>
 
 #include <nbdkit-filter.h>
 
@@ -132,6 +133,40 @@ offset_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
   return next_ops->zero (nxdata, count, offs + offset, flags, err);
 }
 
+/* Extents. */
+static int
+offset_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
+                void *handle, uint32_t count, uint64_t offs, uint32_t flags,
+                struct nbdkit_extents *extents, int *err)
+{
+  size_t i;
+  struct nbdkit_extents *extents2;
+  struct nbdkit_extent e;
+  int64_t real_size = next_ops->get_size (nxdata);
+
+  extents2 = nbdkit_extents_new (offs + offset, real_size - offset);
+  if (extents2 == NULL) {
+    *err = errno;
+    return -1;
+  }
+  if (next_ops->extents (nxdata, count, offs + offset,
+                         flags, extents2, err) == -1)
+    goto error;
+
+  for (i = 0; i < nbdkit_extents_count (extents2); ++i) {
+    e = nbdkit_get_extent (extents2, i);
+    e.offset -= offset;
+    if (nbdkit_add_extent (extents, e.offset, e.length, e.type) == -1)
+      goto error;
+  }
+  nbdkit_extents_free (extents2);
+  return 0;
+
+ error:
+  nbdkit_extents_free (extents2);
+  return -1;
+}
+
 static struct nbdkit_filter filter = {
   .name              = "offset",
   .longname          = "nbdkit offset filter",
@@ -144,6 +179,7 @@ static struct nbdkit_filter filter = {
   .pwrite            = offset_pwrite,
   .trim              = offset_trim,
   .zero              = offset_zero,
+  .extents           = offset_extents,
 };
 
 NBDKIT_REGISTER_FILTER(filter)

commit 5a1db63d71763621a8e8cf977389371d582cc964

offset, partition: Fix .extents with non-zero offset
When querying the extents of the underlying plugin, we should only
translate the starting offset, and let the plugin report for at least
as many bytes as our range permits.  Otherwise, short-changing the
range causes bad behavior such as returning 0 extents, or even failing
the creation of an extents tracker:

$ cat script
case "$1" in
    get_size) echo 1m;;
    can_extents) ;;
    extents) echo 0 1m;;
    *) exit 2 ;;
esac
$ nbdkit -U - --filter=offset sh script offset=64k \
  --run 'qemu-io -r -f raw -c map $nbd'
nbdkit: sh[1]: error: extents: plugin must return at least one extent
896 KiB (0xe0000) bytes     allocated at offset 0 bytes (0x0)
nbdkit: sh[1]: error: extents: plugin must return at least one extent
qemu-io: Failed to get allocation status: Invalid argument
$ nbdkit -U - --filter=offset sh script offset=640k \
  --run 'qemu-io -r -f raw -c map $nbd'
nbdkit: sh[1]: error: nbdkit_extents_new: start (655360) >= end (393216)
qemu-io: Failed to get allocation status: Invalid argument

Fixes: 1a5e2d9c, 624abb36
Signed-off-by: Eric Blake <eblake@redhat.com>

docs/nbdkit-filter.pod
@@ -560,7 +560,7 @@ from the layer below.  Without error checking it would look like this:
    int64_t size;
 
    size = next_ops->get_size (nxdata);
-   extents2 = nbdkit_extents_new (offset + shift, size - shift);
+   extents2 = nbdkit_extents_new (offset + shift, size);
    next_ops->extents (nxdata, count, offset + shift, flags, extents2, err);
    for (i = 0; i < nbdkit_extents_count (extents2); ++i) {
      e = nbdkit_get_extent (extents2, i);

filters/offset/offset.c
@@ -140,9 +140,9 @@ offset_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
   size_t i;
   CLEANUP_EXTENTS_FREE struct nbdkit_extents *extents2 = NULL;
   struct nbdkit_extent e;
-  int64_t real_size = next_ops->get_size (nxdata);
+  int64_t end = range >= 0 ? offset + range : next_ops->get_size (nxdata);
 
-  extents2 = nbdkit_extents_new (offs + offset, real_size - offset);
+  extents2 = nbdkit_extents_new (offs + offset, end);
   if (extents2 == NULL) {
     *err = errno;
     return -1;

filters/partition/partition.c
@@ -230,9 +230,8 @@ partition_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
   size_t i;
   CLEANUP_EXTENTS_FREE struct nbdkit_extents *extents2 = NULL;
   struct nbdkit_extent e;
-  int64_t real_size = next_ops->get_size (nxdata);
 
-  extents2 = nbdkit_extents_new (offs + h->offset, real_size - h->offset);
+  extents2 = nbdkit_extents_new (offs + h->offset, h->offset + h->range);
   if (extents2 == NULL) {
     *err = errno;
     return -1;