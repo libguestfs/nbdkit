libguestfs/nbdkit

commit 602eb0038c2e3fa1fcd92eb50fad7d5a49662b57

cache: Implement cache-max-size and cache space reclaim.
The original plan was to have a background thread doing the reclaim.
However that cannot work given the design of filters, because a
background thread cannot access the next_ops struct which is only
available during requests.

Therefore we spread the work over the request threads.  Each blk_*
function checks whether there is work to do, and if there is will
reclaim up to two blocks from the cache (thus ensuring that we always
make forward progress, since each blk_* function can only add at most
one block to the cache).

Another large change is that the cache block size can no longer be
fixed.  We must use a block size which is at least as large as the
filesystem block size so that FALLOC_FL_PUNCH_HOLE works.  To do this,
test the filesystem block size and set blksize dynamically to
MAX (4096, filesystem block size).

This also adds a test.

filters/cache/reclaim.c
@@ -1,0 +1,207 @@
+/* nbdkit
+ * Copyright (C) 2018-2019 Red Hat Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <nbdkit-filter.h>
+
+#include "bitmap.h"
+
+#include "cache.h"
+#include "reclaim.h"
+#include "lru.h"
+
+#ifndef HAVE_CACHE_RECLAIM
+
+void
+reclaim (int fd, struct bitmap *bm)
+{
+  /* nothing */
+}
+
+#else /* HAVE_CACHE_RECLAIM */
+
+/* If we are currently reclaiming blocks from the cache.
+ *
+ * The state machine starts in the NOT_RECLAIMING state.  When the
+ * size of the cache exceeds the high threshold, we move to
+ * RECLAIMING_LRU.  Once we have exhausted all LRU blocks, we move to
+ * RECLAIMING_ANY (reclaiming any blocks).
+ *
+ * If at any time the size of the cache goes below the low threshold
+ * we move back to the NOT_RECLAIMING state.
+ *
+ * A possible future enhancement is to add an extra state between LRU
+ * and ANY which reclaims blocks from lru.c:bm[1].
+ *
+ * reclaim_blk is the last block that we looked at.
+ */
+enum reclaim_state {
+  NOT_RECLAIMING = 0,
+  RECLAIMING_LRU = 1,
+  RECLAIMING_ANY = 2,
+};
+
+static enum reclaim_state reclaiming = NOT_RECLAIMING;
+static uint64_t reclaim_blk;
+
+static void reclaim_one (int fd, struct bitmap *bm);
+static void reclaim_lru (int fd, struct bitmap *bm);
+static void reclaim_any (int fd, struct bitmap *bm);
+static void reclaim_block (int fd, struct bitmap *bm);
+
+void
+reclaim (int fd, struct bitmap *bm)
+{
+  struct stat statbuf;
+  uint64_t cache_allocated;
+
+  /* If the user didn't set cache-max-size, do nothing. */
+  if (max_size == -1) return;
+
+  /* Check the allocated size of the cache. */
+  if (fstat (fd, &statbuf) == -1) {
+    nbdkit_debug ("cache: fstat: %m");
+    return;
+  }
+  cache_allocated = statbuf.st_blocks * UINT64_C(512);
+
+  if (reclaiming) {
+    /* Keep reclaiming until the cache size drops below the low threshold. */
+    if (cache_allocated < max_size * lo_thresh / 100) {
+      nbdkit_debug ("cache: stop reclaiming");
+      reclaiming = NOT_RECLAIMING;
+      return;
+    }
+  }
+  else {
+    if (cache_allocated < max_size * hi_thresh / 100)
+      return;
+
+    /* Start reclaiming if the cache size goes over the high threshold. */
+    nbdkit_debug ("cache: start reclaiming");
+    reclaiming = RECLAIMING_LRU;
+  }
+
+  /* Reclaim up to 2 cache blocks. */
+  reclaim_one (fd, bm);
+  reclaim_one (fd, bm);
+}
+
+/* Reclaim a single cache block. */
+static void
+reclaim_one (int fd, struct bitmap *bm)
+{
+  assert (reclaiming);
+
+  if (reclaiming == RECLAIMING_LRU)
+    reclaim_lru (fd, bm);
+  else
+    reclaim_any (fd, bm);
+}
+
+static void
+reclaim_lru (int fd, struct bitmap *bm)
+{
+  uint64_t old_reclaim_blk;
+
+  /* Find the next block in the cache. */
+  reclaim_blk = bitmap_next (bm, reclaim_blk+1);
+  old_reclaim_blk = reclaim_blk;
+
+  /* Search for an LRU block after this one. */
+  do {
+    if (! lru_has_been_recently_accessed (reclaim_blk)) {
+      reclaim_block (fd, bm);
+      return;
+    }
+
+    reclaim_blk = bitmap_next (bm, reclaim_blk+1);
+    if (reclaim_blk == -1)    /* wrap around */
+      reclaim_blk = bitmap_next (bm, 0);
+  } while (reclaim_blk >= 0 && old_reclaim_blk != reclaim_blk);
+
+  if (old_reclaim_blk == reclaim_blk) {
+    /* Run out of LRU blocks, so start reclaiming any block in the cache. */
+    nbdkit_debug ("cache: reclaiming any blocks");
+    reclaiming = RECLAIMING_ANY;
+    reclaim_any (fd, bm);
+  }
+}
+
+static void
+reclaim_any (int fd, struct bitmap *bm)
+{
+  /* Find the next block in the cache. */
+  reclaim_blk = bitmap_next (bm, reclaim_blk+1);
+  if (reclaim_blk == -1)        /* wrap around */
+    reclaim_blk = bitmap_next (bm, 0);
+
+  reclaim_block (fd, bm);
+}
+
+static void
+reclaim_block (int fd, struct bitmap *bm)
+{
+  if (reclaim_blk == -1) {
+    nbdkit_debug ("cache: run out of blocks to reclaim!");
+    return;
+  }
+
+  nbdkit_debug ("cache: reclaiming block %" PRIu64, reclaim_blk);
+#ifdef FALLOC_FL_PUNCH_HOLE
+  if (fallocate (fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE,
+                 reclaim_blk * blksize, blksize) == -1) {
+    nbdkit_error ("cache: reclaiming cache blocks: "
+                  "fallocate: FALLOC_FL_PUNCH_HOLE: %m");
+    return;
+  }
+#else
+#error "no implementation for punching holes"
+#endif
+
+  bitmap_set_blk (bm, reclaim_blk, 0);
+}
+
+#endif /* HAVE_CACHE_RECLAIM */

TODO
@@ -100,15 +100,8 @@ Suggestions for filters
 * masking plugin features for testing clients (see 'nozero' and 'fua'
   filters for examples)
 
-nbdkit-cache-filter needs considerable work:
-
-* allow the user to limit the total size of the cache
-
-* handle ENOSPC errors
-
-* implement some cache replacement policies
-
-* some sort of background task or thread to write out dirty blocks
+* nbdkit-cache-filter should handle ENOSPC errors automatically by
+  reclaiming blocks from the cache
 
 Composing nbdkit
 ----------------

filters/cache/Makefile.am
@@ -43,6 +43,8 @@ nbdkit_cache_filter_la_SOURCES = \
 	cache.h \
 	lru.c \
 	lru.h \
+	reclaim.c \
+	reclaim.h \
 	$(top_srcdir)/include/nbdkit-filter.h
 
 nbdkit_cache_filter_la_CPPFLAGS = \

filters/cache/blk.c
@@ -46,14 +46,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/statvfs.h>
 
 #include <nbdkit-filter.h>
 
 #include "bitmap.h"
+#include "minmax.h"
 
 #include "cache.h"
 #include "blk.h"
 #include "lru.h"
+#include "reclaim.h"
 
 /* The cache. */
 static int fd = -1;
@@ -65,11 +68,16 @@ static int fd = -1;
  * 01 = block cached and clean
  * 10 = <unused>
  * 11 = block cached and dirty
+ *
+ * Idea for future enhancement: Use 10 (currently unused) to store
+ * blocks which are known to be zero and are not allocated in the
+ * cache layer.  You can set this when clients call cache_zero and
+ * defer calling plugin->zero until flush.
  */
 static struct bitmap bm;
 
 enum bm_entry {
-  BLOCK_NOT_CACHED = 0,
+  BLOCK_NOT_CACHED = 0, /* assumed to be zero by reclaim code */
   BLOCK_CLEAN = 1,
   BLOCK_DIRTY = 3,
 };
@@ -80,10 +88,7 @@ blk_init (void)
   const char *tmpdir;
   size_t len;
   char *template;
-
-  lru_init ();
-
-  bitmap_init (&bm, BLKSIZE, 2 /* bits per block */);
+  struct statvfs statvfs;
 
   tmpdir = getenv ("TMPDIR");
   if (!tmpdir)
@@ -107,6 +112,24 @@ blk_init (void)
   }
 
   unlink (template);
+
+  /* Choose the block size.
+   *
+   * A 4K block size means that we need 64 MB of memory to store the
+   * bitmaps for a 1 TB underlying image.  However to support
+   * hole-punching (for reclaiming) we need the block size to be at
+   * least as large as the filesystem block size.
+   */
+  if (fstatvfs (fd, &statvfs) == -1) {
+    nbdkit_error ("fstatvfs: %s: %m", tmpdir);
+    return -1;
+  }
+  blksize = MAX (4096, statvfs.f_bsize);
+  nbdkit_debug ("cache: block size: %u", blksize);
+
+  bitmap_init (&bm, blksize, 2 /* bits per block */);
+
+  lru_init ();
 
   return 0;
 }
@@ -143,8 +166,10 @@ int
 blk_read (struct nbdkit_next_ops *next_ops, void *nxdata,
           uint64_t blknum, uint8_t *block, int *err)
 {
-  off_t offset = blknum * BLKSIZE;
+  off_t offset = blknum * blksize;
   enum bm_entry state = bitmap_get_blk (&bm, blknum, BLOCK_NOT_CACHED);
+
+  reclaim (fd, &bm);
 
   nbdkit_debug ("cache: blk_read block %" PRIu64 " (offset %" PRIu64 ") is %s",
                 blknum, (uint64_t) offset,
@@ -154,18 +179,18 @@ blk_read (struct nbdkit_next_ops *next_ops, void *nxdata,
                 "unknown");
 
   if (state == BLOCK_NOT_CACHED) { /* Read underlying plugin. */
-    if (next_ops->pread (nxdata, block, BLKSIZE, offset, 0, err) == -1)
+    if (next_ops->pread (nxdata, block, blksize, offset, 0, err) == -1)
       return -1;
 
     /* If cache-on-read, copy the block to the cache. */
     if (cache_on_read) {
-      off_t offset = blknum * BLKSIZE;
+      off_t offset = blknum * blksize;
 
       nbdkit_debug ("cache: cache-on-read block %" PRIu64
                     " (offset %" PRIu64 ")",
                     blknum, (uint64_t) offset);
 
-      if (pwrite (fd, block, BLKSIZE, offset) == -1) {
+      if (pwrite (fd, block, blksize, offset) == -1) {
         *err = errno;
         nbdkit_error ("pwrite: %m");
         return -1;
@@ -176,7 +201,7 @@ blk_read (struct nbdkit_next_ops *next_ops, void *nxdata,
     return 0;
   }
   else {                        /* Read cache. */
-    if (pread (fd, block, BLKSIZE, offset) == -1) {
+    if (pread (fd, block, blksize, offset) == -1) {
       *err = errno;
       nbdkit_error ("pread: %m");
       return -1;
@@ -191,18 +216,20 @@ blk_writethrough (struct nbdkit_next_ops *next_ops, void *nxdata,
                   uint64_t blknum, const uint8_t *block, uint32_t flags,
                   int *err)
 {
-  off_t offset = blknum * BLKSIZE;
+  off_t offset = blknum * blksize;
+
+  reclaim (fd, &bm);
 
   nbdkit_debug ("cache: writethrough block %" PRIu64 " (offset %" PRIu64 ")",
                 blknum, (uint64_t) offset);
 
-  if (pwrite (fd, block, BLKSIZE, offset) == -1) {
+  if (pwrite (fd, block, blksize, offset) == -1) {
     *err = errno;
     nbdkit_error ("pwrite: %m");
     return -1;
   }
 
-  if (next_ops->pwrite (nxdata, block, BLKSIZE, offset, flags, err) == -1)
+  if (next_ops->pwrite (nxdata, block, blksize, offset, flags, err) == -1)
     return -1;
 
   bitmap_set_blk (&bm, blknum, BLOCK_CLEAN);
@@ -222,12 +249,14 @@ blk_write (struct nbdkit_next_ops *next_ops, void *nxdata,
       (cache_mode == CACHE_MODE_WRITEBACK && (flags & NBDKIT_FLAG_FUA)))
     return blk_writethrough (next_ops, nxdata, blknum, block, flags, err);
 
-  offset = blknum * BLKSIZE;
+  offset = blknum * blksize;
+
+  reclaim (fd, &bm);
 
   nbdkit_debug ("cache: writeback block %" PRIu64 " (offset %" PRIu64 ")",
                 blknum, (uint64_t) offset);
 
-  if (pwrite (fd, block, BLKSIZE, offset) == -1) {
+  if (pwrite (fd, block, blksize, offset) == -1) {
     *err = errno;
     nbdkit_error ("pwrite: %m");
     return -1;

filters/cache/cache.c
@@ -56,6 +56,7 @@
 
 #include "cache.h"
 #include "blk.h"
+#include "reclaim.h"
 
 #define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
 
@@ -64,7 +65,10 @@
  */
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
+unsigned blksize;
 enum cache_mode cache_mode = CACHE_MODE_WRITEBACK;
+int64_t max_size = -1;
+int hi_thresh = 95, lo_thresh = 80;
 bool cache_on_read = false;
 
 static int cache_flush (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle, uint32_t flags, int *err);
@@ -105,6 +109,53 @@ cache_config (nbdkit_next_config *next, void *nxdata,
       return -1;
     }
   }
+#ifdef HAVE_CACHE_RECLAIM
+  else if (strcmp (key, "cache-max-size") == 0) {
+    int64_t r;
+
+    r = nbdkit_parse_size (value);
+    if (r == -1)
+      return -1;
+    /* We set a lower limit for the cache size just to keep out of
+     * trouble.
+     */
+    if (r < 1024*1024) {
+      nbdkit_error ("cache-max-size is too small");
+      return -1;
+    }
+    max_size = r;
+    return 0;
+  }
+  else if (strcmp (key, "cache-high-threshold") == 0) {
+    if (sscanf (value, "%d", &hi_thresh) != 1) {
+      nbdkit_error ("invalid cache-high-threshold parameter: %s", value);
+      return -1;
+    }
+    if (hi_thresh <= 0) {
+      nbdkit_error ("cache-high-threshold must be greater than zero");
+      return -1;
+    }
+    return 0;
+  }
+  else if (strcmp (key, "cache-low-threshold") == 0) {
+    if (sscanf (value, "%d", &lo_thresh) != 1) {
+      nbdkit_error ("invalid cache-low-threshold parameter: %s", value);
+      return -1;
+    }
+    if (lo_thresh <= 0) {
+      nbdkit_error ("cache-low-threshold must be greater than zero");
+      return -1;
+    }
+    return 0;
+  }
+#else /* !HAVE_CACHE_RECLAIM */
+  else if (strcmp (key, "cache-max-size") == 0 ||
+           strcmp (key, "cache-high-threshold") == 0 ||
+           strcmp (key, "cache-low-threshold") == 0) {
+    nbdkit_error ("this platform does not support cache reclaim");
+    return -1;
+  }
+#endif /* !HAVE_CACHE_RECLAIM */
   else if (strcmp (key, "cache-on-read") == 0) {
     int r;
 
@@ -117,6 +168,21 @@ cache_config (nbdkit_next_config *next, void *nxdata,
   else {
     return next (nxdata, key, value);
   }
+}
+
+static int
+cache_config_complete (nbdkit_next_config_complete *next, void *nxdata)
+{
+  /* If cache-max-size was set then check the thresholds. */
+  if (max_size != -1) {
+    if (lo_thresh >= hi_thresh) {
+      nbdkit_error ("cache-low-threshold must be "
+                    "less than cache-high-threshold");
+      return -1;
+    }
+  }
+
+  return next (nxdata);
 }
 
 static void *
@@ -176,7 +242,7 @@ cache_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
   uint8_t *block;
 
   assert (!flags);
-  block = malloc (BLKSIZE);
+  block = malloc (blksize);
   if (block == NULL) {
     *err = errno;
     nbdkit_error ("malloc: %m");
@@ -187,9 +253,9 @@ cache_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
     uint64_t blknum, blkoffs, n;
     int r;
 
-    blknum = offset / BLKSIZE;  /* block number */
-    blkoffs = offset % BLKSIZE; /* offset within the block */
-    n = BLKSIZE - blkoffs;      /* max bytes we can read from this block */
+    blknum = offset / blksize;  /* block number */
+    blkoffs = offset % blksize; /* offset within the block */
+    n = blksize - blkoffs;      /* max bytes we can read from this block */
     if (n > count)
       n = count;
 
@@ -221,7 +287,7 @@ cache_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
   uint8_t *block;
   bool need_flush = false;
 
-  block = malloc (BLKSIZE);
+  block = malloc (blksize);
   if (block == NULL) {
     *err = errno;
     nbdkit_error ("malloc: %m");
@@ -237,9 +303,9 @@ cache_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
     uint64_t blknum, blkoffs, n;
     int r;
 
-    blknum = offset / BLKSIZE;  /* block number */
-    blkoffs = offset % BLKSIZE; /* offset within the block */
-    n = BLKSIZE - blkoffs;      /* max bytes we can read from this block */
+    blknum = offset / blksize;  /* block number */
+    blkoffs = offset % blksize; /* offset within the block */
+    n = blksize - blkoffs;      /* max bytes we can read from this block */
     if (n > count)
       n = count;
 
@@ -278,14 +344,14 @@ cache_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
   uint8_t *block;
   bool need_flush = false;
 
-  block = malloc (BLKSIZE);
+  block = malloc (blksize);
   if (block == NULL) {
     *err = errno;
     nbdkit_error ("malloc: %m");
     return -1;
   }
 
-  flags &= ~NBDKIT_FLAG_MAY_TRIM; /* See BLKSIZE comment above. */
+  flags &= ~NBDKIT_FLAG_MAY_TRIM;
   if ((flags & NBDKIT_FLAG_FUA) &&
       next_ops->can_fua (nxdata) == NBDKIT_FUA_EMULATE) {
     flags &= ~NBDKIT_FLAG_FUA;
@@ -295,9 +361,9 @@ cache_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
     uint64_t blknum, blkoffs, n;
     int r;
 
-    blknum = offset / BLKSIZE;  /* block number */
-    blkoffs = offset % BLKSIZE; /* offset within the block */
-    n = BLKSIZE - blkoffs;      /* max bytes we can read from this block */
+    blknum = offset / blksize;  /* block number */
+    blkoffs = offset % blksize; /* offset within the block */
+    n = blksize - blkoffs;      /* max bytes we can read from this block */
     if (n > count)
       n = count;
 
@@ -351,7 +417,7 @@ cache_flush (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
   assert (!flags);
 
   /* Allocate the bounce buffer. */
-  data.block = malloc (BLKSIZE);
+  data.block = malloc (blksize);
   if (data.block == NULL) {
     *err = errno;
     nbdkit_error ("malloc: %m");
@@ -411,6 +477,7 @@ static struct nbdkit_filter filter = {
   .load              = cache_load,
   .unload            = cache_unload,
   .config            = cache_config,
+  .config_complete   = cache_config_complete,
   .open              = cache_open,
   .prepare           = cache_prepare,
   .get_size          = cache_get_size,

filters/cache/lru.c
@@ -90,8 +90,8 @@ static unsigned N = 100;
 void
 lru_init (void)
 {
-  bitmap_init (&bm[0], BLKSIZE, 1 /* bits per block */);
-  bitmap_init (&bm[1], BLKSIZE, 1 /* bits per block */);
+  bitmap_init (&bm[0], blksize, 1 /* bits per block */);
+  bitmap_init (&bm[1], blksize, 1 /* bits per block */);
 }
 
 void
@@ -109,8 +109,11 @@ lru_set_size (uint64_t new_size)
   if (bitmap_resize (&bm[1], new_size) == -1)
     return -1;
 
-  /* XXX Choose this better. */
-  N = MAX (new_size / BLKSIZE / 4, 100);
+  if (max_size != -1)
+    /* Make the threshold about 1/4 the maximum size of the cache. */
+    N = MAX (max_size / blksize / 4, 100);
+  else
+    N = MAX (new_size / blksize / 4, 100);
 
   return 0;
 }

filters/cache/nbdkit-cache-filter.pod
@@ -5,6 +5,9 @@ nbdkit-cache-filter - nbdkit caching filter
 =head1 SYNOPSIS
 
  nbdkit --filter=cache plugin [cache=writeback|writethrough|unsafe]
+                              [cache-max-size=SIZE]
+                              [cache-high-threshold=N]
+                              [cache-low-threshold=N]
                               [cache-on-read=true|false]
                               [plugin-args...]
 
@@ -51,6 +54,14 @@ This is dangerous and can cause data loss, but this may be acceptable
 if you only use it for testing or with data that you don't care about
 or can cheaply reconstruct.
 
+=item B<cache-max-size=SIZE>
+
+=item B<cache-high-threshold=N>
+
+=item B<cache-low-threshold=N>
+
+Limit the size of the cache to C<SIZE>.  See L</CACHE MAXIMUM SIZE> below.
+
 =item B<cache-on-read=true>
 
 Cache read requests as well as write requests.  Any time a block is
@@ -68,6 +79,45 @@ the plugin.
 Do not cache read requests (this is the default).
 
 =back
+
+=head1 CACHE MAXIMUM SIZE
+
+By default the cache can grow to any size (although not larger than
+the virtual size of the underlying plugin) and you have to ensure
+there is sufficient space in C<$TMPDIR> for it.
+
+Using the parameters C<cache-max-size>, C<cache-high-threshold> and
+C<cache-low-threshold> you can limit the maximum size of the cache.
+
+This requires kernel and filesystem support (for L<fallocate(2)>
+C<FALLOC_FL_PUNCH_HOLE>), so it may not work on all platforms.
+
+Some examples:
+
+=over 4
+
+=item C<cache-max-size=1G>
+
+The cache is limited to around 1 gigabyte.
+
+=item C<cache-max-size=1G cache-high-threshold=95 cache-low-threshold=80>
+
+Once the cache size reaches 972M (95% of 1G), blocks are reclaimed
+from the cache until the size is reduced to 819M (80% of 1G).
+
+=back
+
+The way this works is once the size of the cache exceeds
+S<C<SIZE> ✕ the high threshold>, the filter works to reduce the size
+of the cache until it is less than S<C<SIZE> ✕ the low threshold>.
+Once the size is below the low threshold, no more reclaim work is done
+until the size exceeds the high threshold again.
+
+The default thresholds are high 95% and low 80%.  You must set
+S<0 E<lt> low E<lt> high>.  The thresholds are expressed as integer
+percentages of C<cache-max-size>.
+
+Least recently used blocks are discarded first.
 
 =head1 ENVIRONMENT VARIABLES
 

filters/cache/reclaim.h
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018 Red Hat Inc.
+ * Copyright (C) 2018-2019 Red Hat Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,26 +31,23 @@
  * SUCH DAMAGE.
  */
 
-#ifndef NBDKIT_CACHE_H
-#define NBDKIT_CACHE_H
+#ifndef NBDKIT_RECLAIM_H
+#define NBDKIT_RECLAIM_H
 
-#include <fcntl.h>
+#include "bitmap.h"
 
-/* Caching mode. */
-extern enum cache_mode {
-  CACHE_MODE_WRITEBACK,
-  CACHE_MODE_WRITETHROUGH,
-  CACHE_MODE_UNSAFE,
-} cache_mode;
+/* Do we support reclaiming cache blocks? */
+#ifdef FALLOC_FL_PUNCH_HOLE
+#define HAVE_CACHE_RECLAIM 1
+#else
+#undef HAVE_CACHE_RECLAIM
+#endif
 
-/* Size of a block in the cache. */
-extern unsigned blksize;
+/* Check if we need to reclaim blocks, and if so reclaim up to two
+ * blocks.
+ *
+ * Note this must be called with the blk lock held.
+ */
+extern void reclaim (int fd, struct bitmap *bm);
 
-/* Maximum size of the cache and high/low thresholds. */
-extern int64_t max_size;
-extern int hi_thresh, lo_thresh;
-
-/* Cache read requests. */
-extern bool cache_on_read;
-
-#endif /* NBDKIT_CACHE_H */
+#endif /* NBDKIT_RECLAIM_H */

commit c0d3f2c794f5e60df565321c070a3ae5cc1ea3b2

cache: Use correct type for reclaim_blk
We already prohibit images larger than 2^63, so there are not that
many blocks to worry about.  But using an unsigned type means our loop
is not quite right:

reclaim.c: In function ‘reclaim_lru’:
reclaim.c:162:24: error: comparison of unsigned expression >= 0 is always true [-Werror=type-limits]
  162 |   } while (reclaim_blk >= 0 && old_reclaim_blk != reclaim_blk);
      |                        ^~

Fixes: 602eb003
Signed-off-by: Eric Blake <eblake@redhat.com>

filters/cache/reclaim.c
@@ -83,7 +83,7 @@ enum reclaim_state {
 };
 
 static enum reclaim_state reclaiming = NOT_RECLAIMING;
-static uint64_t reclaim_blk;
+static int64_t reclaim_blk;
 
 static void reclaim_one (int fd, struct bitmap *bm);
 static void reclaim_lru (int fd, struct bitmap *bm);
@@ -143,7 +143,7 @@ reclaim_one (int fd, struct bitmap *bm)
 static void
 reclaim_lru (int fd, struct bitmap *bm)
 {
-  uint64_t old_reclaim_blk;
+  int64_t old_reclaim_blk;
 
   /* Find the next block in the cache. */
   reclaim_blk = bitmap_next (bm, reclaim_blk+1);