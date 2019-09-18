libguestfs/nbdkit

commit e4d334e4d8e09ddec91810b22fec1b013b702ca3

filters: Pass readonly through to .prepare
Some filters need to know whether the underlying plugin supports
various write operations (for example, --filter=nozero zeromode=notrim
requires that the underlying can_zero returns true).  However, if the
filter itself is being used in a readonly mode, .can_zero will fail,
but in this situation we know that .zero will never be called, so it
is not worth treating that failure as fatal.  It is already possible
for a filter to know if it is readonly based on the parameter passed
to .open, but this puts a burden on a filter to implement .open just
to store that bit of information; it's just as easy to alter the
.prepare signature to pass the same value back, for less effort on the
filters.

This is an API change, and thus all filters have to be touched; but it
makes it easier to alter a command line by adding -r without causing
the filter to fall apart over the loss of writes to the underlying
plugin.  But thanks to our recent filter API change (commit fe8ad62e),
we don't have to worry about the change being a problem for mismatched
versions.

Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -181,6 +181,16 @@ backend_open (struct backend *b, struct connection *conn, int readonly)
   return b->open (b, conn, readonly);
 }
 
+int
+backend_prepare (struct backend *b, struct connection *conn)
+{
+  struct b_conn_handle *h = &conn->handles[b->i];
+
+  debug ("%s: backend readonly=%d", b->name, h->can_write == 0);
+
+  return b->prepare (b, conn, h->can_write == 0);
+}
+
 void
 backend_set_handle (struct backend *b, struct connection *conn, void *handle)
 {

docs/nbdkit-filter.pod
@@ -289,6 +289,16 @@ C<NBDKIT_HANDLE_NOT_NEEDED> which is a static non-NULL pointer.
 If there is an error, C<.open> should call C<nbdkit_error> with an
 error message and return C<NULL>.
 
+This callback is optional, but if provided, it must call C<next>,
+passing a value for C<readonly> according to how the filter plans to
+use the plugin.  Typically, the filter passes the same value as it
+received, or passes true to provide a writable layer on top of a
+read-only backend.  However, it is also acceptable to attempt write
+access to the plugin even if this filter is readonly, such as when a
+file system mounted read-only still requires write access to the
+underlying device in case a journal needs to be replayed for
+consistency as part of the mounting process.
+
 =head2 C<.close>
 
  void (*close) (void *handle);
@@ -301,7 +311,7 @@ up any per-connection resources used by the filter.
 =head2 C<.finalize>
 
   int (*prepare) (struct nbdkit_next_ops *next_ops, void *nxdata,
-                  void *handle);
+                  void *handle, int readonly);
   int (*finalize) (struct nbdkit_next_ops *next_ops, void *nxdata,
                    void *handle);
 
@@ -316,6 +326,9 @@ cleanly update superblock data in the image on close you can do it in
 your C<.finalize> method (calling the plugin's C<.pwrite> method).
 Doing these things in the filter's C<.open> or C<.close> method is not
 possible.
+
+For C<.prepare>, the value of C<readonly> is the same as was passed to
+C<.open>, declaring how this filter will be used.
 
 There is no C<next_ops-E<gt>prepare> or C<next_ops-E<gt>finalize>.
 Unlike other filter methods, prepare and finalize are not chained

filters/cache/cache.c
@@ -232,7 +232,7 @@ cache_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,
  */
 static int
 cache_prepare (struct nbdkit_next_ops *next_ops, void *nxdata,
-               void *handle)
+               void *handle, int readonly)
 {
   int64_t r;
 

filters/cow/cow.c
@@ -132,7 +132,7 @@ cow_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,
  */
 static int
 cow_prepare (struct nbdkit_next_ops *next_ops, void *nxdata,
-             void *handle)
+             void *handle, int readonly)
 {
   int64_t r;
 

filters/fua/fua.c
@@ -75,9 +75,14 @@ fua_config (nbdkit_next_config *next, void *nxdata,
 
 /* Check that desired mode is supported by plugin. */
 static int
-fua_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
+fua_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
+             int readonly)
 {
   int r;
+
+  /* If we are opened readonly, this filter has no impact */
+  if (readonly)
+    return 0;
 
   switch (fuamode) {
   case NONE:

filters/log/log.c
@@ -248,7 +248,8 @@ log_close (void *handle)
 }
 
 static int
-log_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
+log_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
+             int readonly)
 {
   struct handle *h = handle;
   int64_t size = next_ops->get_size (nxdata);

filters/nozero/nozero.c
@@ -74,9 +74,14 @@ nozero_config (nbdkit_next_config *next, void *nxdata,
 
 /* Check that desired mode is supported by plugin. */
 static int
-nozero_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
+nozero_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
+                int readonly)
 {
   int r;
+
+  /* If we are opened readonly, this filter has no impact */
+  if (readonly)
+    return 0;
 
   if (zeromode == NOTRIM) {
     r = next_ops->can_zero (nxdata);

filters/partition/partition.c
@@ -113,7 +113,7 @@ partition_close (void *handle)
 
 static int
 partition_prepare (struct nbdkit_next_ops *next_ops, void *nxdata,
-                   void *handle)
+                   void *handle, int readonly)
 {
   struct handle *h = handle;
   int64_t size;

commit 48c60c5e2da3c8d1cda554243415c1fa67cba5fb

server: Fix debug message
In refactoring .prepare, I inadvertently changed the log message.

Fixes: e4d334e4
Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -187,7 +187,7 @@ backend_prepare (struct backend *b, struct connection *conn)
 {
   struct b_conn_handle *h = &conn->handles[b->i];
 
-  debug ("%s: backend readonly=%d", b->name, h->can_write == 0);
+  debug ("%s: prepare readonly=%d", b->name, h->can_write == 0);
 
   return b->prepare (b, conn, h->can_write == 0);
 }