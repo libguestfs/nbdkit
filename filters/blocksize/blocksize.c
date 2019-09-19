d218f64c120a19879731763175dad395142b205b
Richard W.M. Jones <rjones@redhat.com>
2019-09-19 17:49:09 +0100

    filters: blocksize: Set the intended thread model correctly.
    (cherry picked from commit 115f0956d324002e2b56a500a0f1128eb1b3993f)

--- filters/blocksize/blocksize.c
+++ filters/blocksize/blocksize.c
@@ -46,9 +46,6 @@
 #include "minmax.h"
 #include "rounding.h"
 
-/* XXX See design comment in filters/cow/cow.c. */
-#define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS
-
 #define BLOCKSIZE_MIN_LIMIT (64U * 1024)
 
 /* As long as we don't have parallel requests, we can reuse a common
@@ -58,6 +55,15 @@ static char bounce[BLOCKSIZE_MIN_LIMIT];
 static unsigned int minblock;
 static unsigned int maxdata;
 static unsigned int maxlen;
+
+/* We are using a common bounce buffer (see above) so we must
+ * serialize all requests.
+ */
+static int
+blocksize_thread_model (void)
+{
+  return NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS;
+}
 
 static int
 blocksize_parse (const char *name, const char *s, unsigned int *v)
@@ -400,6 +406,7 @@ static struct nbdkit_filter filter = {
   .name              = "blocksize",
   .longname          = "nbdkit blocksize filter",
   .version           = PACKAGE_VERSION,
+  .thread_model      = blocksize_thread_model,
   .config            = blocksize_config,
   .config_complete   = blocksize_config_complete,
   .config_help       = blocksize_config_help,
