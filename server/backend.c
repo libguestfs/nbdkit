libguestfs/nbdkit

commit dbdec62725a2d152148e1ab9feefe640f193ade1

server: Enforce valid ranges to each backend call
We were already validating that client's requests are in range (and in
that case, returning an error but keeping the connection alive).  But
it is also worth validating that no filter passes bogus requests on to
the next layer.

Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -199,6 +199,17 @@ backend_set_handle (struct backend *b, struct connection *conn, void *handle)
   conn->handles[b->i].handle = handle;
 }
 
+bool
+backend_valid_range (struct backend *b, struct connection *conn,
+                     uint64_t offset, uint32_t count)
+{
+  struct b_conn_handle *h = &conn->handles[b->i];
+
+  assert (h->exportsize >= 0); /* Guaranteed by negotiation phase */
+  return count > 0 && offset <= h->exportsize &&
+    offset + count <= h->exportsize;
+}
+
 /* Wrappers for all callbacks in a filter's struct nbdkit_next_ops. */
 
 int64_t
@@ -349,6 +360,7 @@ backend_pread (struct backend *b, struct connection *conn,
 {
   int r;
 
+  assert (backend_valid_range (b, conn, offset, count));
   assert (flags == 0);
   debug ("%s: pread count=%" PRIu32 " offset=%" PRIu64,
          b->name, count, offset);
@@ -369,6 +381,7 @@ backend_pwrite (struct backend *b, struct connection *conn,
   int r;
 
   assert (h->can_write == 1);
+  assert (backend_valid_range (b, conn, offset, count));
   assert (!(flags & ~NBDKIT_FLAG_FUA));
   if (fua)
     assert (h->can_fua > NBDKIT_FUA_NONE);
@@ -409,6 +422,7 @@ backend_trim (struct backend *b, struct connection *conn,
 
   assert (h->can_write == 1);
   assert (h->can_trim == 1);
+  assert (backend_valid_range (b, conn, offset, count));
   assert (!(flags & ~NBDKIT_FLAG_FUA));
   if (fua)
     assert (h->can_fua > NBDKIT_FUA_NONE);
@@ -432,6 +446,7 @@ backend_zero (struct backend *b, struct connection *conn,
 
   assert (h->can_write == 1);
   assert (h->can_zero > NBDKIT_ZERO_NONE);
+  assert (backend_valid_range (b, conn, offset, count));
   assert (!(flags & ~(NBDKIT_FLAG_MAY_TRIM | NBDKIT_FLAG_FUA)));
   if (fua)
     assert (h->can_fua > NBDKIT_FUA_NONE);
@@ -453,6 +468,7 @@ backend_extents (struct backend *b, struct connection *conn,
   int r;
 
   assert (h->can_extents >= 0);
+  assert (backend_valid_range (b, conn, offset, count));
   assert (!(flags & ~NBDKIT_FLAG_REQ_ONE));
   debug ("%s: extents count=%" PRIu32 " offset=%" PRIu64 " req_one=%d",
          b->name, count, offset, !!(flags & NBDKIT_FLAG_REQ_ONE));
@@ -481,6 +497,7 @@ backend_cache (struct backend *b, struct connection *conn,
   int r;
 
   assert (h->can_cache > NBDKIT_CACHE_NONE);
+  assert (backend_valid_range (b, conn, offset, count));
   assert (flags == 0);
   debug ("%s: cache count=%" PRIu32 " offset=%" PRIu64,
          b->name, count, offset);

server/internal.h
@@ -332,6 +332,9 @@ extern int backend_prepare (struct backend *b, struct connection *conn)
 extern void backend_set_handle (struct backend *b, struct connection *conn,
                                 void *handle)
   __attribute__((__nonnull__ (1, 2 /* not 3 */)));
+extern bool backend_valid_range (struct backend *b, struct connection *conn,
+                                 uint64_t offset, uint32_t count)
+  __attribute__((__nonnull__ (1, 2)));
 
 extern int64_t backend_get_size (struct backend *b, struct connection *conn)
   __attribute__((__nonnull__ (1, 2)));

server/protocol.c
@@ -48,15 +48,6 @@
 #include "protocol.h"
 
 static bool
-valid_range (struct connection *conn, uint64_t offset, uint32_t count)
-{
-  uint64_t exportsize = backend_get_size (backend, conn);
-
-  assert (exportsize <= INT64_MAX); /* Guaranteed by negotiation phase */
-  return count > 0 && offset <= exportsize && offset + count <= exportsize;
-}
-
-static bool
 validate_request (struct connection *conn,
                   uint16_t cmd, uint16_t flags, uint64_t offset, uint32_t count,
                   uint32_t *error)
@@ -79,7 +70,7 @@ validate_request (struct connection *conn,
   case NBD_CMD_TRIM:
   case NBD_CMD_WRITE_ZEROES:
   case NBD_CMD_BLOCK_STATUS:
-    if (!valid_range (conn, offset, count)) {
+    if (!backend_valid_range (backend, conn, offset, count)) {
       /* XXX Allow writes to extend the disk? */
       nbdkit_error ("invalid request: %s: offset and count are out of range: "
                     "offset=%" PRIu64 " count=%" PRIu32,

commit 1f2073d8617ad7dd8ec1519589081284fcbc47b5

server: Fix backend range check
h->exportsize is unsigned, so the assertion as written was a no-op.
It was intended to catch cases where exportsize was left at -1 from
prior to .open (which, until recently, could happen in both the xz and
retry filters).

Fixes: dbdec62725
Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -229,7 +229,7 @@ backend_valid_range (struct backend *b, struct connection *conn,
 {
   struct b_conn_handle *h = &conn->handles[b->i];
 
-  assert (h->exportsize >= 0); /* Guaranteed by negotiation phase */
+  assert (h->exportsize <= INT64_MAX); /* Guaranteed by negotiation phase */
   return count > 0 && offset <= h->exportsize &&
     offset + count <= h->exportsize;
 }