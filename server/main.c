libguestfs/nbdkit

commit acc37af9989aae708e8acad3535e1491931e6bdb

server: Implement --swap option.
See also:
https://www.mail-archive.com/linux-kernel@vger.kernel.org/msg1933394.html

This simply runs ‘mlockall (MCL_CURRENT | MCL_FUTURE)’, same as
nbd-server.

I made this conditional on a configure time test because Haiku
currently doesn't implement mlockall.  It is supported on OpenBSD and
FreeBSD so we should be able to remove those conditionals in future.

server/main.c
@@ -47,6 +47,10 @@
 #include <sys/stat.h>
 #include <sys/socket.h>
 
+#ifdef HAVE_SYS_MMAN_H
+#include <sys/mman.h>
+#endif
+
 #ifdef HAVE_LINUX_VM_SOCKETS_H
 #include <linux/vm_sockets.h>
 #endif
@@ -86,6 +90,7 @@ bool read_only;                 /* -r */
 const char *run;                /* --run */
 bool listen_stdin;              /* -s */
 const char *selinux_label;      /* --selinux-label */
+bool swap;                      /* --swap */
 unsigned threads;               /* -t */
 int tls;                        /* --tls : 0=off 1=on 2=require */
 const char *tls_certificates_dir; /* --tls-certificates */
@@ -324,6 +329,10 @@ main (int argc, char *argv[])
           printf ("-%c\n", short_options[i]);
       }
       exit (EXIT_SUCCESS);
+
+    case SWAP_OPTION:
+      swap = 1;
+      break;
 
     case TLS_OPTION:
       tls_set_on_cli = true;
@@ -884,6 +893,21 @@ start_serving (void)
              "in this combination\n",
              program_name);
     exit (EXIT_FAILURE);
+  }
+
+  /* Lock the process into memory if requested. */
+  if (swap) {
+#ifdef HAVE_MLOCKALL
+    if (mlockall (MCL_CURRENT | MCL_FUTURE) == -1) {
+      fprintf (stderr, "%s: --swap: mlockall: %m\n", program_name);
+      exit (EXIT_FAILURE);
+    }
+    debug ("mlockall done");
+#else
+    fprintf (stderr, "%s: mlockall (--swap option) "
+             "is not supported on this platform\n");
+    exit (EXIT_FAILURE);
+#endif
   }
 
   /* Socket activation -- we are handling connections on pre-opened

TODO
@@ -59,12 +59,6 @@ General ideas for improvements
   newline. Commit ef4f72ef has some ideas on smart newlines, but that
   should probably be factored into a utility function.
 
-* We may need a way to lock nbdkit into memory and adjust the OOM
-  killer score.  See this LKML discussion:
-  https://www.mail-archive.com/linux-kernel@vger.kernel.org/msg1933394.html
-  and also look at the implementation of the -swap option in
-  nbd-client.
-
 * Add a mode of operation where nbdkit is handed a pre-opened fd to be
   used immediately in transmission phase (skipping handshake).  There
   are already third-party clients of the kernel's /dev/nbdX which rely

configure.ac
@@ -200,6 +200,7 @@ AC_CHECK_HEADERS([\
 	byteswap.h \
 	endian.h \
 	sys/endian.h \
+	sys/mman.h \
 	sys/prctl.h \
 	sys/procctl.h])
 
@@ -211,6 +212,7 @@ AC_CHECK_FUNCS([\
 	fdatasync \
 	get_current_dir_name \
 	mkostemp \
+	mlockall \
 	pipe2 \
 	ppoll \
 	posix_fadvise])

docs/nbdkit.pod
@@ -360,6 +360,15 @@ Unix domain sockets:
 
  nbdkit --selinux-label system_u:object_r:svirt_t:s0 ...
 
+=item B<--swap>
+
+Specifies that the NBD device will be used as swap space loop mounted
+on the same machine which is running nbdkit.  To avoid deadlocks this
+locks the whole nbdkit process into memory using L<mlockall(2)>.  This
+may require additional permissions, such as starting the server as
+root or raising the C<RLIMIT_MEMLOCK> (L<ulimit(1)> I<-l>) limit on
+the process.
+
 =item B<-t> THREADS
 
 =item B<--threads> THREADS

server/options.h
@@ -51,6 +51,7 @@ enum {
   RUN_OPTION,
   SELINUX_LABEL_OPTION,
   SHORT_OPTIONS_OPTION,
+  SWAP_OPTION,
   TLS_OPTION,
   TLS_CERTIFICATES_OPTION,
   TLS_PSK_OPTION,
@@ -92,6 +93,7 @@ static const struct option long_options[] = {
   { "short-options",    no_argument,       NULL, SHORT_OPTIONS_OPTION },
   { "single",           no_argument,       NULL, 's' },
   { "stdin",            no_argument,       NULL, 's' },
+  { "swap",             no_argument,       NULL, SWAP_OPTION },
   { "threads",          required_argument, NULL, 't' },
   { "tls",              required_argument, NULL, TLS_OPTION },
   { "tls-certificates", required_argument, NULL, TLS_CERTIFICATES_OPTION },

tests/Makefile.am
@@ -128,6 +128,7 @@ EXTRA_DIST = \
 	test-retry-zero-flags.sh \
 	test-shutdown.sh \
 	test-ssh.sh \
+	test-swap.sh \
 	test.tcl \
 	test-shebang-perl.sh \
 	test-shebang-python.sh \
@@ -161,7 +162,7 @@ check-valgrind:
 
 # To run only tests which require root, use:
 check-root:
-	$(MAKE) check TESTS="test-file-block"
+	$(MAKE) check TESTS="test-file-block test-swap.sh"
 
 if HAVE_VDDK
 # Run a basic check against a real copy of VDDK.  You must set
@@ -240,6 +241,7 @@ TESTS += \
 	test-foreground.sh \
 	test-debug-flags.sh \
 	test-long-name.sh \
+	test-swap.sh \
 	$(NULL)
 
 check_PROGRAMS += \

tests/test-swap.sh
@@ -1,0 +1,47 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2019 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Root-only test of the --swap option.
+
+source ./functions.sh
+set -e
+set -x
+
+requires test `id -u` -eq 0
+requires nbdsh --version
+
+nbdsh -c -<<'EOF'
+h.connect_command (
+    ["nbdkit", "-fv", "-s", "--exit-with-parent", "--swap", "null"]
+)
+assert h.get_size () == 0
+EOF

commit 3774e1f08c99c39a73ea804aa2ae515ba1521edc

server: Fix compilation without HAVE_MLOCKALL
Gcc warns about fprintf with %s but no matching argument.

Fixes: acc37af9
Signed-off-by: Eric Blake <eblake@redhat.com>

server/main.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -855,7 +855,7 @@ start_serving (void)
     debug ("mlockall done");
 #else
     fprintf (stderr, "%s: mlockall (--swap option) "
-             "is not supported on this platform\n");
+             "is not supported on this platform\n", program_name);
     exit (EXIT_FAILURE);
 #endif
   }