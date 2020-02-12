libguestfs/nbdkit

commit b81ae5f955f593c7446336757885ccabad74e893

server: Correct logic when filter fails .prepare
If .prepare fails, we do not want to call .finalize (similar to how if
.open fails, we do not want to call .close).  However, the logic in
backend_finalize was checking on the wrong condition ('was the
connection opened' rather than 'was the connection prepared'), which
led to an assertion failure.

Simple reproducer:

$ nbdkit -U - -f --filter cache --run 'qemu-io -r -c quit $nbd' sh - <<\EOF
> case $1 in get_size) echo oops >&2; exit 1 ;; *) exit 2 ;; esac
> EOF
nbdkit: sh[1]: error: /tmp/nbdkitshYvAQbz/inline-script.sh: oops
nbdkit: backend.c:206: backend_finalize: Assertion `h->state & HANDLE_CONNECTED' failed.
qemu-io: can't open device nbd:unix:/tmp/nbdkit60FUTw/socket: Failed to read option reply: Unexpected end-of-file before all bytes were read
nbdkit: nbdkit command was killed by signal 6

With this patch, the command now fails gracefully:
nbdkit: sh[1]: error: /tmp/nbdkitshie18Lp/inline-script.sh: oops
qemu-io: can't open device nbd:unix:/tmp/nbdkitqvAOlr/socket: Requested export not available

Fixes: ffa98c8d
Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -221,15 +221,13 @@ backend_finalize (struct backend *b)
   if (h->state & HANDLE_FAILED)
     return -1;
 
-  if (h->handle) {
-    assert (h->state & HANDLE_CONNECTED);
+  assert (h->state & HANDLE_OPEN);
+  if (h->state & HANDLE_CONNECTED) {
     if (b->finalize (b, h->handle) == -1) {
       h->state |= HANDLE_FAILED;
       return -1;
     }
   }
-  else
-    assert (! (h->state & HANDLE_CONNECTED));
 
   if (b->i)
     return backend_finalize (b->next);

commit f623ca3161cbab03cf2dd0ed9baea6c889c98f37

server: Avoid hang on failed reopen
The previous patch solved one assertion but caused another; the
code can be reached when HANDLE_OPEN is not set.

Fixes: b81ae5f9
Signed-off-by: Eric Blake <eblake@redhat.com>

server/backend.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -221,8 +221,8 @@ backend_finalize (struct backend *b)
   if (h->state & HANDLE_FAILED)
     return -1;
 
-  assert (h->state & HANDLE_OPEN);
   if (h->state & HANDLE_CONNECTED) {
+    assert (h->state & HANDLE_OPEN && h->handle);
     if (b->finalize (b, h->handle) == -1) {
       h->state |= HANDLE_FAILED;
       return -1;