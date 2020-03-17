libguestfs/nbdkit

commit c306fa93abc9abdb46bc3691a4ee26f143ef3538

server: Cache remaining per-connection can_FOO flags
Similar to the previous patches, we want to avoid calling into the
plugin more than once per connection on any of the flag determination
callbacks.  Most of these functions were only called once, but can_fua
demonstrates a real speedup. Pre-patch:

$ cat script
case "$1" in
    get_size) echo 1m;;
    can_fua) sleep 1; echo native;;
    can_write | can_zero | pwrite | zero) ;;
    *) exit 2 ;;
esac
$ /bin/time -f %e ./nbdkit --filter=blocksize sh script maxlen=4k \
  --run 'qemu-io -f raw -c "w -z -f 0 16k" $nbd'
wrote 16384/16384 bytes at offset 0
16 KiB, 1 ops; 0:00:05.07 (3.157 KiB/sec and 0.1973 ops/sec)
6.14

there are six executions (one to determine eflags, one by the
blocksize filter to determine whether to pass the flag down or call
flush, and four by each .zero call over the split range from
blocksize).  After:

$ /bin/time -f %e ./nbdkit --filter=blocksize sh script maxlen=4k \
  --run 'qemu-io -f raw -c "w -z -f 0 16k" $nbd'
wrote 16384/16384 bytes at offset 0
16 KiB, 1 ops; 00.03 sec (585.570 KiB/sec and 36.5981 ops/sec)
1.13

we've reduced things to a single call, and everything else using the
cache.

Signed-off-by: Eric Blake <eblake@redhat.com>

server/plugins.c
@@ -505,7 +505,7 @@ plugin_pwrite (struct backend *b, struct connection *conn,
 
   assert (connection_get_handle (conn, 0));
 
-  if (fua && plugin_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
+  if (fua && backend_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
     flags &= ~NBDKIT_FLAG_FUA;
     need_flush = true;
   }
@@ -537,7 +537,7 @@ plugin_trim (struct backend *b, struct connection *conn,
 
   assert (connection_get_handle (conn, 0));
 
-  if (fua && plugin_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
+  if (fua && backend_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
     flags &= ~NBDKIT_FLAG_FUA;
     need_flush = true;
   }
@@ -569,7 +569,7 @@ plugin_zero (struct backend *b, struct connection *conn,
 
   assert (connection_get_handle (conn, 0));
 
-  if (fua && plugin_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
+  if (fua && backend_can_fua (b, conn) != NBDKIT_FUA_NATIVE) {
     flags &= ~NBDKIT_FLAG_FUA;
     need_flush = true;
   }

docs/nbdkit-filter.pod
@@ -440,10 +440,12 @@ on to only the final sub-request, or by dropping the flag and ending
 with a direct call to C<next_ops-E<gt>flush>).
 
 If there is an error, the callback should call C<nbdkit_error> with an
-error message and return C<-1>.  If these functions are called more
-than once for the same connection, they should return the same value;
-similarly, the filter may cache the results of each counterpart in
-C<next_ops> for a given connection rather than repeating calls.
+error message and return C<-1>.  These functions are called at most
+once per connection and cached by nbdkit. Similarly, repeated calls to
+any of the C<next_ops> counterparts will return a cached value; by
+calling into the plugin during C<.prepare>, you can ensure that later
+use of the cached values during data commands like <.pwrite> will not
+fail.
 
 =head2 C<.pread>
 

docs/nbdkit-plugin.pod
@@ -150,10 +150,8 @@ A new client has connected.
 
 These are called during option negotiation with the client, but before
 any data is served.  These callbacks may return different values
-across different C<.open> calls, but within a single connection, must
-always return the same value; other code in nbdkit may cache the
-per-connection value returned rather than using the callback a second
-time.
+across different C<.open> calls, but within a single connection, they
+are called at most once and cached by nbdkit for that connection.
 
 =item C<.pread>, C<.pwrite> and other data serving callbacks
 

filters/blocksize/blocksize.c
@@ -138,8 +138,6 @@ blocksize_config_complete (nbdkit_next_config_complete *next, void *nxdata)
   "maxdata=<SIZE>       Maximum size for read/write (default 64M).\n" \
   "maxlen=<SIZE>        Maximum size for trim/zero (default 4G-minblock)."
 
-/* TODO: Should we have a .prepare to cache per-connection FUA mode? */
-
 /* Round size down to avoid issues at end of file. */
 static int64_t
 blocksize_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,

filters/cache/cache.c
@@ -239,7 +239,6 @@ cache_prepare (struct nbdkit_next_ops *next_ops, void *nxdata,
   r = cache_get_size (next_ops, nxdata, handle);
   if (r < 0)
     return -1;
-  /* TODO: cache per-connection FUA mode? */
   return 0;
 }
 

server/backend.c
@@ -228,30 +228,44 @@ backend_can_write (struct backend *b, struct connection *conn)
 int
 backend_can_flush (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
+
   debug ("%s: can_flush", b->name);
 
-  return b->can_flush (b, conn);
+  if (h->can_flush == -1)
+    h->can_flush = b->can_flush (b, conn);
+  return h->can_flush;
 }
 
 int
 backend_is_rotational (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
+
   debug ("%s: is_rotational", b->name);
 
-  return b->is_rotational (b, conn);
+  if (h->is_rotational == -1)
+    h->is_rotational = b->is_rotational (b, conn);
+  return h->is_rotational;
 }
 
 int
 backend_can_trim (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
   int r;
 
   debug ("%s: can_trim", b->name);
 
-  r = backend_can_write (b, conn);
-  if (r != 1)
-    return r;
-  return b->can_trim (b, conn);
+  if (h->can_trim == -1) {
+    r = backend_can_write (b, conn);
+    if (r != 1) {
+      h->can_trim = 0;
+      return r;
+    }
+    h->can_trim = b->can_trim (b, conn);
+  }
+  return h->can_trim;
 }
 
 int
@@ -288,22 +302,32 @@ backend_can_extents (struct backend *b, struct connection *conn)
 int
 backend_can_fua (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
   int r;
 
   debug ("%s: can_fua", b->name);
 
-  r = backend_can_write (b, conn);
-  if (r != 1)
-    return r; /* Relies on 0 == NBDKIT_FUA_NONE */
-  return b->can_fua (b, conn);
+  if (h->can_fua == -1) {
+    r = backend_can_write (b, conn);
+    if (r != 1) {
+      h->can_fua = NBDKIT_FUA_NONE;
+      return r; /* Relies on 0 == NBDKIT_FUA_NONE */
+    }
+    h->can_fua = b->can_fua (b, conn);
+  }
+  return h->can_fua;
 }
 
 int
 backend_can_multi_conn (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
+
   debug ("%s: can_multi_conn", b->name);
 
-  return b->can_multi_conn (b, conn);
+  if (h->can_multi_conn == -1)
+    h->can_multi_conn = b->can_multi_conn (b, conn);
+  return h->can_multi_conn;
 }
 
 int
@@ -341,12 +365,15 @@ backend_pwrite (struct backend *b, struct connection *conn,
                 uint32_t flags, int *err)
 {
   struct b_conn_handle *h = &conn->handles[b->i];
+  bool fua = !!(flags & NBDKIT_FLAG_FUA);
   int r;
 
   assert (h->can_write == 1);
   assert (!(flags & ~NBDKIT_FLAG_FUA));
+  if (fua)
+    assert (h->can_fua > NBDKIT_FUA_NONE);
   debug ("%s: pwrite count=%" PRIu32 " offset=%" PRIu64 " fua=%d",
-         b->name, count, offset, !!(flags & NBDKIT_FLAG_FUA));
+         b->name, count, offset, fua);
 
   r = b->pwrite (b, conn, buf, count, offset, flags, err);
   if (r == -1)
@@ -358,8 +385,10 @@ int
 backend_flush (struct backend *b, struct connection *conn,
                uint32_t flags, int *err)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
   int r;
 
+  assert (h->can_flush == 1);
   assert (flags == 0);
   debug ("%s: flush", b->name);
 
@@ -375,12 +404,16 @@ backend_trim (struct backend *b, struct connection *conn,
               int *err)
 {
   struct b_conn_handle *h = &conn->handles[b->i];
+  bool fua = !!(flags & NBDKIT_FLAG_FUA);
   int r;
 
   assert (h->can_write == 1);
+  assert (h->can_trim == 1);
   assert (!(flags & ~NBDKIT_FLAG_FUA));
+  if (fua)
+    assert (h->can_fua > NBDKIT_FUA_NONE);
   debug ("%s: trim count=%" PRIu32 " offset=%" PRIu64 " fua=%d",
-         b->name, count, offset, !!(flags & NBDKIT_FLAG_FUA));
+         b->name, count, offset, fua);
 
   r = b->trim (b, conn, count, offset, flags, err);
   if (r == -1)
@@ -394,14 +427,16 @@ backend_zero (struct backend *b, struct connection *conn,
               int *err)
 {
   struct b_conn_handle *h = &conn->handles[b->i];
+  bool fua = !!(flags & NBDKIT_FLAG_FUA);
   int r;
 
   assert (h->can_write == 1);
   assert (h->can_zero > NBDKIT_ZERO_NONE);
   assert (!(flags & ~(NBDKIT_FLAG_MAY_TRIM | NBDKIT_FLAG_FUA)));
+  if (fua)
+    assert (h->can_fua > NBDKIT_FUA_NONE);
   debug ("%s: zero count=%" PRIu32 " offset=%" PRIu64 " may_trim=%d fua=%d",
-         b->name, count, offset, !!(flags & NBDKIT_FLAG_MAY_TRIM),
-         !!(flags & NBDKIT_FLAG_FUA));
+         b->name, count, offset, !!(flags & NBDKIT_FLAG_MAY_TRIM), fua);
 
   r = b->zero (b, conn, count, offset, flags, err);
   if (r == -1)

server/internal.h
@@ -156,7 +156,12 @@ struct b_conn_handle {
 
   uint64_t exportsize;
   int can_write;
+  int can_flush;
+  int is_rotational;
+  int can_trim;
   int can_zero;
+  int can_fua;
+  int can_multi_conn;
   int can_extents;
   int can_cache;
 };
@@ -176,11 +181,6 @@ struct connection {
 
   uint32_t cflags;
   uint16_t eflags;
-  bool can_flush;
-  bool is_rotational;
-  bool can_trim;
-  bool can_fua;
-  bool can_multi_conn;
   bool using_tls;
   bool structured_replies;
   bool meta_context_base_allocation;

server/protocol-handshake.c
@@ -70,44 +70,34 @@ protocol_compute_eflags (struct connection *conn, uint16_t *flags)
   fl = backend_can_trim (backend, conn);
   if (fl == -1)
     return -1;
-  if (fl) {
+  if (fl)
     eflags |= NBD_FLAG_SEND_TRIM;
-    conn->can_trim = true;
-  }
 
   fl = backend_can_fua (backend, conn);
   if (fl == -1)
     return -1;
-  if (fl) {
+  if (fl)
     eflags |= NBD_FLAG_SEND_FUA;
-    conn->can_fua = true;
-  }
 
   fl = backend_can_flush (backend, conn);
   if (fl == -1)
     return -1;
-  if (fl) {
+  if (fl)
     eflags |= NBD_FLAG_SEND_FLUSH;
-    conn->can_flush = true;
-  }
 
   fl = backend_is_rotational (backend, conn);
   if (fl == -1)
     return -1;
-  if (fl) {
+  if (fl)
     eflags |= NBD_FLAG_ROTATIONAL;
-    conn->is_rotational = true;
-  }
 
   /* multi-conn is useless if parallel connections are not allowed. */
   fl = backend_can_multi_conn (backend, conn);
   if (fl == -1)
     return -1;
   if (fl && (backend->thread_model (backend) >
-             NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS)) {
+             NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS))
     eflags |= NBD_FLAG_CAN_MULTI_CONN;
-    conn->can_multi_conn = true;
-  }
 
   fl = backend_can_cache (backend, conn);
   if (fl == -1)

commit 3801317a657550624ff55c09c8b72a4c17713c98

server: Normalize plugin can_* values
The documentation for .can_write and friends mentioned merely that the
callback must return a boolean value, or -1 on failure.  Historically,
we have treated any non-zero value other than -1 as true, and some
plugins have sloppily relied on this behavior, including the
standalone nbd plugin prior to the use of libnbd in v1.14.  But in
1.15.1, we added an assert in our backend code if an enabled feature
bit is not exactly 1.  To avoid tripping the assertion, we have to
normalize the plugin values before storing our internal value.

Fixes: c306fa93ab
Reported-by: Svenne Krap (https://svenne.dk)
Signed-off-by: Eric Blake <eblake@redhat.com>

server/plugins.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -309,12 +309,21 @@ plugin_get_size (struct backend *b, void *handle)
 }
 
 static int
+normalize_bool (int value)
+{
+  if (value == -1 || value == 0)
+    return value;
+  /* Normalize all other non-zero values to true */
+  return 1;
+}
+
+static int
 plugin_can_write (struct backend *b, void *handle)
 {
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.can_write)
-    return p->plugin.can_write (handle);
+    return normalize_bool (p->plugin.can_write (handle));
   else
     return p->plugin.pwrite || p->plugin._pwrite_v1;
 }
@@ -325,7 +334,7 @@ plugin_can_flush (struct backend *b, void *handle)
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.can_flush)
-    return p->plugin.can_flush (handle);
+    return normalize_bool (p->plugin.can_flush (handle));
   else
     return p->plugin.flush || p->plugin._flush_v1;
 }
@@ -336,7 +345,7 @@ plugin_is_rotational (struct backend *b, void *handle)
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.is_rotational)
-    return p->plugin.is_rotational (handle);
+    return normalize_bool (p->plugin.is_rotational (handle));
   else
     return 0; /* assume false */
 }
@@ -347,7 +356,7 @@ plugin_can_trim (struct backend *b, void *handle)
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.can_trim)
-    return p->plugin.can_trim (handle);
+    return normalize_bool (p->plugin.can_trim (handle));
   else
     return p->plugin.trim || p->plugin._trim_v1;
 }
@@ -380,7 +389,7 @@ plugin_can_fast_zero (struct backend *b, void *handle)
   int r;
 
   if (p->plugin.can_fast_zero)
-    return p->plugin.can_fast_zero (handle);
+    return normalize_bool (p->plugin.can_fast_zero (handle));
   /* Advertise support for fast zeroes if no .zero or .can_zero is
    * false: in those cases, we fail fast instead of using .pwrite.
    * This also works when v1 plugin has only ._zero_v1.
@@ -399,7 +408,7 @@ plugin_can_extents (struct backend *b, void *handle)
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.can_extents)
-    return p->plugin.can_extents (handle);
+    return normalize_bool (p->plugin.can_extents (handle));
   else
     return p->plugin.extents != NULL;
 }
@@ -430,7 +439,7 @@ plugin_can_multi_conn (struct backend *b, void *handle)
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
 
   if (p->plugin.can_multi_conn)
-    return p->plugin.can_multi_conn (handle);
+    return normalize_bool (p->plugin.can_multi_conn (handle));
   else
     return 0; /* assume false */
 }