libguestfs/nbdkit

commit e0d324683c86455a2fe62e97d57f1313cad9c9f3

nbd: Use non-blocking pipe-to-self
Although unlikely, if a user scales up operations by increasing the
number of threads beyond the limits of what a pipe can buffer, then
attempting to write to the pipe-to-self could stall, while the lock is
held. Better is to use a non-blocking pipe-to-self, and ignore EAGAIN
failures (the pipe was full, but that still means poll will see POLLIN
thanks to some other thread, and we didn't need to kick after all), as
well as to drain the pipe without intervening polls if there is a
backlog.

Besides, https://cr.yp.to/docs/selfpipe.html is a fairly canonical
description of pipe-to-self, and mentions that it should be
"non-blocking, just in case". :)

Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/nbd/nbd.c
@@ -45,6 +45,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <poll.h>
+#include <fcntl.h>
 
 #include <libnbd.h>
 
@@ -332,9 +333,13 @@ nbdplug_reader (void *handle)
       nbd_aio_notify_write (h->nbd);
 
     /* Check if we were kicked because a command was started */
-    if (fds[1].revents & POLLIN && read (h->fds[0], &c, 1) != 1) {
-      nbdkit_error ("failed to read pipe: %m");
-      break;
+    if (fds[1].revents & POLLIN) {
+      while (read (h->fds[0], &c, 1) == 1)
+        /* Drain any backlog */;
+      if (errno != EAGAIN) {
+        nbdkit_error ("failed to read pipe: %m");
+        break;
+      }
     }
 
     ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&h->trans_lock);
@@ -417,7 +422,7 @@ nbdplug_register (struct handle *h, int64_t cookie)
 
   /* While locked, kick the reader thread and add our transaction */
   ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&h->trans_lock);
-  if (write (h->fds[1], &c, 1) != 1) {
+  if (write (h->fds[1], &c, 1) != 1 && errno != EAGAIN) {
     nbdkit_error ("write to pipe: %m");
     free (trans);
     return NULL;
@@ -472,7 +477,7 @@ nbdplug_open_handle (int readonly)
     nbdkit_error ("malloc: %m");
     return NULL;
   }
-  if (pipe (h->fds)) {
+  if (pipe2 (h->fds, O_NONBLOCK)) {
     nbdkit_error ("pipe: %m");
     free (h);
     return NULL;

commit 331f2b4d93bfa37655e46ef54ba7a80d006be0e9

nbd: Don't reference stale errno in reader loop
When switching to libnbd in commit ab7760fcfd, I assumed that after a
POLLIN event fires on the pipe-to-self, then read() will return either
1 or -1.  In practice, this assumption worked because POLLIN should
not fire unless there is data, and a single read will either find that
data or hit an (unlikely) error.  But in general, it is a latent bug,
because read() can also return 0 if the pipe hits EOF, in which case
POSIX says errno has an unspecified value, and we should not be
deciding whether to log an error based on a random value; the only
reason we didn't hit EOF is because we didn't loop the read() nor
close() the write end of the pipe until after the reader loop is
joined.

Later, when refactoring the pipe-to-self to be non-blocking, I still
did not adjust the assumption, but made the code harder to read with a
loop body of ';' buried after a comment.  That approach also made the
code less efficient in that it requires at least two read() calls per
POLLIN even when there is no backlog, and where the second read() will
(usually) fail with EAGAIN because we drained available data.  At any
rate, it was still relying on the assumption that a return of 0 will
not happen, which is a latent risk.

It is already hard to create a backlog of data in the pipe-to-self,
but even when one exists, it's worth recognizing that our inner loop
to drain the pipe-to-self will still be reached via the outer loop
performing poll(), so we can rewrite things to be easier to read by
just doing a larger read(), while at the same time ensuring that we
only report errors when errno is actually reliable.

Fixes: e0d32468
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/nbd/nbd.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2017-2019 Red Hat Inc.
+ * Copyright (C) 2017-2020 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -312,7 +312,6 @@ nbdplug_reader (void *handle)
       [1].events = POLLIN,
     };
     unsigned dir;
-    char c;
 
     dir = nbd_aio_get_direction (h->nbd);
     nbdkit_debug ("polling, dir=%d", dir);
@@ -332,9 +331,9 @@ nbdplug_reader (void *handle)
 
     /* Check if we were kicked because a command was started */
     if (fds[1].revents & POLLIN) {
-      while (read (h->fds[0], &c, 1) == 1)
-        /* Drain any backlog */;
-      if (errno != EAGAIN) {
+      char buf[10]; /* Larger than 1 to allow reduction of any backlog */
+
+      if (read (h->fds[0], buf, sizeof buf) == -1 && errno != EAGAIN) {
         nbdkit_error ("failed to read pipe: %m");
         break;
       }
@@ -392,7 +391,7 @@ nbdplug_register (struct handle *h, struct transaction *trans, int64_t cookie)
   nbdkit_debug ("cookie %" PRId64 " started by state machine", cookie);
   trans->cookie = cookie;
 
-  if (write (h->fds[1], &c, 1) != 1 && errno != EAGAIN)
+  if (write (h->fds[1], &c, 1) == -1 && errno != EAGAIN)
     nbdkit_debug ("failed to kick reader thread: %m");
 }
 