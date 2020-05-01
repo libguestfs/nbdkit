libguestfs/nbdkit

commit 7a9f15172ae80747864a2481b2a6c2fd23937cf2

server: sockets: Use a vector to represent the list of sockets.

server/main.c
@@ -907,8 +907,7 @@ open_filter_so (struct backend *next, size_t i,
 static void
 start_serving (void)
 {
-  int *socks;
-  size_t nr_socks;
+  sockets socks = empty_vector;
   size_t i;
 
   set_up_quit_pipe ();
@@ -931,23 +930,24 @@ start_serving (void)
 #endif
   }
 
-  /* Socket activation -- we are handling connections on pre-opened
-   * file descriptors [FIRST_SOCKET_ACTIVATION_FD ..
-   * FIRST_SOCKET_ACTIVATION_FD+nr_socks-1].
+  /* Socket activation: the ‘socket_activation’ variable (> 0) is the
+   * number of file descriptors from FIRST_SOCKET_ACTIVATION_FD to
+   * FIRST_SOCKET_ACTIVATION_FD+socket_activation-1.
    */
   if (socket_activation) {
-    nr_socks = socket_activation;
-    debug ("using socket activation, nr_socks = %zu", nr_socks);
-    socks = malloc (sizeof (int) * nr_socks);
-    if (socks == NULL) {
-      perror ("malloc");
-      exit (EXIT_FAILURE);
+      if (sockets_reserve (&socks, socket_activation) == -1) {
+        perror ("realloc");
+        exit (EXIT_FAILURE);
+      }
+    for (i = 0; i < socket_activation; ++i) {
+      int s = FIRST_SOCKET_ACTIVATION_FD + i;
+      /* This can't fail because of the reservation above. */
+      assert (sockets_append (&socks, s) == 0);
     }
-    for (i = 0; i < nr_socks; ++i)
-      socks[i] = FIRST_SOCKET_ACTIVATION_FD + i;
+    debug ("using socket activation, nr_socks = %zu", socks.size);
     change_user ();
     write_pidfile ();
-    accept_incoming_connections (socks, nr_socks);
+    accept_incoming_connections (&socks);
     return;
   }
 
@@ -964,17 +964,17 @@ start_serving (void)
    * AF_VSOCK.
    */
   if (unixsocket)
-    socks = bind_unix_socket (&nr_socks);
+    bind_unix_socket (&socks);
   else if (vsock)
-    socks = bind_vsock (&nr_socks);
+    bind_vsock (&socks);
   else
-    socks = bind_tcpip_socket (&nr_socks);
+    bind_tcpip_socket (&socks);
 
   run_command ();
   change_user ();
   fork_into_background ();
   write_pidfile ();
-  accept_incoming_connections (socks, nr_socks);
+  accept_incoming_connections (&socks);
 }
 
 static void

server/internal.h
@@ -45,6 +45,7 @@
 #include "nbdkit-filter.h"
 #include "cleanup.h"
 #include "nbd-protocol.h"
+#include "vector.h"
 
 /* Define unlikely macro, but only for GCC.  These are used to move
  * debug and error handling code out of hot paths.
@@ -494,13 +495,11 @@ extern void lock_unload (void);
 extern void unlock_unload (void);
 
 /* sockets.c */
-extern int *bind_unix_socket (size_t *)
-  __attribute__((__nonnull__ (1)));
-extern int *bind_tcpip_socket (size_t *)
-  __attribute__((__nonnull__ (1)));
-extern int *bind_vsock (size_t *)
-  __attribute__((__nonnull__ (1)));
-extern void accept_incoming_connections (int *socks, size_t nr_socks)
+DEFINE_VECTOR_TYPE(sockets, int);
+extern void bind_unix_socket (sockets *) __attribute__((__nonnull__ (1)));
+extern void bind_tcpip_socket (sockets *) __attribute__((__nonnull__ (1)));
+extern void bind_vsock (sockets *) __attribute__((__nonnull__ (1)));
+extern void accept_incoming_connections (const sockets *socks)
   __attribute__((__nonnull__ (1)));
 
 /* threadlocal.c */

server/sockets.c
@@ -60,6 +60,7 @@
 
 #include "internal.h"
 #include "utils.h"
+#include "vector.h"
 
 static void
 set_selinux_label (void)
@@ -93,13 +94,12 @@ clear_selinux_label (void)
 #endif
 }
 
-int *
-bind_unix_socket (size_t *nr_socks)
+void
+bind_unix_socket (sockets *socks)
 {
   size_t len;
   int sock;
   struct sockaddr_un addr;
-  int *ret;
 
   assert (unixsocket);
   assert (unixsocket[0] == '/');
@@ -141,27 +141,21 @@ bind_unix_socket (size_t *nr_socks)
 
   clear_selinux_label ();
 
-  ret = malloc (sizeof (int));
-  if (!ret) {
-    perror ("malloc");
+  if (sockets_append (socks, sock) == -1) {
+    perror ("realloc");
     exit (EXIT_FAILURE);
   }
-  ret[0] = sock;
-  *nr_socks = 1;
 
   debug ("bound to unix socket %s", unixsocket);
-
-  return ret;
 }
 
-int *
-bind_tcpip_socket (size_t *nr_socks)
+void
+bind_tcpip_socket (sockets *socks)
 {
   struct addrinfo *ai = NULL;
   struct addrinfo hints;
   struct addrinfo *a;
   int err, opt;
-  int *socks = NULL;
   bool addr_in_use = false;
 
   if (port == NULL)
@@ -180,8 +174,6 @@ bind_tcpip_socket (size_t *nr_socks)
              gai_strerror (err));
     exit (EXIT_FAILURE);
   }
-
-  *nr_socks = 0;
 
   for (a = ai; a != NULL; a = a->ai_next) {
     int sock;
@@ -229,36 +221,30 @@ bind_tcpip_socket (size_t *nr_socks)
 
     clear_selinux_label ();
 
-    (*nr_socks)++;
-    socks = realloc (socks, sizeof (int) * (*nr_socks));
-    if (!socks) {
+    if (sockets_append (socks, sock) == -1) {
       perror ("realloc");
       exit (EXIT_FAILURE);
     }
-    socks[*nr_socks - 1] = sock;
   }
 
   freeaddrinfo (ai);
 
-  if (*nr_socks == 0 && addr_in_use) {
+  if (socks->size == 0 && addr_in_use) {
     fprintf (stderr, "%s: unable to bind to any sockets: %s\n",
              program_name, strerror (EADDRINUSE));
     exit (EXIT_FAILURE);
   }
 
   debug ("bound to IP address %s:%s (%zu socket(s))",
-         ipaddr ? ipaddr : "<any>", port, *nr_socks);
-
-  return socks;
+         ipaddr ? ipaddr : "<any>", port, socks->size);
 }
 
-int *
-bind_vsock (size_t *nr_socks)
+void
+bind_vsock (sockets *socks)
 {
 #ifdef AF_VSOCK
   uint32_t vsock_port;
   int sock;
-  int *ret;
   struct sockaddr_vm addr;
 
   if (port == NULL)
@@ -295,13 +281,10 @@ bind_vsock (size_t *nr_socks)
     exit (EXIT_FAILURE);
   }
 
-  ret = malloc (sizeof (int));
-  if (!ret) {
-    perror ("malloc");
+  if (sockets_append (socks, sock) == -1) {
+    perror ("realloc");
     exit (EXIT_FAILURE);
   }
-  ret[0] = sock;
-  *nr_socks = 1;
 
   /* It's not easy to get the actual CID here.
    * IOCTL_VM_SOCKETS_GET_LOCAL_CID is documented, but requires
@@ -310,8 +293,6 @@ bind_vsock (size_t *nr_socks)
    * doesn't work.
    */
   debug ("bound to vsock any:%" PRIu32, addr.svm_port);
-
-  return ret;
 
 #else
   /* Can't happen because main() checks if AF_VSOCK is defined and
@@ -438,11 +419,12 @@ accept_connection (int listen_sock)
  * (don't call accept_connection in this case).
  *
  * If POLLIN occurs on one of the sockets, call
- * accept_connection (socks[i]) on each of them.
+ * accept_connection (socks.ptr[i]) on each of them.
  */
 static void
-check_sockets_and_quit_fd (int *socks, size_t nr_socks)
+check_sockets_and_quit_fd (const sockets *socks)
 {
+  const size_t nr_socks = socks->size;
   size_t i;
   int r;
 
@@ -454,7 +436,7 @@ check_sockets_and_quit_fd (int *socks, size_t nr_socks)
   }
 
   for (i = 0; i < nr_socks; ++i) {
-    fds[i].fd = socks[i];
+    fds[i].fd = socks->ptr[i];
     fds[i].events = POLLIN;
     fds[i].revents = 0;
   }
@@ -479,18 +461,18 @@ check_sockets_and_quit_fd (int *socks, size_t nr_socks)
 
   for (i = 0; i < nr_socks; ++i) {
     if (fds[i].revents & POLLIN)
-      accept_connection (socks[i]);
+      accept_connection (socks->ptr[i]);
   }
 }
 
 void
-accept_incoming_connections (int *socks, size_t nr_socks)
+accept_incoming_connections (const sockets *socks)
 {
   size_t i;
   int err;
 
   while (!quit)
-    check_sockets_and_quit_fd (socks, nr_socks);
+    check_sockets_and_quit_fd (socks);
 
   /* Wait for all threads to exit. */
   pthread_mutex_lock (&count_mutex);
@@ -505,7 +487,7 @@ accept_incoming_connections (int *socks, size_t nr_socks)
   }
   pthread_mutex_unlock (&count_mutex);
 
-  for (i = 0; i < nr_socks; ++i)
-    close (socks[i]);
-  free (socks);
+  for (i = 0; i < socks->size; ++i)
+    close (socks->ptr[i]);
+  free (socks->ptr);
 }

commit 37b291b51abbd19816050728aa59504a2c277fc4

server: sockets: Don't use assert around a statement which must run.
If compiled with -DNDEBUG then the statement could have been removed
completely.

Fixes: commit 7a9f15172ae80747864a2481b2a6c2fd23937cf2

server/main.c
@@ -940,9 +940,10 @@ start_serving (void)
         exit (EXIT_FAILURE);
       }
     for (i = 0; i < socket_activation; ++i) {
-      int s = FIRST_SOCKET_ACTIVATION_FD + i;
+      int s = FIRST_SOCKET_ACTIVATION_FD + i, r;
       /* This can't fail because of the reservation above. */
-      assert (sockets_append (&socks, s) == 0);
+      r = sockets_append (&socks, s);
+      assert (r == 0);
     }
     debug ("using socket activation, nr_socks = %zu", socks.size);
     change_user ();