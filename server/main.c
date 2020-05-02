libguestfs/nbdkit

commit a47857b47b88adbf5054772be4d6746ec08ca16f

server: Add path of the server executable to --dump-config output.
  $ ./nbdkit --dump-config
  binary=/home/rjones/d/nbdkit/server/nbdkit
  [etc]

Use this in the tests to verify we are testing the correct executable
(ie. not an installed or other copy).

server/main.c
@@ -129,8 +129,13 @@ display_version (void)
 }
 
 static void
-dump_config (void)
+dump_config (const char *argv0)
 {
+  CLEANUP_FREE char *binary;
+
+  binary = realpath (argv0, NULL);
+  if (binary != NULL)
+    printf ("%s=%s\n", "binary", binary);
   printf ("%s=%s\n", "bindir", bindir);
   printf ("%s=%s\n", "filterdir", filterdir);
   printf ("%s=%s\n", "libdir", libdir);
@@ -211,7 +216,7 @@ main (int argc, char *argv[])
 
     switch (c) {
     case DUMP_CONFIG_OPTION:
-      dump_config ();
+      dump_config (argv[0]);
       exit (EXIT_SUCCESS);
 
     case DUMP_PLUGIN_OPTION:

tests/Makefile.am
@@ -86,6 +86,7 @@ EXTRA_DIST = \
 	shebang.rb \
 	ssh/sshd_config.in \
 	test-ansi-c.sh \
+	test-binary.sh \
 	test-blocksize.sh \
 	test-cache.sh \
 	test-cache-max-size.sh \
@@ -249,6 +250,7 @@ endif HAVE_VDDK
 # Basic server command line and start-up tests.
 
 TESTS += \
+	test-binary.sh \
 	test-help.sh \
 	test-version.sh \
 	test-dump-config.sh \

tests/test-binary.sh
@@ -1,0 +1,55 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Check that the nbdkit command (which should point to the local
+# wrapper) eventually runs the locally built server/nbdkit and not
+# some other binary.  So we're testing the right thing.  This should
+# be ensured by PATH being set in tests/Makefile.am:TESTS_ENVIRONMENT.
+# Also checks that nbdkit --dump-config contains binary=<binary>.
+
+source ./functions.sh
+set -e
+
+requires cut --version
+
+binary1="$( nbdkit --dump-config           | grep ^binary= | cut -d= -f2 )"
+binary2="$( ../server/nbdkit --dump-config | grep ^binary= | cut -d= -f2 )"
+
+echo binary1=$binary1
+echo binary2=$binary2
+
+if [ "$binary1" != "$binary2" ]; then
+    echo "*** TEST ENVIRONMENT PROBLEM ***"
+    echo "nbdkit command does not run locally compiled binary"
+    echo "check PATH=$PATH"
+    exit 1
+fi

commit 43f7e07c5b087996ea0c7a80dcf44976d81620be

server: Fix binary in --dump-config output.
As written this will only work on Linux.  On platforms which lack
/proc/self/exe the binary name will be missing in the output.

Fixes: commit a47857b47b88adbf5054772be4d6746ec08ca16f

server/main.c
@@ -129,11 +129,10 @@ display_version (void)
 }
 
 static void
-dump_config (const char *argv0)
+dump_config (void)
 {
-  CLEANUP_FREE char *binary;
+  CLEANUP_FREE char *binary = realpath ("/proc/self/exe", NULL);
 
-  binary = realpath (argv0, NULL);
   if (binary != NULL)
     printf ("%s=%s\n", "binary", binary);
   printf ("%s=%s\n", "bindir", bindir);
@@ -216,7 +215,7 @@ main (int argc, char *argv[])
 
     switch (c) {
     case DUMP_CONFIG_OPTION:
-      dump_config (argv[0]);
+      dump_config ();
       exit (EXIT_SUCCESS);
 
     case DUMP_PLUGIN_OPTION: