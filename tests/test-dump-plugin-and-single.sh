277ce5df621de1ca7dd412239833eaa3218c1cc7
Richard W.M. Jones <rjones@redhat.com>
2020-05-10 18:33:23 +0100

    tests: Move --dump-plugin & -s test to new script.
    Also corrects an inconsequential typo ecit -> exit (fixes
    commit dfdf3bd9d4896c61f535fedb0ae3bdf2dceae9da).

---
+++ tests/test-dump-plugin-and-single.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2014-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# --dump-plugin and -s are incompatible
+
+source ./functions.sh
+set -e
+set -x
+
+out=dump-plugin-and-single.out
+err=dump-plugin-and-single.err
+files="$out $err"
+rm -f $files
+cleanup_fn rm -f $files
+
+if nbdkit --dump-plugin -s null >$out 2>$err; then
+    echo "$0: unexpected success from nbdkit -s --dump-plugin"
+    echo "out:"
+    cat $out
+    echo "err:"
+    cat $err
+    exit 1
+fi
+if test -s $out; then
+    echo "$0: unexpected output during nbdkit -s --dump-plugin"
+    echo "out:"
+    cat $out
+    echo "err:"
+    cat $err
+    exit 1
+fi
+if test ! -s $err; then
+    echo "$0: missing error message during nbdkit -s --dump-plugin"
+    exit 1
+fi
