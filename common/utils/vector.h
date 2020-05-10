8b51d9484793458febb1889bdb528c81624a0445
Richard W.M. Jones <rjones@redhat.com>
2020-05-10 14:40:16 +0100

    common/utils/vector.h: Add some documentation.

--- common/utils/vector.h
+++ common/utils/vector.h
@@ -44,6 +44,39 @@
 #include <assert.h>
 #include <string.h>
 
+/* Use of this macro defines a new type called ‘name’ containing an
+ * extensible vector of ‘type’ elements.  For example:
+ *
+ *   DEFINE_VECTOR_TYPE(string_vector, char *)
+ *
+ * defines a new type called ‘string_vector’ as a vector of ‘char *’.
+ * You can create variables of this type:
+ *
+ *   string_vector names = empty_vector;
+ *
+ * where ‘names.ptr[]’ will be an array of strings and ‘names.size’
+ * will be the number of strings.  There are no get/set accessors.  To
+ * iterate over the strings you can use the ‘.ptr’ field directly:
+ *
+ *   for (size_t i = 0; i < names.size; ++i)
+ *     printf ("%s\n", names.ptr[i]);
+ *
+ * Initializing with ‘empty_vector’ sets ‘.ptr = NULL’ and ‘.size = 0’.
+ *
+ * DEFINE_VECTOR_TYPE also defines utility functions.  For the full
+ * list see the definition below, but useful functions include:
+ *
+ * ‘name’_append  (eg. ‘string_vector_append’)
+ *   - Append a new element at the end.  This operation is cheap.
+ *
+ * ‘name’_insert  (eg. ‘string_vector_insert’)
+ *   - Insert a new element at the beginning, middle or end.  This
+ *     operation is more expensive because existing elements may need
+ *     to be copied around.
+ *
+ * Both functions extend the vector if required, and so both may fail
+ * (returning -1) which you must check for.
+ */
 #define DEFINE_VECTOR_TYPE(name, type)                                  \
   struct name {                                                         \
     type *ptr;                 /* Pointer to array of items. */         \
@@ -51,12 +84,18 @@
     size_t alloc;              /* Number of items allocated. */         \
   };                                                                    \
   typedef struct name name;                                             \
+                                                                        \
+  /* Reserve n elements at the end of the vector.  Note space is        \
+   * allocated but the vector size is not increased and the new         \
+   * elements are not initialized.                                      \
+   */                                                                   \
   static inline int                                                     \
   name##_reserve (name *v, size_t n)                                    \
   {                                                                     \
     return generic_vector_reserve ((struct generic_vector *)v, n,       \
                                    sizeof (type));                      \
   }                                                                     \
+                                                                        \
   /* Insert at i'th element.  i=0 => beginning  i=size => append */     \
   static inline int                                                     \
   name##_insert (name *v, type elem, size_t i)                          \
@@ -69,11 +108,15 @@
     v->size++;                                                          \
     return 0;                                                           \
   }                                                                     \
+                                                                        \
+  /* Append a new element to the end of the vector. */                  \
   static inline int                                                     \
   name##_append (name *v, type elem)                                    \
   {                                                                     \
     return name##_insert (v, elem, v->size);                            \
   }                                                                     \
+                                                                        \
+  /* Iterate over the vector, calling f() on each element. */           \
   static inline void                                                    \
   name##_iter (name *v, void (*f) (type elem))                          \
   {                                                                     \
