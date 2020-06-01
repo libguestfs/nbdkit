cce18ef642da47e2047c2887064e1f7445cdd395
Richard W.M. Jones <rjones@redhat.com>
2020-06-01 19:15:20 +0100

    tests: Add more extensive testing of nbdkit_read_password.
    Previously this was only tested as a standalone function.  This tests
    it from a plugin with nbdkit.
    
    (cherry picked from commit 64c0ed22509db36fcbf41aa35ddd98dc5d0a0d09)

---
+++ tests/test-read-password-plugin.c
@@ -0,0 +1,131 @@
+/* nbdkit
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* See test-read-password.sh */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <nbdkit-plugin.h>
+
+static char *password;
+static char *file;
+
+static void
+password_unload (void)
+{
+  free (password);
+  free (file);
+}
+
+static int
+password_config (const char *key, const char *value)
+{
+  if (strcmp (key, "password") == 0) {
+    if (nbdkit_read_password (value, &password) == -1)
+      return -1;
+  }
+  else if (strcmp (key, "file") == 0) {
+    file = nbdkit_absolute_path (value);
+    if (file == NULL)
+      return -1;
+  }
+  else {
+    nbdkit_error ("unknown parameter: %s", key);
+    return -1;
+  }
+  return 0;
+}
+
+static int
+password_config_complete (void)
+{
+  FILE *fp;
+
+  if (!file || !password) {
+    nbdkit_error ("file and password parameters are required");
+    return -1;
+  }
+  fp = fopen (file, "w");
+  if (!fp) {
+    nbdkit_error ("%s: %m", file);
+    return -1;
+  }
+  fprintf (fp, "%s", password);
+  fclose (fp);
+  return 0;
+}
+
+static int
+password_get_ready (void)
+{
+  /* This plugin is for testing, so it never serves any data. */
+  nbdkit_shutdown ();
+  return 0;
+}
+
+static void *
+password_open (int readonly)
+{
+  abort ();
+}
+
+static int64_t
+password_get_size (void *handle)
+{
+  abort ();
+}
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+static int
+password_pread (void *handle, void *buf, uint32_t count, uint64_t offset)
+{
+  abort ();
+}
+
+static struct nbdkit_plugin plugin = {
+  .name              = "password",
+  .version           = PACKAGE_VERSION,
+  .unload            = password_unload,
+  .config            = password_config,
+  .config_complete   = password_config_complete,
+  .get_ready         = password_get_ready,
+  .open              = password_open,
+  .get_size          = password_get_size,
+  .pread             = password_pread,
+};
+
+NBDKIT_REGISTER_PLUGIN(plugin)
---
+++ tests/test-read-password.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2019-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -e
+set -x
+
+plugin=.libs/test-read-password-plugin.so
+requires test -f $plugin
+
+# Since we are matching on error messages.
+export LANG=C
+
+f=test-read-password.file
+tf=test-read-password.tmp
+out=test-read-password.out
+rm -f $f $tf $out
+cleanup_fn rm -f $f $tf $out
+
+# Password on the command line.
+nbdkit -fv $plugin file=$f password=abc
+grep '^abc$' $f
+
+# Password +FILENAME.
+echo def > $tf
+nbdkit -fv $plugin file=$f password=+$tf
+grep '^def$' $f
+
+# Password +FILENAME, zero length.
+: > $tf
+nbdkit -fv $plugin file=$f password=+$tf
+test -f $f
+! test -s $f
+
+# Password -FD.
+echo 123 > $tf
+exec 3< $tf
+nbdkit -fv $plugin file=$f password=-3
+exec 3<&-
+grep '^123$' $f
+
+# Password -FD, zero length.
+: > $tf
+exec 3< $tf
+nbdkit -fv $plugin file=$f password=-3
+exec 3<&-
+test -f $f
+! test -s $f
+
+# Reading a password from stdin (non-interactive) should fail.
+if nbdkit -fv $plugin file=$f password=- </dev/null >&$out ; then
+    echo "$0: expected password=- to fail"
+    exit 1
+fi
+cat $out
+grep "stdin is not a tty" $out
+
+# Reading a password from stdin/stdout/stderr using -0/-1/-2 should fail.
+for i in 0 1 2; do
+    if nbdkit -fv $plugin file=$f password=-$i </dev/null >&$out ; then
+        echo "$0: expected password=-$i to fail"
+        exit 1
+    fi
+    cat $out
+    grep "cannot use password -FD" $out
+done
