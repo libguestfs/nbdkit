00173b1df95e4510352c7f9f753abc464b49b9bc
Richard W.M. Jones <rjones@redhat.com>
2020-06-02 13:05:41 +0100

    tests: Enhance dummy-vddk.
    Make it behave more or less like a real VDDK.  Of course it ignores
    all parameters passed and emulates a blank disk, but that is
    sufficient to do some more realistic testing.
    
    This also adds a more realistic test of this blank disk via
    libguestfs.

---
+++ tests/test-vddk.c
@@ -0,0 +1,110 @@
+/* nbdkit
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <guestfs.h>
+
+#include "test.h"
+
+int
+main (int argc, char *argv[])
+{
+  guestfs_h *g;
+  int r;
+
+  /* The LD_LIBRARY_PATH munging that the VDDK plugin does is not
+   * compatible with valgrind.
+   */
+  if (getenv ("NBDKIT_VALGRIND")) {
+    printf ("%s: skipped test with valgrind\n", argv[0]);
+    exit (77);
+  }
+
+  /* This tests the VDDK plugin using dummy-vddk.c which is a dummy
+   * library that looks a bit like VDDK and emulates a blank disk (it
+   * basically ignores whatever paramaters you pass to it).
+   */
+  if (test_start_nbdkit ("vddk",
+                         "libdir=.libs",
+                         "/dev/null", /* Test magic file key. */
+                         NULL) == -1)
+    exit (EXIT_FAILURE);
+
+  g = guestfs_create ();
+  if (g == NULL) {
+    perror ("guestfs_create");
+    exit (EXIT_FAILURE);
+  }
+
+  r = guestfs_add_drive_opts (g, "",
+                              GUESTFS_ADD_DRIVE_OPTS_FORMAT, "raw",
+                              GUESTFS_ADD_DRIVE_OPTS_PROTOCOL, "nbd",
+                              GUESTFS_ADD_DRIVE_OPTS_SERVER, server,
+                              -1);
+  if (r == -1)
+    exit (EXIT_FAILURE);
+
+  if (guestfs_launch (g) == -1)
+    exit (EXIT_FAILURE);
+
+  if (guestfs_part_disk (g, "/dev/sda", "mbr") == -1)
+    exit (EXIT_FAILURE);
+
+  if (guestfs_mkfs (g, "vfat", "/dev/sda1") == -1)
+    exit (EXIT_FAILURE);
+
+  /* Mount it and write a file. */
+  if (guestfs_mount (g, "/dev/sda1", "/") == -1)
+    exit (EXIT_FAILURE);
+
+  if (guestfs_write (g, "/foo", "hello", 5) == -1)
+    exit (EXIT_FAILURE);
+
+  if (guestfs_is_file (g, "/foo") != 1) {
+    fprintf (stderr, "%s FAILED: /foo should be a file\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  if (guestfs_shutdown (g) == -1)
+    exit (EXIT_FAILURE);
+
+  guestfs_close (g);
+  exit (EXIT_SUCCESS);
+}
