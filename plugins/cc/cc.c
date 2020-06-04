libguestfs/nbdkit

commit a4339e3c3c17d8b95ddf0b20cf015de5078620ec

New "CC" plugin for running small C-based plugins from the command line.

plugins/cc/Makefile.am
@@ -1,0 +1,70 @@
+# nbdkit
+# Copyright (C) 2018-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-cc-plugin.pod
+
+plugin_LTLIBRARIES = nbdkit-cc-plugin.la
+
+nbdkit_cc_plugin_la_SOURCES = \
+	cc.c \
+	$(top_srcdir)/include/nbdkit-plugin.h \
+	$(NULL)
+
+nbdkit_cc_plugin_la_CPPFLAGS = \
+	-DCC="\"$(CC)\"" \
+	-DCFLAGS="\"$(CFLAGS)\"" \
+	-I$(top_srcdir)/common/utils \
+	-I$(top_srcdir)/include \
+	-I. \
+	$(NULL)
+nbdkit_cc_plugin_la_CFLAGS = $(WARNINGS_CFLAGS)
+nbdkit_cc_plugin_la_LDFLAGS = \
+	-module -avoid-version -shared $(SHARED_LDFLAGS) \
+	-Wl,--version-script=$(top_srcdir)/plugins/plugins.syms \
+	$(NULL)
+nbdkit_cc_plugin_la_LIBADD = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(DL_LIBS) \
+	$(NULL)
+
+if HAVE_POD
+
+man_MANS = nbdkit-cc-plugin.3
+CLEANFILES += $(man_MANS)
+
+nbdkit-cc-plugin.3: nbdkit-cc-plugin.pod
+	$(PODWRAPPER) --section=3 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

plugins/cc/cc.c
@@ -1,0 +1,545 @@
+/* nbdkit
+ * Copyright (C) 2018-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#define NBDKIT_API_VERSION 2
+#include <nbdkit-plugin.h>
+
+#include "cleanup.h"
+#include "utils.h"
+#include "vector.h"
+
+/* The script name. */
+static char *script;
+static bool unlink_on_exit = false;
+
+/* C compiler and flags. */
+static const char *cc = CC;
+static const char *cflags = CFLAGS " -fPIC -shared";
+static const char *extra_cflags;
+
+/* List of parameters for the subplugin. */
+struct key_value { const char *key, *value; };
+DEFINE_VECTOR_TYPE(string_vector, struct key_value);
+static string_vector params = empty_vector;
+
+/* The subplugin. */
+static void *dl;
+static struct nbdkit_plugin subplugin;
+
+static void
+cc_unload (void)
+{
+  if (unlink_on_exit)
+    unlink (script);
+  if (dl)
+    dlclose (dl);
+  free (params.ptr);
+  free (script);
+}
+
+static void
+cc_dump_plugin (void)
+{
+  printf ("CC=%s\n", CC);
+  printf ("CFLAGS=%s\n", CFLAGS);
+}
+
+#define cc_config_help \
+  "[script=]<FILENAME>   (required) The shell script to run.\n" \
+  "CC=<CC>                          C compiler.\n" \
+  "CFLAGS=<CFLAGS>                  C compiler flags.\n" \
+  "EXTRA_CFLAGS=<CFLAGS>            Extra C compiler flags.\n" \
+  "[other arguments may be used by the plugin that you load]"
+
+static char *
+inline_script (void)
+{
+  CLEANUP_FREE char *cmd = NULL;
+  int fd;
+
+  if (!nbdkit_stdio_safe ()) {
+    nbdkit_error ("inline script is incompatible with -s");
+    return NULL;
+  }
+
+  script = strdup ("/tmp/ccXXXXXX.c");
+  if (script == NULL) {
+    nbdkit_error ("strdup: %m");
+    return NULL;
+  }
+
+  fd = mkstemps (script, 2);
+  if (fd == -1) {
+    nbdkit_error ("mkstemps: %m");
+    return NULL;
+  }
+  close (fd);
+  unlink_on_exit = true;
+
+  if (asprintf (&cmd, "cat > %s", script) == -1) {
+    nbdkit_error ("asprintf: %m");
+    return NULL;
+  }
+
+  if (system (cmd) != 0) {
+    nbdkit_error ("cc: failed to copy inline script to temporary file");
+    return NULL;
+  }
+
+  return script;
+}
+
+static int
+cc_config (const char *key, const char *value)
+{
+  if (!script) {
+    /* The first parameter must be "script". */
+    if (strcmp (key, "script") != 0) {
+      nbdkit_error ("cc: the first parameter must be the C file or \"-\"");
+      return -1;
+    }
+    if (strcmp (value, "-") != 0)
+      script = nbdkit_realpath (value);
+    else
+      script = inline_script ();
+    if (script == NULL)
+      return -1;
+
+    return 0;
+  }
+  else {
+    /* Although not impossible, it's likely to be a bug if there is a
+     * further parameter called "script" so disallow it.
+     */
+    if (strcmp (key, "script") == 0) {
+      nbdkit_error ("cc: script parameter must appear only once");
+      return -1;
+    }
+    /* Otherwise parse our parameters. */
+    else if (strcmp (key, "CC") == 0)
+      cc = value;
+    else if (strcmp (key, "CFLAGS") == 0)
+      cflags = value;
+    else if (strcmp (key, "EXTRA_CFLAGS") == 0)
+      extra_cflags = value;
+    else {
+      /* Anything else is saved for the subplugin. */
+      struct key_value kv = { .key = key, .value = value };
+
+      if (string_vector_append (&params, kv) == -1) {
+        nbdkit_error ("realloc: %m");
+        return -1;
+      }
+    }
+    return 0;
+  }
+}
+
+/* We must compile and load the subplugin here (not in get_ready)
+ * because we must find the subplugin's thread model, and the core
+ * server will query that straight after config_complete.
+ */
+static int
+cc_config_complete (void)
+{
+  CLEANUP_FREE char *command = NULL;
+  size_t len = 0, size, i;
+  FILE *fp;
+  int fd, r;
+  char tmpfile[] = "/tmp/ccXXXXXX.so";
+  struct nbdkit_plugin *(*plugin_init) (void);
+  char *error;
+  const struct nbdkit_plugin *ptr;
+
+  if (!script) {
+    nbdkit_error ("cc: no C program name (or \"-\") given");
+    return -1;
+  }
+
+  /* Create a temporary file to store the compiled plugin. */
+  fd = mkstemps (tmpfile, 3);
+  if (fd == -1) {
+    nbdkit_error ("mkstemps: %m");
+    return -1;
+  }
+  close (fd);
+
+  /* Compile the C program. */
+  fp = open_memstream (&command, &len);
+  if (fp == NULL) {
+    nbdkit_error ("open_memstream: %m");
+    return -1;
+  }
+  shell_quote (cc, fp);
+  /* The C flags don't need to be quoted. */
+  fprintf (fp, " %s ", cflags);
+  if (extra_cflags)
+    fprintf (fp, "%s ", extra_cflags);
+  /* The API version is required and cannot be changed. */
+  fprintf (fp, "-DNBDKIT_API_VERSION=%d ", NBDKIT_API_VERSION);
+  shell_quote (script, fp);
+  fprintf (fp, " -o ");
+  shell_quote (tmpfile, fp);
+  if (fclose (fp) == EOF) {
+    nbdkit_error ("memstream failed: %m");
+    return -1;
+  }
+
+  nbdkit_debug ("cc: %s", command);
+  r = system (command);
+  if (exit_status_to_nbd_error (r, cc) == -1) {
+    unlink (tmpfile);
+    return -1;
+  }
+
+  /* Load the subplugin. */
+  dl = dlopen (tmpfile, RTLD_NOW);
+  unlink (tmpfile);
+  if (dl == NULL) {
+    nbdkit_error ("cannot open the compiled plugin: %s", dlerror ());
+    return -1;
+  }
+
+  /* Now we basically behave like core nbdkit when it loads a plugin. */
+  dlerror ();
+  *(void **) (&plugin_init) = dlsym (dl, "plugin_init");
+  if ((error = dlerror ()) != NULL) {
+    nbdkit_error ("no plugin_init symbol found: %s", dlerror ());
+    return -1;
+  }
+  if (!plugin_init) {
+    nbdkit_error ("invalid plugin_init symbol");
+    return -1;
+  }
+  ptr = plugin_init ();
+  if (!ptr) {
+    nbdkit_error ("plugin registration failed");
+    return -1;
+  }
+
+  /* Are the APIs compatible? */
+  if (ptr->_api_version != NBDKIT_API_VERSION) {
+    nbdkit_error ("plugin uses the wrong NBDKIT_API_VERSION, it must be %d",
+                  NBDKIT_API_VERSION);
+    return -1;
+  }
+
+  /* Copy the subplugins struct into our plugin global, padding or
+   * truncating as necessary.
+   */
+  size = sizeof subplugin;      /* Size of our struct. */
+  if (size > ptr->_struct_size)
+    size = ptr->_struct_size;
+  memcpy (&subplugin, ptr, size);
+
+  /* Check that the plugin has .open, .get_size and .pread. */
+  if (subplugin.open == NULL) {
+    nbdkit_error ("plugin must have a .open callback");
+    return -1;
+  }
+  if (subplugin.get_size == NULL) {
+    nbdkit_error ("plugin must have a .get_size callback");
+    return -1;
+  }
+  if (subplugin.pread == NULL) {
+    nbdkit_error ("plugin must have a .pread callback");
+    return -1;
+  }
+
+  /* Now we have to call the subplugin's load, config and
+   * config_complete.  Everything after that will be called via the
+   * core server through our forwarding functions below.
+   */
+  if (subplugin.load)
+    subplugin.load ();
+  if (subplugin.config) {
+    for (i = 0; i < params.size; ++i) {
+      if (subplugin.config (params.ptr[i].key, params.ptr[i].value) == -1)
+        return -1;
+    }
+  }
+  else if (params.size > 0) {
+    /* Just print the first one in the error message. */
+    nbdkit_error ("unknown parameter: %s", params.ptr[0].key);
+    return -1;
+  }
+  if (subplugin.config_complete) {
+    if (subplugin.config_complete () == -1)
+      return -1;
+  }
+
+  return 0;
+}
+
+/* This is adjusted when we load the subplugin. */
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+/* Set the thread model from the subplugin. */
+static int
+cc_thread_model (void)
+{
+  if (subplugin.thread_model != NULL)
+    return subplugin.thread_model ();
+  else
+    return subplugin._thread_model;
+}
+
+/* All other calls are forwarded to the subplugin. */
+static int
+cc_get_ready (void)
+{
+  if (subplugin.get_ready)
+    return subplugin.get_ready ();
+  return 0;
+}
+
+static int
+cc_preconnect (int readonly)
+{
+  if (subplugin.preconnect)
+    return subplugin.preconnect (readonly);
+  return 0;
+}
+
+static void *
+cc_open (int readonly)
+{
+  return subplugin.open (readonly);
+}
+
+static void
+cc_close (void *handle)
+{
+  if (subplugin.close)
+    subplugin.close (handle);
+}
+
+static int64_t
+cc_get_size (void *handle)
+{
+  return subplugin.get_size (handle);
+}
+
+static int
+cc_can_write (void *handle)
+{
+  if (subplugin.can_write)
+    return subplugin.can_write (handle);
+  else
+    return !!subplugin.pwrite;
+}
+
+static int
+cc_can_flush (void *handle)
+{
+  if (subplugin.can_flush)
+    return subplugin.can_flush (handle);
+  else
+    return !!subplugin.flush;
+}
+
+static int
+cc_is_rotational (void *handle)
+{
+  if (subplugin.is_rotational)
+    return subplugin.is_rotational (handle);
+  else
+    return 0;
+}
+
+static int
+cc_can_trim (void *handle)
+{
+  if (subplugin.can_trim)
+    return subplugin.can_trim (handle);
+  else
+    return !!subplugin.trim;
+}
+
+static int
+cc_can_zero (void *handle)
+{
+  if (subplugin.can_zero)
+    return subplugin.can_zero (handle);
+  else
+    return !!subplugin.zero;
+}
+
+static int
+cc_can_fast_zero (void *handle)
+{
+  if (subplugin.can_fast_zero)
+    return subplugin.can_fast_zero (handle);
+  else
+    return 0;
+}
+
+static int
+cc_can_extents (void *handle)
+{
+  if (subplugin.can_extents)
+    return subplugin.can_extents (handle);
+  else
+    return !!subplugin.extents;
+}
+
+static int
+cc_can_fua (void *handle)
+{
+  if (subplugin.can_fua)
+    return subplugin.can_fua (handle);
+  else if (cc_can_flush (handle))
+    return NBDKIT_FUA_EMULATE;
+  else
+    return NBDKIT_FUA_NONE;
+}
+
+static int
+cc_can_multi_conn (void *handle)
+{
+  if (subplugin.can_multi_conn)
+    return subplugin.can_multi_conn (handle);
+  else
+    return 0;
+}
+
+static int
+cc_can_cache (void *handle)
+{
+  if (subplugin.can_cache)
+    return subplugin.can_cache (handle);
+  else if (subplugin.cache)
+    return NBDKIT_CACHE_NATIVE;
+  else
+    return NBDKIT_CACHE_NONE;
+}
+
+static int
+cc_pread (void *handle, void *buf, uint32_t count, uint64_t offset,
+          uint32_t flags)
+{
+  return subplugin.pread (handle, buf, count, offset, flags);
+}
+
+static int
+cc_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
+           uint32_t flags)
+{
+  return subplugin.pwrite (handle, buf, count, offset, flags);
+}
+
+static int
+cc_flush (void *handle, uint32_t flags)
+{
+  return subplugin.flush (handle, flags);
+}
+
+static int
+cc_trim (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
+{
+  return subplugin.trim (handle, count, offset, flags);
+}
+
+static int
+cc_zero (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
+{
+  return subplugin.zero (handle, count, offset, flags);
+}
+
+static int
+cc_extents (void *handle, uint32_t count, uint64_t offset,
+            uint32_t flags, struct nbdkit_extents *extents)
+{
+  return subplugin.extents (handle, count, offset, flags, extents);
+}
+
+static int
+cc_cache (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
+{
+  return subplugin.cache (handle, count, offset, flags);
+}
+
+static struct nbdkit_plugin plugin = {
+  .name              = "cc",
+  .longname          = "nbdkit C compiler plugin",
+  .version           = PACKAGE_VERSION,
+
+  /* These are the callbacks that this plugin overrides. */
+  .unload            = cc_unload,
+  .dump_plugin       = cc_dump_plugin,
+  .config            = cc_config,
+  .config_complete   = cc_config_complete,
+  .config_help       = cc_config_help,
+  .thread_model      = cc_thread_model,
+
+  /* And we must provide callbacks for everything else, which are
+   * passed through to the subplugin.
+   */
+  .get_ready         = cc_get_ready,
+
+  .preconnect        = cc_preconnect,
+  .open              = cc_open,
+  .close             = cc_close,
+
+  .get_size          = cc_get_size,
+  .can_write         = cc_can_write,
+  .can_flush         = cc_can_flush,
+  .is_rotational     = cc_is_rotational,
+  .can_trim          = cc_can_trim,
+  .can_zero          = cc_can_zero,
+  .can_fast_zero     = cc_can_fast_zero,
+  .can_extents       = cc_can_extents,
+  .can_fua           = cc_can_fua,
+  .can_multi_conn    = cc_can_multi_conn,
+  .can_cache         = cc_can_cache,
+
+  .pread             = cc_pread,
+  .pwrite            = cc_pwrite,
+  .flush             = cc_flush,
+  .trim              = cc_trim,
+  .zero              = cc_zero,
+  .extents           = cc_extents,
+  .cache             = cc_cache,
+
+  .errno_is_preserved = 1,
+};
+
+NBDKIT_REGISTER_PLUGIN(plugin)

tests/Makefile.am
@@ -1076,6 +1076,15 @@ test_golang_LDADD = libtest.la $(LIBGUESTFS_LIBS)
 
 endif HAVE_GOLANG
 
+# CC plugin test.
+TESTS += \
+	test-shebang-cc.sh \
+	$(NULL)
+EXTRA_DIST += \
+	shebang.c \
+	test-shebang-cc.sh \
+	$(NULL)
+
 #----------------------------------------------------------------------
 # Tests of filters.
 

TODO
@@ -140,19 +140,10 @@ VDDK:
 Suggestions for language plugins
 --------------------------------
 
-"CC" plugin
-
-* Analogous to sh plugin, would let you write a C plugin on the
-  command line or in a script.
-
-* Would work by compiling the plugin to a temporary file at runtime
-  using GCC or clang.
+CC plugin:
 
 * Could be used to replace the explicit plugins used for
   testing (see tests/*-plugin.c).
-
-* Tricky to get compiler flags right.  Should we bake them into the
-  plugin or let them be set at runtime (or both?)
 
 Python:
 

configure.ac
@@ -49,6 +49,7 @@ LT_INIT
 
 dnl List of plugins and filters.
 lang_plugins="\
+        cc \
         golang \
         lua \
         ocaml \
@@ -1048,6 +1049,7 @@ AC_CONFIG_FILES([Makefile
                  include/Makefile
                  include/nbdkit-version.h
                  plugins/Makefile
+                 plugins/cc/Makefile
                  plugins/curl/Makefile
                  plugins/data/Makefile
                  plugins/eval/Makefile

plugins/cc/nbdkit-cc-plugin.pod
@@ -1,0 +1,178 @@
+=head1 NAME
+
+nbdkit-cc-plugin - write small nbdkit plugins in inline C
+
+=head1 SYNOPSIS
+
+ nbdkit cc /path/to/plugin.c [CC=<CC>] [CFLAGS=<CFLAGS>]
+                             [EXTRA_CFLAGS=<EXTRA_CFLAGS>]
+ nbdkit cc - <<'EOF'
+ ... C code ...
+ EOF
+
+=head1 DESCRIPTION
+
+This plugin allows you to write small L<nbdkit(1)> plugins in C.  When
+you use this plugin it compiles your source code to a temporary plugin
+and then jumps into your compiled plugin.  It is somewhat similar to
+L<nbdkit-sh-plugin(3)>, except for C source code.  This can also be
+used to write plugins which are "C scripts".
+
+B<Note> this is not the way you normally write nbdkit plugins in C.
+To understand how to write plugins in C normally, read
+L<nbdkit-plugin(3)>.
+
+=head2 Simple plugin example
+
+Simple plugins from the nbdkit source tree can be compiled and run
+directly using commands such as:
+
+ $ nbdkit cc plugins/example1/example1.c EXTRA_CFLAGS="-I."
+
+You can also read the source from stdin using C<->:
+
+ $ nbdkit cc - EXTRA_CFLAGS="-I." < plugins/example1/example1.c
+
+To replace the compiler flags:
+
+ $ nbdkit cc plugins/example1/example1.c \
+             CFLAGS="-O3 -mavx2 -fPIC -shared"
+
+=head2 Compiler name and flags
+
+The plugin parameters C<CC>, C<CFLAGS> and C<EXTRA_CFLAGS> (written in
+uppercase) can be used to control which C compiler and C compiler
+flags are used.  If not set, the default compiler and flags from when
+nbdkit was itself compiled from source are used.  To see what those
+were you can do:
+
+ $ nbdkit cc --dump-plugin
+ ...
+ CC=gcc
+ CFLAGS=-g -O2 -fPIC -shared
+
+The C<CFLAGS> parameter overrides the built-in flags completely.  The
+C<EXTRA_CFLAGS> parameter adds extra flags to the built-in flags.
+
+=head2 Plugin API version
+
+Plugins compiled this way must use the same API version as the cc
+plugin itself uses.  Currently this is C<NBDKIT_API_VERSION=2>.
+
+=head2 C plugin as a self-contained script
+
+You can create a C plugin which is a self-contained script by adding
+the following lines at the top and ensuring the C source is executable
+(S<C<chmod +x plugin.c>>):
+
+ #if 0
+ exec nbdkit cc "$0" "$@"
+ #endif
+
+The script can be run as a command with additional nbdkit flags and
+plugin parameters, eg:
+
+ ./plugin.c -f -v
+ ./plugin.c -p 10000 --filter=cow
+ ./plugin.c param=1
+
+=head1 PARAMETERS
+
+The script name, or C<->, must appear as the first parameter.
+
+=over 4
+
+=item B<CC=>CC
+
+=item B<CFLAGS=">CFLAGSB<">
+
+=item B<EXTRA_CFLAGS=">EXTRA_CFLAGSB<">
+
+Override the compiler and flags.  See L</Compiler name and flags>
+above.
+
+=back
+
+All other parameters on the command line are passed to the plugin.
+
+=head1 EXAMPLE
+
+ $ nbdkit cc - <<'EOF'
+ #include <stdint.h>
+ #include <string.h>
+ #include <nbdkit-plugin.h>
+ 
+ char data[10*1024*1024];
+ 
+ #define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+ 
+ static void *
+ my_open (int readonly)
+ {
+   return NBDKIT_HANDLE_NOT_NEEDED;
+ }
+ 
+ static int64_t
+ my_get_size (void *handle)
+ {
+   return (int64_t) sizeof (data);
+ }
+ 
+ static int
+ my_pread (void *handle, void *buf,
+           uint32_t count, uint64_t offset,
+           uint32_t flags)
+ {
+   memcpy (buf, data+offset, count);
+   return 0;
+ }
+ 
+ static int
+ my_pwrite (void *handle, const void *buf,
+            uint32_t count, uint64_t offset,
+            uint32_t flags)
+ {
+   memcpy (data+offset, buf, count);
+   return 0;
+ }
+ 
+ static struct nbdkit_plugin plugin = {
+   .name              = "myplugin",
+   .open              = my_open,
+   .get_size          = my_get_size,
+   .pread             = my_pread,
+   .pwrite            = my_pwrite,
+ };
+ 
+ NBDKIT_REGISTER_PLUGIN(plugin)
+ EOF
+
+=head1 FILES
+
+=over 4
+
+=item F<$plugindir/nbdkit-cc-plugin.so>
+
+The plugin.
+
+Use C<nbdkit --dump-config> to find the location of C<$plugindir>.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-cc-plugin> first appeared in nbdkit 1.22.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<nbdkit-sh-plugin(3)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2020 Red Hat Inc.

plugins/eval/nbdkit-eval-plugin.pod
@@ -188,6 +188,7 @@ C<nbdkit-eval-plugin> first appeared in nbdkit 1.18.
 L<nbdkit(1)>,
 L<nbdkit-plugin(3)>,
 L<nbdkit-sh-plugin(1)>.
+L<nbdkit-cc-plugin(1)>.
 
 =head1 AUTHORS
 

plugins/sh/nbdkit-sh-plugin.pod
@@ -475,7 +475,8 @@ C<nbdkit-sh-plugin> first appeared in nbdkit 1.8.
 
 L<nbdkit(1)>,
 L<nbdkit-plugin(3)>,
-L<nbdkit-eval-plugin(1)>.
+L<nbdkit-eval-plugin(1)>,
+L<nbdkit-cc-plugin(1)>.
 
 =head1 AUTHORS
 

commit 1271945aa9144090f9c74c1915c4c1d431344df3

cc: Add -fPIC -shared to -DCFLAGS rather than in the plugin.
This fixes the output of ‘nbdkit cc --dump-plugin’ so it matches the
documentation, and it's slightly cleaner than doing this munging in
the cc plugin source code.

Fixes: commit a4339e3c3c17d8b95ddf0b20cf015de5078620ec

plugins/cc/Makefile.am
@@ -42,7 +42,7 @@ nbdkit_cc_plugin_la_SOURCES = \
 
 nbdkit_cc_plugin_la_CPPFLAGS = \
 	-DCC="\"$(CC)\"" \
-	-DCFLAGS="\"$(CFLAGS)\"" \
+	-DCFLAGS="\"$(CFLAGS) -fPIC -shared\"" \
 	-I$(top_srcdir)/common/utils \
 	-I$(top_srcdir)/include \
 	-I. \

plugins/cc/cc.c
@@ -53,7 +53,7 @@ static bool unlink_on_exit = false;
 
 /* C compiler and flags. */
 static const char *cc = CC;
-static const char *cflags = CFLAGS " -fPIC -shared";
+static const char *cflags = CFLAGS;
 static const char *extra_cflags;
 
 /* List of parameters for the subplugin. */