libguestfs/nbdkit

commit 4aa577e081e6b3d62287f354a73a7aabd4696c99

common/allocators: Implement new allocator=zstd.
Add a new allocator which uses zstd.  This is derived from the sparse
allocator, with the difference being that each page is compressed in
memory using the Zstandard fast compression library.

Creating ordinary filesystems filled with mostly text content gives me
excellent compression ratios (up to 10:1), so this should allow much
larger RAM disks.  Of course since this is based on the sparse array
work, it already supports huge virtual sizes.

Note this means that nbdkit-basic-plugins (downstream in Fedora) will
now depend on libzstd.  This is an extra dependency, but seems OK
since the library is small and doesn't depend on anything else except
glibc.  The alternative would be more awkward packaging - either
splitting nbdkit-memory-plugin and nbdkit-data-plugin into separate
subpackages, or somehow working out how to package the allocators as
plugin-plugins.

common/allocators/zstd.c
@@ -1,0 +1,616 @@
+/* nbdkit
+ * Copyright (C) 2017-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <pthread.h>
+
+#include <nbdkit-plugin.h>
+
+#include "cleanup.h"
+#include "iszero.h"
+#include "vector.h"
+
+#include "allocator.h"
+#include "allocator-internal.h"
+
+#ifdef HAVE_LIBZSTD
+
+#include <zstd.h>
+
+/* This is derived from the sparse array implementation - see
+ * common/allocators/sparse.c for details of how it works.
+ */
+#define PAGE_SIZE 32768
+#define L2_SIZE   4096
+
+struct l2_entry {
+  void *page;                   /* Pointer to compressed data. */
+  size_t zsize;                 /* Size of compressed data in this page. */
+};
+
+struct l1_entry {
+  uint64_t offset;              /* Virtual offset of this entry. */
+  struct l2_entry *l2_dir;      /* Pointer to L2 directory (L2_SIZE entries). */
+};
+
+DEFINE_VECTOR_TYPE(l1_dir, struct l1_entry);
+
+struct zstd_array {
+  struct allocator a;           /* Must come first. */
+  pthread_mutex_t lock;
+  l1_dir l1_dir;                /* L1 directory. */
+
+  /* Compression and decompression context.  If we ever get serious
+   * about making this allocator work well multi-threaded [at the
+   * moment the locking is too course-grained], then the zstd
+   * documentation recommends creating a context per thread.
+   */
+  ZSTD_CCtx *zcctx;
+  ZSTD_DCtx *zdctx;
+
+  /* Collect stats when we compress a page. */
+  uint64_t stats_uncompressed_bytes;
+  uint64_t stats_compressed_bytes;
+};
+
+/* Free L1 and/or L2 directories. */
+static void
+free_l2_dir (struct l2_entry *l2_dir)
+{
+  size_t i;
+
+  for (i = 0; i < L2_SIZE; ++i)
+    free (l2_dir[i].page);
+  free (l2_dir);
+}
+
+static void
+zstd_array_free (struct allocator *a)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  size_t i;
+
+  if (za) {
+    if (za->stats_compressed_bytes > 0)
+      nbdkit_debug ("zstd: compression ratio: %g : 1",
+                    (double) za->stats_uncompressed_bytes /
+                    za->stats_compressed_bytes);
+
+    ZSTD_freeCCtx (za->zcctx);
+    ZSTD_freeDCtx (za->zdctx);
+    for (i = 0; i < za->l1_dir.size; ++i)
+      free_l2_dir (za->l1_dir.ptr[i].l2_dir);
+    free (za->l1_dir.ptr);
+    pthread_mutex_destroy (&za->lock);
+    free (za);
+  }
+}
+
+static int
+zstd_array_set_size_hint (struct allocator *a, uint64_t size)
+{
+  /* Ignored. */
+  return 0;
+}
+
+/* Comparison function used when searching through the L1 directory. */
+static int
+compare_l1_offsets (const void *offsetp, const struct l1_entry *e)
+{
+  const uint64_t offset = *(uint64_t *)offsetp;
+
+  if (offset < e->offset) return -1;
+  if (offset >= e->offset + PAGE_SIZE*L2_SIZE) return 1;
+  return 0;
+}
+
+/* Insert an entry in the L1 directory, keeping it ordered by offset.
+ * This involves an expensive linear scan but should be very rare.
+ */
+static int
+insert_l1_entry (struct zstd_array *za, const struct l1_entry *entry)
+{
+  size_t i;
+
+  for (i = 0; i < za->l1_dir.size; ++i) {
+    if (entry->offset < za->l1_dir.ptr[i].offset) {
+      /* Insert new entry before i'th directory entry. */
+      if (l1_dir_insert (&za->l1_dir, *entry, i) == -1) {
+        nbdkit_error ("realloc: %m");
+        return -1;
+      }
+      if (za->a.debug)
+        nbdkit_debug ("%s: inserted new L1 entry for %" PRIu64
+                      " at l1_dir.ptr[%zu]",
+                      __func__, entry->offset, i);
+      return 0;
+    }
+
+    /* This should never happens since each entry in the the L1
+     * directory is supposed to be unique.
+     */
+    assert (entry->offset != za->l1_dir.ptr[i].offset);
+  }
+
+  /* Insert new entry at the end. */
+  if (l1_dir_append (&za->l1_dir, *entry) == -1) {
+    nbdkit_error ("realloc: %m");
+    return -1;
+  }
+  if (za->a.debug)
+    nbdkit_debug ("%s: inserted new L1 entry for %" PRIu64
+                  " at end of l1_dir", __func__, entry->offset);
+  return 0;
+}
+
+/* Look up a virtual offset.
+ *
+ * If the L2 page is mapped then this uncompresses the page into the
+ * caller's buffer (of size PAGE_SIZE), returning the address of the
+ * offset, the count of bytes to the end of the page, and a pointer to
+ * the L2 directory entry containing the page pointer.
+ *
+ * If the L2 page is not mapped this clears the caller's buffer, also
+ * returning the pointer.
+ *
+ * To read data you don't need to do anything else.
+ *
+ * To write data, after updating the buffer, you must subsequently
+ * call compress() below.
+ *
+ * This function cannot return an error.
+ */
+static void *
+lookup_decompress (struct zstd_array *za, uint64_t offset, void *buf,
+                   uint32_t *remaining, struct l2_entry **l2_entry)
+{
+  struct l1_entry *entry;
+  struct l2_entry *l2_dir;
+  uint64_t o;
+  void *page;
+  size_t n;
+
+  *remaining = PAGE_SIZE - (offset & (PAGE_SIZE-1));
+
+  /* Search the L1 directory. */
+  entry = l1_dir_search (&za->l1_dir, &offset, compare_l1_offsets);
+
+  if (za->a.debug) {
+    if (entry)
+      nbdkit_debug ("%s: search L1 dir: entry found: offset %" PRIu64,
+                    __func__, entry->offset);
+    else
+      nbdkit_debug ("%s: search L1 dir: no entry found", __func__);
+  }
+
+  if (entry) {
+    l2_dir = entry->l2_dir;
+
+    /* Which page in the L2 directory? */
+    o = (offset - entry->offset) / PAGE_SIZE;
+    if (l2_entry)
+      *l2_entry = &l2_dir[o];
+    page = l2_dir[o].page;
+
+    /* Decompress the page into the user buffer.  We assume this
+     * can never fail since the only pages we decompress are ones
+     * we have compressed.
+     */
+    if (page) {
+      n = ZSTD_decompressDCtx (za->zdctx, buf, PAGE_SIZE,
+                               page, l2_dir[o].zsize);
+      assert (n == PAGE_SIZE);
+    }
+    else
+      memset (buf, 0, PAGE_SIZE);
+
+    return buf + (offset & (PAGE_SIZE-1));
+  }
+
+  /* No L1 directory entry found. */
+  memset (buf, 0, PAGE_SIZE);
+  return buf + (offset & (PAGE_SIZE-1));
+}
+
+/* Compress a page back after modifying it.
+ *
+ * This replaces a L2 page with a new version compressed from the
+ * modified user buffer.
+ *
+ * It may fail, calling nbdkit_error and returning -1.
+ */
+static int
+compress (struct zstd_array *za, uint64_t offset, void *buf)
+{
+  struct l1_entry *entry;
+  struct l2_entry *l2_dir;
+  uint64_t o;
+  void *page;
+  struct l1_entry new_entry;
+  size_t n;
+
+ again:
+  /* Search the L1 directory. */
+  entry = l1_dir_search (&za->l1_dir, &offset, compare_l1_offsets);
+
+  if (za->a.debug) {
+    if (entry)
+      nbdkit_debug ("%s: search L1 dir: entry found: offset %" PRIu64,
+                    __func__, entry->offset);
+    else
+      nbdkit_debug ("%s: search L1 dir: no entry found", __func__);
+  }
+
+  if (entry) {
+    l2_dir = entry->l2_dir;
+
+    /* Which page in the L2 directory? */
+    o = (offset - entry->offset) / PAGE_SIZE;
+    free (l2_dir[o].page);
+    l2_dir[o].page = NULL;
+
+    /* Allocate a new page. */
+    n = ZSTD_compressBound (PAGE_SIZE);
+    page = malloc (n);
+    if (page == NULL) {
+      nbdkit_error ("malloc: %m");
+      return -1;
+    }
+    n = ZSTD_compressCCtx (za->zcctx, page, n,
+                           buf, PAGE_SIZE, ZSTD_CLEVEL_DEFAULT);
+    if (ZSTD_isError (n)) {
+      nbdkit_error ("ZSTD_compressCCtx: %s", ZSTD_getErrorName (n));
+      return -1;
+    }
+    page = realloc (page, n);
+    assert (page != NULL);
+    l2_dir[o].page = page;
+    l2_dir[o].zsize = n;
+    za->stats_uncompressed_bytes += PAGE_SIZE;
+    za->stats_compressed_bytes += n;
+    return 0;
+  }
+
+  /* No L1 directory entry, so we need to allocate a new L1 directory
+   * entry and insert it in the L1 directory, and allocate the L2
+   * directory with NULL page pointers.  Then we can repeat the above
+   * search to create the page.
+   */
+  new_entry.offset = offset & ~(PAGE_SIZE*L2_SIZE-1);
+  new_entry.l2_dir = calloc (L2_SIZE, sizeof (struct l2_entry));
+  if (new_entry.l2_dir == NULL) {
+    nbdkit_error ("calloc: %m");
+    return -1;
+  }
+  if (insert_l1_entry (za, &new_entry) == -1) {
+    free (new_entry.l2_dir);
+    return -1;
+  }
+  goto again;
+}
+
+static int
+zstd_array_read (struct allocator *a,
+                 void *buf, uint32_t count, uint64_t offset)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&za->lock);
+  CLEANUP_FREE void *tbuf = NULL;
+  uint32_t n;
+  void *p;
+
+  tbuf = malloc (PAGE_SIZE);
+  if (tbuf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za, offset, tbuf, &n, NULL);
+    if (n > count)
+      n = count;
+
+    memcpy (buf, p, n);
+
+    buf += n;
+    count -= n;
+    offset += n;
+  }
+
+  return 0;
+}
+
+static int
+zstd_array_write (struct allocator *a,
+                  const void *buf, uint32_t count, uint64_t offset)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&za->lock);
+  CLEANUP_FREE void *tbuf = NULL;
+  uint32_t n;
+  void *p;
+
+  tbuf = malloc (PAGE_SIZE);
+  if (tbuf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za, offset, tbuf, &n, NULL);
+
+    if (n > count)
+      n = count;
+    memcpy (p, buf, n);
+
+    if (compress (za, offset, tbuf) == -1)
+      return -1;
+
+    buf += n;
+    count -= n;
+    offset += n;
+  }
+
+  return 0;
+}
+
+static int zstd_array_zero (struct allocator *a,
+                            uint32_t count, uint64_t offset);
+
+static int
+zstd_array_fill (struct allocator *a, char c,
+                   uint32_t count, uint64_t offset)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  CLEANUP_FREE void *tbuf = NULL;
+  uint32_t n;
+  void *p;
+
+  if (c == 0) {
+    zstd_array_zero (a, count, offset);
+    return 0;
+  }
+
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&za->lock);
+
+  tbuf = malloc (PAGE_SIZE);
+  if (tbuf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za, offset, tbuf, &n, NULL);
+
+    if (n > count)
+      n = count;
+    memset (p, c, n);
+
+    if (compress (za, offset, tbuf) == -1)
+      return -1;
+
+    count -= n;
+    offset += n;
+  }
+
+  return 0;
+}
+
+static int
+zstd_array_zero (struct allocator *a, uint32_t count, uint64_t offset)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&za->lock);
+  CLEANUP_FREE void *tbuf = NULL;
+  uint32_t n;
+  void *p;
+  struct l2_entry *l2_entry;
+
+  tbuf = malloc (PAGE_SIZE);
+  if (tbuf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za, offset, tbuf, &n, &l2_entry);
+
+    if (n > count)
+      n = count;
+    memset (p, 0, n);
+
+    if (l2_entry->page) {
+      /* If the whole page is now zero, free it. */
+      if (n >= PAGE_SIZE || is_zero (l2_entry->page, PAGE_SIZE)) {
+        if (za->a.debug)
+          nbdkit_debug ("%s: freeing zero page at offset %" PRIu64,
+                        __func__, offset);
+        free (l2_entry->page);
+        l2_entry->page = NULL;
+      }
+      else {
+        if (compress (za, offset, tbuf) == -1)
+          return -1;
+      }
+    }
+
+    count -= n;
+    offset += n;
+  }
+
+  return 0;
+}
+
+static int
+zstd_array_blit (struct allocator *a1,
+                 struct allocator *a2,
+                 uint32_t count,
+                 uint64_t offset1, uint64_t offset2)
+{
+  /* XXX Does not lock the target.  As it happens we only call this
+   * from non-threaded code currently.
+   */
+  struct zstd_array *za2 = (struct zstd_array *) a2;
+  CLEANUP_FREE void *tbuf = NULL;
+  uint32_t n;
+  void *p;
+
+  tbuf = malloc (PAGE_SIZE);
+  if (tbuf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za2, offset2, tbuf, &n, NULL);
+
+    if (n > count)
+      n = count;
+
+    /* Read the source allocator (a1) directly to p which points into
+     * the right place in za2.
+     */
+    if (a1->read (a1, p, n, offset1) == -1)
+      return -1;
+
+    if (compress (za2, offset2, tbuf) == -1)
+      return -1;
+
+    count -= n;
+    offset1 += n;
+    offset2 += n;
+  }
+
+  return 0;
+}
+
+static int
+zstd_array_extents (struct allocator *a,
+                      uint32_t count, uint64_t offset,
+                      struct nbdkit_extents *extents)
+{
+  struct zstd_array *za = (struct zstd_array *) a;
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&za->lock);
+  CLEANUP_FREE void *buf = NULL;
+  uint32_t n, type;
+  void *p;
+  struct l2_entry *l2_entry;
+
+  buf = malloc (PAGE_SIZE);
+  if (buf == NULL) {
+    nbdkit_error ("malloc");
+    return -1;
+  }
+
+  while (count > 0) {
+    p = lookup_decompress (za, offset, buf, &n, &l2_entry);
+
+    /* Work out the type of this extent. */
+    if (l2_entry->page == NULL)
+      /* No backing page, so it's a hole. */
+      type = NBDKIT_EXTENT_HOLE | NBDKIT_EXTENT_ZERO;
+    else {
+      if (is_zero (p, n))
+        /* A backing page and it's all zero, it's a zero extent. */
+        type = NBDKIT_EXTENT_ZERO;
+      else
+        /* Normal allocated data. */
+        type = 0;
+    }
+    if (nbdkit_add_extent (extents, offset, n, type) == -1)
+      return -1;
+
+    if (n > count)
+      n = count;
+
+    count -= n;
+    offset += n;
+  }
+
+  return 0;
+}
+
+static struct allocator functions = {
+  .free = zstd_array_free,
+  .set_size_hint = zstd_array_set_size_hint,
+  .read = zstd_array_read,
+  .write = zstd_array_write,
+  .fill = zstd_array_fill,
+  .zero = zstd_array_zero,
+  .blit = zstd_array_blit,
+  .extents = zstd_array_extents,
+};
+
+struct allocator *
+create_zstd_array (void)
+{
+  struct zstd_array *za;
+
+  za = calloc (1, sizeof *za);
+  if (za == NULL) {
+    nbdkit_error ("calloc: %m");
+    return NULL;
+  }
+  za->a = functions;
+
+  pthread_mutex_init (&za->lock, NULL);
+
+  za->zcctx = ZSTD_createCCtx ();
+  if (za->zcctx == NULL) {
+    nbdkit_error ("ZSTD_createCCtx: %m");
+    free (za);
+    return NULL;
+  }
+  za->zdctx = ZSTD_createDCtx ();
+  if (za->zdctx == NULL) {
+    nbdkit_error ("ZSTD_createDCtx: %m");
+    ZSTD_freeCCtx (za->zcctx);
+    free (za);
+    return NULL;
+  }
+
+  za->stats_uncompressed_bytes = za->stats_compressed_bytes = 0;
+
+  return (struct allocator *) za;
+}
+
+#endif /* HAVE_LIBZSTD */

.gitignore
@@ -126,6 +126,7 @@ plugins/*/*.3
 /tests/test-lua
 /tests/test-memory
 /tests/test-memory-allocator-malloc
+/tests/test-memory-allocator-zstd
 /tests/test-newstyle
 /tests/test-nbd
 /tests/test-null

README
@@ -73,6 +73,10 @@ For the xz filter:
 
  - liblzma
 
+For the memory plugin with allocator=zstd:
+
+ - zstd
+
 For the curl (HTTP/FTP) plugin:
 
  - libcurl

common/allocators/Makefile.am
@@ -39,6 +39,7 @@ liballocators_la_SOURCES = \
 	allocator-internal.h \
 	malloc.c \
         sparse.c \
+	zstd.c \
 	$(NULL)
 liballocators_la_CPPFLAGS = \
 	-I$(top_srcdir)/include \
@@ -46,3 +47,8 @@ liballocators_la_CPPFLAGS = \
 	-I$(top_srcdir)/common/utils \
 	$(NULL)
 liballocators_la_CFLAGS = $(WARNINGS_CFLAGS)
+liballocators_la_LIBADD =
+if HAVE_LIBZSTD
+liballocators_la_CFLAGS += $(LIBZSTD_CFLAGS)
+liballocators_la_LIBADD += $(LIBZSTD_LIBS)
+endif

common/allocators/allocator-internal.h
@@ -38,4 +38,8 @@
 extern struct allocator *create_malloc (bool use_mlock);
 extern struct allocator *create_sparse_array (void);
 
+#ifdef HAVE_LIBZSTD
+extern struct allocator *create_zstd_array (void);
+#endif
+
 #endif /* NBDKIT_ALLOCATOR_INTERNAL_H */

common/allocators/allocator.c
@@ -49,11 +49,22 @@ create_allocator (const char *type, bool debug)
 
   if (strcmp (type, "sparse") == 0)
     ret = create_sparse_array ();
+
   else if (strcmp (type, "malloc") == 0)
     ret = create_malloc (false);
   /* XXX In the end we want to split and parse these subparameters. */
   else if (strcmp (type, "malloc,mlock=true") == 0)
     ret = create_malloc (true);
+
+  else if (strcmp (type, "zstd") == 0) {
+#ifdef HAVE_LIBZSTD
+    ret = create_zstd_array ();
+#else
+    nbdkit_error ("allocator=zstd is not supported in this build of nbdkit");
+    ret = NULL;
+#endif
+  }
+
   else {
     nbdkit_error ("unknown allocator \"%s\"", type);
     ret = NULL;

common/allocators/allocator.h
@@ -60,7 +60,7 @@ struct allocator {
   /* Set the size hint.  The size hint is used in various ways by some
    * allocators, and ignored by others.
    *
-   * The sparse array allocator ignores the size hint.
+   * The sparse and zstd array allocators ignore the size hint.
    *
    * The malloc allocator uses it to reserve the expected size of disk
    * in memory (especially important if using mlock so we fail during

configure.ac
@@ -944,6 +944,22 @@ AS_IF([test "$with_liblzma" != "no"],[
 ])
 AM_CONDITIONAL([HAVE_LIBLZMA],[test "x$LIBLZMA_LIBS" != "x"])
 
+dnl Check for zstd (only if you want to compile allocator=zstd).
+AC_ARG_WITH([libzstd],
+    [AS_HELP_STRING([--without-libzstd],
+                    [disable allocator=zstd @<:@default=check@:>@])],
+    [],
+    [with_libzstd=check])
+AS_IF([test "$with_libzstd" != "no"],[
+    PKG_CHECK_MODULES([LIBZSTD], [libzstd],[
+        AC_SUBST([LIBZSTD_CFLAGS])
+        AC_SUBST([LIBZSTD_LIBS])
+        AC_DEFINE([HAVE_LIBZSTD],[1],[libzstd found at compile time.])
+    ],
+    [AC_MSG_WARN([libzstd not found, allocator=zstd will be disabled])])
+])
+AM_CONDITIONAL([HAVE_LIBZSTD],[test "x$LIBZSTD_LIBS" != "x"])
+
 dnl Check for libguestfs (only for the guestfs plugin and the test suite).
 AC_ARG_WITH([libguestfs],
     [AS_HELP_STRING([--without-libguestfs],
@@ -1212,6 +1228,12 @@ feature "ext2 ................................... " \
         test "x$HAVE_EXT2_TRUE" = "x"
 feature "xz ..................................... " \
         test "x$HAVE_LIBLZMA_TRUE" = "x"
+
+echo
+echo "Other optional features:"
+echo
+feature "allocator=zstd ......................... " \
+        test "x$HAVE_LIBZSTD_TRUE" = "x"
 
 echo
 echo "If any optional component is configured ‘no’ when you expected ‘yes’"

commit 398bd85208aaebb72a845552d0037125bd2cff52

common/allocators: Print errno if calloc or malloc fails.
Fixes: commit 4aa577e081e6b3d62287f354a73a7aabd4696c99
See-Also: commit f129c893d52e496bc99dd143390becb2c5f6faae

common/allocators/malloc.c
@@ -259,7 +259,7 @@ create_malloc (bool use_mlock)
 
   ma = calloc (1, sizeof *ma);
   if (ma == NULL) {
-    nbdkit_error ("calloc");
+    nbdkit_error ("calloc: %m");
     return NULL;
   }
   ma->a = functions;

common/allocators/zstd.c
@@ -340,7 +340,7 @@ zstd_array_read (struct allocator *a,
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 
@@ -371,7 +371,7 @@ zstd_array_write (struct allocator *a,
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 
@@ -414,7 +414,7 @@ zstd_array_fill (struct allocator *a, char c,
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 
@@ -447,7 +447,7 @@ zstd_array_zero (struct allocator *a, uint32_t count, uint64_t offset)
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 
@@ -496,7 +496,7 @@ zstd_array_blit (struct allocator *a1,
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 
@@ -537,7 +537,7 @@ zstd_array_extents (struct allocator *a,
 
   buf = malloc (PAGE_SIZE);
   if (buf == NULL) {
-    nbdkit_error ("malloc");
+    nbdkit_error ("malloc: %m");
     return -1;
   }
 