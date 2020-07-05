libguestfs/nbdkit

commit 87ea5853a8fc0ec8773d25d740722c60e5b9d6c2

common/allocators: Implement new allocator=malloc.
This implements a simple malloc-based allocator, designed to be
simpler and faster than the sparse array allocator, but with the
obvious shortcoming that the whole RAM disk must be direct-mapped into
memory.  There is also the option of using mlock(2) to lock the memory
in RAM.

common/allocators/allocator-internal.h
@@ -33,6 +33,9 @@
 #ifndef NBDKIT_ALLOCATOR_INTERNAL_H
 #define NBDKIT_ALLOCATOR_INTERNAL_H
 
+#include <stdbool.h>
+
+extern struct allocator *create_malloc (bool use_mlock);
 extern struct allocator *create_sparse_array (void);
 
 #endif /* NBDKIT_ALLOCATOR_INTERNAL_H */

common/allocators/allocator.c
@@ -49,6 +49,11 @@ create_allocator (const char *type, bool debug)
 
   if (strcmp (type, "sparse") == 0)
     ret = create_sparse_array ();
+  else if (strcmp (type, "malloc") == 0)
+    ret = create_malloc (false);
+  /* XXX In the end we want to split and parse these subparameters. */
+  else if (strcmp (type, "malloc,mlock=true") == 0)
+    ret = create_malloc (true);
   else {
     nbdkit_error ("unknown allocator \"%s\"", type);
     ret = NULL;

common/allocators/allocator.h
@@ -62,6 +62,10 @@ struct allocator {
    *
    * The sparse array allocator ignores the size hint.
    *
+   * The malloc allocator uses it to reserve the expected size of disk
+   * in memory (especially important if using mlock so we fail during
+   * start-up if there's not enough RAM).
+   *
    * Note this does not set or enforce the virtual size of the disk,
    * nor does it implement bounds checking.
    */

common/allocators/malloc.c
@@ -1,0 +1,267 @@
+/* nbdkit
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#include <pthread.h>
+
+#include <nbdkit-plugin.h>
+
+#include "cleanup.h"
+#include "vector.h"
+
+#include "allocator.h"
+#include "allocator-internal.h"
+
+/* This allocator implements a direct-mapped non-sparse RAM disk using
+ * malloc, with optional mlock.
+ */
+
+DEFINE_VECTOR_TYPE(bytearray, uint8_t);
+
+struct m_alloc {
+  struct allocator a;           /* Must come first. */
+  bool use_mlock;
+
+  /* Byte array (vector) implementing the direct-mapped disk.  Note we
+   * don't use the .size field.  Accesses must be protected by the
+   * lock since writes may try to extend the array.
+   */
+  pthread_rwlock_t lock;
+  bytearray ba;
+};
+
+static void
+m_alloc_free (struct allocator *a)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+
+  if (ma) {
+    free (ma->ba.ptr);
+    pthread_rwlock_destroy (&ma->lock);
+    free (ma);
+  }
+}
+
+/* Extend the underlying bytearray if needed.  mlock if requested. */
+static int
+extend (struct m_alloc *ma, uint64_t new_size)
+{
+  ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&ma->lock);
+  size_t old_size, n;
+
+  if (ma->ba.alloc < new_size) {
+    old_size = ma->ba.alloc;
+    n = new_size - ma->ba.alloc;
+
+    /* Since the memory might be moved by realloc, we must unlock the
+     * original array.
+     */
+    if (ma->use_mlock)
+      munlock (ma->ba.ptr, ma->ba.alloc);
+
+    if (bytearray_reserve (&ma->ba, n) == -1) {
+      nbdkit_error ("realloc: %m");
+      return -1;
+    }
+
+    /* Hints to the kernel.  Doesn't matter if these fail.
+     * XXX Consider in future: MADV_MERGEABLE (tunable)
+     */
+#ifdef MADV_RANDOM
+    madvise (ma->ba.ptr, ma->ba.alloc, MADV_RANDOM);
+#endif
+#ifdef MADV_WILLNEED
+    madvise (ma->ba.ptr, ma->ba.alloc, MADV_WILLNEED);
+#endif
+#ifdef MADV_DONTFORK
+    madvise (ma->ba.ptr, ma->ba.alloc, MADV_DONTFORK);
+#endif
+#ifdef MADV_HUGEPAGE
+    madvise (ma->ba.ptr, ma->ba.alloc, MADV_HUGEPAGE);
+#endif
+#ifdef MADV_DONTDUMP
+    madvise (ma->ba.ptr, ma->ba.alloc, MADV_DONTDUMP);
+#endif
+
+    /* Initialize the newly allocated memory to 0. */
+    memset (ma->ba.ptr + old_size, 0, n);
+
+    if (ma->use_mlock) {
+      if (mlock (ma->ba.ptr, ma->ba.alloc) == -1) {
+        nbdkit_error ("allocator=malloc: mlock: %m");
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+static int
+m_alloc_set_size_hint (struct allocator *a, uint64_t size_hint)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+  return extend (ma, size_hint);
+}
+
+static void
+m_alloc_read (struct allocator *a, void *buf,
+              uint32_t count, uint64_t offset)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma->lock);
+
+  /* Avoid reading beyond the end of the allocated array.  Return
+   * zeroes for that part.
+   */
+  if (offset >= ma->ba.alloc)
+    memset (buf, 0, count);
+  else if (offset + count > ma->ba.alloc) {
+    memcpy (buf, ma->ba.ptr + offset, ma->ba.alloc - offset);
+    memset (buf + ma->ba.alloc - offset, 0, offset + count - ma->ba.alloc);
+  }
+  else
+    memcpy (buf, ma->ba.ptr + offset, count);
+}
+
+static int
+m_alloc_write (struct allocator *a, const void *buf,
+               uint32_t count, uint64_t offset)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+
+  if (extend (ma, offset+count) == -1)
+    return -1;
+
+  /* This is correct: Even though we are writing, we only need to
+   * acquire the read lock here.  The write lock applies to changing
+   * the metadata and it was acquired if we called extend().
+   */
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma->lock);
+  memcpy (ma->ba.ptr + offset, buf, count);
+  return 0;
+}
+
+static int
+m_alloc_fill (struct allocator *a, char c, uint32_t count, uint64_t offset)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+
+  if (extend (ma, offset+count) == -1)
+    return -1;
+
+  /* See comment in m_alloc_write. */
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma->lock);
+  memset (ma->ba.ptr + offset, 0, count);
+  return 0;
+}
+
+static void
+m_alloc_zero (struct allocator *a, uint32_t count, uint64_t offset)
+{
+  struct m_alloc *ma = (struct m_alloc *) a;
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma->lock);
+
+  /* Try to avoid extending the array, since the unallocated part
+   * always reads as zero.
+   */
+  if (offset < ma->ba.alloc) {
+    if (offset + count > ma->ba.alloc)
+      memset (ma->ba.ptr + offset, 0, ma->ba.alloc - offset);
+    else
+      memset (ma->ba.ptr + offset, 0, count);
+  }
+}
+
+static int
+m_alloc_blit (struct allocator *a1, struct allocator *a2,
+              uint32_t count, uint64_t offset1, uint64_t offset2)
+{
+  struct m_alloc *ma2 = (struct m_alloc *) a2;
+
+  if (extend (ma2, offset2+count) == -1)
+    return -1;
+
+  /* See comment in m_alloc_write. */
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma2->lock);
+  a1->read (a1, ma2->ba.ptr + offset2, count, offset1);
+  return 0;
+}
+
+static int
+m_alloc_extents (struct allocator *a,
+                 uint32_t count, uint64_t offset,
+                 struct nbdkit_extents *extents)
+{
+  /* Always fully allocated.  XXX In theory we could detect zeroes
+   * quite quickly and return that information, allowing the client to
+   * avoid reads.  However we'd probably want to store a bitmap of
+   * which sectors we are known to have written to, and that
+   * complicates the implementation quite a lot.
+   */
+  return nbdkit_add_extent (extents, offset, count, 0);
+}
+
+static struct allocator functions = {
+  .free = m_alloc_free,
+  .set_size_hint = m_alloc_set_size_hint,
+  .read = m_alloc_read,
+  .write = m_alloc_write,
+  .fill = m_alloc_fill,
+  .zero = m_alloc_zero,
+  .blit = m_alloc_blit,
+  .extents = m_alloc_extents,
+};
+
+struct allocator *
+create_malloc (bool use_mlock)
+{
+  struct m_alloc *ma;
+
+  ma = calloc (1, sizeof *ma);
+  if (ma == NULL) {
+    nbdkit_error ("calloc");
+    return NULL;
+  }
+  ma->a = functions;
+  ma->use_mlock = use_mlock;
+  pthread_rwlock_init (&ma->lock, NULL);
+  ma->ba = (bytearray) empty_vector;
+  return (struct allocator *) ma;
+}

.gitignore
@@ -125,6 +125,7 @@ plugins/*/*.3
 /tests/test-layers
 /tests/test-lua
 /tests/test-memory
+/tests/test-memory-allocator-malloc
 /tests/test-newstyle
 /tests/test-nbd
 /tests/test-null

plugins/data/nbdkit-data-plugin.pod
@@ -185,6 +185,8 @@ of the C<data>, C<raw> or C<base64> parameter.
 
 =item B<allocator=sparse>
 
+=item B<allocator=malloc>[,B<mlock=true>]
+
 (nbdkit E<ge> 1.22)
 
 Select the backend allocation strategy.  See

plugins/memory/nbdkit-memory-plugin.pod
@@ -56,6 +56,8 @@ See L<nbdkit(1)/Magic parameters>.
 
 =item B<allocator=sparse>
 
+=item B<allocator=malloc>[,B<mlock=true>]
+
 (nbdkit E<ge> 1.22)
 
 Select the backend allocation strategy.  See L</ALLOCATORS> below.
@@ -113,6 +115,22 @@ maximum supported by nbdkit (S<2⁶³-1 bytes>).
 This is the default, and was the only allocator available before
 S<nbdkit 1.22>.
 
+=item B<allocator=malloc>
+
+=item B<allocator=malloc,mlock=true>
+
+The disk image is stored directly in memory allocated using
+L<malloc(3)> on the heap.  No sparseness is possible: you must have
+enough memory for the whole disk.  Very large virtual sizes will
+usually fail.  However this can be faster because the implementation
+is simpler and the locking strategy allows more concurrency.
+
+If C<mlock=true> is added then additionally the array is locked into
+RAM using L<mlock(2)> (so it should never be swapped out).  This
+usually requires you to adjust the L<ulimit(1)> associated with the
+process and on some operating systems may require you to run nbdkit as
+root.  (See also the L<nbdkit(1)> I<--swap> option).
+
 =back
 
 =head1 FILES
@@ -139,7 +157,9 @@ L<nbdkit-loop(1)>,
 L<nbdkit-data-plugin(1)>,
 L<nbdkit-file-plugin(1)>,
 L<nbdkit-info-plugin(1)>,
-L<nbdkit-tmpdisk-plugin(1)>.
+L<nbdkit-tmpdisk-plugin(1)>,
+L<mlock(2)>,
+L<malloc(3)>.
 
 =head1 AUTHORS
 

tests/Makefile.am
@@ -504,6 +504,7 @@ TESTS += \
 	test-data-extents.sh \
 	test-data-file.sh \
 	test-data-nest.sh \
+	test-data-nest-malloc.sh \
 	test-data-raw.sh \
 	$(NULL)
 EXTRA_DIST += \
@@ -512,6 +513,7 @@ EXTRA_DIST += \
 	test-data-extents.sh \
 	test-data-file.sh \
 	test-data-nest.sh \
+	test-data-nest-malloc.sh \
 	test-data-raw.sh \
 	$(NULL)
 
@@ -635,12 +637,19 @@ EXTRA_DIST += \
 	$(NULL)
 
 # memory plugin test.
-LIBGUESTFS_TESTS += test-memory
+LIBGUESTFS_TESTS += \
+	test-memory \
+	test-memory-allocator-malloc \
+	$(NULL)
 TESTS += \
+	test-memory-allocator-malloc.sh \
+	test-memory-allocator-malloc-mlock.sh \
 	test-memory-largest.sh \
 	test-memory-largest-for-qemu.sh \
 	$(NULL)
 EXTRA_DIST += \
+	test-memory-allocator-malloc.sh \
+	test-memory-allocator-malloc-mlock.sh \
 	test-memory-largest.sh \
 	test-memory-largest-for-qemu.sh \
 	$(NULL)
@@ -648,6 +657,10 @@ EXTRA_DIST += \
 test_memory_SOURCES = test-memory.c test.h
 test_memory_CFLAGS = $(WARNINGS_CFLAGS) $(LIBGUESTFS_CFLAGS)
 test_memory_LDADD = libtest.la $(LIBGUESTFS_LIBS)
+
+test_memory_allocator_malloc_SOURCES = test-memory-allocator-malloc.c test.h
+test_memory_allocator_malloc_CFLAGS = $(WARNINGS_CFLAGS) $(LIBGUESTFS_CFLAGS)
+test_memory_allocator_malloc_LDADD = libtest.la $(LIBGUESTFS_LIBS)
 
 if HAVE_LIBNBD
 # nbd plugin test.

commit a6c5ad2529f94656d90d10b8754a1199bdfecfae

common: allocators: Proper code for handling allocator parameters.
Fixes: commit 87ea5853a8fc0ec8773d25d740722c60e5b9d6c2

common/allocators/allocator-internal.h
@@ -35,11 +35,16 @@
 
 #include <stdbool.h>
 
-extern struct allocator *create_malloc (bool use_mlock);
-extern struct allocator *create_sparse_array (void);
+#include "vector.h"
+
+struct key_value { char *key, *value; };
+DEFINE_VECTOR_TYPE(parameters, struct key_value);
+
+extern struct allocator *create_malloc (const parameters *);
+extern struct allocator *create_sparse_array (const parameters *);
 
 #ifdef HAVE_LIBZSTD
-extern struct allocator *create_zstd_array (void);
+extern struct allocator *create_zstd_array (const parameters *);
 #endif
 
 #endif /* NBDKIT_ALLOCATOR_INTERNAL_H */

common/allocators/allocator.c
@@ -42,38 +42,111 @@
 #include "allocator.h"
 #include "allocator-internal.h"
 
+static void
+free_key_value (struct key_value kv)
+{
+  free (kv.key);
+  free (kv.value);
+}
+
+static void
+free_parameters (parameters *params)
+{
+  parameters_iter (params, free_key_value);
+  free (params->ptr);
+}
+
+/* The type may be followed by parameters "type,key=value[,...]" */
+static int
+parse_parameters (const char *type, size_t *type_len, parameters *params)
+{
+  size_t i, j, len;
+
+  *type_len = strcspn (type, ",");
+
+  nbdkit_debug ("allocator: %*s", (int) *type_len, type);
+
+  /* Split the parameters. */
+  for (i = *type_len; type[i] == ',';) {
+    struct key_value kv;
+
+    i++;
+    len = strcspn (&type[i], ",");
+    if (len == 0) {
+      i++;
+      continue;
+    }
+
+    j = strcspn (&type[i], "=");
+    if (j == 0) {
+      nbdkit_error ("invalid allocator parameter");
+      free_parameters (params);
+      return -1;
+    }
+    if (j < len) {
+      kv.key = strndup (&type[i], j);
+      kv.value = strndup (&type[i+j+1], len-j-1);
+    }
+    else {
+      kv.key = strndup (&type[i], len);
+      kv.value = strdup ("1");
+    }
+    if (kv.key == NULL || kv.value == NULL) {
+      nbdkit_error ("strdup: %m");
+      free (kv.key);
+      free (kv.value);
+      free_parameters (params);
+      return -1;
+    }
+
+    nbdkit_debug ("allocator parameter: %s=%s", kv.key, kv.value);
+    if (parameters_append (params, kv) == -1) {
+      nbdkit_error ("realloc: %m");
+      free_parameters (params);
+      return -1;
+    }
+    i += len;
+  }
+
+  return 0;
+}
+
 struct allocator *
 create_allocator (const char *type, bool debug)
 {
-  struct allocator *ret;
+  struct allocator *ret = NULL;
+  parameters params = empty_vector;
+  size_t type_len;
 
-  if (strcmp (type, "sparse") == 0)
-    ret = create_sparse_array ();
+  if (parse_parameters (type, &type_len, &params) == -1)
+    return NULL;
 
-  else if (strcmp (type, "malloc") == 0)
-    ret = create_malloc (false);
-  /* XXX In the end we want to split and parse these subparameters. */
-  else if (strcmp (type, "malloc,mlock=true") == 0)
-    ret = create_malloc (true);
+  if (strncmp (type, "sparse", type_len) == 0) {
+    ret = create_sparse_array (&params);
+    if (ret) ret->type = "sparse";
+  }
 
-  else if (strcmp (type, "zstd") == 0) {
+  else if (strncmp (type, "malloc", type_len) == 0) {
+    ret = create_malloc (&params);
+    if (ret) ret->type = "malloc";
+  }
+
+  else if (strncmp (type, "zstd", type_len) == 0) {
 #ifdef HAVE_LIBZSTD
-    ret = create_zstd_array ();
+    ret = create_zstd_array (&params);
+    if (ret) ret->type = "zstd";
 #else
     nbdkit_error ("allocator=zstd is not supported in this build of nbdkit");
-    ret = NULL;
 #endif
   }
 
-  else {
+  else
     nbdkit_error ("unknown allocator \"%s\"", type);
-    ret = NULL;
-  }
 
-  if (ret) {
-    ret->type = type;
-    ret->debug = debug;
-  }
+  /* Free the parameters allocated above. */
+  free_parameters (&params);
+
+  if (ret) ret->debug = debug;
   return ret;
 }
 

common/allocators/allocator.h
@@ -48,7 +48,9 @@
 struct nbdkit_extents;
 
 struct allocator {
-  /* Type string passed to create_allocator. */
+  /* Allocator type (eg. "sparse").
+   * This does not include the parameters.
+   */
   const char *type;
 
   /* Debug flag. */

common/allocators/malloc.c
@@ -220,7 +220,7 @@ m_alloc_blit (struct allocator *a1, struct allocator *a2,
   struct m_alloc *ma2 = (struct m_alloc *) a2;
 
   assert (a1 != a2);
-  assert (strncmp (a2->type, "malloc", 6) == 0);
+  assert (strcmp (a2->type, "malloc") == 0);
 
   if (extend (ma2, offset2+count) == -1)
     return -1;
@@ -256,9 +256,25 @@ static struct allocator functions = {
 };
 
 struct allocator *
-create_malloc (bool use_mlock)
+create_malloc (const parameters *params)
 {
   struct m_alloc *ma;
+  bool use_mlock = false;
+  size_t i;
+
+  /* Parse the optional mlock=true|false parameter. */
+  for (i = 0; i < params->size; ++i) {
+    if (strcmp (params->ptr[i].key, "mlock") == 0) {
+      int r = nbdkit_parse_bool (params->ptr[i].value);
+      if (r == -1) return NULL;
+      use_mlock = r;
+    }
+    else {
+      nbdkit_error ("allocator=malloc: unknown parameter %s",
+                    params->ptr[i].key);
+      return NULL;
+    }
+  }
 
   ma = calloc (1, sizeof *ma);
   if (ma == NULL) {

common/allocators/sparse.c
@@ -503,9 +503,14 @@ static struct allocator functions = {
 };
 
 struct allocator *
-create_sparse_array (void)
+create_sparse_array (const parameters *params)
 {
   struct sparse_array *sa;
+
+  if (params->size > 0) {
+    nbdkit_error ("allocator=sparse does not take extra parameters");
+    return NULL;
+  }
 
   sa = calloc (1, sizeof *sa);
   if (sa == NULL) {

common/allocators/zstd.c
@@ -605,9 +605,14 @@ static struct allocator functions = {
 };
 
 struct allocator *
-create_zstd_array (void)
+create_zstd_array (const parameters *params)
 {
   struct zstd_array *za;
+
+  if (params->size > 0) {
+    nbdkit_error ("allocator=zstd does not take extra parameters");
+    return NULL;
+  }
 
   za = calloc (1, sizeof *za);
   if (za == NULL) {