libguestfs/nbdkit

commit 5c3ed04ccbac80e7c934405a4c6310123a5ad649

nbd: Re-check nbd_aio_get_direction after poll.
As per the libnbd documentation, you must check nbd_aio_get_direction
after poll returns.

http://libguestfs.org/libnbd.3.html#Notifying-libnbd-when-an-event-happens

I also added parentheses around ‘&’ operators, and error checking.

plugins/nbd/nbd.c
@@ -314,6 +314,7 @@ nbdplug_reader (void *handle)
   nbdkit_debug ("nbd: started reader thread");
 
   while (!nbd_aio_is_dead (h->nbd) && !nbd_aio_is_closed (h->nbd)) {
+    int r;
     struct pollfd fds[2] = {
       [0].fd = nbd_aio_get_fd (h->nbd),
       [1].fd = h->fds[0],
@@ -332,10 +333,16 @@ nbdplug_reader (void *handle)
       break;
     }
 
-    if (dir & LIBNBD_AIO_DIRECTION_READ && fds[0].revents & POLLIN)
-      nbd_aio_notify_read (h->nbd);
-    else if (dir & LIBNBD_AIO_DIRECTION_WRITE && fds[0].revents & POLLOUT)
-      nbd_aio_notify_write (h->nbd);
+    dir = nbd_aio_get_direction (h->nbd);
+
+    if ((dir & LIBNBD_AIO_DIRECTION_READ) && (fds[0].revents & POLLIN))
+      r = nbd_aio_notify_read (h->nbd);
+    else if ((dir & LIBNBD_AIO_DIRECTION_WRITE) && (fds[0].revents & POLLOUT))
+      r = nbd_aio_notify_write (h->nbd);
+    if (r == -1) {
+      nbdkit_error ("%s", nbd_get_error ());
+      break;
+    }
 
     /* Check if we were kicked because a command was started */
     if (fds[1].revents & POLLIN) {

commit 59b14bc846baa5d0e804205f2755e79dceab8aa5

nbd: Initialize variable in case we don't call nbd_aio_notify_*.
It's possible that we check the value of r without ever setting it.
This error was found by valgrind:

==2362943== Thread 3:
==2362943== Conditional jump or move depends on uninitialised value(s)
==2362943==    at 0x8C70262: nbdplug_reader (nbd.c:388)
==2362943==    by 0x8EB8478: start_thread (in /usr/lib64/libpthread-2.31.9000.so)
==2362943==    by 0x8FD2B52: clone (in /usr/lib64/libc-2.31.9000.so)

Fixes: commit 5c3ed04ccbac80e7c934405a4c6310123a5ad649

plugins/nbd/nbd.c
@@ -381,6 +381,7 @@ nbdplug_reader (void *handle)
 
     dir = nbd_aio_get_direction (h->nbd);
 
+    r = 0;
     if ((dir & LIBNBD_AIO_DIRECTION_READ) && (fds[0].revents & POLLIN))
       r = nbd_aio_notify_read (h->nbd);
     else if ((dir & LIBNBD_AIO_DIRECTION_WRITE) && (fds[0].revents & POLLOUT))