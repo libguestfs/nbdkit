libguestfs/nbdkit

commit bae4a28d1a177f60a689ca663a7f99a2d0a577f1

New filter: swab (swap bytes)
Mostly useful for Atari Falcon formated harddisks.

filters/swab/swab.c
@@ -1,0 +1,162 @@
+/* nbdkit
+ * Copyright (C) 2020 François Revol.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include <nbdkit-filter.h>
+
+#include "byte-swapping.h"
+#include "isaligned.h"
+#include "cleanup.h"
+
+/* The request must be aligned.
+ * XXX We could lift this restriction with more work.
+ */
+static bool
+is_aligned (uint32_t count, uint64_t offset)
+{
+  if (!IS_ALIGNED (count, 2) || !IS_ALIGNED (offset, 2)) {
+    nbdkit_error ("swab: requests to this filter must be aligned");
+    return false;
+  }
+  return true;
+}
+
+/* Read data. */
+static int
+swab_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
+            void *handle, void *buf, uint32_t count, uint64_t offset,
+            uint32_t flags, int *err)
+{
+  size_t i;
+  uint16_t *p;
+  int r;
+
+  if (!is_aligned (count, offset)) return -1;
+
+  r = next_ops->pread (nxdata, buf, count, offset, flags, err);
+  if (r == -1)
+    return -1;
+
+  /* for reads we can do it in-place */
+  for (i = 0, p = (uint16_t *)buf; i < count; i += 2, p++)
+    *p = bswap_16 (*p);
+
+  return r;
+}
+
+/* Write data. */
+static int
+swab_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
+             void *handle, const void *buf, uint32_t count, uint64_t offset,
+             uint32_t flags, int *err)
+{
+  CLEANUP_FREE uint16_t *block = NULL;
+  size_t i;
+  uint16_t *p = (uint16_t *)buf;
+  uint16_t *q;
+
+  if (!is_aligned (count, offset)) return -1;
+
+  block = malloc (count);
+  if (block == NULL) {
+    *err = errno;
+    nbdkit_error ("malloc: %m");
+    return -1;
+  }
+
+  for (i = 0, q = block; i < count; i += 2)
+    *q++ = bswap_16 (*p++);
+
+  return next_ops->pwrite (nxdata, block, count, offset, flags, err);
+}
+
+/* Trim data. */
+static int
+swab_trim (struct nbdkit_next_ops *next_ops, void *nxdata,
+           void *handle, uint32_t count, uint64_t offset, uint32_t flags,
+           int *err)
+{
+  if (!is_aligned (count, offset)) return -1;
+  return next_ops->trim (nxdata, count, offset, flags, err);
+}
+
+/* Zero data. */
+static int
+swab_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
+           void *handle, uint32_t count, uint64_t offset, uint32_t flags,
+           int *err)
+{
+  if (!is_aligned (count, offset)) return -1;
+  return next_ops->zero (nxdata, count, offset, flags, err);
+}
+
+/* Extents. */
+static int
+swab_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
+              void *handle, uint32_t count, uint64_t offset, uint32_t flags,
+              struct nbdkit_extents *extents, int *err)
+{
+  if (!is_aligned (count, offset)) return -1;
+  return next_ops->extents (nxdata, count, offset, flags, extents, err);
+}
+
+/* Cache. */
+static int
+swab_cache (struct nbdkit_next_ops *next_ops, void *nxdata,
+            void *handle, uint32_t count, uint64_t offset, uint32_t flags,
+            int *err)
+{
+  if (!is_aligned (count, offset)) return -1;
+  return next_ops->cache (nxdata, count, offset, flags, err);
+}
+
+
+static struct nbdkit_filter filter = {
+  .name              = "swab",
+  .longname          = "nbdkit byte-swapping filter",
+  .pread             = swab_pread,
+  .pwrite            = swab_pwrite,
+  .trim              = swab_trim,
+  .zero              = swab_zero,
+  .extents           = swab_extents,
+  .cache             = swab_cache,
+};
+
+NBDKIT_REGISTER_FILTER(filter)

README
@@ -172,6 +172,8 @@ For non-essential enhancements to the test suite:
 
  - expect
 
+ - hexdump
+
  - ip, ss (from iproute package)
 
  - jq

TODO
@@ -200,6 +200,8 @@ Suggestions for filters
   could inject a flush after pausing.  However this requires that
   filter background threads have access to the plugin (see above).
 
+* nbdkit-swab-filter should be able to swap 32 and 64 bits.
+
 nbdkit-rate-filter:
 
 * allow other kinds of traffic shaping such as VBR

configure.ac
@@ -121,6 +121,7 @@ filters="\
         readahead \
         retry \
         stats \
+        swab \
         truncate \
         xz \
         "
@@ -1159,6 +1160,7 @@ AC_CONFIG_FILES([Makefile
                  filters/readahead/Makefile
                  filters/retry/Makefile
                  filters/stats/Makefile
+                 filters/swab/Makefile
                  filters/truncate/Makefile
                  filters/xz/Makefile
                  fuzzing/Makefile

filters/swab/Makefile.am
@@ -1,0 +1,75 @@
+# nbdkit
+# Copyright (C) 2018 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = \
+	nbdkit-swab-filter.pod \
+	$(NULL)
+
+filter_LTLIBRARIES = nbdkit-swab-filter.la
+
+nbdkit_swab_filter_la_SOURCES = \
+	swab.c \
+	$(top_srcdir)/include/nbdkit-filter.h \
+	$(NULL)
+
+nbdkit_swab_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/sparse \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdkit_swab_filter_la_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(GNUTLS_CFLAGS) \
+	$(NULL)
+nbdkit_swab_filter_la_LDFLAGS = \
+	-module -avoid-version -shared $(SHARED_LDFLAGS) \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms \
+	$(NULL)
+nbdkit_swab_filter_la_LIBADD = \
+	$(top_builddir)/common/sparse/libsparse.la \
+	$(top_builddir)/common/utils/libutils.la \
+	$(GNUTLS_LIBS) \
+	$(NULL)
+
+if HAVE_POD
+
+man_MANS = nbdkit-swab-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-swab-filter.1: nbdkit-swab-filter.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

filters/swab/nbdkit-swab-filter.pod
@@ -1,0 +1,62 @@
+=head1 NAME
+
+nbdkit-swab-filter - nbdkit filter for swapping byte order
+
+=head1 SYNOPSIS
+
+ nbdkit --filter=swab PLUGIN [PLUGIN-ARGS...]
+
+=head1 DESCRIPTION
+
+C<nbdkit-swab-filter> is a filter for L<nbdkit(1)> which swaps bytes
+in the underlying plugin.
+
+One use for this is to read disk images which were formatted on the
+Atari Falcon, where the IDE controller is wired in reverse order.
+
+=head1 EXAMPLE
+
+=head2 Swap bytes from a disk image
+
+ nbdkit --filter=swab file file=disk.img
+
+The above command serves the disk image F<disk.img>, swapping even and
+odd bytes:
+
+ disk.img   0   1   2   3   4   5 ...
+              ╳       ╳       ╳
+   output   1   0   3   2   5   4 ...
+
+=head1 PARAMETERS
+
+There are no parameters specific to this filter.
+
+=head1 FILES
+
+=over 4
+
+=item F<$filterdir/nbdkit-swab-filter.so>
+
+The filter.
+
+Use C<nbdkit --dump-config> to find the location of C<$filterdir>.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-swab-filter> first appeared in nbdkit 1.22.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-file-plugin(1)>,
+L<nbdkit-filter(3)>.
+
+=head1 AUTHORS
+
+François Revol
+
+=head1 COPYRIGHT
+
+Copyright (C) 2020 François Revol

tests/Makefile.am
@@ -1360,6 +1360,16 @@ EXTRA_DIST += \
 	test-retry-zero-flags.sh \
 	$(NULL)
 
+# swab filter test.
+TESTS += \
+	test-swab-16r.sh \
+	test-swab-16w.sh \
+	$(NULL)
+EXTRA_DIST += \
+	test-swab-16r.sh \
+	test-swab-16w.sh \
+	$(NULL)
+
 # truncate filter tests.
 TESTS += \
 	test-truncate1.sh \

tests/test-swab-16r.sh
@@ -1,0 +1,67 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test swab filter.
+
+source ./functions.sh
+set -e
+set -x
+
+requires nbdsh --version
+
+sock=`mktemp -u`
+files="$sock swab-16r.pid"
+rm -f $files
+cleanup_fn rm -f $files
+
+start_nbdkit -P swab-16r.pid -U $sock --filter=swab pattern size=$((128*1024))
+
+nbdsh --connect "nbd+unix://?socket=$sock" \
+      -c '
+# Read back part of the pattern and check it is byte-swapped.
+# Original pattern is:
+# 00 00 00 00 00 00 ff f8
+# 00 00 00 00 00 01 00 00
+# 00 00 00 00 00 01 00 08
+# 00 00 00 00 00 01 00 10
+# Expected output is:
+# 00 00 00 00 00 00 f8 ff
+# 00 00 00 00 01 00 00 00
+# 00 00 00 00 01 00 08 00
+# 00 00 00 00 01 00 10 00
+
+buf = h.pread (32, 65528)
+assert buf == bytearray ([0, 0, 0, 0, 0, 0, 0xf8, 0xff,
+                          0, 0, 0, 0, 1, 0, 0,    0,
+                          0, 0, 0, 0, 1, 0, 8,    0,
+                          0, 0, 0, 0, 1, 0, 0x10, 0])
+'

commit 7c32bcf3593a3daaf89aaa53188053595cda3db5

swab: Set err on unaligned client requests
Otherwise, nbdkit dies by assertion:
$ ./nbdkit --filter=swab pattern 128k -U - --run 'nbdsh -u $uri -c "h.pread(1,1)"'
nbdkit: pattern.0: error: swab: requests to this filter must be aligned
nbdkit: backend.c:485: backend_pread: Assertion `*err' failed.
Traceback (most recent call last):
...
nbd.Error: nbd_pread: read: command failed: Transport endpoint is not connected (ENOTCONN)
nbdkit: nbdkit command was killed by signal 6

instead of the desired client error with server still alive:

nbd.Error: nbd_pread: read: command failed: Invalid argument (EINVAL)

Fixes: bae4a28d1a
Signed-off-by: Eric Blake <eblake@redhat.com>

filters/swab/swab.c
@@ -89,10 +89,11 @@ swab_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,
  * XXX We could lift this restriction with more work.
  */
 static bool
-is_aligned (uint32_t count, uint64_t offset)
+is_aligned (uint32_t count, uint64_t offset, int *err)
 {
   if (!IS_ALIGNED (count, bits/8) || !IS_ALIGNED (offset, bits/8)) {
     nbdkit_error ("swab: requests to this filter must be aligned");
+    *err = EINVAL;
     return false;
   }
   return true;
@@ -138,7 +139,7 @@ swab_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
 {
   int r;
 
-  if (!is_aligned (count, offset)) return -1;
+  if (!is_aligned (count, offset, err)) return -1;
 
   r = next_ops->pread (nxdata, buf, count, offset, flags, err);
   if (r == -1)
@@ -157,7 +158,7 @@ swab_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
 {
   CLEANUP_FREE uint16_t *block = NULL;
 
-  if (!is_aligned (count, offset)) return -1;
+  if (!is_aligned (count, offset, err)) return -1;
 
   block = malloc (count);
   if (block == NULL) {
@@ -177,7 +178,7 @@ swab_trim (struct nbdkit_next_ops *next_ops, void *nxdata,
            void *handle, uint32_t count, uint64_t offset, uint32_t flags,
            int *err)
 {
-  if (!is_aligned (count, offset)) return -1;
+  if (!is_aligned (count, offset, err)) return -1;
   return next_ops->trim (nxdata, count, offset, flags, err);
 }
 
@@ -187,7 +188,7 @@ swab_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
            void *handle, uint32_t count, uint64_t offset, uint32_t flags,
            int *err)
 {
-  if (!is_aligned (count, offset)) return -1;
+  if (!is_aligned (count, offset, err)) return -1;
   return next_ops->zero (nxdata, count, offset, flags, err);
 }
 
@@ -208,7 +209,7 @@ swab_cache (struct nbdkit_next_ops *next_ops, void *nxdata,
             void *handle, uint32_t count, uint64_t offset, uint32_t flags,
             int *err)
 {
-  if (!is_aligned (count, offset)) return -1;
+  if (!is_aligned (count, offset, err)) return -1;
   return next_ops->cache (nxdata, count, offset, flags, err);
 }
 