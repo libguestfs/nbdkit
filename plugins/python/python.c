c7f57ab929893f1c4b27a3788c86f7e130bc882c
Richard W.M. Jones <rjones@redhat.com>
2020-08-05 15:17:53 +0100

    python: Remove some whitespace.

--- plugins/python/python.c
+++ plugins/python/python.c
@@ -47,7 +47,6 @@
 #include <Python.h>
 
 #define NBDKIT_API_VERSION 2
-
 #include <nbdkit-plugin.h>
 
 #include "cleanup.h"
@@ -316,7 +315,6 @@ static void
 py_unload (void)
 {
   Py_XDECREF (module);
-
   Py_Finalize ();
 }
 
