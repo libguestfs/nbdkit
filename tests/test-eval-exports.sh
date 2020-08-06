ea1cd0a777afe845a64ce4adfa242b7dc7ba092f
Eric Blake <eblake@redhat.com>
2020-08-06 13:24:05 -0500

    tests: Strengthen test-eval-exports
    Add some additional testing that nbdkit is correctly resolving the
    canonical name when a client requests the default export.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- tests/test-eval-exports.sh
+++ tests/test-eval-exports.sh
@@ -45,66 +45,71 @@ files="eval-exports.list eval-exports.out"
 rm -f $files
 cleanup_fn rm -f $files
 
-# do_nbdkit [skip_list] EXPOUT
+# do_nbdkit [--skip-list] EXPNAME EXPOUT
 do_nbdkit ()
 {
     # Hack: since we never pass args that would go through .config, we can
     # define a dummy .config to avoid defining .list_export
     hack=
-    if test $1 = skip_list; then
+    if test $1 = --skip-list; then
         hack=config=
         shift
     else
         cat eval-exports.list
     fi
+    # Check how the default export name is handled
+    nbdkit -U - -v eval ${hack}list_exports='cat eval-exports.list' \
+      open='[ "$3" = "'"$1"'" ] || { echo EINVAL wrong export >&2; exit 1; }' \
+      get_size='echo 0' --run 'nbdsh -u "$uri" -c "exit()"'
+    # Check what exports are listed
     nbdkit -U - -v eval ${hack}list_exports='cat eval-exports.list' \
       get_size='echo 0' --run 'nbdinfo --list --json "$uri"' >eval-exports.out
     cat eval-exports.out
     diff -u <(jq -c '[.exports[] | [."export-name", .description]]' \
-              eval-exports.out) <(printf %s\\n "$1")
+              eval-exports.out) <(printf %s\\n "$2")
 }
 
 # Control case: no .list_exports, which defaults to advertising ""
 rm -f eval-exports.list
-do_nbdkit skip_list '[["",null]]'
+do_nbdkit --skip-list '' '[["",null]]'
 
 # Various spellings of empty lists, producing 0 exports
 for fmt in '' 'NAMES\n' 'INTERLEAVED\n' 'NAMES+DESCRIPTIONS\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[]'
+    do_nbdkit '' '[]'
 done
 
 # Various spellings of explicit list for the default export, no description
 for fmt in '\n' 'NAMES\n\n' 'INTERLEAVED\n\n' 'INTERLEAVED\n\n\n' \
            'NAMES+DESCRIPTIONS\n\n' 'NAMES+DESCRIPTIONS\n\n\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[["",null]]'
+    do_nbdkit '' '[["",null]]'
 done
 
 # A non-default name
 for fmt in 'name\n' 'NAMES\nname\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[["name",null]]'
+    do_nbdkit name '[["name",null]]'
 done
 
 # One export with a description
 for fmt in 'INTERLEAVED\nname\ndesc\n' 'NAMES+DESCRIPTIONS\nname\ndesc\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[["name","desc"]]'
+    do_nbdkit name '[["name","desc"]]'
 done
 
 # Multiple exports, with correct number of lines
 for fmt in 'INTERLEAVED\nname 1\ndesc 1\nname 2\ndesc 2\n' \
            'NAMES+DESCRIPTIONS\nname 1\nname 2\ndesc 1\ndesc 2\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[["name 1","desc 1"],["name 2","desc 2"]]'
+    do_nbdkit 'name 1' '[["name 1","desc 1"],["name 2","desc 2"]]'
 done
 
 # Multiple exports, with final description line missing
 for fmt in 'INTERLEAVED\nname 1\ndesc 1\nname 2\n' \
            'NAMES+DESCRIPTIONS\nname 1\nname 2\ndesc 1\n'; do
     printf "$fmt" >eval-exports.list
-    do_nbdkit '[["name 1","desc 1"],["name 2",null]]'
+    do_nbdkit 'name 1' '[["name 1","desc 1"],["name 2",null]]'
 done
 
 # Largest possible name and description
@@ -112,7 +117,7 @@ long=$(printf %04096d 1)
 echo NAMES+DESCRIPTIONS >eval-exports.list
 echo $long >>eval-exports.list
 echo $long >>eval-exports.list
-do_nbdkit "[[\"$long\",\"$long\"]]"
+do_nbdkit $long "[[\"$long\",\"$long\"]]"
 
 # Invalid name (too long) causes an error response to NBD_OPT_LIST
 if nbdkit -U - -v eval list_exports="echo 2$long" \
