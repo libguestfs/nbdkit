7623b2cc45078cca88fdd2d96c70c7f82a0db49d
Eric Blake <eblake@redhat.com>
2020-08-06 16:16:17 -0500

    exportname: New filter
    Add a new filter to make it easier to add exports to a plugin that
    does advertise them, to avoid advertising where a plugin's list might
    be an information leak, and to alter which export name is used in
    place of "".
    
    I would love to be able to have a strict mode enforcing that .open is
    called only with an export name that the plugin was willing to
    advertise, but for that, we'll need a way to call the plugin's
    .list_exports from within the context of .open (since we can't
    guarantee the guest will call NBD_OPT_LIST).  So for now, strict mode
    requires redundant exportname= command line parameters.  It may also
    be worth adding a way to pass exportname-file=/path/to/file, which
    then gets parsed the same was as the sh plugin (right now with NAMES,
    INTERLEAVED, or NAMES+DESCRIPTIONS), to avoid having to do so many
    export names directly on the command line.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ filters/exportname/exportname.c
@@ -0,0 +1,342 @@
+/* nbdkit
+ * Copyright (C) 2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <nbdkit-filter.h>
+
+#include "cleanup.h"
+#include "utils.h"
+
+static const char *default_export;
+static enum {
+  LIST_KEEP,
+  LIST_ERROR,
+  LIST_EMPTY,
+  LIST_DEFAULT,
+  LIST_EXPLICIT,
+} list;
+static bool strict;
+static enum {
+  DESC_KEEP,
+  DESC_NONE,
+  DESC_FIXED,
+  DESC_SCRIPT,
+} desc_mode;
+static const char *desc;
+struct nbdkit_exports *exports;
+
+static void
+exportname_load (void)
+{
+  exports = nbdkit_exports_new ();
+  if (!exports) {
+    nbdkit_error ("malloc: %m");
+    exit (EXIT_FAILURE);
+  }
+}
+
+static void
+exportname_unload (void)
+{
+  nbdkit_exports_free (exports);
+}
+
+/* Called for each key=value passed on the command line. */
+static int
+exportname_config (nbdkit_next_config *next, void *nxdata,
+                   const char *key, const char *value)
+{
+  int r;
+
+  if (strcmp (key, "default-export") == 0 ||
+      strcmp (key, "default_export") == 0) {
+    default_export = value;
+    return 0;
+  }
+  if (strcmp (key, "exportname-list") == 0 ||
+      strcmp (key, "exportname_list") == 0) {
+    if (strcmp (value, "keep") == 0)
+      list = LIST_KEEP;
+    else if (strcmp (value, "error") == 0)
+      list = LIST_ERROR;
+    else if (strcmp (value, "empty") == 0)
+      list = LIST_EMPTY;
+    else if (strcmp (value, "defaultonly") == 0 ||
+             strcmp (value, "default-only") == 0)
+      list = LIST_DEFAULT;
+    else if (strcmp (value, "explicit") == 0)
+      list = LIST_EXPLICIT;
+    else {
+      nbdkit_error ("unrecognized exportname-list mode: %s", value);
+      return -1;
+    }
+    return 0;
+  }
+  if (strcmp (key, "exportname-strict") == 0 ||
+      strcmp (key, "exportname_strict") == 0) {
+    r = nbdkit_parse_bool (value);
+    if (r == -1)
+      return -1;
+    strict = r;
+    return 0;
+  }
+  if (strcmp (key, "exportname") == 0)
+    return nbdkit_add_export (exports, value, NULL);
+  if (strcmp (key, "exportdesc") == 0) {
+    if (strcmp (value, "keep") == 0)
+      desc_mode = DESC_KEEP;
+    else if (strcmp (value, "none") == 0) {
+      desc_mode = DESC_NONE;
+      desc = NULL;
+    }
+    else if (strncmp (value, "fixed:", 6) == 0) {
+      desc_mode = DESC_FIXED;
+      desc = value + 6;
+    }
+    else if (strncmp (value, "script:", 7) == 0) {
+      desc_mode = DESC_SCRIPT;
+      desc = value + 7;
+    }
+    else {
+      nbdkit_error ("unrecognized exportdesc mode: %s", value);
+      return -1;
+    }
+    return 0;
+  }
+  return next (nxdata, key, value);
+}
+
+#define exportname_config_help \
+  "default-export=<NAME>     Canonical name for the \"\" default export.\n" \
+  "exportname-list=<MODE>    Which exports to advertise: keep (default), error,\n" \
+  "                          empty, defaultonly, explicit.\n" \
+  "exportname-strict=<BOOL>  Limit clients to explicit exports (default false).\n" \
+  "exportname=<NAME>         Add an explicit export name, may be repeated.\n" \
+  "exportdesc=<MODE>         Set descriptions according to mode: keep (default),\n" \
+  "                          none, fixed:STRING, script:SCRIPT.\n" \
+
+static const char *
+get_desc (const char *name, const char *def)
+{
+  FILE *fp;
+  CLEANUP_FREE char *cmd = NULL;
+  size_t cmdlen = 0;
+  char buf[4096]; /* Maximum NBD string; we truncate any longer response */
+  size_t r;
+
+  switch (desc_mode) {
+  case DESC_KEEP:
+    return def;
+  case DESC_NONE:
+  case DESC_FIXED:
+    return desc;
+  case DESC_SCRIPT:
+    break;
+  default:
+    abort ();
+  }
+
+  /* Construct the command. */
+  fp = open_memstream (&cmd, &cmdlen);
+  if (fp == NULL) {
+    nbdkit_debug ("open_memstream: %m");
+    return NULL;
+  }
+  fprintf (fp, "export name; name=");
+  shell_quote (name, fp);
+  fprintf (fp, "\n%s\n", desc);
+  if (fclose (fp) == EOF) {
+    nbdkit_debug ("memstream failed: %m");
+    return NULL;
+  }
+  nbdkit_debug ("%s", cmd);
+  fp = popen (cmd, "r");
+  if (fp == NULL) {
+    nbdkit_debug ("popen: %m");
+    return NULL;
+  }
+
+  /* Now read the description */
+  r = fread (buf, 1, sizeof buf, fp);
+  if (r == 0 && ferror (fp)) {
+    nbdkit_debug ("fread: %m");
+    pclose (fp);
+    return NULL;
+  }
+  pclose (fp);
+  if (r && buf[r-1] == '\n')
+    r--;
+  return nbdkit_strndup_intern (buf, r);
+}
+
+static int
+exportname_list_exports (nbdkit_next_list_exports *next, void *nxdata,
+                         int readonly, int is_tls,
+                         struct nbdkit_exports *exps)
+{
+  size_t i;
+  struct nbdkit_exports *source;
+  CLEANUP_EXPORTS_FREE struct nbdkit_exports *exps2 = NULL;
+
+  switch (list) {
+  case LIST_KEEP:
+    source = exps2 = nbdkit_exports_new ();
+    if (exps2 == NULL)
+      return -1;
+    if (next (nxdata, readonly, exps2) == -1)
+      return -1;
+    break;
+  case LIST_ERROR:
+    nbdkit_error ("export list restricted by policy");
+    return -1;
+  case LIST_EMPTY:
+    return 0;
+  case LIST_DEFAULT:
+    return nbdkit_use_default_export (exps);
+  case LIST_EXPLICIT:
+    source = exports;
+    break;
+  default:
+    abort ();
+  }
+
+  for (i = 0; i < nbdkit_exports_count (source); i++) {
+    struct nbdkit_export e = nbdkit_get_export (source, i);
+
+    if (nbdkit_add_export (exps, e.name,
+                           get_desc (e.name, e.description)) == -1)
+      return -1;
+  }
+  return 0;
+}
+
+static const char *
+exportname_default_export (nbdkit_next_default_export *next, void *nxdata,
+                           int readonly, int is_tls)
+{
+  size_t i;
+
+  /* If we are strict, do not allow connection unless "" was advertised. */
+  if (strict) {
+    for (i = 0; i < nbdkit_exports_count (exports); i++) {
+      if (nbdkit_get_export (exports, i).name[0] == '\0')
+        return default_export ?: "";
+    }
+    return NULL;
+  }
+
+  if (default_export)
+    return default_export;
+  return next (nxdata, readonly);
+}
+
+struct handle {
+  const char *name;
+};
+
+static void *
+exportname_open (nbdkit_next_open *next, void *nxdata,
+                 int readonly, const char *exportname, int is_tls)
+{
+  size_t i;
+  struct handle *h;
+
+  if (strict) {
+    for (i = 0; i < nbdkit_exports_count (exports); i++) {
+      if (strcmp (nbdkit_get_export (exports, i).name, exportname) == 0)
+        break;
+    }
+    if (i == nbdkit_exports_count (exports)) {
+      nbdkit_error ("export '%s not found", exportname);
+      errno = ENOENT;
+      return NULL;
+    }
+  }
+
+  h = malloc (sizeof *h);
+  if (h == NULL) {
+    nbdkit_error ("malloc: %m");
+    return NULL;
+  }
+
+  h->name = nbdkit_strdup_intern (exportname);
+  if (h->name == NULL) {
+    free (h);
+    return NULL;
+  }
+  if (next (nxdata, readonly, exportname) == -1) {
+    free (h);
+    return NULL;
+  }
+
+  return h;
+}
+
+static void
+exportname_close (void *handle)
+{
+  free (handle);
+}
+
+static const char *
+exportname_export_description (struct nbdkit_next_ops *next_ops, void *nxdata,
+                               void *handle)
+{
+  struct handle *h = handle;
+  const char *def = NULL;
+
+  if (desc_mode == DESC_KEEP)
+    def = next_ops->export_description (nxdata);
+
+  return get_desc (h->name, def);
+}
+
+static struct nbdkit_filter filter = {
+  .name               = "exportname",
+  .longname           = "nbdkit exportname filter",
+  .load               = exportname_load,
+  .unload             = exportname_unload,
+  .config             = exportname_config,
+  .config_help        = exportname_config_help,
+  .list_exports       = exportname_list_exports,
+  .default_export     = exportname_default_export,
+  .open               = exportname_open,
+  .close              = exportname_close,
+  .export_description = exportname_export_description,
+};
+
+NBDKIT_REGISTER_FILTER(filter)
---
+++ tests/test-exportname.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2019-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -e
+set -x
+
+requires_plugin sh
+requires nbdinfo --version
+requires nbdsh -c 'print(h.set_full_info)'
+requires jq --version
+
+files="exportname.out exportname.sh"
+rm -f $files
+cleanup_fn rm -f $files
+
+query='[ [.exports[]] | sort_by(."export-name")[] |
+  [."export-name", .description, ."export-size"] ]'
+fail=0
+
+cat >exportname.sh <<\EOF
+case $1 in
+  list_exports)
+    echo INTERLEAVED
+    echo a; echo x
+    echo b; echo y
+    echo c; echo z
+    ;;
+  default_export) echo a ;;
+  open) echo "$3" ;;
+  export_description | get_size)
+    case $2 in
+      a) echo 1 ;;
+      b) echo 2 ;;
+      c) echo 3 ;;
+      *) exit 1 ;
+    esac ;;
+  *) exit 2 ;;
+esac
+EOF
+chmod +x exportname.sh
+
+# Establish a baseline
+nbdkit -U - sh exportname.sh \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a","x",1],["b","y",2],["c","z",3]]'
+
+# Set the default export
+nbdkit -U - --filter=exportname sh exportname.sh default-export= \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["","1",1]]'
+
+nbdkit -U - --filter=exportname sh exportname.sh default-export=b \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["b","2",2]]'
+
+# Test export list policies
+nbdkit -U - --filter=exportname sh exportname.sh exportname-list=keep \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a","x",1],["b","y",2],["c","z",3]]'
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-list=error \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out && fail=1 || :
+test ! -s exportname.out
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-list=empty \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[]'
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-list=defaultonly \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+got="$(jq -c "$query" exportname.out)"
+# libnbd 1.4.0 and 1.4.1 differ on whether --list grabs description
+test "$got" = '[["a",null,1]]' || test "$got" = '[["a","1",1]]' || fail=1
+
+nbdkit -U - --filter=exportname sh exportname.sh default-export=b \
+       exportname-list=defaultonly exportname=a exportname=b \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+got="$(jq -c "$query" exportname.out)"
+test "$got" = '[["b",null,2]]' || test "$got" = '[["b","2",2]]' || fail=1
+
+nbdkit -U - --filter=exportname sh exportname.sh \
+       exportname-list=explicit exportname=b exportname=a \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+got="$(jq -c "$query" exportname.out)"
+test "$got" = '[["a",null,1],["b",null,2]]' ||
+    test "$got" = '[["a","1",1],["b","2",2]]' || fail=1
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-list=explicit \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[]'
+
+# Test description modes with lists
+nbdkit -U - --filter=exportname sh exportname.sh exportdesc=keep \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a","x",1],["b","y",2],["c","z",3]]'
+
+nbdkit -U - --filter=exportname sh exportname.sh exportdesc=none \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a",null,1],["b",null,2],["c",null,3]]'
+
+nbdkit -U - --filter=exportname sh exportname.sh exportdesc=fixed:hi \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a","hi",1],["b","hi",2],["c","hi",3]]'
+
+nbdkit -U - --filter=exportname sh exportname.sh \
+       exportdesc=script:'echo $name$name' \
+       --run 'nbdinfo --json --list "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = \
+     '[["a","aa",1],["b","bb",2],["c","cc",3]]'
+
+# Test description modes with connections
+nbdkit -U - -e c --filter=exportname sh exportname.sh exportdesc=fixed:hi \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["c","hi",3]]'
+
+nbdkit -U - -e c --filter=exportname sh exportname.sh \
+       exportdesc=script:'echo $name$name' \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["c","cc",3]]'
+
+# Test strict mode
+nbdkit -U - --filter=exportname sh exportname.sh exportname-strict=true \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out && fail=1
+test ! -s exportname.out
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-strict=true \
+       exportname=a exportname=b exportname=c \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out && fail=1
+test ! -s exportname.out
+
+nbdkit -U - --filter=exportname sh exportname.sh exportname-strict=true \
+       exportname=a exportname=b exportname= default-export=a\
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["a","1",1]]'
+
+nbdkit -U - -e a --filter=exportname sh exportname.sh exportname-strict=true \
+       exportname=a exportname=b exportname=c \
+       --run 'nbdinfo --no-content --json "$uri"' > exportname.out
+cat exportname.out
+test "$(jq -c "$query" exportname.out)" = '[["a","1",1]]'
+
+exit $fail
