860f98325ba39b2070ba65996ecf0791388fb326
Eric Blake <eblake@redhat.com>
2020-08-07 15:42:42 -0500

    tls-fallback: New filter
    Take advantage of the fact that we can now detect the type of client
    during --tls=on in order to provide safe dummy content for plaintext
    clients without having to rewrite plugins to do so.
    
    The filter isn't quite perfect yet: we need to fix nbdkit to allow
    .list_exports to know when tls is in use, as well as to allow us to
    completely skip the plugin's next_open().  But those can be followup
    patches, hopefully before we stabilize the 1.22 release.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ filters/tls-fallback/tls-fallback.c
@@ -0,0 +1,199 @@
+/* nbdkit
+ * Copyright (C) 2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <string.h>
+#include <assert.h>
+
+#include <nbdkit-filter.h>
+
+/* Needed to shut up newer gcc about use of strncpy on our message buffer */
+#if __GNUC__ >= 8
+#define NONSTRING __attribute__ ((nonstring))
+#else
+#define NONSTRING
+#endif
+
+static char message[512] NONSTRING = "This NBD server requires TLS "
+  "authentication before it will serve useful data.\n";
+
+/* Called for each key=value passed on the command line. */
+static int
+tls_fallback_config (nbdkit_next_config *next, void *nxdata,
+                     const char *key, const char *value)
+{
+  if (strcmp (key, "tlsreadme") == 0) {
+    strncpy (message, value, sizeof message); /* Yes, we really mean strncpy */
+    return 0;
+  }
+  return next (nxdata, key, value);
+}
+
+#define tls_fallback_config_help \
+  "tlsreadme=<MESSAGE>  Alternative contents for the plaintext dummy export.\n"
+
+int
+tls_fallback_get_ready (nbdkit_next_get_ready *next, void *nxdata,
+                        int thread_model)
+{
+  if (thread_model == NBDKIT_THREAD_MODEL_SERIALIZE_CONNECTIONS) {
+    nbdkit_error ("the tls-fallback filter requires parallel connection "
+                  "support");
+    return -1;
+  }
+  return next (nxdata);
+}
+
+/* TODO: init_exports needs is_tls parameter */
+
+/* Helper for determining if this connection is insecure.  This works
+ * because we can treat all handles on a binary basis: secure or
+ * insecure, which lets .open get away without allocating a more
+ * complex handle.
+ */
+#define NOT_TLS (handle == &message)
+
+static void *
+tls_fallback_open (nbdkit_next_open *next, void *nxdata, int readonly,
+                   const char *exportname, int is_tls)
+{
+  /* Bummer - we really do NOT want to call next() when insecure,
+   * because we don't know how long it will take.  But that requires a
+   * fix to nbdkit; right now, it asserts if we skip this :(
+   */
+  if (next (nxdata, readonly, exportname) == -1)
+    return NULL;
+  if (!is_tls)
+    return &message; /* See NOT_TLS for this choice of handle */
+  return NBDKIT_HANDLE_NOT_NEEDED;
+}
+
+/* When insecure, override any plugin .can_FOO not gated by another in
+ * order to avoid an information leak. (can_write gates can_trim,
+ * can_zero, can_fast_zero, and can_fua).
+ */
+
+static int64_t
+tls_fallback_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,
+                       void *handle)
+{
+  if (NOT_TLS)
+    return sizeof message;
+  return next_ops->get_size (nxdata);
+}
+
+static int
+tls_fallback_can_write (struct nbdkit_next_ops *next_ops, void *nxdata,
+                        void *handle)
+{
+  if (NOT_TLS)
+    return 0;
+  return next_ops->can_write (nxdata);
+}
+
+static int
+tls_fallback_can_flush (struct nbdkit_next_ops *next_ops, void *nxdata,
+                        void *handle)
+{
+  if (NOT_TLS)
+    return 0;
+  return next_ops->can_flush (nxdata);
+}
+
+static int
+tls_fallback_is_rotational (struct nbdkit_next_ops *next_ops, void *nxdata,
+                            void *handle)
+{
+  if (NOT_TLS)
+    return 0;
+  return next_ops->is_rotational (nxdata);
+}
+
+static int
+tls_fallback_can_extents (struct nbdkit_next_ops *next_ops, void *nxdata,
+                          void *handle)
+{
+  if (NOT_TLS)
+    return 0;
+  return next_ops->can_extents (nxdata);
+}
+
+static int
+tls_fallback_can_multi_conn (struct nbdkit_next_ops *next_ops, void *nxdata,
+                             void *handle)
+{
+  if (NOT_TLS)
+    return 0;
+  return next_ops->can_multi_conn (nxdata);
+}
+
+static int
+tls_fallback_can_cache (struct nbdkit_next_ops *next_ops, void *nxdata,
+                        void *handle)
+{
+  if (NOT_TLS)
+    return NBDKIT_CACHE_NONE;
+  return next_ops->can_cache (nxdata);
+}
+
+static int
+tls_fallback_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
+                    void *handle, void *b, uint32_t count, uint64_t offs,
+                    uint32_t flags, int *err)
+{
+  if (NOT_TLS) {
+    memcpy (b, message + offs, count);
+    return 0;
+  }
+  return next_ops->pread (nxdata, b, count, offs, flags, err);
+}
+
+static struct nbdkit_filter filter = {
+  .name              = "tls-fallback",
+  .longname          = "nbdkit tls-fallback filter",
+  .config            = tls_fallback_config,
+  .config_help       = tls_fallback_config_help,
+  .get_ready         = tls_fallback_get_ready,
+  /* XXX .init_exports needs is_tls parameter */
+  .open              = tls_fallback_open,
+  .get_size          = tls_fallback_get_size,
+  .can_write         = tls_fallback_can_write,
+  .can_flush         = tls_fallback_can_flush,
+  .is_rotational     = tls_fallback_is_rotational,
+  .can_extents       = tls_fallback_can_extents,
+  .can_multi_conn    = tls_fallback_can_multi_conn,
+  .can_cache         = tls_fallback_can_cache,
+  .pread             = tls_fallback_pread,
+};
+
+NBDKIT_REGISTER_FILTER(filter)
---
+++ tests/test-tls-fallback.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2019-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+source ./functions.sh
+set -e
+set -x
+
+requires nbdsh -c 'exit (not h.supports_tls ())'
+
+# Does the nbdkit binary support TLS?
+if ! nbdkit --dump-config | grep -sq tls=yes; then
+    echo "$0: nbdkit built without TLS support"
+    exit 77
+fi
+
+# Did we create the PSK keys file?
+# Probably 'certtool' is missing.
+if [ ! -s keys.psk ]; then
+    echo "$0: PSK keys file was not created by the test harness"
+    exit 77
+fi
+
+export sock=`mktemp -u`
+pid="tls-fallback.pid"
+
+files="$sock $pid"
+rm -f $files
+cleanup_fn rm -f $files
+
+# Run dual-mode server
+start_nbdkit -P $pid -U $sock --tls=on --tls-psk=keys.psk \
+    --filter=tls-fallback sh - tlsreadme=$'dummy\n' <<\EOF
+if test "$2" = insecure; then
+    echo 'EINVAL insecure handle' 2>&1; exit 1
+fi
+case $1 in
+  list_exports) echo NAMES; echo hello; echo world ;;
+  open) if test "$4" != true; then
+      # nbdkit needs a fix to let tls-fallback skip .open when insecure...
+      # echo 'EINVAL unexpected tls mode' 2>&1; exit 1
+      echo insecure; exit 0
+    fi
+    echo $3 ;;
+  get_size) echo 6 ;;
+  pread) echo "$2" | dd skip=$4 count=$3 iflag=skip_bytes,count_bytes ;;
+  can_write | can_trim) exit 0 ;;
+  *) exit 2;
+esac
+EOF
+
+# Plaintext client sees only dummy volume
+nbdsh -c '
+import os
+h.set_export_name ("hello")
+h.connect_unix (os.environ["sock"])
+assert h.get_size () == 512
+assert not h.can_trim()
+assert h.pread (5, 0) == b"dummy"
+'
+
+# Encrypted client sees desired volumes
+nbdsh -c '
+import os
+h.set_export_name ("hello")
+h.set_tls (nbd.TLS_REQUIRE)
+h.set_tls_psk_file ("keys.psk")
+h.set_tls_username ("qemu")
+h.connect_unix (os.environ["sock"])
+assert h.get_size () == 6
+assert h.can_trim ()
+assert h.pread (5, 0) == b"hello"
+'
