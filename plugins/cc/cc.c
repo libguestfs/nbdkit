libguestfs/nbdkit

commit a31efe33388046ec9af345ce2d3892d15116f9ce

cc: Handle missing callbacks.
If can_write is defined by the subplugin and returns true, but pwrite
is missing, then the plugin would crash.  nbdkit itself handles these
kinds of cases in different ways, and this commit replicates that
handling.  Except for .zero where the fallback is complex.

Thanks: Eric Blake
Fixes: commit a4339e3c3c17d8b95ddf0b20cf015de5078620ec

plugins/cc/cc.c
@@ -37,6 +37,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <dlfcn.h>
 
 #define NBDKIT_API_VERSION 2
@@ -463,38 +464,75 @@ static int
 cc_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
            uint32_t flags)
 {
-  return subplugin.pwrite (handle, buf, count, offset, flags);
+  if (subplugin.pwrite)
+    return subplugin.pwrite (handle, buf, count, offset, flags);
+  else {
+    nbdkit_error ("missing %s callback", "pwrite");
+    errno = EROFS;
+    return -1;
+  }
 }
 
 static int
 cc_flush (void *handle, uint32_t flags)
 {
-  return subplugin.flush (handle, flags);
+  if (subplugin.flush)
+    return subplugin.flush (handle, flags);
+  else {
+    nbdkit_error ("missing %s callback", "flush");
+    errno = EINVAL;
+    return -1;
+  }
 }
 
 static int
 cc_trim (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
 {
-  return subplugin.trim (handle, count, offset, flags);
+  if (subplugin.trim)
+    return subplugin.trim (handle, count, offset, flags);
+  else {
+    nbdkit_error ("missing %s callback", "trim");
+    errno = EINVAL;
+    return -1;
+  }
 }
 
 static int
 cc_zero (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
 {
-  return subplugin.zero (handle, count, offset, flags);
+  if (subplugin.zero)
+    return subplugin.zero (handle, count, offset, flags);
+  else {
+    /* XXX nbdkit tries to emulate this and we should too. */
+    nbdkit_error ("missing %s callback", "zero");
+    errno = EINVAL;
+    return -1;
+  }
 }
 
 static int
 cc_extents (void *handle, uint32_t count, uint64_t offset,
             uint32_t flags, struct nbdkit_extents *extents)
 {
-  return subplugin.extents (handle, count, offset, flags, extents);
+  if (subplugin.extents)
+    return subplugin.extents (handle, count, offset, flags, extents);
+  else {
+    nbdkit_error ("missing %s callback", "extents");
+    errno = EINVAL;
+    return -1;
+  }
 }
 
 static int
 cc_cache (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
 {
-  return subplugin.cache (handle, count, offset, flags);
+  if (subplugin.cache)
+    return subplugin.cache (handle, count, offset, flags);
+  else
+    /* A plugin may advertise caching but not provide .cache; in that
+     * case, caching is explicitly a no-op.
+     */
+    return 0;
 }
 
 static struct nbdkit_plugin plugin = {

commit 1ec0df47bed7b860c13c1d31d19293d1221ea00f

cc: Let missing .zero fall back to .pwrite
Failing with EOPNOTSUPP instead of EINVAL is more like python and
other language bindings for sane behavior when .has_zero claimed
support but .zero is missing, because then nbdkit will use .pwrite.

Tested by applying this temporary diff, then using nbdsh to drive
calls to .zero; pre-patch, the calls fail and the image is unchanged,
post-patch, nbdkit still warns about the missing callback, but the
image has zeroes written and the client is unaware of the server
warning.

| diff --git i/tests/shebang.c w/tests/shebang.c
| index 50e07f84..525f7337 100755
| --- i/tests/shebang.c
| +++ w/tests/shebang.c
| @@ -41,12 +41,19 @@ my_pwrite (void *handle, const void *buf,
|    return 0;
|  }
|
| +static int
| +my_can_zero (void *handle)
| +{
| +  return 1;
| +}
| +
|  static struct nbdkit_plugin plugin = {
|    .name              = "myplugin",
|    .open              = my_open,
|    .get_size          = my_get_size,
|    .pread             = my_pread,
|    .pwrite            = my_pwrite,
| +  .can_zero = my_can_zero,
|  };
|
|  NBDKIT_REGISTER_PLUGIN(plugin)

Fixes: a31efe33
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/cc/cc.c
@@ -511,9 +511,9 @@ cc_zero (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
   if (subplugin.zero)
     return subplugin.zero (handle, count, offset, flags);
   else {
-    /* XXX nbdkit tries to emulate this and we should too. */
+    /* Inform nbdkit to fall back to pwrite. */
     nbdkit_error ("missing %s callback", "zero");
-    errno = EINVAL;
+    errno = EOPNOTSUPP;
     return -1;
   }
 }