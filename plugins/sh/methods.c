libguestfs/nbdkit

commit 01049aed719c3304a2feb4853db23b25c657c0c5

eval, sh: Add preconnect method.

plugins/sh/methods.c
@@ -139,6 +139,35 @@ sh_thread_model (void)
   }
 }
 
+int
+sh_preconnect (int readonly)
+{
+  const char *method = "preconnect";
+  const char *script = get_script (method);
+  const char *args[] =
+    { script, method,
+      readonly ? "true" : "false",
+      nbdkit_export_name () ? : "",
+      NULL };
+
+  switch (call (args)) {
+  case OK:
+  case MISSING:
+    return 0;
+
+  case ERROR:
+    return -1;
+
+  case RET_FALSE:
+    nbdkit_error ("%s: %s method returned unexpected code (3/false)",
+                  script, method);
+    errno = EIO;
+    return -1;
+
+  default: abort ();
+  }
+}
+
 void *
 sh_open (int readonly)
 {

plugins/sh/nbdkit-sh-plugin.pod
@@ -246,6 +246,10 @@ actually gets selected.
 If an error occurs, the script should output an error message and exit
 with status C<1>; unrecognized output is ignored.
 
+=item C<preconnect>
+
+ /path/to/script preconnect <readonly> <exportname>
+
 =item C<open>
 
  /path/to/script open <readonly> <exportname>

plugins/eval/eval.c
@@ -75,6 +75,7 @@ static const char *known_methods[] = {
   "missing",
   "open",
   "pread",
+  "preconnect",
   "pwrite",
   "thread_model",
   "trim",
@@ -409,6 +410,7 @@ static struct nbdkit_plugin plugin = {
   .config_help       = eval_config_help,
   .thread_model      = sh_thread_model,
 
+  .preconnect        = sh_preconnect,
   .open              = sh_open,
   .close             = sh_close,
 

plugins/eval/nbdkit-eval-plugin.pod
@@ -108,6 +108,8 @@ features):
 
 =item B<pread=>SCRIPT
 
+=item B<preconnect=>SCRIPT
+
 =item B<pwrite=>SCRIPT
 
 =item B<thread_model=>SCRIPT

plugins/sh/methods.h
@@ -41,6 +41,7 @@ extern const char *get_script (const char *method);
 
 extern void sh_dump_plugin (void);
 extern int sh_thread_model (void);
+extern int sh_preconnect (int readonly);
 extern void *sh_open (int readonly);
 extern void sh_close (void *handle);
 extern int64_t sh_get_size (void *handle);

plugins/sh/sh.c
@@ -311,6 +311,7 @@ static struct nbdkit_plugin plugin = {
   .magic_config_key  = "script",
   .thread_model      = sh_thread_model,
 
+  .preconnect        = sh_preconnect,
   .open              = sh_open,
   .close             = sh_close,
 

tests/test-eval.sh
@@ -59,5 +59,6 @@ grep 'in missing: config_complete' eval.missing
 grep 'in missing: thread_model' eval.missing
 grep 'in missing: can_write' eval.missing
 grep 'in missing: is_rotational' eval.missing
+grep 'in missing: preconnect' eval.missing
 grep 'in missing: open' eval.missing
 grep 'in missing: close' eval.missing

commit f53e20f7e308587712cab4340e29c853bf5b4f13

sh: Don't pass exportname to .preconnect
nbdkit calls .preconnect prior to the client actually requesting any
export name, so it is always going to be NULL at the point of the
query.  As this is a pointless parameter that has always been empty,
we can drop it without breaking existing sh scripts.

Fixes: 01049aed71
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/sh/methods.c
@@ -198,7 +198,6 @@ sh_preconnect (int readonly)
   const char *args[] =
     { script, method,
       readonly ? "true" : "false",
-      nbdkit_export_name () ? : "",
       NULL };
 
   switch (call (args)) {

plugins/sh/nbdkit-sh-plugin.pod
@@ -264,7 +264,7 @@ with status C<1>; unrecognized output is ignored.
 
 =item C<preconnect>
 
- /path/to/script preconnect <readonly> <exportname>
+ /path/to/script preconnect <readonly>
 
 =item C<list_exports>
 