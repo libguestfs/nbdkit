libguestfs/nbdkit

commit 5c1f4d69b6a73f57a4fab4b1093c3910dcabb443

file: Add directory= for serving files in a dir
Add a new mode to the file plugin, using directory=DIR instead of
[file=]FILE, to allow it to serve all regular/block files in a given
directory.  A later patch will implement .list_exports to advertise
those files, or even select the largest such file as a default.

Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/file/file.c
@@ -65,6 +65,7 @@
 #endif
 
 static char *filename = NULL;
+static char *directory = NULL;
 
 /* posix_fadvise mode: -1 = don't set it, or POSIX_FADV_*. */
 static int fadvise_mode =
@@ -94,19 +95,32 @@ static void
 file_unload (void)
 {
   free (filename);
+  free (directory);
 }
 
 /* Called for each key=value passed on the command line.  This plugin
- * only accepts file=<filename>, which is required.
+ * only accepts file=<filename> and directory=<dirname>, where exactly
+ * one is required.
  */
 static int
 file_config (const char *key, const char *value)
 {
+  /* See FILENAMES AND PATHS in nbdkit-plugin(3).
+   * Our use of nbdkit_realpath requires the destination to exist at
+   * startup; use nbdkit_absolute_path instead if we wanted to defer
+   * existence checks to the last possible moment.
+   */
   if (strcmp (key, "file") == 0) {
-    /* See FILENAMES AND PATHS in nbdkit-plugin(3). */
     free (filename);
     filename = nbdkit_realpath (value);
     if (!filename)
+      return -1;
+  }
+  else if (strcmp (key, "directory") == 0 ||
+           strcmp (key, "dir") == 0) {
+    free (directory);
+    directory = nbdkit_realpath (value);
+    if (!directory)
       return -1;
   }
   else if (strcmp (key, "fadvise") == 0) {
@@ -162,13 +176,41 @@ file_config (const char *key, const char *value)
   return 0;
 }
 
-/* Check the user did pass a file=<FILENAME> parameter. */
+/* Check the user passed exactly one parameter. */
 static int
 file_config_complete (void)
 {
-  if (filename == NULL) {
-    nbdkit_error ("you must supply the file=<FILENAME> parameter "
-                  "after the plugin name on the command line");
+  int r;
+  struct stat sb;
+
+  if (!filename && !directory) {
+    nbdkit_error ("you must supply either [file=]<FILENAME> or "
+                  "directory=<DIRNAME> parameter after the plugin name "
+                  "on the command line");
+    return -1;
+  }
+  if (filename && directory) {
+    nbdkit_error ("file= and directory= cannot be used at the same time");
+    return -1;
+  }
+
+  /* Sanity check now, rather than waiting for first client open.
+   * See also comment in .config about use of nbdkit_realpath.
+   * Yes, this is a harmless TOCTTOU race.
+   */
+  if (filename) {
+    r = stat (filename, &sb);
+    if (r == 0 && S_ISDIR (sb.st_mode)) {
+      nbdkit_error ("use directory= to serve files within %s", filename);
+      return -1;
+    }
+    if (r == -1 || !(S_ISBLK (sb.st_mode) || S_ISREG (sb.st_mode))) {
+      nbdkit_error ("file is not regular or block device: %s", filename);
+      return -1;
+    }
+  }
+  else if (stat (directory, &sb) == -1 || !S_ISDIR (sb.st_mode)) {
+    nbdkit_error ("expecting a directory: %s", directory);
     return -1;
   }
 
@@ -176,7 +218,8 @@ file_config_complete (void)
 }
 
 #define file_config_help \
-  "file=<FILENAME>     (required) The filename to serve." \
+  "[file=]<FILENAME>     The filename to serve.\n" \
+  "directory=<DIRNAME>   A directory containing files to serve.\n" \
 
 /* Print some extra information about how the plugin was compiled. */
 static void
@@ -214,6 +257,24 @@ file_open (int readonly)
   struct handle *h;
   struct stat statbuf;
   int flags;
+  const char *file;
+  int dfd = -1;
+
+  if (directory) {
+    file = nbdkit_export_name ();
+    if (strchr (file, '/')) {
+      nbdkit_error ("exportname cannot contain /");
+      errno = EINVAL;
+      return NULL;
+    }
+    dfd = open (directory, O_RDONLY | O_DIRECTORY | O_CLOEXEC);
+    if (dfd == -1) {
+      nbdkit_error ("open %s: %m", directory);
+      return NULL;
+    }
+  }
+  else
+    file = filename;
 
   h = malloc (sizeof *h);
   if (h == NULL) {
@@ -227,15 +288,19 @@ file_open (int readonly)
   else
     flags |= O_RDWR;
 
-  h->fd = open (filename, flags);
+  h->fd = openat (dfd, file, flags);
   if (h->fd == -1) {
-    nbdkit_error ("open: %s: %m", filename);
+    nbdkit_error ("openat: %s: %m", file);
+    if (dfd != -1)
+      close (dfd);
     free (h);
     return NULL;
   }
+  if (dfd != -1)
+    close (dfd);
 
   if (fstat (h->fd, &statbuf) == -1) {
-    nbdkit_error ("fstat: %s: %m", filename);
+    nbdkit_error ("fstat: %s: %m", file);
     free (h);
     return NULL;
   }
@@ -256,7 +321,7 @@ file_open (int readonly)
   else if (S_ISREG (statbuf.st_mode))
     h->is_block_device = false;
   else {
-    nbdkit_error ("file is not regular or block device: %s", filename);
+    nbdkit_error ("file is not regular or block device: %s", file);
     close (h->fd);
     free (h);
     return NULL;
@@ -266,7 +331,7 @@ file_open (int readonly)
 #ifdef BLKSSZGET
   if (h->is_block_device) {
     if (ioctl (h->fd, BLKSSZGET, &h->sector_size))
-      nbdkit_debug ("cannot get sector size: %s: %m", filename);
+      nbdkit_debug ("cannot get sector size: %s: %m", file);
   }
 #endif
 

plugins/file/nbdkit-file-plugin.pod
@@ -7,14 +7,21 @@ nbdkit-file-plugin - nbdkit file plugin
  nbdkit file [file=]FILENAME
              [cache=default|none] [fadvise=normal|random|sequential]
 
+ nbdkit file directory=DIRNAME
+
 =head1 DESCRIPTION
 
 C<nbdkit-file-plugin> is a file serving plugin for L<nbdkit(1)>.
 
 It serves the named C<FILENAME> over NBD.  Local block devices
-(eg. F</dev/sda>) may also be served.
+(eg. F</dev/sda>) may also be served.  It may also be used to serve
+any file within a given C<DIRECTORY>, according to which export name
+the guest requests.
 
 =head1 PARAMETERS
+
+One of B<file> or B<directory> must be given to determine which mode
+the server will use.
 
 =over 4
 
@@ -43,12 +50,26 @@ The default is C<normal>.
 =item [B<file=>]FILENAME
 
 Serve the file named C<FILENAME>.  A local block device name can also
-be used here.
-
-This parameter is required.
+be used here.  When this mode is used, the export name requested by
+the client is ignored.
 
 C<file=> is a magic config key and may be omitted in most cases.
 See L<nbdkit(1)/Magic parameters>.
+
+=item B<directory=>DIRNAME
+
+(nbdkit E<ge> 1.22)
+
+Serve all regular files and block devices located directly within the
+directory named C<DIRNAME>, including those found by following
+symbolic links.  Other special files in the directory (such as
+subdirectories, fifos, or Unix sockets) are ignored.  When this mode
+is used, the file to be served is chosen by the export name passed by
+the client.  For now, the client must already know what files are
+in the directory, although there are plans to expose them via
+NBD_OPT_LIST; likewise, a client that requests the default export
+(C<"">) will be rejected.  For security, when using directory mode,
+this plugin will not accept export names containing slash (C</>).
 
 =back
 

tests/Makefile.am
@@ -640,8 +640,8 @@ test_file_block_SOURCES = test-file-block.c test.h
 test_file_block_CFLAGS = $(WARNINGS_CFLAGS) $(LIBGUESTFS_CFLAGS)
 test_file_block_LDADD = libtest.la $(LIBGUESTFS_LIBS)
 
-TESTS += test-file-extents.sh
-EXTRA_DIST += test-file-extents.sh
+TESTS += test-file-extents.sh test-file-dir.sh
+EXTRA_DIST += test-file-extents.sh test-file-dir.sh
 
 # floppy plugin test.
 TESTS += test-floppy.sh

tests/test-file-dir.sh
@@ -1,0 +1,111 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test the use of the directory mode of the file plugin.
+
+source ./functions.sh
+set -e
+set -x
+
+# requires nbdinfo --version # nbdinfo 1.3.9 was broken, so check this instead:
+requires nbdkit -U - memory 1 --run 'nbdinfo --size --json "$uri"'
+requires jq --version
+
+files="file-dir file-dir.out file-dir.witness"
+rm -rf $files
+cleanup_fn rm -rf $files
+fail=0
+
+# do_nbdkit_fail NAME
+# Check that attempting to connect to export NAME fails
+do_nbdkit_fail ()
+{
+    # The --run script occurs only if nbdkit gets past .config_complete;
+    # testing for witness proves that our failure was during .open and
+    # not at some earlier point
+    rm -f file-dir.witness
+    nbdkit -U - -v -e "$1" file directory=file-dir \
+        --run 'touch file-dir.witness; nbdsh -u "$uri" -c "quit()"' && fail=1
+    test -f file-dir.witness || fail=1
+}
+
+# do_nbdkit_pass NAME DATA
+# Check that export NAME serves DATA as its first byte
+do_nbdkit_pass ()
+{
+    out=$(nbdkit -U - -v -e "$1" file directory=file-dir \
+        --run 'nbdsh -u "$uri" -c "print (h.pread (1, 0).decode (\"utf-8\"))"')
+    test "$out" = "$2" || fail=1
+}
+
+# Not possible to serve a missing directory
+nbdkit -vf file directory=nosuchdir && fail=1
+
+# Serving an empty directory
+mkdir file-dir
+do_nbdkit_fail ''
+do_nbdkit_fail 'a'
+do_nbdkit_fail '..'
+do_nbdkit_fail '/'
+
+# Serving a directory with one file
+echo 1 > file-dir/a
+do_nbdkit_fail ''
+do_nbdkit_pass a 1
+do_nbdkit_fail b
+
+# Serving a directory with multiple files.
+echo 2 > file-dir/b
+do_nbdkit_fail ''
+do_nbdkit_pass 'a' 1
+do_nbdkit_pass 'b' 2
+do_nbdkit_fail 'c'
+
+# Serving a directory with non-regular files
+ln -s b file-dir/c
+mkfifo file-dir/d
+mkdir file-dir/e
+ln -s /dev/null file-dir/f
+ln -s . file-dir/g
+ln -s dangling file-dir/h
+do_nbdkit_pass 'a' 1
+do_nbdkit_pass 'b' 2
+do_nbdkit_pass 'c' 2
+do_nbdkit_fail 'd'
+do_nbdkit_fail 'e'
+do_nbdkit_fail 'f'
+do_nbdkit_fail 'g'
+do_nbdkit_fail 'h'
+do_nbdkit_fail './a'
+do_nbdkit_fail '../file-dir/a'
+
+exit $fail

commit d5b32ec0c1e984322674640136f9dc87f2a99728

file: Avoid printf %s on NULL
Due to a semantic merge conflict (competing introduction of fadvise=
vs. dir= configuration parameters), a failure in posix_fadvise while
in dir mode would pass NULL to a %s format.  While glibc handles this
fine, it is not portable.

Fixes: 5c1f4d69
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/file/file.c
@@ -325,7 +325,7 @@ file_open (int readonly)
 #ifdef HAVE_POSIX_FADVISE
     int r = posix_fadvise (h->fd, 0, 0, fadvise_mode);
     if (r == -1)
-      nbdkit_debug ("posix_fadvise: %s: %m (ignored)", filename);
+      nbdkit_debug ("posix_fadvise: %s: %m (ignored)", file);
 #else
     nbdkit_debug ("fadvise is not supported");
 #endif