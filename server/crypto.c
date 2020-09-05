libguestfs/nbdkit

commit 43c2b0b63a90c8621646947857464e6405a80221

server: Add -D nbdkit.gnutls.log=N debug flag.

server/crypto.c
@@ -45,8 +45,10 @@
 #include <sys/types.h>
 #include <assert.h>
 
+#include "cleanup.h"
 #include "internal.h"
 #include "realpath.h"
+#include "strndup.h"
 
 #ifdef HAVE_GNUTLS
 
@@ -420,6 +422,27 @@ crypto_close (void)
   conn->crypto_session = NULL;
 }
 
+/* Turn GnuTLS debug messages into nbdkit debug messages
+ * when nbdkit -D nbdkit.gnutls.log > 0
+ */
+int nbdkit_debug_gnutls_log = 0;
+
+static void
+tls_log (int level, const char *msg)
+{
+  size_t len;
+  CLEANUP_FREE char *copy = NULL;
+
+  /* Strip trailing \n added by GnuTLS. */
+  len = strlen (msg);
+  if (len > 0 && msg[len-1] == '\n') {
+    copy = strndup (msg, len-1);
+    msg = copy;
+  }
+
+  nbdkit_debug ("gnutls: %d: %s", level, msg);
+}
+
 /* Upgrade an existing connection to TLS.  Also this should do access
  * control if enabled.  The protocol code ensures this function can
  * only be called once per connection.
@@ -439,6 +462,9 @@ crypto_negotiate_tls (int sockin, int sockout)
     free (session);
     return -1;
   }
+
+  gnutls_global_set_log_level (nbdkit_debug_gnutls_log);
+  gnutls_global_set_log_function (tls_log);
 
   switch (crypto_auth) {
   case CRYPTO_AUTH_CERTIFICATES:

docs/nbdkit.pod
@@ -586,6 +586,11 @@ and not usually very interesting.
 C<-D nbdkit.backend.controlpath=0> suppresses the non-datapath
 commands (config, open, close, can_write, etc.)
 
+=item B<-D nbdkit.gnutls.log=>N
+
+Enable GnuTLS logging.  C<N> can be in the range 0 (no logging) to 99.
+See L<gnutls_global_set_log_level(3)>.
+
 =back
 
 =head1 SIGNALS

commit e08c0007de937f690176662b5bf41cd4756a76b4

server: Don't overwrite GNUTLS_DEBUG_LEVEL log setting.
Only call gnutls_global_set_log_level if -D nbdkit.gnutls.log > 0.
This is because you can also set the log level through setting the
environment variable GNUTLS_DEBUG_LEVEL.  We don't want
gnutls_global_set_log_level to override that setting.  However
unconditionally install the log message to debug message translator.

Fixes: commit 43c2b0b63a90c8621646947857464e6405a80221

server/crypto.c
@@ -501,7 +501,8 @@ crypto_negotiate_tls (int sockin, int sockout)
     return -1;
   }
 
-  gnutls_global_set_log_level (nbdkit_debug_gnutls_log);
+  if (nbdkit_debug_gnutls_log > 0)
+    gnutls_global_set_log_level (nbdkit_debug_gnutls_log);
   gnutls_global_set_log_function (tls_log);
 
   switch (crypto_auth) {