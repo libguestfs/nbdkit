libguestfs/nbdkit

commit fa3aabc90b2b1af2c3a72a9d8d9668461ea31618

common/allocators: Refactor allocator registration.
Make allocators a little bit more independent.  They now register
themselves with the allocator code at start up.

This is just refactoring and should change nothing about how it works.
Unfortunately it does require every call to go through an extra
indirection although I guess that doesn't really matter.  It would be
possible to optimize this by copying the whole function table into
‘struct allocator’.

common/allocators/zstd.c
@@ -516,7 +516,7 @@ zstd_array_blit (struct allocator *a1,
   void *p;
 
   assert (a1 != a2);
-  assert (strcmp (a2->type, "zstd") == 0);
+  assert (strcmp (a2->f->type, "zstd") == 0);
 
   tbuf = malloc (PAGE_SIZE);
   if (tbuf == NULL) {
@@ -533,7 +533,7 @@ zstd_array_blit (struct allocator *a1,
     /* Read the source allocator (a1) directly to p which points into
      * the right place in za2.
      */
-    if (a1->read (a1, p, n, offset1) == -1)
+    if (a1->f->read (a1, p, n, offset1) == -1)
       return -1;
 
     if (compress (za2, offset2, tbuf) == -1)
@@ -593,20 +593,10 @@ zstd_array_extents (struct allocator *a,
   return 0;
 }
 
-static struct allocator functions = {
-  .free = zstd_array_free,
-  .set_size_hint = zstd_array_set_size_hint,
-  .read = zstd_array_read,
-  .write = zstd_array_write,
-  .fill = zstd_array_fill,
-  .zero = zstd_array_zero,
-  .blit = zstd_array_blit,
-  .extents = zstd_array_extents,
-};
-
 struct allocator *
-create_zstd_array (const parameters *params)
+zstd_array_create (const void *paramsv)
 {
+  const allocator_parameters *params  = paramsv;
   struct zstd_array *za;
 
   if (params->size > 0) {
@@ -619,7 +609,6 @@ create_zstd_array (const parameters *params)
     nbdkit_error ("calloc: %m");
     return NULL;
   }
-  za->a = functions;
 
   pthread_mutex_init (&za->lock, NULL);
 
@@ -642,4 +631,42 @@ create_zstd_array (const parameters *params)
   return (struct allocator *) za;
 }
 
-#endif /* HAVE_LIBZSTD */
+static struct allocator_functions functions = {
+  .type = "zstd",
+  .create = zstd_array_create,
+  .free = zstd_array_free,
+  .set_size_hint = zstd_array_set_size_hint,
+  .read = zstd_array_read,
+  .write = zstd_array_write,
+  .fill = zstd_array_fill,
+  .zero = zstd_array_zero,
+  .blit = zstd_array_blit,
+  .extents = zstd_array_extents,
+};
+
+#else /* !HAVE_LIBZSTD */
+
+/* If zstd is not compiled in, this is a dummy allocator that always
+ * fails with an error.
+ */
+struct allocator *
+zstd_array_create (const void *params)
+{
+  nbdkit_error ("allocator=zstd is not supported in this build of nbdkit");
+  return NULL;
+}
+
+static struct allocator_functions functions = {
+  .type = "zstd",
+  .create = zstd_array_create,
+};
+
+#endif /* !HAVE_LIBZSTD */
+
+static void register_zstd_array (void) __attribute__((constructor));
+
+static void
+register_zstd_array (void)
+{
+  register_allocator (&functions);
+}

common/allocators/allocator-internal.h
@@ -33,18 +33,11 @@
 #ifndef NBDKIT_ALLOCATOR_INTERNAL_H
 #define NBDKIT_ALLOCATOR_INTERNAL_H
 
-#include <stdbool.h>
-
 #include "vector.h"
 
 struct key_value { char *key, *value; };
-DEFINE_VECTOR_TYPE(parameters, struct key_value);
+DEFINE_VECTOR_TYPE(allocator_parameters, struct key_value);
 
-extern struct allocator *create_malloc (const parameters *);
-extern struct allocator *create_sparse_array (const parameters *);
-
-#ifdef HAVE_LIBZSTD
-extern struct allocator *create_zstd_array (const parameters *);
-#endif
+extern void register_allocator (const struct allocator_functions *);
 
 #endif /* NBDKIT_ALLOCATOR_INTERNAL_H */

common/allocators/allocator.c
@@ -41,6 +41,20 @@
 
 #include "allocator.h"
 #include "allocator-internal.h"
+#include "vector.h"
+
+/* The list of registered allocators. */
+DEFINE_VECTOR_TYPE(allocator_list, const struct allocator_functions *);
+static allocator_list allocators = empty_vector;
+
+void
+register_allocator (const struct allocator_functions *f)
+{
+  if (allocator_list_append (&allocators, f) == -1) {
+    perror ("realloc");
+    exit (EXIT_FAILURE);
+  }
+}
 
 static void
 free_key_value (struct key_value kv)
@@ -50,15 +64,16 @@ free_key_value (struct key_value kv)
 }
 
 static void
-free_parameters (parameters *params)
+free_allocator_parameters (allocator_parameters *params)
 {
-  parameters_iter (params, free_key_value);
+  allocator_parameters_iter (params, free_key_value);
   free (params->ptr);
 }
 
 /* The type may be followed by parameters "type,key=value[,...]" */
 static int
-parse_parameters (const char *type, size_t *type_len, parameters *params)
+parse_parameters (const char *type, size_t *type_len,
+                  allocator_parameters *params)
 {
   size_t i, j, len;
 
@@ -80,7 +95,7 @@ parse_parameters (const char *type, size_t *type_len, parameters *params)
     j = strcspn (&type[i], "=");
     if (j == 0) {
       nbdkit_error ("invalid allocator parameter");
-      free_parameters (params);
+      free_allocator_parameters (params);
       return -1;
     }
     if (j < len) {
@@ -95,14 +110,14 @@ parse_parameters (const char *type, size_t *type_len, parameters *params)
       nbdkit_error ("strdup: %m");
       free (kv.key);
       free (kv.value);
-      free_parameters (params);
+      free_allocator_parameters (params);
       return -1;
     }
 
     nbdkit_debug ("allocator parameter: %s=%s", kv.key, kv.value);
-    if (parameters_append (params, kv) == -1) {
+    if (allocator_parameters_append (params, kv) == -1) {
       nbdkit_error ("realloc: %m");
-      free_parameters (params);
+      free_allocator_parameters (params);
       return -1;
     }
     i += len;
@@ -115,38 +130,30 @@ struct allocator *
 create_allocator (const char *type, bool debug)
 {
   struct allocator *ret = NULL;
-  parameters params = empty_vector;
-  size_t type_len;
+  allocator_parameters params = empty_vector;
+  size_t i, type_len;
 
   if (parse_parameters (type, &type_len, &params) == -1)
     return NULL;
 
-  if (strncmp (type, "sparse", type_len) == 0) {
-    ret = create_sparse_array (&params);
-    if (ret) ret->type = "sparse";
+  /* See if we can find the allocator. */
+  for (i = 0; i < allocators.size; ++i) {
+    if (strncmp (type, allocators.ptr[i]->type, type_len) == 0) {
+      ret = allocators.ptr[i]->create (&params);
+      break;
+    }
   }
 
-  else if (strncmp (type, "malloc", type_len) == 0) {
-    ret = create_malloc (&params);
-    if (ret) ret->type = "malloc";
-  }
-
-  else if (strncmp (type, "zstd", type_len) == 0) {
-#ifdef HAVE_LIBZSTD
-    ret = create_zstd_array (&params);
-    if (ret) ret->type = "zstd";
-#else
-    nbdkit_error ("allocator=zstd is not supported in this build of nbdkit");
-#endif
-  }
-
-  else
+  if (ret == NULL)
     nbdkit_error ("unknown allocator \"%s\"", type);
 
   /* Free the parameters allocated above. */
-  free_parameters (&params);
+  free_allocator_parameters (&params);
 
-  if (ret) ret->debug = debug;
+  if (ret) {
+    ret->debug = debug;
+    ret->f = allocators.ptr[i];
+  }
   return ret;
 }
 
@@ -155,6 +162,6 @@ cleanup_free_allocator (struct allocator **ap)
 {
   struct allocator *a = *ap;
 
-  if (a)
-    a->free (a);
+  if (a && a->f)
+    a->f->free (a);
 }

common/allocators/allocator.h
@@ -47,14 +47,17 @@
 
 struct nbdkit_extents;
 
-struct allocator {
+struct allocator_functions {
   /* Allocator type (eg. "sparse").
    * This does not include the parameters.
    */
   const char *type;
 
-  /* Debug flag. */
-  bool debug;
+  /* Private function called to create an allocator.  It should return
+   * a new struct which is at least as large as struct allocator.  It
+   * may have extra fields for the specific allocator to use.
+   */
+  struct allocator *(*create) (const void *);
 
   /* Free the allocator instance. */
   void (*free) (struct allocator *a);
@@ -118,6 +121,13 @@ struct allocator {
                   uint64_t count, uint64_t offset,
                   struct nbdkit_extents *extents)
   __attribute__((__nonnull__ (1, 4)));
+};
+
+struct allocator {
+  const struct allocator_functions *f;
+
+  /* Debug flag. */
+  bool debug;
 };
 
 /* Create a new allocator, usually from the type passed in the

common/allocators/malloc.c
@@ -227,14 +227,14 @@ m_alloc_blit (struct allocator *a1, struct allocator *a2,
   struct m_alloc *ma2 = (struct m_alloc *) a2;
 
   assert (a1 != a2);
-  assert (strcmp (a2->type, "malloc") == 0);
+  assert (strcmp (a2->f->type, "malloc") == 0);
 
   if (extend (ma2, offset2+count) == -1)
     return -1;
 
   /* See comment in m_alloc_write. */
   ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&ma2->lock);
-  return a1->read (a1, ma2->ba.ptr + offset2, count, offset1);
+  return a1->f->read (a1, ma2->ba.ptr + offset2, count, offset1);
 }
 
 static int
@@ -251,20 +251,10 @@ m_alloc_extents (struct allocator *a,
   return nbdkit_add_extent (extents, offset, count, 0);
 }
 
-static struct allocator functions = {
-  .free = m_alloc_free,
-  .set_size_hint = m_alloc_set_size_hint,
-  .read = m_alloc_read,
-  .write = m_alloc_write,
-  .fill = m_alloc_fill,
-  .zero = m_alloc_zero,
-  .blit = m_alloc_blit,
-  .extents = m_alloc_extents,
-};
-
 struct allocator *
-create_malloc (const parameters *params)
+m_alloc_create (const void *paramsv)
 {
+  const allocator_parameters *params  = paramsv;
   struct m_alloc *ma;
   bool use_mlock = false;
   size_t i;
@@ -294,9 +284,29 @@ create_malloc (const parameters *params)
     nbdkit_error ("calloc: %m");
     return NULL;
   }
-  ma->a = functions;
   ma->use_mlock = use_mlock;
   pthread_rwlock_init (&ma->lock, NULL);
   ma->ba = (bytearray) empty_vector;
   return (struct allocator *) ma;
+}
+
+static struct allocator_functions functions = {
+  .type = "malloc",
+  .create = m_alloc_create,
+  .free = m_alloc_free,
+  .set_size_hint = m_alloc_set_size_hint,
+  .read = m_alloc_read,
+  .write = m_alloc_write,
+  .fill = m_alloc_fill,
+  .zero = m_alloc_zero,
+  .blit = m_alloc_blit,
+  .extents = m_alloc_extents,
+};
+
+static void register_malloc (void) __attribute__((constructor));
+
+static void
+register_malloc (void)
+{
+  register_allocator (&functions);
 }

common/allocators/sparse.c
@@ -430,7 +430,7 @@ sparse_array_blit (struct allocator *a1,
   struct l2_entry *l2_entry;
 
   assert (a1 != a2);
-  assert (strcmp (a2->type, "sparse") == 0);
+  assert (strcmp (a2->f->type, "sparse") == 0);
 
   while (count > 0) {
     p = lookup (sa2, offset2, true, &n, &l2_entry);
@@ -443,7 +443,7 @@ sparse_array_blit (struct allocator *a1,
     /* Read the source allocator (a1) directly to p which points into
      * the right place in sa2.
      */
-    if (a1->read (a1, p, n, offset1) == -1)
+    if (a1->f->read (a1, p, n, offset1) == -1)
       return -1;
 
     /* If the whole page is now zero, free it. */
@@ -501,20 +501,10 @@ sparse_array_extents (struct allocator *a,
   return 0;
 }
 
-static struct allocator functions = {
-  .free = sparse_array_free,
-  .set_size_hint = sparse_array_set_size_hint,
-  .read = sparse_array_read,
-  .write = sparse_array_write,
-  .fill = sparse_array_fill,
-  .zero = sparse_array_zero,
-  .blit = sparse_array_blit,
-  .extents = sparse_array_extents,
-};
-
-struct allocator *
-create_sparse_array (const parameters *params)
+static struct allocator *
+sparse_array_create (const void *paramsv)
 {
+  const allocator_parameters *params  = paramsv;
   struct sparse_array *sa;
 
   if (params->size > 0) {
@@ -527,7 +517,28 @@ create_sparse_array (const parameters *params)
     nbdkit_error ("calloc: %m");
     return NULL;
   }
-  sa->a = functions;
   pthread_mutex_init (&sa->lock, NULL);
+
   return (struct allocator *) sa;
+}
+
+static struct allocator_functions functions = {
+  .type = "sparse",
+  .create = sparse_array_create,
+  .free = sparse_array_free,
+  .set_size_hint = sparse_array_set_size_hint,
+  .read = sparse_array_read,
+  .write = sparse_array_write,
+  .fill = sparse_array_fill,
+  .zero = sparse_array_zero,
+  .blit = sparse_array_blit,
+  .extents = sparse_array_extents,
+};
+
+static void register_sparse_array (void) __attribute__((constructor));
+
+static void
+register_sparse_array (void)
+{
+  register_allocator (&functions);
 }

plugins/data/data.c
@@ -78,7 +78,7 @@ static void
 data_unload (void)
 {
   if (a)
-    a->free (a);
+    a->f->free (a);
 }
 
 /* Parse the base64 parameter. */
@@ -97,7 +97,7 @@ read_base64 (const char *value, uint64_t *size_ret)
     return -1;
   }
 
-  if (a->write (a, out.data, out.size, 0) == -1)
+  if (a->f->write (a, out.data, out.size, 0) == -1)
     return -1;
   free (out.data);
   *size_ret = out.size;
@@ -201,7 +201,7 @@ data_get_ready (void)
   switch (data_seen) {
   case RAW:
     data_size = strlen (data_param);
-    if (a->write (a, data_param, data_size, 0) == -1)
+    if (a->f->write (a, data_param, data_size, 0) == -1)
       return -1;
     break;
 
@@ -229,7 +229,7 @@ data_get_ready (void)
     size = data_size;
   nbdkit_debug ("final size: %" PRIi64, size);
 
-  if (a->set_size_hint (a, size) == -1)
+  if (a->f->set_size_hint (a, size) == -1)
     return -1;
 
   return 0;
@@ -307,7 +307,7 @@ data_pread (void *handle, void *buf, uint32_t count, uint64_t offset,
             uint32_t flags)
 {
   assert (!flags);
-  return a->read (a, buf, count, offset);
+  return a->f->read (a, buf, count, offset);
 }
 
 /* Write data. */
@@ -317,7 +317,7 @@ data_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
 {
   /* Flushing, and thus FUA flag, is a no-op */
   assert ((flags & ~NBDKIT_FLAG_FUA) == 0);
-  return a->write (a, buf, count, offset);
+  return a->f->write (a, buf, count, offset);
 }
 
 /* Zero. */
@@ -325,10 +325,10 @@ static int
 data_zero (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
 {
   /* Flushing, and thus FUA flag, is a no-op. Assume that
-   * a->zero generally beats writes, so FAST_ZERO is a no-op. */
+   * a->f->zero generally beats writes, so FAST_ZERO is a no-op. */
   assert ((flags & ~(NBDKIT_FLAG_FUA | NBDKIT_FLAG_MAY_TRIM |
                      NBDKIT_FLAG_FAST_ZERO)) == 0);
-  return a->zero (a, count, offset);
+  return a->f->zero (a, count, offset);
 }
 
 /* Trim (same as zero). */
@@ -337,7 +337,7 @@ data_trim (void *handle, uint32_t count, uint64_t offset, uint32_t flags)
 {
   /* Flushing, and thus FUA flag, is a no-op */
   assert ((flags & ~NBDKIT_FLAG_FUA) == 0);
-  return a->zero (a, count, offset);
+  return a->f->zero (a, count, offset);
 }
 
 /* Nothing is persistent, so flush is trivially supported */
@@ -352,7 +352,7 @@ static int
 data_extents (void *handle, uint32_t count, uint64_t offset,
               uint32_t flags, struct nbdkit_extents *extents)
 {
-  return a->extents (a, count, offset, extents);
+  return a->f->extents (a, count, offset, extents);
 }
 
 static struct nbdkit_plugin plugin = {

plugins/data/format.c
@@ -839,7 +839,7 @@ evaluate (const dict_t *dict, const expr_t *e,
 
     case EXPR_BYTE:
       /* Store the byte. */
-      if (a->write (a, &e->b, 1, *offset) == -1)
+      if (a->f->write (a, &e->b, 1, *offset) == -1)
         return -1;
       (*offset)++;
       break;
@@ -876,7 +876,7 @@ evaluate (const dict_t *dict, const expr_t *e,
 
     case EXPR_STRING:
       /* Copy the string into the allocator. */
-      if (a->write (a, e->string.ptr, e->string.size, *offset) == -1)
+      if (a->f->write (a, e->string.ptr, e->string.size, *offset) == -1)
         return -1;
       *offset += e->string.size;
       break;
@@ -921,7 +921,7 @@ evaluate (const dict_t *dict, const expr_t *e,
       if (evaluate (t->next, t->expr, a2, &offset2, &size2) == -1)
         return -1;
 
-      if (a->blit (a2, a, size2, 0, *offset) == -1)
+      if (a->f->blit (a2, a, size2, 0, *offset) == -1)
         return -1;
       *offset += size2;
       break;
@@ -938,7 +938,7 @@ evaluate (const dict_t *dict, const expr_t *e,
        * sense to optimize it here.
        */
       if (e->t == EXPR_REPEAT && e->expr->t == EXPR_BYTE) {
-        if (a->fill (a, e->expr->b, e->r.n, *offset) == -1)
+        if (a->f->fill (a, e->expr->b, e->r.n, *offset) == -1)
           return -1;
         *offset += e->r.n;
       }
@@ -981,14 +981,14 @@ evaluate (const dict_t *dict, const expr_t *e,
 
         switch (e->t) {
         case EXPR_EXPR:
-          if (a->blit (a2, a, size2, 0, *offset) == -1)
+          if (a->f->blit (a2, a, size2, 0, *offset) == -1)
             return -1;
           *offset += size2;
           break;
         case EXPR_REPEAT:
           /* Duplicate the allocator a2 N times. */
           for (j = 0; j < e->r.n; ++j) {
-            if (a->blit (a2, a, size2, 0, *offset) == -1)
+            if (a->f->blit (a2, a, size2, 0, *offset) == -1)
               return -1;
             *offset += size2;
           }
@@ -1003,7 +1003,7 @@ evaluate (const dict_t *dict, const expr_t *e,
             return -1;
           }
           /* Take a slice from the allocator. */
-          if (a->blit (a2, a, m-e->sl.n, e->sl.n, *offset) == -1)
+          if (a->f->blit (a2, a, m-e->sl.n, e->sl.n, *offset) == -1)
             return -1;
           *offset += m-e->sl.n;
           break;
@@ -1047,7 +1047,7 @@ store_file (struct allocator *a,
   while (!feof (fp)) {
     n = fread (buf, 1, BUFSIZ, fp);
     if (n > 0) {
-      if (a->write (a, buf, n, *offset) == -1) {
+      if (a->f->write (a, buf, n, *offset) == -1) {
         fclose (fp);
         return -1;
       }
@@ -1101,7 +1101,7 @@ store_file_slice (struct allocator *a,
   while (!feof (fp) && (end == -1 || len > 0)) {
     n = fread (buf, 1, end == -1 ? BUFSIZ : MIN (len, BUFSIZ), fp);
     if (n > 0) {
-      if (a->write (a, buf, n, *offset) == -1) {
+      if (a->f->write (a, buf, n, *offset) == -1) {
         fclose (fp);
         return -1;
       }
@@ -1143,7 +1143,7 @@ store_script (struct allocator *a,
   while (!feof (pp)) {
     n = fread (buf, 1, BUFSIZ, pp);
     if (n > 0) {
-      if (a->write (a, buf, n, *offset) == -1) {
+      if (a->f->write (a, buf, n, *offset) == -1) {
         pclose (pp);
         return -1;
       }
@@ -1185,7 +1185,7 @@ store_script_len (struct allocator *a,
   while (!feof (pp) && len > 0) {
     n = fread (buf, 1, MIN (len, BUFSIZ), pp);
     if (n > 0) {
-      if (a->write (a, buf, n, *offset) == -1) {
+      if (a->f->write (a, buf, n, *offset) == -1) {
         pclose (pp);
         return -1;
       }

commit 197e87d32c025879d595a3d8e0470521159307bf

common/allocators/zstd.c: Don't register zstd at all when not compiled.
Fixes: commit fa3aabc90b2b1af2c3a72a9d8d9668461ea31618

common/allocators/zstd.c
@@ -644,25 +644,6 @@ static struct allocator_functions functions = {
   .extents = zstd_array_extents,
 };
 
-#else /* !HAVE_LIBZSTD */
-
-/* If zstd is not compiled in, this is a dummy allocator that always
- * fails with an error.
- */
-struct allocator *
-zstd_array_create (const void *params)
-{
-  nbdkit_error ("allocator=zstd is not supported in this build of nbdkit");
-  return NULL;
-}
-
-static struct allocator_functions functions = {
-  .type = "zstd",
-  .create = zstd_array_create,
-};
-
-#endif /* !HAVE_LIBZSTD */
-
 static void register_zstd_array (void) __attribute__((constructor));
 
 static void
@@ -670,3 +651,5 @@ register_zstd_array (void)
 {
   register_allocator (&functions);
 }
+
+#endif /* !HAVE_LIBZSTD */