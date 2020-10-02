libguestfs/nbdkit

commit cad809adcfd612f5cb2272cead79050a2d8b7337

windows: wrapper: Fix "really weird" spawn escaping.

wrapper.c
@@ -135,14 +135,15 @@ print_command (void)
 #ifdef WIN32
 /* Windows behaviour of _spawnvp is completely insane:
  * https://stackoverflow.com/questions/4146980/how-to-avoid-space-splitting-and-quote-removal-with-spawnvp
+ * See also glib sources where it's described as "really weird".
  */
 static const char *
 quote_string_for_spawn (const char *str)
 {
-  size_t i, len;
+  size_t i, len, pb;
   char *p, *ret = (char *) str;
 
-  if (strchr (str, ' ') || strchr (str, '"')) {
+  if (strchr (str, ' ') || strchr (str, '\t')) {
     len = strlen (str);
 
     p = ret = malloc (2 + len*2 + 1);
@@ -152,17 +153,26 @@ quote_string_for_spawn (const char *str)
     }
 
     *p++ = '"';
+    pb = 0;
     for (i = 0; i < len; ++i) {
       switch (str[i]) {
       case '"':
-      case '\\':
         *p++ = '\\';
+        for (; pb > 0; --pb)
+          *p++ = '\\';
+        *p++ = '"';
+        break;
+      case '\\':
+        pb++;
         *p++ = str[i];
         break;
       default:
+        pb = 0;
         *p++ = str[i];
       }
     }
+    for (; pb > 0; --pb)
+      *p++ = '\\';
     *p++ = '"';
     *p++ = '\0';
   }

commit 241318c70c4e5674cb665dd948108564b82a2804

windows: wrapper: Quote the empty parameter correctly for _spawnvp.
Our quoting for _spawnvp was not correct when the empty parameter is
present on the command line as in this example:

$ ./nbdkit.exe -fv data ''
server/nbdkit.exe -f -v -- plugins/data/.libs/nbdkit-data-plugin.dll ""
00bc:fixme:advapi:RegisterEventSourceA ((null),"nbdkit"): stub
00bc:fixme:advapi:RegisterEventSourceW (L"",L"nbdkit"): stub
nbdkit: debug: TLS disabled: could not load TLS certificates
nbdkit: debug: registering plugin plugins/data/.libs/nbdkit-data-plugin.dll
nbdkit: debug: registered plugin plugins/data/.libs/nbdkit-data-plugin.dll (name data)
nbdkit: debug: data: load
nbdkit: debug: data: config_complete
nbdkit: error: raw|base64|data parameter was not specified

Fixes: commit cad809adcfd612f5cb2272cead79050a2d8b7337

wrapper.c
@@ -143,7 +143,7 @@ quote_string_for_spawn (const char *str)
   size_t i, len, pb;
   char *p, *ret = (char *) str;
 
-  if (strchr (str, ' ') || strchr (str, '\t')) {
+  if (*str == 0 || strchr (str, ' ') || strchr (str, '\t')) {
     len = strlen (str);
 
     p = ret = malloc (2 + len*2 + 1);