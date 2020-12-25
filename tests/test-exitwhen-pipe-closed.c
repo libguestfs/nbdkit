libguestfs/nbdkit

commit 5c70b3bf3726b1271a93ed5244260e854f86b509

New filter: exitwhen: exit gracefully when an event occurs.

filters/exitwhen/Makefile.am
@@ -1,0 +1,67 @@
+# nbdkit
+# Copyright (C) 2019-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-exitwhen-filter.pod
+
+filter_LTLIBRARIES = nbdkit-exitwhen-filter.la
+
+nbdkit_exitwhen_filter_la_SOURCES = \
+	exitwhen.c \
+	$(top_srcdir)/include/nbdkit-filter.h \
+	$(NULL)
+
+nbdkit_exitwhen_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdkit_exitwhen_filter_la_CFLAGS = $(WARNINGS_CFLAGS)
+nbdkit_exitwhen_filter_la_LIBADD = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(IMPORT_LIBRARY_ON_WINDOWS) \
+	$(NULL)
+nbdkit_exitwhen_filter_la_LDFLAGS = \
+	-module -avoid-version -shared $(NO_UNDEFINED_ON_WINDOWS) \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms \
+	$(NULL)
+
+if HAVE_POD
+
+man_MANS = nbdkit-exitwhen-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-exitwhen-filter.1: nbdkit-exitwhen-filter.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

filters/exitwhen/exitwhen.c
@@ -1,0 +1,480 @@
+/* nbdkit
+ * Copyright (C) 2019-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "cleanup.h"
+#include "utils.h"
+#include "vector.h"
+
+static unsigned pollsecs = 60;
+
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static unsigned connections = 0;
+static bool exiting = false;
+
+/* The list of events generated from command line parameters. */
+struct event {
+  enum { EVENT_FILE_CREATED = 1, EVENT_FILE_DELETED, EVENT_PROCESS_EXITS,
+         EVENT_FD_CLOSED, EVENT_SCRIPT } type;
+  union {
+    char *filename;             /* Filename or script. */
+    int fd;                     /* For PROCESS_EXITS or FD_CLOSED. */
+#ifndef __linux__
+    pid_t pid;                  /* For PROCESS_EXITS on non-Linux. */
+#endif
+  } u;
+};
+DEFINE_VECTOR_TYPE(event_list, struct event);
+static event_list events = empty_vector;
+
+static void
+free_event (struct event event)
+{
+  switch (event.type) {
+  case EVENT_FILE_CREATED:
+  case EVENT_FILE_DELETED:
+  case EVENT_SCRIPT:
+    free (event.u.filename);
+    break;
+  case EVENT_PROCESS_EXITS:
+#ifdef __linux__
+  case EVENT_FD_CLOSED:
+#endif
+    close (event.u.fd);
+    break;
+#ifndef __linux__
+  case EVENT_FD_CLOSED:
+    break;
+#endif
+  }
+}
+
+static void
+exitwhen_unload (void)
+{
+  event_list_iter (&events, free_event);
+  free (events.ptr);
+}
+
+/* If exiting is already true, this does nothing and returns true.
+ * Otherwise it checks if any event in the list has happened.  If an
+ * event has happened, sets exiting to true.  It returns the exiting
+ * flag.
+ *
+ * This must be called with &lock held.
+ */
+static void check_for_event_file_created (const struct event *);
+static void check_for_event_file_deleted (const struct event *);
+static void check_for_event_process_exits (const struct event *);
+static void check_for_event_fd_closed (const struct event *);
+static void check_for_event_script (const struct event *);
+
+static bool
+check_for_event (void)
+{
+  size_t i;
+
+  if (!exiting) {
+    for (i = 0; i < events.size; ++i) {
+      const struct event *event = &events.ptr[i];
+
+      switch (event->type) {
+      case EVENT_FILE_CREATED:
+        check_for_event_file_created (event);
+        break;
+      case EVENT_FILE_DELETED:
+        check_for_event_file_deleted (event);
+        break;
+      case EVENT_PROCESS_EXITS:
+        check_for_event_process_exits (event);
+        break;
+      case EVENT_FD_CLOSED:
+        check_for_event_fd_closed (event);
+        break;
+      case EVENT_SCRIPT:
+        check_for_event_script (event);
+        break;
+      }
+    }
+  }
+
+  return exiting;
+}
+
+static void
+check_for_event_file_created (const struct event *event)
+{
+  if (access (event->u.filename, R_OK) == 0) {
+    nbdkit_debug ("exit-when-file-created: detected %s created",
+                  event->u.filename);
+    exiting = true;
+  }
+}
+
+static void
+check_for_event_file_deleted (const struct event *event)
+{
+  if (access (event->u.filename, R_OK) == -1) {
+    if (errno == ENOTDIR || errno == ENOENT) {
+      nbdkit_debug ("exit-when-file-deleted: detected %s deleted",
+                    event->u.filename);
+      exiting = true;
+    }
+    else {
+      /* Log the error but continue. */
+      nbdkit_error ("exit-when-file-deleted: access: %s: %m",
+                    event->u.filename);
+    }
+  }
+}
+
+static void
+check_for_event_process_exits (const struct event *event)
+{
+  char c;
+
+#ifdef __linux__
+  /* https://gitlab.freedesktop.org/polkit/polkit/-/issues/75
+   *
+   * event->u.fd holds /proc/PID/stat of the original process open.
+   * If we can still read a byte from it then the original process is
+   * still around.  If we get ESRCH then the process has exited.
+   */
+  lseek (event->u.fd, 0, SEEK_SET);
+  if (read (event->u.fd, &c, 1) == -1) {
+    if (errno == ESRCH) {
+      nbdkit_debug ("exit-when-process-exits: detected process exit");
+      exiting = true;
+    }
+    else {
+      /* Log the error but continue. */
+      nbdkit_error ("exit-when-process-exits: read: %m");
+    }
+  }
+#else /* !__linux__ */
+  /* XXX Find a safe way to do this on BSD at least. */
+  if (kill (event->u.pid, 0) == -1 && errno == ESRCH) {
+    nbdkit_debug ("exit-when-process-exits: detected process exit");
+    exiting = true;
+  }
+#endif /* !__linux__ */
+}
+
+static void
+check_for_event_fd_closed (const struct event *event)
+{
+  int r;
+  struct pollfd fds[1];
+
+  /* event->u.fd is the read side of a pipe or socket.  Check it is
+   * not closed.  We don't actually read anything from the pipe.
+   */
+  fds[0].fd = event->u.fd;
+  fds[0].events = 0;
+  r = poll (fds, 1, 0);
+  if (r == 1) {
+    if ((fds[0].revents & POLLHUP) != 0) {
+      nbdkit_debug ("exit-when-pipe-closed: detected pipe closed");
+      exiting = true;
+    }
+    else if ((fds[0].revents & POLLNVAL) != 0) {
+      /* If we were passed a bad file descriptor that is user error
+       * and we should exit with an error early.  Because
+       * check_for_event() is called first in get_ready() this should
+       * cause this to happen.
+       */
+      nbdkit_error ("exit-when-pipe-closed: invalid file descriptor");
+      exiting = true;
+    }
+  }
+  else if (r == -1) {
+    /* Log the error but continue. */
+    nbdkit_error ("exit-when-pipe-closed: poll: %m");
+  }
+}
+
+static void
+check_for_event_script (const struct event *event)
+{
+  int r;
+
+  /* event->u.filename is a script filename or command.  Exit code 88
+   * indicates the event has happened.
+   */
+  r = system (event->u.filename);
+  if (r == -1) {
+    /* Log the error but continue. */
+    nbdkit_error ("exit-when-script: %m");
+  }
+  else if (WIFEXITED (r) && WEXITSTATUS (r) == 0) {
+    /* Normal case, do nothing. */
+  }
+  else if (WIFEXITED (r) && WEXITSTATUS (r) == 88) {
+    nbdkit_debug ("exit-when-script: detected scripted event");
+    exiting = true;
+  }
+  else {
+    /* Log the error but continue. */
+    exit_status_to_nbd_error (r, "exit-when-script");
+  }
+}
+
+/* The background polling thread.
+ *
+ * This runs continuously in the background, but you can pause it by
+ * grabbing &pause_lock (use the pause/resume_polling_thread()
+ * wrappers below).
+ */
+static pthread_mutex_t pause_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static void *
+polling_thread (void *vp)
+{
+  for (;;) {
+    {
+      /* Note the order here is chosen to avoid possible deadlock
+       * because the caller of pause_polling_thread() always acquires
+       * &lock before &pause_lock.
+       */
+      ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+      ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&pause_lock);
+      if (check_for_event ()) {
+        nbdkit_debug ("exitwhen: shutdown from polling thread");
+        nbdkit_shutdown ();
+      }
+    }
+
+    sleep (pollsecs);
+  }
+}
+
+/* Call this to pause the polling thread.  &lock must be held. */
+static void
+pause_polling_thread (void)
+{
+  pthread_mutex_lock (&pause_lock);
+}
+
+/* Call this to resume the polling thread. */
+static void
+resume_polling_thread (void)
+{
+  pthread_mutex_unlock (&pause_lock);
+}
+
+/* Read command line parameters are build events list. */
+static int
+exitwhen_config (nbdkit_next_config *next, void *nxdata,
+                 const char *key, const char *value)
+{
+  struct event event;
+
+  if (strcmp (key, "exit-when-file-created") == 0 ||
+      strcmp (key, "exit-when-file-deleted") == 0) {
+    char c = key[15];
+
+    assert (c == 'c' || c == 'd');
+    event.type = c == 'c' ? EVENT_FILE_CREATED : EVENT_FILE_DELETED;
+    event.u.filename = nbdkit_absolute_path (value);
+    if (event.u.filename == NULL)
+      return -1;
+  append_event:
+    if (event_list_append (&events, event) == -1)
+      return -1;
+    return 0;
+  }
+  else if (strcmp (key, "exit-when-pipe-closed") == 0 ||
+           strcmp (key, "exit-when-fd-closed") == 0) {
+    event.type = EVENT_FD_CLOSED;
+    if (nbdkit_parse_int ("exit-when-pipe-closed", value, &event.u.fd) == -1)
+      return -1;
+    goto append_event;
+  }
+  else if (strcmp (key, "exit-when-process-exits") == 0 ||
+           strcmp (key, "exit-when-pid-exits") == 0) {
+    uint64_t pid;
+    CLEANUP_FREE char *str = NULL;
+
+    event.type = EVENT_PROCESS_EXITS;
+    if (nbdkit_parse_uint64_t ("exit-when-process-exits", value, &pid) == -1)
+      return -1;
+#ifdef __linux__
+    /* See: https://gitlab.freedesktop.org/polkit/polkit/-/issues/75 */
+    if (asprintf (&str, "/proc/%" PRIu64 "/stat", pid) == -1) {
+      nbdkit_error ("asprintf: %m");
+      return -1;
+    }
+    event.u.fd = open (str, O_RDONLY);
+    if (event.u.fd == -1) {
+      nbdkit_error ("exit-when-process-exits: %s: %m", str);
+      return -1;
+    }
+#else
+    event.u.pid = (pid_t) pid;
+#endif
+    goto append_event;
+  }
+  else if (strcmp (key, "exit-when-script") == 0) {
+    event.type = EVENT_SCRIPT;
+    event.u.filename = strdup (value);
+    if (event.u.filename == NULL) {
+      nbdkit_error ("strdup: %m");
+      return -1;
+    }
+    goto append_event;
+  }
+
+  /* This is a setting, not an event. */
+  if (strcmp (key, "exit-when-poll") == 0) {
+    if (nbdkit_parse_unsigned ("exit-when-poll", value, &pollsecs) == -1)
+      return -1;
+    return 0;
+  }
+
+  /* Otherwise pass the parameter to the plugin. */
+  return next (nxdata, key, value);
+}
+
+/* Before forking, run the check.  If the event has already happened
+ * then we exit immediately.
+ */
+static int
+exitwhen_get_ready (nbdkit_next_get_ready *next, void *nxdata,
+                    int thread_model)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+
+  if (check_for_event ())
+    exit (EXIT_SUCCESS);
+
+  return next (nxdata);
+}
+
+/* After forking, start the background thread.  Initially it is
+ * polling.
+ */
+static int
+exitwhen_after_fork (nbdkit_next_after_fork *next, void *nxdata)
+{
+  int err;
+  pthread_t thread;
+
+  err = pthread_create (&thread, NULL, polling_thread, NULL);
+  if (err != 0) {
+    errno = err;
+    nbdkit_error ("pthread_create: %m");
+    return -1;
+  }
+  return next (nxdata);
+}
+
+static int
+exitwhen_preconnect (nbdkit_next_preconnect *next, void *nxdata, int readonly)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+
+  if (check_for_event ()) {
+    nbdkit_error ("exitwhen: nbdkit is exiting: rejecting new connection");
+    return -1;
+  }
+
+  if (next (nxdata, readonly) == -1)
+    return -1;
+
+  return 0;
+}
+
+static void *
+exitwhen_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
+               int readonly, const char *exportname, int is_tls)
+{
+  if (next (nxdata, readonly, exportname) == -1)
+    return NULL;
+
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+  connections++;
+  if (connections == 1)
+    pause_polling_thread ();
+
+  return NBDKIT_HANDLE_NOT_NEEDED;
+}
+
+static void
+exitwhen_close (void *handle)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+
+  check_for_event ();
+
+  --connections;
+  if (connections == 0) {
+    if (exiting) {
+      nbdkit_debug ("exitwhen: exiting on last client connection");
+      nbdkit_shutdown ();
+    }
+    else
+      resume_polling_thread ();
+  }
+}
+
+static struct nbdkit_filter filter = {
+  .name              = "exitwhen",
+  .longname          = "nbdkit exitwhen filter",
+  .unload            = exitwhen_unload,
+
+  .config            = exitwhen_config,
+  .get_ready         = exitwhen_get_ready,
+  .after_fork        = exitwhen_after_fork,
+
+  .preconnect        = exitwhen_preconnect,
+  .open              = exitwhen_open,
+  .close             = exitwhen_close,
+};
+
+NBDKIT_REGISTER_FILTER(filter)

tests/test-exitwhen-pipe-closed.c
@@ -1,0 +1,81 @@
+/* nbdkit
+ * Copyright (C) 2017-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int argc, char *argv[])
+{
+  char *param;
+  int fd[2];
+  pid_t pid;
+
+  if (pipe (fd) == -1) {
+    perror ("pipe");
+    exit (EXIT_FAILURE);
+  }
+  if (asprintf (&param, "exit-when-pipe-closed=%d", fd[0]) == -1) {
+    perror ("asprintf");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Run nbdkit. */
+  pid = fork ();
+  if (pid == -1) {
+    perror ("fork");
+    exit (EXIT_FAILURE);
+  }
+  if (pid == 0) {               /* Child - run nbdkit. */
+    /* Close the write side of the pipe. */
+    close (fd[1]);
+
+    /* Run nbdkit. */
+    execlp ("nbdkit", "nbdkit", "-v", "--filter=exitwhen",
+            "null", "1M", param, "exit-when-poll=1",
+            NULL);
+    perror ("execvp");
+    _exit (EXIT_FAILURE);
+  }
+
+  /* Close the read side of the pipe. */
+  close (fd[0]);
+
+  /* The test here is simply that nbdkit exits because we exit and our
+   * side of the pipe is closed.
+   */
+  free (param);
+  exit (EXIT_SUCCESS);
+}

tests/Makefile.am
@@ -1382,6 +1382,30 @@ EXTRA_DIST += \
 TESTS += test-exitlast.sh
 EXTRA_DIST += test-exitlast.sh
 
+# exitwhen filter test.
+check_PROGRAMS += test-exitwhen-pipe-closed
+TESTS += \
+	test-exitwhen-file-already-created.sh \
+	test-exitwhen-file-created.sh \
+	test-exitwhen-file-created-reject-new.sh \
+	test-exitwhen-file-created-when-idle.sh \
+	test-exitwhen-file-deleted.sh \
+	test-exitwhen-pipe-closed \
+	test-exitwhen-process-exits.sh \
+	test-exitwhen-script.sh \
+	$(NULL)
+EXTRA_DIST += \
+	test-exitwhen-file-already-created.sh \
+	test-exitwhen-file-created.sh \
+	test-exitwhen-file-created-reject-new.sh \
+	test-exitwhen-file-created-when-idle.sh \
+	test-exitwhen-file-deleted.sh \
+	test-exitwhen-process-exits.sh \
+	test-exitwhen-script.sh \
+	$(NULL)
+
+test_exitwhen_pipe_closed_CFLAGS = $(WARNINGS_CFLAGS)
+
 # exportname filter test.
 TESTS += test-exportname.sh
 EXTRA_DIST += test-exportname.sh

.gitignore
@@ -127,6 +127,7 @@ plugins/*/*.3
 /tests/test-data
 /tests/test-delay
 /tests/test-exit-with-parent
+/tests/test-exitwhen-pipe-closed
 /tests/test-ext2
 /tests/test-file-block
 /tests/test-golang

configure.ac
@@ -104,6 +104,7 @@ filters="\
         delay \
         error \
         exitlast \
+        exitwhen \
 	exportname \
         ext2 \
         extentlist \
@@ -1228,6 +1229,7 @@ AC_CONFIG_FILES([Makefile
                  filters/delay/Makefile
                  filters/error/Makefile
                  filters/exitlast/Makefile
+                 filters/exitwhen/Makefile
                  filters/exportname/Makefile
                  filters/ext2/Makefile
                  filters/extentlist/Makefile

docs/nbdkit-captive.pod
@@ -15,8 +15,9 @@ You can run nbdkit under another process and have nbdkit reliably
 clean up.  There are two techniques depending on whether you want
 nbdkit to start the other process (L</CAPTIVE NBDKIT>), or if you want
 the other process to start nbdkit (L</EXIT WITH PARENT>).  Another way
-is to have nbdkit exit after the last client connection, see
-L<nbdkit-exitlast-filter(1)>.
+is to have nbdkit exit after the last client connection
+(L<nbdkit-exitlast-filter(1)>) or after an event
+(L<nbdkit-exitwhen-filter(1)>).
 
 =head1 CAPTIVE NBDKIT
 
@@ -150,10 +151,14 @@ C<nbdkit --exit-with-parent> only from the main thread (unless you
 actually want nbdkit to exit with the thread, but that may not work
 reliably on all operating systems).
 
+To exit when an unrelated process exits, use
+L<nbdkit-exitwhen-filter(1)> C<exit-when-process-exits> feature.
+
 =head1 SEE ALSO
 
 L<nbdkit(1)>,
 L<nbdkit-exitlast-filter(1)>,
+L<nbdkit-exitwhen-filter(1)>,
 L<prctl(2)> (on Linux),
 L<procctl(2)> (on FreeBSD).
 

docs/nbdkit-service.pod
@@ -145,6 +145,7 @@ L</SOCKET ACTIVATION>.
 L<nbdkit(1)>,
 L<nbdkit-client(1)>,
 L<nbdkit-exitlast-filter(1)>,
+L<nbdkit-exitwhen-filter(1)>,
 L<nbdkit-ip-filter(1)>,
 L<nbdkit-limit-filter(1)>,
 L<systemd(1)>,

commit 9ae4e98e36afa30ca423ecd834907b7b000b86f8

exitwhen: Fix filter on Windows.
Fixes: commit 5c70b3bf3726b1271a93ed5244260e854f86b509

filters/exitwhen/Makefile.am
@@ -42,11 +42,13 @@ nbdkit_exitwhen_filter_la_SOURCES = \
 
 nbdkit_exitwhen_filter_la_CPPFLAGS = \
 	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/replacements \
 	-I$(top_srcdir)/common/utils \
 	$(NULL)
 nbdkit_exitwhen_filter_la_CFLAGS = $(WARNINGS_CFLAGS)
 nbdkit_exitwhen_filter_la_LIBADD = \
 	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/common/replacements/libcompat.la \
 	$(IMPORT_LIBRARY_ON_WINDOWS) \
 	$(NULL)
 nbdkit_exitwhen_filter_la_LDFLAGS = \

filters/exitwhen/exitwhen.c
@@ -39,7 +39,6 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <poll.h>
 #include <assert.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -49,6 +48,7 @@
 #include <nbdkit-filter.h>
 
 #include "cleanup.h"
+#include "poll.h"
 #include "utils.h"
 #include "vector.h"
 
@@ -60,8 +60,14 @@ static bool exiting = false;
 
 /* The list of events generated from command line parameters. */
 struct event {
-  enum { EVENT_FILE_CREATED = 1, EVENT_FILE_DELETED, EVENT_PROCESS_EXITS,
-         EVENT_FD_CLOSED, EVENT_SCRIPT } type;
+  int type;
+#define EVENT_FILE_CREATED  1
+#define EVENT_FILE_DELETED  2
+#ifndef WIN32
+#define EVENT_PROCESS_EXITS 3
+#define EVENT_FD_CLOSED     4
+#define EVENT_SCRIPT        5
+#endif
   union {
     char *filename;             /* Filename or script. */
     int fd;                     /* For PROCESS_EXITS or FD_CLOSED. */
@@ -77,21 +83,27 @@ static void
 free_event (struct event event)
 {
   switch (event.type) {
+#ifdef EVENT_FILE_CREATED
   case EVENT_FILE_CREATED:
+#endif
+#ifdef EVENT_FILE_DELETED
   case EVENT_FILE_DELETED:
+#endif
+#ifdef EVENT_SCRIPT
   case EVENT_SCRIPT:
+#endif
     free (event.u.filename);
     break;
+#ifdef EVENT_PROCESS_EXITS
   case EVENT_PROCESS_EXITS:
+#endif
+#ifdef EVENT_FD_CLOSED
+  case EVENT_FD_CLOSED:
+#endif
 #ifdef __linux__
-  case EVENT_FD_CLOSED:
-#endif
     close (event.u.fd);
-    break;
-#ifndef __linux__
-  case EVENT_FD_CLOSED:
-    break;
 #endif
+    break;
   }
 }
 
@@ -109,11 +121,21 @@ exitwhen_unload (void)
  *
  * This must be called with &lock held.
  */
+#ifdef EVENT_FILE_CREATED
 static void check_for_event_file_created (const struct event *);
+#endif
+#ifdef EVENT_FILE_DELETED
 static void check_for_event_file_deleted (const struct event *);
+#endif
+#ifdef EVENT_PROCESS_EXITS
 static void check_for_event_process_exits (const struct event *);
+#endif
+#ifdef EVENT_FD_CLOSED
 static void check_for_event_fd_closed (const struct event *);
+#endif
+#ifdef EVENT_SCRIPT
 static void check_for_event_script (const struct event *);
+#endif
 
 static bool
 check_for_event (void)
@@ -125,21 +147,31 @@ check_for_event (void)
       const struct event *event = &events.ptr[i];
 
       switch (event->type) {
+#ifdef EVENT_FILE_CREATED
       case EVENT_FILE_CREATED:
         check_for_event_file_created (event);
         break;
+#endif
+#ifdef EVENT_FILE_DELETED
       case EVENT_FILE_DELETED:
         check_for_event_file_deleted (event);
         break;
+#endif
+#ifdef EVENT_PROCESS_EXITS
       case EVENT_PROCESS_EXITS:
         check_for_event_process_exits (event);
         break;
+#endif
+#ifdef EVENT_FD_CLOSED
       case EVENT_FD_CLOSED:
         check_for_event_fd_closed (event);
         break;
+#endif
+#ifdef EVENT_SCRIPT
       case EVENT_SCRIPT:
         check_for_event_script (event);
         break;
+#endif
       }
     }
   }
@@ -147,6 +179,7 @@ check_for_event (void)
   return exiting;
 }
 
+#ifdef EVENT_FILE_CREATED
 static void
 check_for_event_file_created (const struct event *event)
 {
@@ -156,7 +189,9 @@ check_for_event_file_created (const struct event *event)
     exiting = true;
   }
 }
+#endif
 
+#ifdef EVENT_FILE_DELETED
 static void
 check_for_event_file_deleted (const struct event *event)
 {
@@ -173,7 +208,9 @@ check_for_event_file_deleted (const struct event *event)
     }
   }
 }
+#endif
 
+#ifdef EVENT_PROCESS_EXITS
 static void
 check_for_event_process_exits (const struct event *event)
 {
@@ -205,7 +242,9 @@ check_for_event_process_exits (const struct event *event)
   }
 #endif /* !__linux__ */
 }
+#endif
 
+#ifdef EVENT_FD_CLOSED
 static void
 check_for_event_fd_closed (const struct event *event)
 {
@@ -238,7 +277,9 @@ check_for_event_fd_closed (const struct event *event)
     nbdkit_error ("exit-when-pipe-closed: poll: %m");
   }
 }
+#endif
 
+#ifdef EVENT_SCRIPT
 static void
 check_for_event_script (const struct event *event)
 {
@@ -264,6 +305,7 @@ check_for_event_script (const struct event *event)
     exit_status_to_nbd_error (r, "exit-when-script");
   }
 }
+#endif
 
 /* The background polling thread.
  *
@@ -315,7 +357,9 @@ exitwhen_config (nbdkit_next_config *next, void *nxdata,
 {
   struct event event;
 
-  if (strcmp (key, "exit-when-file-created") == 0 ||
+  if (0) ;
+#if defined(EVENT_FILE_CREATED) && defined(EVENT_FILE_DELETED)
+  else if (strcmp (key, "exit-when-file-created") == 0 ||
       strcmp (key, "exit-when-file-deleted") == 0) {
     char c = key[15];
 
@@ -329,6 +373,8 @@ exitwhen_config (nbdkit_next_config *next, void *nxdata,
       return -1;
     return 0;
   }
+#endif
+#ifdef EVENT_FD_CLOSED
   else if (strcmp (key, "exit-when-pipe-closed") == 0 ||
            strcmp (key, "exit-when-fd-closed") == 0) {
     event.type = EVENT_FD_CLOSED;
@@ -336,6 +382,8 @@ exitwhen_config (nbdkit_next_config *next, void *nxdata,
       return -1;
     goto append_event;
   }
+#endif
+#ifdef EVENT_PROCESS_EXITS
   else if (strcmp (key, "exit-when-process-exits") == 0 ||
            strcmp (key, "exit-when-pid-exits") == 0) {
     uint64_t pid;
@@ -360,6 +408,8 @@ exitwhen_config (nbdkit_next_config *next, void *nxdata,
 #endif
     goto append_event;
   }
+#endif
+#ifdef EVENT_SCRIPT
   else if (strcmp (key, "exit-when-script") == 0) {
     event.type = EVENT_SCRIPT;
     event.u.filename = strdup (value);
@@ -369,6 +419,7 @@ exitwhen_config (nbdkit_next_config *next, void *nxdata,
     }
     goto append_event;
   }
+#endif
 
   /* This is a setting, not an event. */
   if (strcmp (key, "exit-when-poll") == 0) {

tests/test-exitwhen-pipe-closed.c
@@ -43,6 +43,11 @@ main (int argc, char *argv[])
   int fd[2];
   pid_t pid;
 
+#ifdef WIN32
+  fprintf (stderr, "%s: test skipped on Windows: no pipe(2)\n", argv[0]);
+  exit (77);
+#endif
+
   if (pipe (fd) == -1) {
     perror ("pipe");
     exit (EXIT_FAILURE);