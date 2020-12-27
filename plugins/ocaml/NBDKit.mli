libguestfs/nbdkit

commit 52e5eb78250c50c141dd440c3b3d7b15ab2f7744

ocaml: Add bindings for nbdkit_peer_{pid,uid,gid}.

plugins/ocaml/NBDKit.mli
@@ -162,3 +162,10 @@ val shutdown : unit -> unit
 
 (** Print a debug message when nbdkit is in verbose mode. *)
 val debug : ('a, unit, string, unit) format4 -> 'a
+
+(** Binding for [nbdkit_peer_pid]. *)
+val peer_pid : unit -> int64
+(** Binding for [nbdkit_peer_uid]. *)
+val peer_uid : unit -> int64
+(** Binding for [nbdkit_peer_gid]. *)
+val peer_gid : unit -> int64

plugins/ocaml/NBDKit.ml
@@ -257,3 +257,7 @@ external _debug : string -> unit = "ocaml_nbdkit_debug" "noalloc"
 
 let debug fs =
   ksprintf _debug fs
+
+external peer_pid : unit -> int64 = "ocaml_nbdkit_peer_pid"
+external peer_uid : unit -> int64 = "ocaml_nbdkit_peer_uid"
+external peer_gid : unit -> int64 = "ocaml_nbdkit_peer_gid"

plugins/ocaml/bindings.c
@@ -190,3 +190,36 @@ ocaml_nbdkit_debug (value strv)
 
   return Val_unit;
 }
+
+value
+ocaml_nbdkit_peer_pid (value unitv)
+{
+  CAMLparam1 (unitv);
+  CAMLlocal1 (rv);
+  int64_t id = nbdkit_peer_pid ();
+  if (id == -1) caml_failwith ("nbdkit_peer_pid");
+  rv = caml_copy_int64 (id);
+  CAMLreturn (rv);
+}
+
+value
+ocaml_nbdkit_peer_uid (value unitv)
+{
+  CAMLparam1 (unitv);
+  CAMLlocal1 (rv);
+  int64_t id = nbdkit_peer_uid ();
+  if (id == -1) caml_failwith ("nbdkit_peer_uid");
+  rv = caml_copy_int64 (id);
+  CAMLreturn (rv);
+}
+
+value
+ocaml_nbdkit_peer_gid (value unitv)
+{
+  CAMLparam1 (unitv);
+  CAMLlocal1 (rv);
+  int64_t id = nbdkit_peer_gid ();
+  if (id == -1) caml_failwith ("nbdkit_peer_gid");
+  rv = caml_copy_int64 (id);
+  CAMLreturn (rv);
+}

commit 801adad7b59a5fb1d7077e09bb414bbed102ab57

ocaml: Fix documentation for peer_* bindings.
Without the extra whitespace ocamldoc gets confused about which
binding the comment refers to.

Fixes: commit 52e5eb78250c50c141dd440c3b3d7b15ab2f7744

plugins/ocaml/NBDKit.mli
@@ -168,7 +168,9 @@ val debug : ('a, unit, string, unit) format4 -> 'a
 
 (** Binding for [nbdkit_peer_pid]. *)
 val peer_pid : unit -> int64
+
 (** Binding for [nbdkit_peer_uid]. *)
 val peer_uid : unit -> int64
+
 (** Binding for [nbdkit_peer_gid]. *)
 val peer_gid : unit -> int64
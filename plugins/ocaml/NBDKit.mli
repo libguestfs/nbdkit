801adad7b59a5fb1d7077e09bb414bbed102ab57
Richard W.M. Jones <rjones@redhat.com>
2020-12-27 17:10:54 +0000

    ocaml: Fix documentation for peer_* bindings.
    Without the extra whitespace ocamldoc gets confused about which
    binding the comment refers to.
    
    Fixes: commit 52e5eb78250c50c141dd440c3b3d7b15ab2f7744

--- plugins/ocaml/NBDKit.mli
+++ plugins/ocaml/NBDKit.mli
@@ -168,7 +168,9 @@ val debug : ('a, unit, string, unit) format4 -> 'a
 
 (** Binding for [nbdkit_peer_pid]. *)
 val peer_pid : unit -> int64
+
 (** Binding for [nbdkit_peer_uid]. *)
 val peer_uid : unit -> int64
+
 (** Binding for [nbdkit_peer_gid]. *)
 val peer_gid : unit -> int64
