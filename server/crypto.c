libguestfs/nbdkit

commit fab4a090d09fa213ee0b6fb8fbaa386ef9a6641c

server: Add -D nbdkit.gnutls.session=1 to debug TLS session info.
Prints details such as the type of encryption, client username, client
certificates.

For x.509 certificates, when --tls-verify-peer (client certificate
verification) is enabled something like the following will be printed:

nbdkit: example1[1]: debug: TLS session: (TLS1.3)-(ECDHE-SECP256R1)-(RSA-PSS-RSAE-SHA256)-(AES-256-GCM)
nbdkit: example1[1]: debug: TLS: authentication: certificate
nbdkit: example1[1]: debug: TLS: peer provided 1 certificate(s)
nbdkit: example1[1]: debug: TLS: subject `C=US,ST=New York,L=New York,O=Test,CN=localhost', issuer `CN=Test', serial 0x40903982da82786269023a49b5fcfef3c23a1bb9, RSA key 3072 bits, signed using RSA-SHA256, activated `2020-09-22 16:14:35 UTC', expires `2021-09-22 16:14:35 UTC', pin-sha256="JPBlI39MIRgX8rZVRbeLc2Sb3RO3u2wrAmeOLUrwYQY="
nbdkit: example1[1]: debug: TLS: DN: CN=localhost,O=Test,L=New York,ST=New York,C=US
nbdkit: example1[1]: debug: TLS: issuer's DN: CN=Test
nbdkit: example1[1]: debug: TLS: negotiated group: SECP256R1

For PSK, the client-provided username is printed:

nbdkit: example1[1]: debug: TLS session: (TLS1.3)-(ECDHE-PSK-SECP256R1)-(CHACHA20-POLY1305)
nbdkit: example1[1]: debug: TLS: authentication: PSK (Pre-Shared Key)
nbdkit: example1[1]: debug: TLS: PSK username: qemu
nbdkit: example1[1]: debug: TLS: negotiated group: SECP256R1

server/crypto.c
@@ -53,6 +53,7 @@
 #ifdef HAVE_GNUTLS
 
 #include <gnutls/gnutls.h>
+#include <gnutls/x509.h>
 
 static int crypto_auth;
 #define CRYPTO_AUTH_CERTIFICATES 1
@@ -465,6 +466,11 @@ pull_timeout (gnutls_transport_ptr_t ptr, unsigned ms)
  */
 SERVER_DEBUG_FLAG(nbdkit_debug_gnutls_log) = 0;
 
+/* Print additional information about the session using
+ * nbdkit -D nbdkit.gnutls.session=1
+ */
+SERVER_DEBUG_FLAG(nbdkit_debug_gnutls_session) = 0;
+
 static void
 tls_log (int level, const char *msg)
 {
@@ -479,6 +485,119 @@ tls_log (int level, const char *msg)
   }
 
   nbdkit_debug ("gnutls: %d: %s", level, msg);
+}
+
+/* https://gnutls.org/manual/html_node/Obtaining-session-information.html */
+static void
+debug_x590_cert (gnutls_session_t session)
+{
+  const gnutls_datum_t *cert_list;
+  unsigned int i, cert_list_size = 0;
+
+  cert_list = gnutls_certificate_get_peers (session, &cert_list_size);
+  if (cert_list == NULL) {
+    /* Note unless you use --tls-verify-peer you will always see the
+     * following message.
+     */
+    nbdkit_debug ("TLS: no peer certificates found");
+    return;
+  }
+
+  nbdkit_debug ("TLS: peer provided %u certificate(s)", cert_list_size);
+  for (i = 0; i < cert_list_size; ++i) {
+    int ret;
+    gnutls_x509_crt_t cert;
+    size_t size;
+    gnutls_datum_t cinfo;
+    char dn[256];
+
+    gnutls_x509_crt_init (&cert);
+    gnutls_x509_crt_import (cert, &cert_list[i], GNUTLS_X509_FMT_DER);
+
+    ret = gnutls_x509_crt_print (cert, GNUTLS_CRT_PRINT_ONELINE, &cinfo);
+    if (ret == 0) {
+      nbdkit_debug ("TLS: %s", cinfo.data);
+      gnutls_free (cinfo.data);
+    }
+
+    size = sizeof dn;
+    gnutls_x509_crt_get_dn (cert, dn, &size);
+    nbdkit_debug ("TLS: DN: %s", dn);
+
+    size = sizeof dn;
+    gnutls_x509_crt_get_issuer_dn (cert, dn, &size);
+    nbdkit_debug ("TLS: issuer's DN: %s", dn);
+
+    gnutls_x509_crt_deinit (cert);
+  }
+}
+
+static void
+debug_session (gnutls_session_t session)
+{
+  gnutls_credentials_type_t cred;
+  gnutls_kx_algorithm_t kx;
+  bool dhe = false, ecdh = false;
+  int grp;
+  const char *desc, *username, *hint;
+
+  desc = gnutls_session_get_desc (session);
+  if (desc) nbdkit_debug ("TLS session: %s", desc);
+
+  kx = gnutls_kx_get (session);
+  cred = gnutls_auth_get_type (session);
+  switch (cred) {
+  case GNUTLS_CRD_SRP:
+    nbdkit_debug ("TLS: authentication: SRP (Secure Remote Password)");
+    username = gnutls_srp_server_get_username (session);
+    if (username)
+      nbdkit_debug ("TLS: SRP session username: %s", username);
+    break;
+  case GNUTLS_CRD_PSK:
+    nbdkit_debug ("TLS: authentication: PSK (Pre-Shared Key)");
+    hint = gnutls_psk_client_get_hint (session);
+    if (hint)
+      nbdkit_debug ("TLS: PSK hint: %s", hint);
+    username = gnutls_psk_server_get_username (session);
+    if (username)
+      nbdkit_debug ("TLS: PSK username: %s", username);
+    if (kx == GNUTLS_KX_ECDHE_PSK)
+      ecdh = true;
+    else if (kx == GNUTLS_KX_DHE_PSK)
+      dhe = true;
+    break;
+  case GNUTLS_CRD_ANON:
+    nbdkit_debug ("TLS: authentication: anonymous");
+    if (kx == GNUTLS_KX_ANON_ECDH)
+      ecdh = true;
+    else if (kx == GNUTLS_KX_ANON_DH)
+      dhe = true;
+    break;
+  case GNUTLS_CRD_CERTIFICATE:
+    nbdkit_debug ("TLS: authentication: certificate");
+    if (gnutls_certificate_type_get (session) == GNUTLS_CRT_X509)
+      debug_x590_cert (session);
+    if (kx == GNUTLS_KX_DHE_RSA || kx == GNUTLS_KX_DHE_DSS)
+      dhe = true;
+    else if (kx == GNUTLS_KX_ECDHE_RSA || kx == GNUTLS_KX_ECDHE_ECDSA)
+      ecdh = true;
+    break;
+  default:
+    nbdkit_debug ("TLS: authentication: unknown (%d)", (int) cred);
+  }
+
+  grp = gnutls_group_get (session);
+  if (grp)
+    nbdkit_debug ("TLS: negotiated group: %s",
+                  gnutls_group_get_name (grp));
+  else {
+    if (ecdh)
+      nbdkit_debug ("TLS: ephemeral ECDH using curve %s",
+                    gnutls_ecc_curve_get_name (gnutls_ecc_curve_get (session)));
+    else if (dhe)
+      nbdkit_debug ("TLS: ephemeral DH using prime of %d bits",
+                    gnutls_dh_get_prime_bits (session));
+  }
 }
 
 /* Upgrade an existing connection to TLS.  Also this should do access
@@ -594,6 +713,10 @@ crypto_negotiate_tls (int sockin, int sockout)
     goto error;
   }
   debug ("TLS handshake completed");
+
+  /* Print some additional information about the negotiated encryption. */
+  if (nbdkit_debug_gnutls_session > 0)
+    debug_session (session);
 
   /* Set up the connection recv/send/close functions so they call
    * GnuTLS wrappers instead.

docs/nbdkit.pod
@@ -591,6 +591,11 @@ commands (config, open, close, can_write, etc.)
 Enable GnuTLS logging.  C<N> can be in the range 0 (no logging) to 99.
 See L<gnutls_global_set_log_level(3)>.
 
+=item B<-D nbdkit.gnutls.session=1>
+
+Print additional information about the TLS session, such as the type
+of authentication and encryption, and client certificate information.
+
 =back
 
 =head1 SIGNALS

tests/test-nbd-tls-psk.sh
@@ -81,7 +81,7 @@ sleep 1
 
 # Run encrypted server
 start_nbdkit -P "$pid1" -U "$sock1" \
-    --tls=require --tls-psk=keys.psk example1
+    --tls=require --tls-psk=keys.psk -D nbdkit.gnutls.session=1 example1
 
 wait $info_pid
 cat nbd-tls-psk.out

tests/test-nbd-tls.sh
@@ -73,7 +73,8 @@ start_nbdkit -P "$pid2" -U "$sock2" --tls=off \
 
 # Run encrypted server
 start_nbdkit -P "$pid1" -U "$sock1" \
-    --tls=require --tls-certificates="$pkidir" example1
+    --tls=require --tls-certificates="$pkidir" -D nbdkit.gnutls.session=1 \
+    example1
 
 # Run unencrypted client
 qemu-img info --output=json -f raw "nbd+unix:///?socket=$sock2" > nbd-tls.out

tests/test-tls-fallback.sh
@@ -58,8 +58,9 @@ rm -f $files
 cleanup_fn rm -f $files
 
 # Run dual-mode server
-start_nbdkit -P $pid -U $sock --tls=on --tls-psk=keys.psk \
-    --filter=tls-fallback sh - tlsreadme=$'dummy\n' <<\EOF
+start_nbdkit -P $pid -U $sock \
+             --tls=on --tls-psk=keys.psk -D nbdkit.gnutls.session=1 \
+             --filter=tls-fallback sh - tlsreadme=$'dummy\n' <<\EOF
 check () {
  if test "$1" != true; then
    echo 'EINVAL unexpected tls mode' 2>&1; exit 1

tests/test-tls-psk.sh
@@ -69,7 +69,8 @@ pick_unused_port
 
 cleanup_fn rm -f tls-psk.pid tls-psk.out
 start_nbdkit -P tls-psk.pid -p $port -n \
-             --tls=require --tls-psk=keys.psk example1
+             --tls=require --tls-psk=keys.psk -D nbdkit.gnutls.session=1 \
+             example1
 
 # Run qemu-img against the server.
 qemu-img info --output=json \

tests/test-tls.sh
@@ -61,8 +61,10 @@ fi
 pick_unused_port
 
 cleanup_fn rm -f tls.pid tls.out
-start_nbdkit -P tls.pid -p $port -n --tls=require \
-       --tls-certificates="$pkidir" example1
+start_nbdkit -P tls.pid -p $port -n \
+             --tls=require --tls-certificates="$pkidir" \
+             -D nbdkit.gnutls.session=1 \
+             example1
 
 # Run qemu-img against the server.
 qemu-img info --output=json \

commit dae9b2b62fb1ae4ba91a604ab11342f1a517486f

server/crypto: Fix debugging messages in RHEL 7.
gnutls 3.3.29 (used in RHEL 7) lacks several APIs we use to print
encryption parameters.  As these are only debugging messages simply
omit the information if the APIs are not present.

Fixes: commit fab4a090d09fa213ee0b6fb8fbaa386ef9a6641c

configure.ac
@@ -568,7 +568,11 @@ AS_IF([test "$GNUTLS_LIBS" != ""],[
     AC_CHECK_FUNCS([\
 	gnutls_base64_decode2 \
 	gnutls_certificate_set_known_dh_params \
-	gnutls_session_set_verify_cert])
+	gnutls_group_get \
+	gnutls_group_get_name \
+	gnutls_session_set_verify_cert \
+	gnutls_srp_server_get_username \
+    ])
     LIBS="$old_LIBS"
 ])
 

server/crypto.c
@@ -543,7 +543,11 @@ debug_session (gnutls_session_t session)
   switch (cred) {
   case GNUTLS_CRD_SRP:
     nbdkit_debug ("TLS: authentication: SRP (Secure Remote Password)");
+#ifdef HAVE_GNUTLS_SRP_SERVER_GET_USERNAME
     username = gnutls_srp_server_get_username (session);
+#else
+    username = NULL;
+#endif
     if (username)
       nbdkit_debug ("TLS: SRP session username: %s", username);
     break;
@@ -580,10 +584,19 @@ debug_session (gnutls_session_t session)
     nbdkit_debug ("TLS: authentication: unknown (%d)", (int) cred);
   }
 
+#ifdef HAVE_GNUTLS_GROUP_GET
   grp = gnutls_group_get (session);
-  if (grp)
-    nbdkit_debug ("TLS: negotiated group: %s",
-                  gnutls_group_get_name (grp));
+#else
+  grp = 0;
+#endif
+  if (grp) {
+    nbdkit_debug ("TLS: negotiated group: "
+#ifdef HAVE_GNUTLS_GROUP_GET_NAME
+                  "%s", gnutls_group_get_name (grp));
+#else
+                  "%d", grp);
+#endif
+  }
   else {
     if (ecdh)
       nbdkit_debug ("TLS: ephemeral ECDH using curve %s",