libguestfs/nbdkit

commit 6715c3d8b3e67c2ad279e435ec0c406b34ea09c8

New plugin: S3 plugin for accessing disks stored in AWS S3 and Ceph.
Thanks: Alice Frosi for suggesting this plugin.

plugins/S3/S3.py
@@ -1,0 +1,100 @@
+#!@sbindir@/nbdkit python
+# -*- python -*-
+# Copyright (C) 2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+import nbdkit
+import boto3
+from contextlib import closing
+
+API_VERSION = 2
+
+access_key = None
+secret_key = None
+session_token = None
+endpoint_url = None
+bucket_name = None
+key_name = None
+
+def thread_model():
+    return nbdkit.THREAD_MODEL_PARALLEL
+
+def config(key, value):
+    global access_key, secret_key, session_token, endpoint_url, bucket_name, key_name
+
+    if key == "access-key" or key == "access_key":
+        access_key = value
+    elif key == "secret-key" or key == "secret_key":
+        secret_key = value
+    elif key == "session-token" or key == "session_token":
+        session_token = value
+    elif key == "endpoint-url" or key == "endpoint_url":
+        endpoint_url = value
+    elif key == "bucket":
+        bucket_name = value
+    elif key == "key":
+        key_name = value
+    else:
+        raise Exception("unknown parameter %s" % key)
+
+def config_complete():
+    if bucket_name is None:
+        raise Exception("bucket parameter missing")
+    if key_name is None:
+        raise Exception("key parameter missing")
+
+def open(readonly):
+    global access_key, secret_key, session_token, endpoint_url
+
+    s3 = boto3.client("s3",
+                      aws_access_key_id = access_key,
+                      aws_secret_access_key = secret_key,
+                      aws_session_token = session_token,
+                      endpoint_url = endpoint_url)
+    if s3 is None:
+        raise Exception("could not connect to S3")
+    return s3
+
+def get_size(s3):
+    global bucket_name, key_name
+
+    resp = s3.get_object(Bucket = bucket_name, Key = key_name)
+    size = resp['ResponseMetadata']['HTTPHeaders']['content-length']
+    return int(size)
+
+def pread(s3, buf, offset, flags):
+    global bucket_name, key_name
+
+    size = len(buf)
+    rnge = 'bytes=%d-%d' % (offset, offset+size-1)
+    resp = s3.get_object(Bucket = bucket_name, Key = key_name, Range = rnge)
+    body = resp['Body']
+    with closing(body):
+        buf[:] = body.read(size)

.gitignore
@@ -85,6 +85,7 @@ plugins/*/*.3
 /plugins/ondemand/default-command.c
 /plugins/rust/Cargo.lock
 /plugins/rust/target
+/plugins/S3/nbdkit-S3-plugin
 /plugins/tmpdisk/default-command.c
 /podwrapper.pl
 /server/libnbdkit.a

README
@@ -149,6 +149,8 @@ For the Python plugin:
 
  - python unittest to run the test suite
 
+ - boto3 is required to run the S3 plugin written in Python
+
 For the OCaml plugin:
 
  - OCaml >= 4.02.2

configure.ac
@@ -85,6 +85,7 @@ non_lang_plugins="\
         partitioning \
         pattern \
         random \
+        S3 \
         split \
         ssh \
         streaming \
@@ -1210,6 +1211,7 @@ AC_CONFIG_FILES([Makefile
                  plugins/random/Makefile
                  plugins/ruby/Makefile
                  plugins/rust/Makefile
+                 plugins/S3/Makefile
                  plugins/sh/Makefile
                  plugins/ssh/Makefile
                  plugins/split/Makefile
@@ -1313,6 +1315,8 @@ feature "linuxdisk .............................. " \
         test "x$HAVE_MKE2FS_WITH_D_TRUE" = "x"
 feature "nbd .................................... " \
         test "x$HAVE_LIBNBD_TRUE" = "x"
+feature "S3 ..................................... " \
+        test "x$HAVE_PYTHON_TRUE" = "x"
 feature "ssh .................................... " \
         test "x$HAVE_SSH_TRUE" = "x"
 feature "torrent ................................ " \

plugins/S3/Makefile.am
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
 # nbdkit
-# Copyright (C) 2016 Red Hat Inc.
+# Copyright (C) 2013-2020 Red Hat Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -30,36 +29,36 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-source ./functions.sh
-set -e
-set -x
+include $(top_srcdir)/common-rules.mk
 
-run_test ()
-{
-    nbdkit $1 --version
-}
+source = S3.py
 
-do_test ()
-{
-    vg=; [ "$NBDKIT_VALGRIND" = "1" ] && vg="-valgrind"
-    case "$1$vg" in
-        python-valgrind | ruby-valgrind | tcl-valgrind)
-            echo "$0: skipping $1$vg because this language doesn't support valgrind"
-            ;;
-        example4*)
-            # These tests are written in Perl so we have to check that
-            # the Perl plugin was compiled.
-            if nbdkit perl --version; then run_test $1; fi
-            ;;
-        S3*)
-            # Requires Python plugin and boto3 library.
-            if nbdkit python --version && $PYTHON -c 'import boto3'; then
-                run_test $1
-            fi
-            ;;
-        *)
-            run_test $1
-            ;;
-    esac
-}
-foreach_plugin do_test
+EXTRA_DIST = \
+	$(source) \
+	$(NULL)
+
+if HAVE_PYTHON
+
+plugin_SCRIPTS = nbdkit-S3-plugin
+CLEANFILES += nbdkit-S3-plugin
+
+# We have to do the rewriting here to avoid stupid exec_prefix.
+nbdkit-S3-plugin: $(source)
+	rm -f $@ $@-t
+	sed 's,\@sbindir\@,${sbindir},g' < $< > $@-t
+	mv $@-t $@
+	chmod 0555 $@
+
+if HAVE_POD
+
+man_MANS = nbdkit-S3-plugin.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-S3-plugin.1: nbdkit-S3-plugin.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD
+
+endif

plugins/S3/nbdkit-S3-plugin.pod
@@ -1,0 +1,123 @@
+=head1 NAME
+
+nbdkit-S3-plugin - open disk images stored in Amazon S3 or Ceph
+
+=head1 SYNOPSIS
+
+ nbdkit S3 [access-key=...] [secret-key=...] [session-token=...]
+           [endpoint-url=...]
+           bucket=BUCKET key=FILENAME
+
+=head1 DESCRIPTION
+
+C<nbdkit-S3-plugin> is a plugin for L<nbdkit(1)> which lets you open
+single files stored in Amazon S3 or Ceph as disk images.
+
+Currently it only allows read access, although write access may be
+added in future.
+
+This plugin uses the Python Amazon Web Services SDK called Boto3.
+
+=head1 EXAMPLE
+
+ nbdkit S3 endpoint-url=https://ceph.example.com \
+           bucket=MY-BUCKET key=disk.img
+
+=head1 PARAMETERS
+
+=over 4
+
+=item B<access-key=>ACCESS_KEY
+
+=item B<secret-key=>SECRET_KEY
+
+=item B<session-token=>SESSION_TOKEN
+
+The AWS credentials may be specified on the command line using these
+parameters, but it is more secure to pass them using files (see
+L</CREDENTIALS> below).
+
+=item B<endpoint-url=>ENDPOINT
+
+If accessing Ceph or another compatible S3 service, provide the
+endpoint URL through this parameter.
+
+=item B<bucket=>BUCKET
+
+The bucket containing the file.  This parameter is required.
+
+=item B<key=>FILENAME
+
+The file name within the bucket.  This parameter is required.
+
+=back
+
+=head1 CREDENTIALS
+
+Although AWS credentials can be passed to nbdkit on the command line,
+this is not secure since a user on the same machine could read them
+using L<ps(1)>.
+
+A better way is to pass the credentials through the
+F<~/.aws/credentials> file.  This takes the form:
+
+ [default]
+ aws_access_key_id = XXX
+ aws_secret_access_key = YYY
+ 
+ [profile]
+ aws_access_key_id = XXX
+ aws_secret_access_key = YYY
+
+Different profiles from the file can be selected by setting
+the C<AWS_PROFILE> environment variable.
+
+There is much more information about credentials in
+L<the Boto3 documentation|https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html>.
+
+=head1 FILES
+
+=over 4
+
+=item F<$plugindir/nbdkit-S3-plugin>
+
+The plugin.
+
+Use C<nbdkit --dump-config> to find the location of C<$plugindir>.
+
+=item F<$HOME/.aws/credentials>
+
+AWS credentials can be passed to boto3 using this file.
+
+=back
+
+=head1 ENVIRONMENT VARIABLES
+
+=over 4
+
+=item C<AWS_*>
+
+Boto3 reads some credential information from C<AWS_*> environment variables.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-S3-plugin> first appeared in nbdkit 1.24.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<nbdkit-python-plugin(3)>,
+L<https://pypi.org/project/boto3/>,
+L<https://boto3.amazonaws.com/v1/documentation/api/latest/index.html>,
+L<https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2020 Red Hat Inc.

tests/test-dump-plugin.sh
@@ -51,6 +51,12 @@ do_test ()
             # the Perl plugin was compiled.
             if nbdkit perl --version; then run_test $1; fi
             ;;
+        S3*)
+            # Requires Python plugin and boto3 library.
+            if nbdkit python --version && $PYTHON -c 'import boto3'; then
+                run_test $1
+            fi
+            ;;
         *)
             run_test $1
             ;;

tests/test-help-plugin.sh
@@ -51,6 +51,12 @@ do_test ()
             # the Perl plugin was compiled.
             if nbdkit perl --version; then run_test $1; fi
             ;;
+        S3*)
+            # Requires Python plugin and boto3 library.
+            if nbdkit python --version && $PYTHON -c 'import boto3'; then
+                run_test $1
+            fi
+            ;;
         *)
             run_test $1
             ;;

commit b949634a18f8a123b6bed3aaa491eb4bbbd7417b

S3: Fix Python code style.
Fixes: commit 6715c3d8b3e67c2ad279e435ec0c406b34ea09c8

plugins/S3/S3.py
@@ -34,6 +34,7 @@ import nbdkit
 import boto3
 from contextlib import closing
 
+
 API_VERSION = 2
 
 access_key = None
@@ -43,11 +44,14 @@ endpoint_url = None
 bucket_name = None
 key_name = None
 
+
 def thread_model():
     return nbdkit.THREAD_MODEL_PARALLEL
 
+
 def config(key, value):
-    global access_key, secret_key, session_token, endpoint_url, bucket_name, key_name
+    global access_key, secret_key, session_token, endpoint_url, \
+           bucket_name, key_name
 
     if key == "access-key" or key == "access_key":
         access_key = value
@@ -64,37 +68,41 @@ def config(key, value):
     else:
         raise Exception("unknown parameter %s" % key)
 
+
 def config_complete():
     if bucket_name is None:
         raise Exception("bucket parameter missing")
     if key_name is None:
         raise Exception("key parameter missing")
 
+
 def open(readonly):
     global access_key, secret_key, session_token, endpoint_url
 
     s3 = boto3.client("s3",
-                      aws_access_key_id = access_key,
-                      aws_secret_access_key = secret_key,
-                      aws_session_token = session_token,
-                      endpoint_url = endpoint_url)
+                      aws_access_key_id=access_key,
+                      aws_secret_access_key=secret_key,
+                      aws_session_token=session_token,
+                      endpoint_url=endpoint_url)
     if s3 is None:
         raise Exception("could not connect to S3")
     return s3
 
+
 def get_size(s3):
     global bucket_name, key_name
 
-    resp = s3.get_object(Bucket = bucket_name, Key = key_name)
+    resp = s3.get_object(Bucket=bucket_name, Key=key_name)
     size = resp['ResponseMetadata']['HTTPHeaders']['content-length']
     return int(size)
+
 
 def pread(s3, buf, offset, flags):
     global bucket_name, key_name
 
     size = len(buf)
     rnge = 'bytes=%d-%d' % (offset, offset+size-1)
-    resp = s3.get_object(Bucket = bucket_name, Key = key_name, Range = rnge)
+    resp = s3.get_object(Bucket=bucket_name, Key=key_name, Range=rnge)
     body = resp['Body']
     with closing(body):
         buf[:] = body.read(size)