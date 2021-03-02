libguestfs/nbdkit

commit 80842ac3c858d38b02d434286daeb23004a5d3de

file: Add debug message when falling back to readonly
Most open errors (ENOENT, ELOOP, ...) are independent of whether the
flags included O_RDONLY or O_RDWR; and when we fail to open something
read-only, the second error may be nicer than EACCES or EPERM from the
first.  Meanwhile, including the first errno in a debug message may
help debugging such scenarios.

Suggested-by: Nir Soffer

plugins/file/winfile.c
@@ -128,6 +128,8 @@ winfile_open (int readonly)
   fh = CreateFile (filename, flags, FILE_SHARE_READ|FILE_SHARE_WRITE,
                    NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
   if (fh == INVALID_HANDLE_VALUE && !readonly) {
+    nbdkit_debug ("open for writing failed, falling back to read-only: %s: %lu",
+                  file, GetLastError ());
     flags &= ~GENERIC_WRITE;
     readonly = true;
     fh = CreateFile (filename, flags, FILE_SHARE_READ|FILE_SHARE_WRITE,

plugins/file/file.c
@@ -355,13 +355,11 @@ file_open (int readonly)
 
   h->fd = openat (dfd, file, flags);
   if (h->fd == -1 && !readonly) {
-    int saved_errno = errno;
+    nbdkit_debug ("open O_RDWR failed, falling back to read-only: %s: %m",
+                  file);
     flags = (flags & ~O_ACCMODE) | O_RDONLY;
     h->fd = openat (dfd, file, flags);
-    if (h->fd == -1)
-      errno = saved_errno;
-    else
-      h->can_write = false;
+    h->can_write = false;
   }
   if (h->fd == -1) {
     nbdkit_error ("open: %s: %m", file);

commit 032a37148ed896c73233a17d46b3d270d33b51d1

windows: file: Fix debug message.
Fixes: commit 80842ac3c858d38b02d434286daeb23004a5d3de

plugins/file/winfile.c
@@ -128,8 +128,9 @@ winfile_open (int readonly)
   fh = CreateFile (filename, flags, FILE_SHARE_READ|FILE_SHARE_WRITE,
                    NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
   if (fh == INVALID_HANDLE_VALUE && !readonly) {
-    nbdkit_debug ("open for writing failed, falling back to read-only: %s: %lu",
-                  file, GetLastError ());
+    nbdkit_debug ("open for writing failed, falling back to read-only: "
+                  "%s: %lu",
+                  filename, GetLastError ());
     flags &= ~GENERIC_WRITE;
     readonly = true;
     fh = CreateFile (filename, flags, FILE_SHARE_READ|FILE_SHARE_WRITE,