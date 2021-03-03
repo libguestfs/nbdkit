libguestfs/nbdkit

commit 50b1abc94118cd177a17839e8378f6320d0db6d9

retry: Plug memory leak
Valgrind pointed out that we were leaking h->exportname.

Fixes: 5f7dfce619 (server: Pass the export name through filter .open calls)

filters/retry/retry.c
@@ -146,6 +146,7 @@ retry_close (void *handle)
   struct retry_handle *h = handle;
 
   nbdkit_debug ("reopens needed: %u", h->reopens);
+  free (h->exportname);
   free (h);
 }
 

commit 99a6667f60306e01eec36883df1ca53ec98972e6

retry: Avoid const misuse
Depending on compiler warning levels, attempting to free(const char *)
does not compile.

Fixes: 50b1abc941 (retry: Plug memory leak)

filters/retry/retry.c
@@ -107,7 +107,7 @@ retry_config (nbdkit_next_config *next, void *nxdata,
 
 struct retry_handle {
   int readonly;                 /* Save original readonly setting. */
-  const char *exportname;       /* Client exportname. */
+  char *exportname;             /* Client exportname. */
   unsigned reopens;
   bool open;
 };