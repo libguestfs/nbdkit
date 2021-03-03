libguestfs/nbdkit

commit 5f7dfce619eed5ae048df805f4b253bd828df832

server: Pass the export name through filter .open calls.
To allow filters to modify the export name as it passes through the
layers this commit makes several changes:

The filter .open callback now takes an extra parameter, the export
name.  This is always non-NULL (for oldstyle it is "").  This string
has a short lifetime and filters that need to hang on to it must take
a copy.  The filter must pass the exportname parameter down to the
next layer (or potentially modify it).

The plugin .open callback is unchanged, but the binding to it in
server/plugins.c takes a copy of the exportname in the handle and this
is what nbdkit_export_name returns to plugins.  Eventually we will
deprecate that function and the V3 .open callback in plugins will have
the extra parameter, but this is not implemented yet.

nbdkit_export_name can only be called from plugins, not filters.
Filters should use the .open(...exportname) parameter if they need the
export name.

Filter .reopen also takes the exportname.  The only filter that uses
it (retry) must save the exportname from .open in order to pass it to
.reopen.  This filter already does the same for the readonly flag so
this seems reasonable.

The handling of NBD_OPT_SET_META_CONTEXT interacted with the export
name (see commit 20b8509a9ccdab118ce7b7043be63bbad74f1e79).  I have
attempted to keep previous behaviour in this change, but note that
there is no regression test for this.  I added a debug message so we
can tell when this unusual case actually happens which should help
with diagnosis of problems.

All of the above is internal refactoring and should have no visible
external effect on server behaviour or how plugins are written.
Filters need to be updated as discussed above.  All the in-tree
filters have been, and we assume there are no out of tree filters
because of the unstable filter API.

See also this thread:
https://www.redhat.com/archives/libguestfs/2020-July/thread.html#00087

filters/retry/retry.c
@@ -106,16 +106,18 @@ retry_config (nbdkit_next_config *next, void *nxdata,
 
 struct retry_handle {
   int readonly;                 /* Save original readonly setting. */
+  const char *exportname;       /* Client exportname. */
   unsigned reopens;
   bool open;
 };
 
 static void *
-retry_open (nbdkit_next_open *next, void *nxdata, int readonly)
+retry_open (nbdkit_next_open *next, void *nxdata,
+            int readonly, const char *exportname)
 {
   struct retry_handle *h;
 
-  if (next (nxdata, readonly) == -1)
+  if (next (nxdata, readonly, exportname) == -1)
     return NULL;
 
   h = malloc (sizeof *h);
@@ -125,6 +127,12 @@ retry_open (nbdkit_next_open *next, void *nxdata, int readonly)
   }
 
   h->readonly = readonly;
+  h->exportname = strdup (exportname);
+  if (h->exportname == NULL) {
+    nbdkit_error ("strdup: %m");
+    free (h);
+    return NULL;
+  }
   h->reopens = 0;
   h->open = true;
 
@@ -198,7 +206,8 @@ do_retry (struct retry_handle *h,
 
   /* Reopen the connection. */
   h->reopens++;
-  if (next_ops->reopen (nxdata, h->readonly || force_readonly) == -1) {
+  if (next_ops->reopen (nxdata,
+                        h->readonly || force_readonly, h->exportname) == -1) {
     /* If the reopen fails we treat it the same way as a command
      * failing.
      */

docs/nbdkit-filter.pod
@@ -329,7 +329,7 @@ an error message and return C<-1>.
 =head2 C<.open>
 
  void * (*open) (nbdkit_next_open *next, void *nxdata,
-                 int readonly);
+                 int readonly, const char *exportname);
 
 This is called when a new client connection is opened and can be used
 to allocate any per-connection data structures needed by the filter.
@@ -345,17 +345,22 @@ If there is an error, C<.open> should call C<nbdkit_error> with an
 error message and return C<NULL>.
 
 This callback is optional, but if provided, it must call C<next>,
-passing a value for C<readonly> according to how the filter plans to
-use the plugin.  Typically, the filter passes the same value as it
-received, or passes true to provide a writable layer on top of a
-read-only backend.  However, it is also acceptable to attempt write
-access to the plugin even if this filter is readonly, such as when a
-file system mounted read-only still requires write access to the
-underlying device in case a journal needs to be replayed for
-consistency as part of the mounting process.  The filter should
-generally call C<next> as its first step, to allocate from the plugin
-outwards, so that C<.close> running from the outer filter to the
-plugin will be in reverse.
+passing C<readonly> and C<exportname> possibly modified according to
+how the filter plans to use the plugin.  Typically, the filter passes
+the same values as it received, or passes readonly=true to provide a
+writable layer on top of a read-only backend.  However, it is also
+acceptable to attempt write access to the plugin even if this filter
+is readonly, such as when a file system mounted read-only still
+requires write access to the underlying device in case a journal needs
+to be replayed for consistency as part of the mounting process.
+
+The C<exportname> string is only guaranteed to be available during the
+call.  If the filter needs to use it (other than immediately passing
+it down to the next layer) it must take a copy.
+
+The filter should generally call C<next> as its first step, to
+allocate from the plugin outwards, so that C<.close> running from the
+outer filter to the plugin will be in reverse.
 
 =head2 C<.close>
 

filters/cow/cow.c
@@ -93,10 +93,11 @@ cow_config (nbdkit_next_config *next, void *nxdata,
   "cow-on-cache=<BOOL>  Set to true to treat client cache requests as writes.\n"
 
 static void *
-cow_open (nbdkit_next_open *next, void *nxdata, int readonly)
+cow_open (nbdkit_next_open *next, void *nxdata,
+          int readonly, const char *exportname)
 {
   /* Always pass readonly=1 to the underlying plugin. */
-  if (next (nxdata, 1) == -1)
+  if (next (nxdata, 1, exportname) == -1)
     return NULL;
 
   return NBDKIT_HANDLE_NOT_NEEDED;

filters/exitlast/exitlast.c
@@ -51,9 +51,9 @@ static _Atomic unsigned connections;
 
 static void *
 exitlast_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
-               int readonly)
+               int readonly, const char *exportname)
 {
-  if (next (nxdata, readonly) == -1)
+  if (next (nxdata, readonly, exportname) == -1)
     return NULL;
 
   connections++;

filters/ext2/ext2.c
@@ -112,6 +112,7 @@ ext2_config_complete (nbdkit_next_config_complete *next, void *nxdata)
 
 /* The per-connection handle. */
 struct handle {
+  char *exportname;             /* Client export name. */
   ext2_filsys fs;               /* Filesystem handle. */
   ext2_ino_t ino;               /* Inode of open file. */
   ext2_file_t file;             /* File handle. */
@@ -120,17 +121,34 @@ struct handle {
 
 /* Create the per-connection handle. */
 static void *
-ext2_open (nbdkit_next_open *next, void *nxdata, int readonly)
+ext2_open (nbdkit_next_open *next, void *nxdata,
+           int readonly, const char *exportname)
 {
   struct handle *h;
-
-  /* Request write access to the underlying plugin, for journal replay. */
-  if (next (nxdata, 0) == -1)
-    return NULL;
 
   h = calloc (1, sizeof *h);
   if (h == NULL) {
     nbdkit_error ("calloc: %m");
+    return NULL;
+  }
+
+  /* Save the client exportname in the handle. */
+  h->exportname = strdup (exportname);
+  if (h->exportname == NULL) {
+    nbdkit_error ("strdup: %m");
+    free (h);
+    return NULL;
+  }
+
+  /* If file == NULL (ie. using exportname) then don't
+   * pass the client exportname to the lower layers.
+   */
+  exportname = file ? exportname : "";
+
+  /* Request write access to the underlying plugin, for journal replay. */
+  if (next (nxdata, 0, exportname) == -1) {
+    free (h->exportname);
+    free (h);
     return NULL;
   }
 
@@ -148,7 +166,7 @@ ext2_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
   struct ext2_inode inode;
   int64_t r;
   CLEANUP_FREE char *name = NULL;
-  const char *fname = file ?: nbdkit_export_name ();
+  const char *fname = file ?: h->exportname;
   CLEANUP_FREE char *absname = NULL;
 
   fs_flags = 0;
@@ -242,6 +260,7 @@ ext2_close (void *handle)
     ext2fs_file_close (h->file);
     ext2fs_close (h->fs);
   }
+  free (h->exportname);
   free (h);
 }
 

filters/gzip/gzip.c
@@ -74,10 +74,11 @@ gzip_thread_model (void)
 }
 
 static void *
-gzip_open (nbdkit_next_open *next, nbdkit_backend *nxdata, int readonly)
+gzip_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
+           int readonly, const char *exportname)
 {
   /* Always pass readonly=1 to the underlying plugin. */
-  if (next (nxdata, 1) == -1)
+  if (next (nxdata, 1, exportname) == -1)
     return NULL;
 
   return NBDKIT_HANDLE_NOT_NEEDED;

filters/limit/limit.c
@@ -91,9 +91,9 @@ limit_preconnect (nbdkit_next_preconnect *next, nbdkit_backend *nxdata,
 
 static void *
 limit_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
-            int readonly)
+            int readonly, const char *exportname)
 {
-  if (next (nxdata, readonly) == -1)
+  if (next (nxdata, readonly, exportname) == -1)
     return NULL;
 
   ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);

filters/log/log.c
@@ -133,6 +133,7 @@ log_get_ready (nbdkit_next_get_ready *next, void *nxdata)
 struct handle {
   uint64_t connection;
   uint64_t id;
+  char *exportname;
 };
 
 /* Compute the next id number on the current connection. */
@@ -227,16 +228,28 @@ output_return (struct handle *h, const char *act, uint64_t id, int r, int *err)
 
 /* Open a connection. */
 static void *
-log_open (nbdkit_next_open *next, void *nxdata, int readonly)
+log_open (nbdkit_next_open *next, void *nxdata,
+          int readonly, const char *exportname)
 {
   struct handle *h;
 
-  if (next (nxdata, readonly) == -1)
+  if (next (nxdata, readonly, exportname) == -1)
     return NULL;
 
   h = malloc (sizeof *h);
   if (h == NULL) {
     nbdkit_error ("malloc: %m");
+    return NULL;
+  }
+
+  /* Save the exportname in the handle so we can display it in
+   * log_prepare.  We must take a copy because this string has a short
+   * lifetime.
+   */
+  h->exportname = strdup (exportname);
+  if (h->exportname == NULL) {
+    nbdkit_error ("strdup: %m");
+    free (h);
     return NULL;
   }
 
@@ -251,6 +264,7 @@ log_close (void *handle)
 {
   struct handle *h = handle;
 
+  free (h->exportname);
   free (h);
 }
 
@@ -259,7 +273,7 @@ log_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
              int readonly)
 {
   struct handle *h = handle;
-  const char *exportname = nbdkit_export_name ();
+  const char *exportname = h->exportname;
   int64_t size = next_ops->get_size (nxdata);
   int w = next_ops->can_write (nxdata);
   int f = next_ops->can_flush (nxdata);

commit 50b1abc94118cd177a17839e8378f6320d0db6d9

retry: Plug memory leak
Valgrind pointed out that we were leaking h->exportname.

Fixes: 5f7dfce619 (server: Pass the export name through filter .open calls)

filters/retry/retry.c
@@ -146,6 +146,7 @@ retry_close (void *handle)
   struct retry_handle *h = handle;
 
   nbdkit_debug ("reopens needed: %u", h->reopens);
+  free (h->exportname);
   free (h);
 }
 