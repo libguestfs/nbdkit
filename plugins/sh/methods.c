libguestfs/nbdkit

commit 01d37afbda16e26f83f2e5308d1925947074057e

server: Implement nbdkit_is_tls for use during .open
Now that we can differentiate content based on export name, we also
need to be able to differentiate content for a --tls=on server, since
TLS is optional according to whether the client has authenticated.

For internal code and filters, this means adding a new parameter; the
sh plugin can do likewise.  For plugins, we can't add a parameter
until the V3 protocol, so in the meantime, we add nbdkit_is_tls(),
even though it will be deprecated alongside nbdkit_export_name() when
we do get to V3.

Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/sh/methods.c
@@ -341,6 +341,7 @@ sh_open (int readonly)
     { script, method,
       readonly ? "true" : "false",
       nbdkit_export_name () ? : "",
+      nbdkit_is_tls () ? "true" : "false",
       NULL };
   struct sh_handle *h = malloc (sizeof *h);
 

TODO
@@ -62,11 +62,11 @@ General ideas for improvements
   continue to keep their non-standard handshake while utilizing nbdkit
   to prototype new behaviors in serving the kernel.
 
-* When using --tls=on (encryption is supported but not required),
-  plugins need to be able to differentiate which content they serve to
-  plaintext clients vs. secure clients.  This may require an
-  alteration of the signature for .list_exports and .open, or a new
-  utility function nbdkit_tls_mode() similar to nbdkit_export_name().
+* The current signature of .list_exports is awkward; it overloads the
+  list response to NBD_OPT_LIST with the resolution of the default
+  export name in .open, and it is missing a tls parameter.  It is
+  probably worth splitting out a new .default_export callback for the
+  latter purpose, as well as fixing the signature for the former.
 
 * Background thread for filters.  Some filters (readahead, cache and
   proposed scan filter - see below) could be more effective if they
@@ -308,7 +308,6 @@ using ‘#define NBDKIT_API_VERSION <version>’.
   and filters, and the type of the values, and both check and parse
   them for the plugin.
 
-* Modify open() API so it takes an export name parameter, and maybe a
-  tls mode.
+* Modify open() API so it takes an export name and tls parameter.
 
 * Change config_help from a variable to a function.

docs/nbdkit-filter.pod
@@ -404,7 +404,7 @@ Returns a copy of the C<i>'th export.
 =head2 C<.open>
 
  void * (*open) (nbdkit_next_open *next, void *nxdata,
-                 int readonly, const char *exportname);
+                 int readonly, const char *exportname, int is_tls);
 
 This is called when a new client connection is opened and can be used
 to allocate any per-connection data structures needed by the filter.
@@ -421,8 +421,9 @@ error message and return C<NULL>.
 
 This callback is optional, but if provided, it must call C<next>,
 passing C<readonly> and C<exportname> possibly modified according to
-how the filter plans to use the plugin.  Typically, the filter passes
-the same values as it received, or passes readonly=true to provide a
+how the filter plans to use the plugin (C<is_tls> is not passed,
+because a filter cannot modify it).  Typically, the filter passes the
+same values as it received, or passes readonly=true to provide a
 writable layer on top of a read-only backend.  However, it is also
 acceptable to attempt write access to the plugin even if this filter
 is readonly, such as when a file system mounted read-only still
@@ -431,7 +432,10 @@ to be replayed for consistency as part of the mounting process.
 
 The C<exportname> string is only guaranteed to be available during the
 call.  If the filter needs to use it (other than immediately passing
-it down to the next layer) it must take a copy.
+it down to the next layer) it must take a copy.  The C<exportname> and
+C<is_tls> parameters are provided so that filters do not need to use
+the plugin-only interfaces of C<nbdkit_export_name> and
+C<nbdkit_is_tls>.
 
 The filter should generally call C<next> as its first step, to
 allocate from the plugin outwards, so that C<.close> running from the

docs/nbdkit-plugin.pod
@@ -729,10 +729,18 @@ connection to be readonly (even if this flag is false) by returning
 false from the C<.can_write> callback.  So if your plugin can only
 serve read-only, you can ignore this parameter.
 
-This callback is called after the NBD handshake has completed, which
-includes TLS authentication (if required).  If the plugin defines a
-C<.preconnect> callback, then it must be called and return with
-success before C<.open> is called.
+If the plugin wants to differentiate the content it serves based on
+client input, then this is the spot to use C<nbdkit_export_name()> to
+determine which export the client requested.  See also L</EXPORT NAME>
+below.
+
+This callback is called after the NBD handshake has completed; if the
+server requires TLS authentication, then that has occurred as well.
+But if the server is set up to have optional TLS authentication, you
+may check C<nbdkit_is_tls> to learn whether the client has completed
+TLS authentication.  When running the server in a mode that permits
+but not requires TLS, be careful that you do not allow unauthenticated
+clients to cause a denial of service against authentication.
 
 If there is an error, C<.open> should call C<nbdkit_error> with an
 error message and return C<NULL>.
@@ -1466,6 +1474,22 @@ export name does not make sense.  B<The export name is untrusted
 client data, be cautious when parsing it.>
 
 On error, C<nbdkit_error> is called and the call returns C<NULL>.
+
+=head1 AUTHENTICATION
+
+A server may use C<nbdkit_is_tls> to limit which export names work
+until after a client has completed TLS authentication.  See
+L<nbdkit-tls(1)>.
+
+=head2 C<nbdkit_is_tls>
+
+ int nbdkit_is_tls (void);
+
+Return true if the client has completed TLS authentication, or false
+if the connection is still plaintext.
+
+On error (such as calling this function outside of the context of
+C<.open>), C<nbdkit_error> is called and the call returns C<-1>.
 
 =head1 PEER NAME
 

docs/nbdkit-tls.pod
@@ -22,7 +22,10 @@ connect, it will be rejected by the server (in other words, as if the
 server doesn't support TLS).
 
 I<--tls=on> means that the client may choose to connect either with or
-without TLS.
+without TLS.  In this mode, a plugin may wish to serve different
+content depending on whether the client has authenticated; the
+function C<nbdkit_is_tls()> can be used during the C<.open> callback
+for that purpose.
 
 Because I<--tls=on> is subject to downgrade attacks where a malicious
 proxy pretends not to support TLS in order to force either the client

filters/cow/cow.c
@@ -94,7 +94,7 @@ cow_config (nbdkit_next_config *next, void *nxdata,
 
 static void *
 cow_open (nbdkit_next_open *next, void *nxdata,
-          int readonly, const char *exportname)
+          int readonly, const char *exportname, int is_tls)
 {
   /* Always pass readonly=1 to the underlying plugin. */
   if (next (nxdata, 1, exportname) == -1)

filters/exitlast/exitlast.c
@@ -51,7 +51,7 @@ static _Atomic unsigned connections;
 
 static void *
 exitlast_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
-               int readonly, const char *exportname)
+               int readonly, const char *exportname, int is_tls)
 {
   if (next (nxdata, readonly, exportname) == -1)
     return NULL;

filters/ext2/ext2.c
@@ -122,7 +122,7 @@ struct handle {
 /* Create the per-connection handle. */
 static void *
 ext2_open (nbdkit_next_open *next, void *nxdata,
-           int readonly, const char *exportname)
+           int readonly, const char *exportname, int is_tls)
 {
   struct handle *h;
 

commit 0c142b789d2626b2ad0ab882076dc2b01d834211

sh, eval: Don't advertise tls on unlikely failure
nbdkit_is_tls() can return -1 if called outside a connection; while
this is unlikely, we are better off not advertising TLS support to the
plugin in that case.

Fixes: 01d37afbda (server: Implement nbdkit_is_tls for use during .open)

plugins/sh/methods.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2021 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -382,7 +382,7 @@ sh_open (int readonly)
     { script, method,
       readonly ? "true" : "false",
       nbdkit_export_name () ? : "",
-      nbdkit_is_tls () ? "true" : "false",
+      nbdkit_is_tls () > 0 ? "true" : "false",
       NULL };
   struct sh_handle *h = malloc (sizeof *h);
 