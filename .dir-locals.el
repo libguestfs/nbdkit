dbb1c852307891357653c61a766160c168ceae5c
Eric Blake <eblake@redhat.com>
2021-06-11 12:14:49 -0500

    maint: Untabify configure.ac
    We had a haphazard mix of tabs and spaces; most uses were locally
    consistent, but there were a few lists where there was one line of
    tabs among others with spaces.  Easiest is just to use spaces
    everywhere.  Teach emacs our preferences, while at it.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
-((c-mode . ((indent-tabs-mode . nil)
-            (c-basic-offset . 2))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((autoconf-mode . ((indent-tabs-mode . nil)))
+ (c-mode . ((indent-tabs-mode . nil)
+	    (c-basic-offset . 2))))
