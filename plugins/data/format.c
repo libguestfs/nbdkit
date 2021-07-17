libguestfs/nbdkit

commit ee4a8631544150dcf4693bffb5d7308809ca21c5

data: Add le<NN>: and be<NN>: prefixes for little and big endian words
This commit allows you to specify little and big endian integers in
common formats.  Prefixes such as:

 nbdkit data ' le16:0x1 '

 nbdkit data ' be32:123 '

can be used.

The example of MBR partitioning in the man page is rewritten using the
le32: prefix.

This commit adds several tests.

plugins/data/format.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2021 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -63,6 +63,15 @@ NBDKIT_DLL_PUBLIC int data_debug_AST = 0;
 typedef struct expr expr_t;
 
 DEFINE_VECTOR_TYPE(string, char); /* string + length, \0 allowed */
+
+#define CLEANUP_FREE_STRING \
+  __attribute__((cleanup (cleanup_free_string)))
+
+static void
+cleanup_free_string (string *v)
+{
+  free (v->ptr);
+}
 
 typedef size_t node_id;         /* references a node in expr_table below */
 DEFINE_VECTOR_TYPE(node_ids, node_id);
@@ -212,7 +221,7 @@ debug_expr (node_id id, int level)
     nbdkit_debug ("%s<(%s)", debug_indent (level), e->script);
     break;
   case EXPR_STRING: {
-    string s = empty_vector;
+    CLEANUP_FREE_STRING string s = empty_vector;
     static const char hex[] = "0123456789abcdef";
 
     for (i = 0; i < e->string.size; ++i) {
@@ -228,7 +237,6 @@ debug_expr (node_id id, int level)
     }
     string_append (&s, '\0');
     nbdkit_debug ("%s\"%s\"", debug_indent (level), s.ptr);
-    free (s.ptr);
     break;
   }
   case EXPR_NAME:
@@ -311,6 +319,8 @@ read_data_format (const char *value, struct allocator *a, uint64_t *size_rtn)
 
 static int parse_string (const char *value, size_t *start, size_t len,
                          string *rtn);
+static int parse_word (const char *value, size_t *start, size_t len,
+                       string *rtn);
 static size_t get_name (const char *value, size_t i, size_t len,
                         size_t *initial);
 static size_t get_var (const char *value, size_t i, size_t len,
@@ -634,6 +644,13 @@ parser (int level, const char *value, size_t *start, size_t len,
       APPEND_EXPR;
       break;
 
+    case 'l': case 'b':         /* le or be + NN: + WORD */
+      e.t = EXPR_STRING;
+      if (parse_word (value, &i, len, &e.string) == -1)
+        return -1;
+      APPEND_EXPR;
+      break;
+
     case ' ': case '\t': case '\n': /* Skip whitespace. */
     case '\f': case '\r': case '\v':
       i++;
@@ -825,6 +842,128 @@ parse_string (const char *value, size_t *start, size_t len, string *rtn)
  unexpected_end_of_string:
   nbdkit_error ("data parameter: unterminated string");
   return -1;
+}
+
+/* Parse le<NN>:WORD be<NN>:WORD expressions.  These are parsed into strings. */
+static int
+parse_word (const char *value, size_t *start, size_t len, string *rtn)
+{
+  size_t i = *start;
+  CLEANUP_FREE_STRING string copy = empty_vector;
+  size_t n;
+  enum { little, big } endian;
+  uint16_t u16;
+  uint32_t u32;
+  uint64_t u64;
+
+  *rtn = (string) empty_vector;
+
+  /* It's convenient to use the nbdkit_parse* functions below because
+   * they deal already with overflow etc.  However these functions
+   * require a \0-terminated string, so we have to copy what we are
+   * parsing to a new string here.
+   */
+  while (i < len) {
+    if (ascii_isspace (value[i]))
+      break;
+    i++;
+  }
+  n = i - *start;
+  assert (n > 0);          /* must be because caller parsed 'l'/'b' */
+  if (string_reserve (&copy, n + 1) == -1) {
+    nbdkit_error ("realloc: %m");
+    return -1;
+  }
+  memcpy (copy.ptr, &value[*start], n);
+  copy.ptr[n] = '\0';
+  copy.size = n + 1;
+  *start = i;
+
+  /* Reserve enough space in the return buffer for the longest
+   * possible bitstring (64 bits / 8 bytes).
+   */
+  if (string_reserve (rtn, 8) == -1) {
+    nbdkit_error ("realloc: %m");
+    return -1;
+  }
+
+  /* Parse the rest of {le|be}{16|32|64}: */
+  if (strncmp (copy.ptr, "le16:", 5) == 0) {
+    endian = little; rtn->size = 2;
+  }
+  else if (strncmp (copy.ptr, "le32:", 5) == 0) {
+    endian = little; rtn->size = 4;
+  }
+  else if (strncmp (copy.ptr, "le64:", 5) == 0) {
+    endian = little; rtn->size = 8;
+  }
+  else if (strncmp (copy.ptr, "be16:", 5) == 0) {
+    endian = big; rtn->size = 2;
+  }
+  else if (strncmp (copy.ptr, "be32:", 5) == 0) {
+    endian = big; rtn->size = 4;
+  }
+  else if (strncmp (copy.ptr, "be64:", 5) == 0) {
+    endian = big; rtn->size = 8;
+  }
+  else {
+    nbdkit_error ("data parameter: expected \"le16/32/64:\" "
+                  "or \"be16/32/64:\" at offset %zu", i);
+    return -1;
+  }
+
+  /* Parse the word field into a host-order unsigned int. */
+  switch (rtn->size) {
+  case 2:
+    if (nbdkit_parse_uint16_t ("data", &copy.ptr[5], &u16) == -1)
+      return -1;
+    break;
+  case 4:
+    if (nbdkit_parse_uint32_t ("data", &copy.ptr[5], &u32) == -1)
+      return -1;
+    break;
+  case 8:
+    if (nbdkit_parse_uint64_t ("data", &copy.ptr[5], &u64) == -1)
+      return -1;
+    break;
+  default: abort ();
+  }
+
+  /* Now depending on the endianness and size, convert to the final
+   * bitstring.
+   */
+  switch (endian) {
+  case little:
+    switch (rtn->size) {
+    case 2:                     /* le16: */
+      *((uint16_t *) rtn->ptr) = htole16 (u16);
+      break;
+    case 4:                     /* le32: */
+      *((uint32_t *) rtn->ptr) = htole32 (u32);
+      break;
+    case 8:                     /* le64: */
+      *((uint64_t *) rtn->ptr) = htole64 (u64);
+      break;
+    default: abort ();
+    }
+    break;
+
+  case big:
+    switch (rtn->size) {
+    case 2:                     /* be16: */
+      *((uint16_t *) rtn->ptr) = htobe16 (u16);
+      break;
+    case 4:                     /* be32: */
+      *((uint32_t *) rtn->ptr) = htobe32 (u32);
+      break;
+    case 8:                     /* be64: */
+      *((uint64_t *) rtn->ptr) = htobe64 (u64);
+      break;
+    default: abort ();
+    }
+  }
+
+  return 0;
 }
 
 /* This simple optimization pass over the AST simplifies some

plugins/data/nbdkit-data-plugin.pod
@@ -47,26 +47,25 @@ C<HelloHelloHello...>, truncating the disk to exactly 8192 bytes.
 
 See also L<nbdkit-pattern-plugin(3)>.
 
-=head2 Create a 1 MB disk with one empty MBR-formatted partition
+=head2 Create a 1 MB disk with MBR-format partition table
 
  nbdkit data '
-   @0x1b8 178 190 207 221 0 0 0 0 2 0 131 32 32 0 1 0 0 0 255 7
-   @0x1fe 85 170
+   @0x1be    # MBR first partition entry
+     0           # Partition status
+     0 2 0       # CHS start
+     0x83        # Partition type (Linux)
+     0x20 0x20 0 # CHS last sector
+     le32:1      # LBA first sector
+     le32:0x7ff  # LBA number of sectors
+   @0x1fe    # Boot signature
+     0x55 0xaa
    ' size=1M
 
-This example was created by running:
-
- $ rm -f disk
- $ truncate -s 1M disk
- $ echo start=1 | sfdisk disk
- Device Boot Start   End Sectors    Size Id Type
- disk1           1  2047    2047 1023.5K 83 Linux
- $ ./disk2data.pl disk
-
-The C<disk2data.pl> script is provided in the nbdkit sources
+A more flexible way to create partitions is to use
+L<nbdkit-partitioning-plugin(1)>.  To create a data string from an
+existing disk use the C<disk2data.pl> script provided in the nbdkit
+sources
 (L<https://gitlab.com/nbdkit/nbdkit/blob/master/plugins/data/disk2data.pl>).
-
-See also L<nbdkit-partitioning-plugin(1)>.
 
 =head2 Create a disk image with sector-aligned data
 
@@ -195,6 +194,28 @@ Write a byte at the current offset and advance the offset by 1.  The
 byte may be specified as either decimal, octal (prefixed by C<0>) or
 hexadecimal (prefixed by C<0x>).  To add repeated bytes use the C<*>
 operator (eg. C<0xFF*16>).
+
+=item B<le16:>WORD
+
+=item B<be16:>WORD
+
+=item B<le32:>WORD
+
+=item B<be32:>WORD
+
+=item B<le64:>WORD
+
+=item B<be64:>WORD
+
+(nbdkit E<ge> 1.28)
+
+Write a word expressed in little endian (le) or big endian (be) of
+length 16/32/64 bits and advance the offset by 2/4/8.  The word may be
+specified in decimal, octal or hexadecimal.  For example:
+
+ nbdkit data ' be32:0x1 '
+
+generates the 4 byte sequence C<0 0 0 1>.
 
 =item B<@>OFFSET
 

tests/Makefile.am
@@ -640,6 +640,7 @@ TESTS += \
 	test-data-extents.sh \
 	test-data-file.sh \
 	test-data-format.sh \
+	test-data-partition.sh \
 	test-data-raw.sh \
 	test-data-raw-copy.sh \
 	test-data-random-slice.sh \
@@ -655,6 +656,7 @@ EXTRA_DIST += \
 	test-data-extents.sh \
 	test-data-file.sh \
 	test-data-format.sh \
+	test-data-partition.sh \
 	test-data-raw.sh \
 	test-data-raw-copy.sh \
 	test-data-random-slice.sh \

tests/test-data-format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbdkit
-# Copyright (C) 2018-2020 Red Hat Inc.
+# Copyright (C) 2018-2021 Red Hat Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -99,6 +99,19 @@ do_test '0 1' 'b"\x00\x01"'
 do_test '1 0' 'b"\x01\x00"'
 do_test '1 2' 'b"\x01\x02"'
 do_test '1 0xcc' 'b"\x01\xcc"'
+
+do_test 'le16:1' 'b"\x01\x00"'
+do_test 'be16:1' 'b"\x00\x01"'
+do_test 'le32:1' 'b"\x01\x00\x00\x00"'
+do_test 'be32:1' 'b"\x00\x00\x00\x01"'
+do_test 'le64:1' 'b"\x01\x00\x00\x00\x00\x00\x00\x00"'
+do_test 'be64:1' 'b"\x00\x00\x00\x00\x00\x00\x00\x01"'
+do_test 'le16:0xffff' 'b"\xff\xff"'
+do_test 'be16:0xffff' 'b"\xff\xff"'
+do_test 'le32:0xffffffff' 'b"\xff\xff\xff\xff"'
+do_test 'be32:0xffffffff' 'b"\xff\xff\xff\xff"'
+do_test 'le64:0xffffffffffffffff' 'b"\xff\xff\xff\xff\xff\xff\xff\xff"'
+do_test 'be64:0xffffffffffffffff' 'b"\xff\xff\xff\xff\xff\xff\xff\xff"'
 
 do_test '""' 'b""'
 do_test '"foo"' 'b"foo"'

tests/test-data-partition.sh
@@ -1,0 +1,68 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2021 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test the MBR-formatted partition example from the manual.
+
+source ./functions.sh
+set -e
+set -x
+
+requires_nbdsh_uri
+requires nbdcopy --version
+requires hexdump --version
+
+out=data-partition.out
+rm -f $out
+cleanup_fn rm -f $out
+
+nbdkit -U - --filter=partition data partition=1 size=1M '
+  @0x1be    # MBR first partition entry
+    0           # Partition status
+    0 2 0       # CHS start
+    0x83        # Partition type (Linux)
+    0x20 0x20 0 # CHS last sector
+    le32:1      # LBA first sector
+    le32:0x7ff  # LBA number of sectors
+  @0x1fe    # Boot signature
+    0x55 0xaa
+
+  # Data which should appear in the first partition.
+  @0x200 "hello world"
+
+  ' --run "nbdcopy \$uri $out"
+
+hexdump -C $out
+
+test "$( hexdump -C $out )" = '00000000  68 65 6c 6c 6f 20 77 6f  72 6c 64 00 00 00 00 00  |hello world.....|
+00000010  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+*
+000ffe00'

commit e510b9c0a061966d07e3f56c975a968f277913d1

data: Include byte-swapping.h
The functions/macros like htole32 are not defined implicitly on
non-Unix platforms (in this case mingw), so the new "le32:" etc
prefixes caused compile-time failures.  Fix by including the
compatibility header in this file.

Fixes: commit ee4a8631544150dcf4693bffb5d7308809ca21c5

plugins/data/format.c
@@ -45,6 +45,7 @@
 
 #include "allocator.h"
 #include "ascii-ctype.h"
+#include "byte-swapping.h"
 #include "cleanup.h"
 #include "ispowerof2.h"
 #include "minmax.h"