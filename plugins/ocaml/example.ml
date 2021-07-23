NetworkBlockDevice/nbd

commit e3abac4e5519b0d51c9a34a41af97f63920f3f32

ocaml: New plugin lets you write plugins as natively compiled OCaml programs.
This requires your OCaml compiler to support native compilation and be
compiled with -fPIC (ie. it won't work with most distro builds of
OCaml).  I have filed a bug upstream to add support for building
shared libraries from OCaml:

http://caml.inria.fr/mantis/view.php?id=6693

plugins/ocaml/nbdkit-ocaml-plugin.pod
@@ -1,0 +1,134 @@
+=encoding utf8
+
+=head1 NAME
+
+nbdkit-ocaml-plugin - nbdkit OCaml plugin
+
+=head1 SYNOPSIS
+
+ nbdkit /path/to/plugin.so [arguments...]
+
+ nbdkit plugin [arguments...]
+
+=head1 DESCRIPTION
+
+C<nbdkit-ocaml-plugin> allows you to write nbdkit plugins in natively
+compiled OCaml code.
+
+=head1 IMPORTANT NOTE
+
+Currently this requires a specially compiled version of OCaml, and so
+it won't work with the OCaml compiler provided by most Linux distros.
+
+We hope to have this problem resolved in a future version of OCaml.
+You can track progress in this upstream bug:
+L<http://caml.inria.fr/mantis/view.php?id=6693>
+
+=head1 COMPILING AN OCAML NBDKIT PLUGIN
+
+After writing your OCaml plugin (C<myplugin.ml>), link it using this
+command:
+
+ ocamlopt.opt -output-obj -o nbdkit-myplugin-plugin.so \
+   NBDKit.cmx myplugin.ml $plugindir/nbdkit-plugin-ocaml.a
+
+You can use it as if it were a regular nbdkit plugin (see
+L<nbdkit(1)>, L<nbdkit-plugin(3)>).
+
+=head1 WRITING AN OCAML NBDKIT PLUGIN
+
+Broadly speaking, OCaml nbdkit plugins work like C ones, so you should
+read L<nbdkit-plugin(3)> first.
+
+You should also look at C<NBDKit.mli> which describes the plugin
+interface for OCaml plugins.
+
+Your OCaml code should call C<NBDKit.register_plugin> like this:
+
+ let plugin = {
+   NBDKit.default_callbacks with
+     NBDKit.name     = "myplugin";
+     version         = "1.0";
+     open_connection = Some myplugin_open;
+     get_size        = Some myplugin_get_size;
+     pread           = Some myplugin_pread;
+     (* etc *)
+ }
+ 
+ let thread_model = NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS
+ 
+ let () = NBDKit.register_plugin thread_model plugin
+
+=head2 THREADS
+
+The first parameter of C<NBDKit.register_plugin> is the thread model,
+which can be one of the values in the table below.  For more
+information on thread models, see L<nbdkit-plugin(3)/THREADS>.  Note
+that because of the garbage collector lock in OCaml, callbacks are
+never truly concurrent.
+
+=over 4
+
+=item C<NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS>
+
+=item C<NBDKit.THREAD_MODEL_SERIALIZE_ALL_REQUESTS>
+
+=item C<NBDKit.THREAD_MODEL_SERIALIZE_REQUESTS>
+
+=item C<NBDKit.THREAD_MODEL_PARALLEL>
+
+=back
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<ocamlopt(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 Red Hat Inc.
+
+=head1 LICENSE
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+=over 4
+
+=item *
+
+Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+=item *
+
+Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+=item *
+
+Neither the name of Red Hat nor the names of its contributors may be
+used to endorse or promote products derived from this software without
+specific prior written permission.
+
+=back
+
+THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.

tests/test_ocaml_plugin.ml
@@ -1,0 +1,63 @@
+type handle = {
+  disk : string;                        (* The disk image. *)
+}
+
+let test_load () =
+  ()
+let test_unload () =
+  ()
+
+let params = ref []
+
+let test_config k v =
+  params := (k, v) :: !params
+
+let test_config_complete () =
+  let params = List.rev !params in
+  assert (params = [ "a", "1"; "b", "2"; "c", "3" ])
+
+let test_open readonly =
+  let disk =
+    let s = String.create (512 * 8) in
+    for i = 0 to 511 do
+      for j = 0 to 7 do
+        s.[i*8+j] <- Char.chr (j+1)
+      done
+    done;
+    s in
+  { disk = disk }
+
+let test_close h =
+  ()
+
+let test_get_size h =
+  Int64.of_int (String.length h.disk)
+
+let test_pread h buf offset =
+  let len = String.length buf in
+  String.blit h.disk (Int64.to_int offset) buf 0 len
+
+let test_pwrite h buf offset =
+  let len = String.length buf in
+  String.blit buf 0 h.disk (Int64.to_int offset) len
+
+let plugin = {
+  NBDKit.default_callbacks with
+    NBDKit.name     = "testocaml";
+    version         = "1.0";
+
+    load            = Some test_load;
+    unload          = Some test_unload;
+    config          = Some test_config;
+    config_complete = Some test_config_complete;
+
+    open_connection = Some test_open;
+    close           = Some test_close;
+    get_size        = Some test_get_size;
+    pread           = Some test_pread;
+    pwrite          = Some test_pwrite;
+}
+
+let thread_model = NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS
+
+let () = NBDKit.register_plugin thread_model plugin

.gitignore
@@ -1,6 +1,8 @@
 *~
 *.1
 *.3
+*.cmi
+*.cmx
 *.la
 *.lo
 *.log
@@ -41,6 +43,8 @@ Makefile.in
 /tests/test-connect
 /tests/test-file
 /tests/test-gzip
+/tests/test-ocaml
+/tests/test-ocaml-plugin.so
 /tests/test-perl
 /tests/test-python
 /tests/test-streaming

README
@@ -71,6 +71,13 @@ For the Python plugin:
 
  - python development libraries
 
+For the OCaml plugin:
+
+ - OCaml native compiler that was compiled using -fPIC, see:
+   http://www.camlcity.org/knowledge/kb_002_shared_library.html
+   Hopefully this will become the default in a future OCaml, see:
+   http://caml.inria.fr/mantis/view.php?id=6693
+
 After installing any dependencies:
 
   To build from tarball:         To build from git:

configure.ac
@@ -196,6 +196,32 @@ AC_SUBST([PYTHON_CFLAGS])
 AC_SUBST([PYTHON_LIBS])
 AC_SUBST([PYTHON_LDFLAGS])
 
+dnl For the OCaml plugin, you can set OCAMLOPTFLAGS before running
+dnl ./configure to specify any extra flags you want to pass to
+dnl ocamlopt.
+AC_SUBST([OCAMLOPTFLAGS])
+
+dnl Check for OCaml, for embedding in the ocaml plugin.
+AC_PROG_OCAML
+AC_ARG_ENABLE([ocaml],
+    AS_HELP_STRING([--disable-ocaml], [disable OCaml embed plugin]),
+    [],
+    [enable_ocaml=yes])
+AS_IF([test "x$OCAMLOPT" != "xno" && test "x$enable_ocaml" != "xno"],[
+    dnl Check OCaml can create a shared library (see README for details).
+    AC_MSG_CHECKING([if $OCAMLOPT can create a shared library])
+    echo 'print_endline "test"' > conftest.ml
+    AS_IF([$OCAMLOPT $OCAMLOPTFLAGS -output-obj -o conftest.so conftest.ml >&AS_MESSAGE_LOG_FD 2>&1],[
+        AC_MSG_RESULT([yes])
+        ocaml_link_shared=yes
+    ],[
+        AC_MSG_RESULT([no])
+    ])
+    rm -f conftest.ml conftest.cmi conftest.cmx conftest.so conftest.o
+])
+AM_CONDITIONAL([HAVE_OCAML],[test "x$OCAMLOPT" != "xno" &&
+                             test "x$ocaml_link_shared" = "xyes"])
+
 dnl Check for curl (only if you want to compile the curl plugin).
 AC_ARG_WITH([curl],[
     AS_HELP_STRING([--without-curl],
@@ -321,6 +347,7 @@ AC_CONFIG_FILES([Makefile
                  plugins/guestfs/Makefile
                  plugins/gzip/Makefile
                  plugins/libvirt/Makefile
+                 plugins/ocaml/Makefile
                  plugins/perl/Makefile
                  plugins/python/Makefile
                  plugins/streaming/Makefile

docs/nbdkit-plugin.pod
@@ -46,6 +46,7 @@ For example plugins, take a look at the source of nbdkit, in the
 C<plugins> directory.
 
 To write plugins in other languages, see:
+L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>.
 
@@ -531,6 +532,7 @@ You can also write nbdkit plugins in Perl or Python.  Other
 programming languages may be offered in future.
 
 For more information see:
+L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>.
 
@@ -540,6 +542,7 @@ L<nbdkit(1)>,
 L<nbdkit-example1-plugin(1)>,
 L<nbdkit-example2-plugin(1)>,
 L<nbdkit-example3-plugin(1)>,
+L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>.
 

docs/nbdkit.pod
@@ -305,6 +305,7 @@ L<nbdkit-example3-plugin(1)>,
 L<nbdkit-file-plugin(1)>,
 L<nbdkit-gzip-plugin(1)>,
 L<nbdkit-libvirt-plugin(1)>,
+L<nbdkit-ocaml-plugin(3)>,
 L<nbdkit-perl-plugin(3)>,
 L<nbdkit-python-plugin(3)>,
 L<nbdkit-vddk-plugin(1)>.

m4/ocaml.m4
@@ -1,0 +1,240 @@
+dnl autoconf macros for OCaml
+dnl
+dnl Copyright © 2009      Richard W.M. Jones
+dnl Copyright © 2009      Stefano Zacchiroli
+dnl Copyright © 2000-2005 Olivier Andrieu
+dnl Copyright © 2000-2005 Jean-Christophe Filliâtre
+dnl Copyright © 2000-2005 Georges Mariano
+dnl
+dnl For documentation, please read the ocaml.m4 man page.
+
+AC_DEFUN([AC_PROG_OCAML],
+[dnl
+  # checking for ocamlc
+  AC_CHECK_TOOL([OCAMLC],[ocamlc],[no])
+
+  if test "$OCAMLC" != "no"; then
+     OCAMLVERSION=`$OCAMLC -v | sed -n -e 's|.*version* *\(.*\)$|\1|p'`
+     AC_MSG_RESULT([OCaml version is $OCAMLVERSION])
+     # If OCAMLLIB is set, use it
+     if test "$OCAMLLIB" = ""; then
+        OCAMLLIB=`$OCAMLC -where 2>/dev/null || $OCAMLC -v|tail -1|cut -d ' ' -f 4`
+     else
+        AC_MSG_RESULT([OCAMLLIB previously set; preserving it.])
+     fi
+     AC_MSG_RESULT([OCaml library path is $OCAMLLIB])
+
+     AC_SUBST([OCAMLVERSION])
+     AC_SUBST([OCAMLLIB])
+
+     # checking for ocamlopt
+     AC_CHECK_TOOL([OCAMLOPT],[ocamlopt],[no])
+     OCAMLBEST=byte
+     if test "$OCAMLOPT" = "no"; then
+	AC_MSG_WARN([Cannot find ocamlopt; bytecode compilation only.])
+     else
+	TMPVERSION=`$OCAMLOPT -v | sed -n -e 's|.*version* *\(.*\)$|\1|p' `
+	if test "$TMPVERSION" != "$OCAMLVERSION" ; then
+	    AC_MSG_RESULT([versions differs from ocamlc; ocamlopt discarded.])
+	    OCAMLOPT=no
+	else
+	    OCAMLBEST=opt
+	fi
+     fi
+
+     AC_SUBST([OCAMLBEST])
+
+     # checking for ocamlc.opt
+     AC_CHECK_TOOL([OCAMLCDOTOPT],[ocamlc.opt],[no])
+     if test "$OCAMLCDOTOPT" != "no"; then
+	TMPVERSION=`$OCAMLCDOTOPT -v | sed -n -e 's|.*version* *\(.*\)$|\1|p' `
+	if test "$TMPVERSION" != "$OCAMLVERSION" ; then
+	    AC_MSG_RESULT([versions differs from ocamlc; ocamlc.opt discarded.])
+	else
+	    OCAMLC=$OCAMLCDOTOPT
+	fi
+     fi
+
+     # checking for ocamlopt.opt
+     if test "$OCAMLOPT" != "no" ; then
+	AC_CHECK_TOOL([OCAMLOPTDOTOPT],[ocamlopt.opt],[no])
+	if test "$OCAMLOPTDOTOPT" != "no"; then
+	   TMPVERSION=`$OCAMLOPTDOTOPT -v | sed -n -e 's|.*version* *\(.*\)$|\1|p' `
+	   if test "$TMPVERSION" != "$OCAMLVERSION" ; then
+	      AC_MSG_RESULT([version differs from ocamlc; ocamlopt.opt discarded.])
+	   else
+	      OCAMLOPT=$OCAMLOPTDOTOPT
+	   fi
+        fi
+     fi
+
+     AC_SUBST([OCAMLOPT])
+  fi
+
+  AC_SUBST([OCAMLC])
+
+  # checking for ocaml toplevel
+  AC_CHECK_TOOL([OCAML],[ocaml],[no])
+
+  # checking for ocamldep
+  AC_CHECK_TOOL([OCAMLDEP],[ocamldep],[no])
+
+  # checking for ocamlmktop
+  AC_CHECK_TOOL([OCAMLMKTOP],[ocamlmktop],[no])
+
+  # checking for ocamlmklib
+  AC_CHECK_TOOL([OCAMLMKLIB],[ocamlmklib],[no])
+
+  # checking for ocamldoc
+  AC_CHECK_TOOL([OCAMLDOC],[ocamldoc],[no])
+
+  # checking for ocamlbuild
+  AC_CHECK_TOOL([OCAMLBUILD],[ocamlbuild],[no])
+])
+
+
+AC_DEFUN([AC_PROG_OCAMLLEX],
+[dnl
+  # checking for ocamllex
+  AC_CHECK_TOOL([OCAMLLEX],[ocamllex],[no])
+  if test "$OCAMLLEX" != "no"; then
+    AC_CHECK_TOOL([OCAMLLEXDOTOPT],[ocamllex.opt],[no])
+    if test "$OCAMLLEXDOTOPT" != "no"; then
+	OCAMLLEX=$OCAMLLEXDOTOPT
+    fi
+  fi
+  AC_SUBST([OCAMLLEX])
+])
+
+AC_DEFUN([AC_PROG_OCAMLYACC],
+[dnl
+  AC_CHECK_TOOL([OCAMLYACC],[ocamlyacc],[no])
+  AC_SUBST([OCAMLYACC])
+])
+
+
+AC_DEFUN([AC_PROG_CAMLP4],
+[dnl
+  AC_REQUIRE([AC_PROG_OCAML])dnl
+
+  # checking for camlp4
+  AC_CHECK_TOOL([CAMLP4],[camlp4],[no])
+  if test "$CAMLP4" != "no"; then
+     TMPVERSION=`$CAMLP4 -v 2>&1| sed -n -e 's|.*version *\(.*\)$|\1|p'`
+     if test "$TMPVERSION" != "$OCAMLVERSION" ; then
+	AC_MSG_RESULT([versions differs from ocamlc])
+        CAMLP4=no
+     fi
+  fi
+  AC_SUBST([CAMLP4])
+
+  # checking for companion tools
+  AC_CHECK_TOOL([CAMLP4BOOT],[camlp4boot],[no])
+  AC_CHECK_TOOL([CAMLP4O],[camlp4o],[no])
+  AC_CHECK_TOOL([CAMLP4OF],[camlp4of],[no])
+  AC_CHECK_TOOL([CAMLP4OOF],[camlp4oof],[no])
+  AC_CHECK_TOOL([CAMLP4ORF],[camlp4orf],[no])
+  AC_CHECK_TOOL([CAMLP4PROF],[camlp4prof],[no])
+  AC_CHECK_TOOL([CAMLP4R],[camlp4r],[no])
+  AC_CHECK_TOOL([CAMLP4RF],[camlp4rf],[no])
+  AC_SUBST([CAMLP4BOOT])
+  AC_SUBST([CAMLP4O])
+  AC_SUBST([CAMLP4OF])
+  AC_SUBST([CAMLP4OOF])
+  AC_SUBST([CAMLP4ORF])
+  AC_SUBST([CAMLP4PROF])
+  AC_SUBST([CAMLP4R])
+  AC_SUBST([CAMLP4RF])
+])
+
+
+AC_DEFUN([AC_PROG_FINDLIB],
+[dnl
+  AC_REQUIRE([AC_PROG_OCAML])dnl
+
+  # checking for ocamlfind
+  AC_CHECK_TOOL([OCAMLFIND],[ocamlfind],[no])
+  AC_SUBST([OCAMLFIND])
+])
+
+
+dnl Thanks to Jim Meyering for working this next bit out for us.
+dnl XXX We should define AS_TR_SH if it's not defined already
+dnl (eg. for old autoconf).
+AC_DEFUN([AC_CHECK_OCAML_PKG],
+[dnl
+  AC_REQUIRE([AC_PROG_FINDLIB])dnl
+
+  AC_MSG_CHECKING([for OCaml findlib package $1])
+
+  unset found
+  unset pkg
+  found=no
+  for pkg in $1 $2 ; do
+    if $OCAMLFIND query $pkg >/dev/null 2>/dev/null; then
+      AC_MSG_RESULT([found])
+      AS_TR_SH([OCAML_PKG_$1])=$pkg
+      found=yes
+      break
+    fi
+  done
+  if test "$found" = "no" ; then
+    AC_MSG_RESULT([not found])
+    AS_TR_SH([OCAML_PKG_$1])=no
+  fi
+
+  AC_SUBST(AS_TR_SH([OCAML_PKG_$1]))
+])
+
+
+AC_DEFUN([AC_CHECK_OCAML_MODULE],
+[dnl
+  AC_MSG_CHECKING([for OCaml module $2])
+
+  cat > conftest.ml <<EOF
+open $3
+EOF
+  unset found
+  for $1 in $$1 $4 ; do
+    if $OCAMLC -c -I "$$1" conftest.ml >&5 2>&5 ; then
+      found=yes
+      break
+    fi
+  done
+
+  if test "$found" ; then
+    AC_MSG_RESULT([$$1])
+  else
+    AC_MSG_RESULT([not found])
+    $1=no
+  fi
+  AC_SUBST([$1])
+])
+
+
+dnl XXX Cross-compiling
+AC_DEFUN([AC_CHECK_OCAML_WORD_SIZE],
+[dnl
+  AC_REQUIRE([AC_PROG_OCAML])dnl
+  AC_MSG_CHECKING([for OCaml compiler word size])
+  cat > conftest.ml <<EOF
+  print_endline (string_of_int Sys.word_size)
+  EOF
+  OCAML_WORD_SIZE=`$OCAML conftest.ml`
+  AC_MSG_RESULT([$OCAML_WORD_SIZE])
+  AC_SUBST([OCAML_WORD_SIZE])
+])
+
+AC_DEFUN([AC_CHECK_OCAML_OS_TYPE],
+[dnl
+  AC_REQUIRE([AC_PROG_OCAML])dnl
+  AC_MSG_CHECKING([OCaml Sys.os_type])
+
+  cat > conftest.ml <<EOF
+  print_string(Sys.os_type);;
+EOF
+
+  OCAML_OS_TYPE=`$OCAML conftest.ml`
+  AC_MSG_RESULT([$OCAML_OS_TYPE])
+  AC_SUBST([OCAML_OS_TYPE])
+])

libguestfs/nbdkit

commit 5a6681e3dedec4d748f0e351d4452c4c5c4581f6

ocaml: Change thread model in examples to SERIALIZE_ALL_REQUESTS
For unclear reasons this was set to
NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS everywhere (dating back to
the earliest version of the plugin).  A more sensible choice as a
default for example code is SERIALIZE_ALL_REQUESTS.  The default if
the .thread_model method is not specified is PARALLEL.

Fixes: commit e3abac4e5519b0d51c9a34a41af97f63920f3f32

plugins/ocaml/example.ml
@@ -73,7 +73,7 @@ let pwrite h buf offset _ =
   String.blit buf 0 !disk offset len
 
 let thread_model () =
-  NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS
+  NBDKit.THREAD_MODEL_SERIALIZE_ALL_REQUESTS
 
 let plugin = {
   NBDKit.default_callbacks with

plugins/ocaml/nbdkit-ocaml-plugin.pod
@@ -36,7 +36,8 @@ Your OCaml code should call C<NBDKit.register_plugin> like this:
      open_connection = Some myplugin_open;
      get_size        = Some myplugin_get_size;
      pread           = Some myplugin_pread;
-     thread_model    = Some (fun () -> NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS);
+     thread_model    =
+         Some (fun () -> NBDKit.THREAD_MODEL_SERIALIZE_ALL_REQUESTS);
      (* etc *)
  }
  

tests/test_ocaml_plugin.ml
@@ -97,7 +97,7 @@ let extents () count offset _ =
   Array.to_list extents
 
 let thread_model () =
-  NBDKit.THREAD_MODEL_SERIALIZE_CONNECTIONS
+  NBDKit.THREAD_MODEL_SERIALIZE_ALL_REQUESTS
 
 let plugin = {
   NBDKit.default_callbacks with