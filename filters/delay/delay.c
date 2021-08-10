libguestfs/nbdkit

commit de8dcd3a34a38b088a0f9a6f8ca754702ad1f598

delay: Add delay-open and delay-close
Useful for simulating VDDK which has very slow connection.

filters/delay/delay.c
@@ -49,6 +49,8 @@ static int delay_trim_ms = 0;   /* trim delay (milliseconds) */
 static int delay_extents_ms = 0;/* extents delay (milliseconds) */
 static int delay_cache_ms = 0;  /* cache delay (milliseconds) */
 static int delay_fast_zero = 1; /* whether delaying zero includes fast zero */
+static int delay_open_ms = 0;   /* open delay (milliseconds) */
+static int delay_close_ms = 0;  /* close delay (milliseconds) */
 
 static int
 parse_delay (const char *key, const char *value)
@@ -128,6 +130,18 @@ cache_delay (int *err)
   return delay (delay_cache_ms, err);
 }
 
+static int
+open_delay (int *err)
+{
+  return delay (delay_open_ms, err);
+}
+
+static int
+close_delay (int *err)
+{
+  return delay (delay_close_ms, err);
+}
+
 /* Called for each key=value passed on the command line. */
 static int
 delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
@@ -191,6 +205,18 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
       return -1;
     return 0;
   }
+  else if (strcmp (key, "delay-open") == 0) {
+    delay_open_ms = parse_delay (key, value);
+    if (delay_open_ms == -1)
+      return -1;
+    return 0;
+  }
+  else if (strcmp (key, "delay-close") == 0) {
+    delay_close_ms = parse_delay (key, value);
+    if (delay_close_ms == -1)
+      return -1;
+    return 0;
+  }
   else
     return next (nxdata, key, value);
 }
@@ -204,7 +230,9 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
   "delay-extents=<NN>[ms]         Extents delay in seconds/milliseconds.\n" \
   "delay-cache=<NN>[ms]           Cache delay in seconds/milliseconds.\n" \
   "wdelay=<NN>[ms]                Write, zero and trim delay in secs/msecs.\n" \
-  "delay-fast-zero=<BOOL>         Delay fast zero requests (default true).\n"
+  "delay-fast-zero=<BOOL>         Delay fast zero requests (default true).\n" \
+  "delay-open=<NN>[ms]            Open delay in seconds/milliseconds.\n" \
+  "delay-close=<NN>[ms]           Close delay in seconds/milliseconds."
 
 /* Override the plugin's .can_fast_zero if needed */
 static int
@@ -215,6 +243,34 @@ delay_can_fast_zero (nbdkit_next *next,
   if (delay_zero_ms && !delay_fast_zero)
     return 1;
   return next->can_fast_zero (next);
+}
+
+/* Open connection. */
+static void *
+delay_open (nbdkit_next_open *next, nbdkit_context *nxdata,
+            int readonly, const char *exportname, int is_tls)
+{
+  int err;
+
+  if (open_delay (&err) == -1) {
+    errno = err;
+    nbdkit_error ("delay: %m");
+    return NULL;
+  }
+
+  if (next (nxdata, readonly, exportname) == -1)
+    return NULL;
+
+  return NBDKIT_HANDLE_NOT_NEEDED;
+}
+
+/* Close connection. */
+static void
+delay_close (void *handle)
+{
+  int err;
+
+  close_delay (&err);
 }
 
 /* Read data. */
@@ -294,6 +350,8 @@ static struct nbdkit_filter filter = {
   .config            = delay_config,
   .config_help       = delay_config_help,
   .can_fast_zero     = delay_can_fast_zero,
+  .open              = delay_open,
+  .close             = delay_close,
   .pread             = delay_pread,
   .pwrite            = delay_pwrite,
   .zero              = delay_zero,

filters/delay/nbdkit-delay-filter.pod
@@ -9,10 +9,15 @@ nbdkit-delay-filter - nbdkit delay filter
  nbdkit --filter=delay plugin rdelay=NNms wdelay=NNms [plugin-args...]
 
  nbdkit --filter=delay plugin [plugin-args ...]
-          delay-read=(SECS|NNms) delay-write=(SECS|NNms)
-          delay-zero=(SECS|NNms) delay-trim=(SECS|NNms)
-          delay-extents=(SECS|NNms) delay-cache=(SECS|NNms)
+          delay-read=(SECS|NNms)
+          delay-write=(SECS|NNms)
+          delay-zero=(SECS|NNms)
+          delay-trim=(SECS|NNms)
+          delay-extents=(SECS|NNms)
+          delay-cache=(SECS|NNms)
           delay-fast-zero=BOOL
+          delay-open=(SECS|NNms)
+          delay-close=(SECS|NNms)
 
 =head1 DESCRIPTION
 
@@ -108,6 +113,20 @@ delay as any other zero request; but setting this parameter to false
 instantly fails a fast zero response without waiting for or consulting
 the plugin.
 
+=item B<delay-open=>SECS
+
+=item B<delay-open=>NNB<ms>
+
+=item B<delay-close=>SECS
+
+=item B<delay-close=>NNB<ms>
+
+(nbdkit E<ge> 1.28)
+
+Delay open and close operations by C<SECS> seconds or C<NN>
+milliseconds.  Open corresponds to client connection.  Close may not
+be visible to clients if they abruptly disconnect.
+
 =back
 
 =head1 FILES
@@ -140,4 +159,4 @@ Richard W.M. Jones
 
 =head1 COPYRIGHT
 
-Copyright (C) 2018 Red Hat Inc.
+Copyright (C) 2018-2021 Red Hat Inc.

commit 0cafebdb67d0d557ba1be8ea306b8acc5d9b2203

delay: Fix delay-close
See comments in the code for how this has been fixed.

This only delays clients which use NBD_CMD_DISC (libnbd
nbd_shutdown(3)).  Clients which drop the connection obviously cannot
be delayed.  For example:

$ nbdkit --filter=delay null delay-close=3 \
    --run 'time nbdsh -u $uri -c "h.shutdown()"
           time nbdsh -u $uri -c "pass"'

real	0m3.061s     # Client used shutdown, was delayed
user	0m0.028s
sys	0m0.030s

real	0m0.058s     # Client disconnected, was not delayed
user	0m0.029s
sys	0m0.027s

Reported-by: Ming Xie
Fixes: commit de8dcd3a34a38b088a0f9a6f8ca754702ad1f598
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1991652

filters/delay/delay.c
@@ -39,6 +39,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <limits.h>
+#include <time.h>
 
 #include <nbdkit-filter.h>
 
@@ -132,12 +133,6 @@ static int
 open_delay (int *err)
 {
   return delay (delay_open_ms, err);
-}
-
-static int
-close_delay (int *err)
-{
-  return delay (delay_close_ms, err);
 }
 
 /* Called for each key=value passed on the command line. */
@@ -253,13 +248,36 @@ delay_open (nbdkit_next_open *next, nbdkit_context *nxdata,
   return NBDKIT_HANDLE_NOT_NEEDED;
 }
 
-/* Close connection. */
-static void
-delay_close (void *handle)
+/* Close connection.
+ *
+ * We cannot call nbdkit_nanosleep here because the socket may have
+ * been closed and that function will abort and return immediately.
+ * However we want to force a sleep (even if the server is shutting
+ * down) so use regular nanosleep instead.
+ *
+ * We cannot use the .close callback because that happens after the
+ * socket has closed, thus not delaying the client.  By using
+ * .finalize we can delay well-behaved clients (those that use
+ * NBD_CMD_DISC).  We cannot delay clients that drop the connection.
+ */
+static int
+delay_finalize (nbdkit_next *next, void *handle)
 {
-  int err;
+  const unsigned ms = delay_close_ms;
 
-  close_delay (&err);
+  if (ms > 0) {
+    struct timespec ts;
+
+    ts.tv_sec = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000;
+    /* If nanosleep fails we don't really want to interrupt the chain
+     * of finalize calls through the other filters, so ignore any
+     * error here.
+     */
+    nanosleep (&ts, NULL);
+  }
+
+  return next->finalize (next);
 }
 
 /* Read data. */
@@ -340,7 +358,7 @@ static struct nbdkit_filter filter = {
   .config_help       = delay_config_help,
   .can_fast_zero     = delay_can_fast_zero,
   .open              = delay_open,
-  .close             = delay_close,
+  .finalize          = delay_finalize,
   .pread             = delay_pread,
   .pwrite            = delay_pwrite,
   .zero              = delay_zero,

filters/delay/nbdkit-delay-filter.pod
@@ -117,15 +117,23 @@ the plugin.
 
 =item B<delay-open=>NNB<ms>
 
+(nbdkit E<ge> 1.28)
+
+Delay open (client connection) by C<SECS> seconds or C<NN>
+milliseconds.
+
 =item B<delay-close=>SECS
 
 =item B<delay-close=>NNB<ms>
 
 (nbdkit E<ge> 1.28)
 
-Delay open and close operations by C<SECS> seconds or C<NN>
-milliseconds.  Open corresponds to client connection.  Close may not
-be visible to clients if they abruptly disconnect.
+Delay close (client disconnection) by C<SECS> seconds or C<NN>
+milliseconds.  This can also cause server shutdown to be delayed if
+clients are connected at the time.  This only affects clients that
+gracefully disconnect (using C<NBD_CMD_DISC> / libnbd function
+L<nbd_shutdown(3)>).  Clients that abruptly disconnect from the server
+cannot be delayed.
 
 =back
 