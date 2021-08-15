71d3940b2587e39ae70d6bdeafdb27c9a0243f59
Richard W.M. Jones <rjones@redhat.com>
2021-08-15 18:52:32 +0100

    tests/test-data-extents.sh: Remove some bogus tests of zero extents
    nbdkit data "0" has never been guaranteed to produce an
    allocated-but-zero extent.  Writing zeroes can always sparsify an
    extent.  Recent optimizations have broken this assumption, so remove
    the bogus tests.
    
    It might be possible in future to have some sort of
    "allocated-but-zero" pseudo-byte in the data plugin.

--- tests/test-data-extents.sh
+++ tests/test-data-extents.sh
@@ -85,34 +85,6 @@ cat > $expected <<'EOF'
 EOF
 do_test "1" 1M
 
-# Allocated zeroes at the start of the disk.  This should create a
-# zero extent (data=true zero=true).
-cat > $expected <<'EOF'
-{"start":0,"length":32768,"data":true,"zero":true}
-{"start":32768,"length":1015808,"data":false,"zero":true}
-EOF
-do_test "0" 1M
-
-# Completely zero disk.
-cat > $expected <<'EOF'
-{"start":0,"length":32768,"data":true,"zero":true}
-EOF
-do_test "0 0 0" 32K
-
-# Allocated data and zero extents in a few places in the middle.
-cat > $expected <<'EOF'
-{"start":0,"length":32768,"data":false,"zero":true}
-{"start":32768,"length":32768,"data":true,"zero":false}
-{"start":65536,"length":32768,"data":true,"zero":true}
-{"start":98304,"length":32768,"data":false,"zero":true}
-{"start":131072,"length":32768,"data":true,"zero":false}
-{"start":163840,"length":98304,"data":false,"zero":true}
-{"start":262144,"length":32768,"data":true,"zero":true}
-{"start":294912,"length":32768,"data":true,"zero":false}
-{"start":327680,"length":720896,"data":false,"zero":true}
-EOF
-do_test "@32768 1 @65536 0 @131072 1 @262144 0 @294912 1" 1M
-
 # This should test coalescing in the extents code.
 cat > $expected <<'EOF'
 {"start":0,"length":32768,"data":false,"zero":true}
