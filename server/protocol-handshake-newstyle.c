4c07516eca730291e2c5746e2139bf063f70e451
Eric Blake <eblake@redhat.com>
2021-08-16 13:43:29 -0500

    server: reset meta context replies on starttls
    Related to CVE-2021-3716, but not as severe.  No compliant client will
    send NBD_CMD_BLOCK_STATUS unless it first negotiates
    NBD_OPT_SET_META_CONTEXT.  If an attacker injects a premature
    SET_META_CONTEXT, either the client will never notice (because it
    never uses BLOCK_STATUS), or the client will overwrite the attacker's
    attempt with the client's own SET_META_CONTEXT request after
    encryption is enabled.  So I don't class this as having the potential
    to trigger denial-of-service due to any protocol mismatch between
    compliant client and server (I don't care what happens with
    non-compliant clients).
    
    Fixes: 26455d45 (server: protocol: Implement Block Status "base:allocation".)
    (cherry picked from commit 6c5faac6a37077cf2366388a80862bb00616d0d8)
    
    Conflicts:
    	server/protocol-handshake-newstyle.c - no cached export names

--- server/protocol-handshake-newstyle.c
+++ server/protocol-handshake-newstyle.c
@@ -357,6 +357,7 @@ negotiate_handshake_newstyle_options (struct connection *conn)
         debug ("using TLS on this connection");
         /* Wipe out any cached state. */
         conn->structured_replies = false;
+        conn->meta_context_base_allocation = false;
       }
       break;
 
