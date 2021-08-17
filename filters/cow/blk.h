7182c47d04d2b68005fceadefc0c14bfaa61a533
Richard W.M. Jones <rjones@redhat.com>
2021-08-17 22:03:11 +0100

    cow: Make the block size configurable
    Commit c1905b0a28 ("cache, cow: Use a 64K block size by default")
    changed the nbdkit-cow-filter block size to 64K, but it was still a
    fixed size.  In contrast the cache filter allows the block size to be
    adjusted.
    
    Allow the block size in this filter to be adjusted up or down with a
    new cow-block-size=N parameter.
    
    When using the VDDK plugin, adjusting this setting can make a
    difference.  The following timings come from a modified virt-v2v which
    sets cow-block-size and was used to convert from a VMware server to
    -o null (this is also using cow-on-read=true):
    
      cow-block-size=64K:   18m18
      cow-block-size=256K:  14m13
      cow-block-size=1M:    14m19
      cow-block-size=4M:    37m33
    
    As you can see it's not obvious how to choose a good block size, but
    at least by allowing adjustment we can tune things.

--- filters/cow/blk.h
+++ filters/cow/blk.h
@@ -33,11 +33,6 @@
 #ifndef NBDKIT_BLK_H
 #define NBDKIT_BLK_H
 
-/* Size of a block in the overlay.  A 4K block size means that we need
- * 64 MB of memory to store the bitmap for a 1 TB underlying image.
- */
-#define BLKSIZE 65536
-
 /* Initialize the overlay and bitmap. */
 extern int blk_init (void);
 
