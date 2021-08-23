libguestfs/nbdkit

commit 9bea4b396edd350f13a4c57e455c482a9853e415

data: Fix optimization of inlined lists
Commit 6cfbcf9002 ("data: Get rid of EXPR_EXPR") is generally correct
but made a mistake with inlining of lists.  Offsets are scoped and so
should not be inlined.  For example:

  1 (@2 2) (@3 3)

should expand to:

  1 0 0 2 0 0 0 3

but because the lists are inlined it currently expands to:

  1 0 2 3

Fix this optimization and add a test.

Note that this only worked in the old (EXPR_EXPR) code by accident
because I missed an optimization there.

Fixes: commit 6cfbcf900273fd444426bf446a5d96a874dc3e9d

plugins/data/format.c
@@ -1113,7 +1113,7 @@ parse_word (const char *value, size_t *start, size_t len, string *rtn)
 /* This simple optimization pass over the AST simplifies some
  * expressions.
  */
-static bool expr_contains_assignments (const expr_t);
+static bool list_safe_to_inline (const node_ids);
 static bool expr_is_single_byte (const expr_t, uint8_t *b);
 static bool exprs_can_combine (expr_t e0, expr_t e1, node_id *id_rtn);
 
@@ -1139,11 +1139,11 @@ optimize_ast (node_id root, node_id *root_rtn)
         /* null elements of a list can be ignored. */
         break;
       case EXPR_LIST:
-        /* List with a list can be flattened, but only if it doesn't
-         * contain assignments.  The reason is that assignments are
-         * scoped and flattening a list here could change the scope.
+        /* Simple lists can be inlined, but be careful with
+         * assignments, offsets and other expressions which are scoped
+         * because flattening the list changes the scope.
          */
-        if (! expr_contains_assignments (get_node (id))) {
+        if (list_safe_to_inline (get_node (id).list)) {
           for (j = 0; j < get_node (id).list.size; ++j) {
             if (node_ids_append (&list, get_node (id).list.ptr[j]) == -1)
               goto list_append_error;
@@ -1396,31 +1396,42 @@ optimize_ast (node_id root, node_id *root_rtn)
   abort ();
 }
 
-/* Test if an expression contains assignments or names. */
+/* Test if a list can be inlined without changing any scoped expressions. */
 static bool
-expr_contains_assignments (const expr_t e)
+list_safe_to_inline (const node_ids list)
 {
   size_t i;
 
-  switch (e.t) {
-  case EXPR_ASSIGN:
-  case EXPR_NAME:
-    return true;
+  for (i = 0; i < list.size; ++i) {
+    const expr_t e = get_node (list.ptr[i]);
 
-  case EXPR_REPEAT:
-    return expr_contains_assignments (get_node (e.r.id));
-  case EXPR_SLICE:
-    return expr_contains_assignments (get_node (e.sl.id));
-  case EXPR_LIST:
-    for (i = 0; i < e.list.size; ++i) {
-      if (expr_contains_assignments (get_node (e.list.ptr[i])))
-        return true;
+    switch (e.t) {
+      /* Assignments and named expressions are scoped. */
+    case EXPR_ASSIGN:
+    case EXPR_NAME:
+      return false;
+
+      /* @Offsets are scoped. */
+    case EXPR_ABS_OFFSET:
+    case EXPR_REL_OFFSET:
+    case EXPR_ALIGN_OFFSET:
+      return false;
+
+      /* Everything else should be safe. */
+    case EXPR_NULL:
+    case EXPR_LIST:
+    case EXPR_BYTE:
+    case EXPR_FILE:
+    case EXPR_SCRIPT:
+    case EXPR_STRING:
+    case EXPR_FILL:
+    case EXPR_REPEAT:
+    case EXPR_SLICE:
+      ;
     }
-    return false;
-
-  default:
-    return false;
   }
+
+  return true;
 }
 
 /* For some constant expressions which are a length 1 byte, return

tests/test-data-format.sh
@@ -147,6 +147,9 @@ do_test '( @4 )' 'bytearray(4)'
 do_test '( @4 )*4 1' 'bytearray(16) + b"\x01"'
 do_test '( @7 )*4' 'bytearray(28)'
 
+# Nested offsets apply only to the nested expression.
+do_test '1 (@2 2) (@3 3)' 'b"\1\0\0\2\0\0\0\3"'
+
 # These should all expand to nothing.  They are here to test corner
 # cases in the parser and optimizer.
 do_test '

commit 4d0e6fae2f835782909a352a3dbf24468687bb9f

data: Fix inlining of unsafe expressions
Commit 9bea4b396e ("data: Fix optimization of inlined lists") fixed
inlining of lists in lists.  However it missed the case where a single
element is inlined in the superior list.

An example which was optimized wrongly was:

  ( 0 -> \a ) \a

This was incorrectly optimized by inlining to:

  0 -> \a \a

However it should have failed (because the use of \a is out of scope
of its definition).

This commit corrects this and also adds a general test of some
expressions that should fail to parse or be optimized.

Fixes: commit 9bea4b396edd350f13a4c57e455c482a9853e415
Fixes: commit 6cfbcf900273fd444426bf446a5d96a874dc3e9d

plugins/data/format.c
@@ -1113,6 +1113,7 @@ parse_word (const char *value, size_t *start, size_t len, string *rtn)
 /* This simple optimization pass over the AST simplifies some
  * expressions.
  */
+static bool expr_safe_to_inline (const expr_t);
 static bool list_safe_to_inline (const node_ids);
 static bool expr_is_single_byte (const expr_t, uint8_t *b);
 static bool exprs_can_combine (expr_t e0, expr_t e1, node_id *id_rtn);
@@ -1180,8 +1181,10 @@ optimize_ast (node_id root, node_id *root_rtn)
       return 0;
     }
 
-    /* List of length 1 is replaced with the first element. */
-    if (list.size == 1) {
+    /* List of length 1 is replaced with the first element, but as
+     * above avoid inlining if it is not a safe expression.
+     */
+    if (list.size == 1 && expr_safe_to_inline (get_node (list.ptr[0]))) {
       id = list.ptr[0];
       free (list.ptr);
       *root_rtn = id;
@@ -1396,39 +1399,49 @@ optimize_ast (node_id root, node_id *root_rtn)
   abort ();
 }
 
-/* Test if a list can be inlined without changing any scoped expressions. */
+/* Test if an expression can be safely inlined in a superior list
+ * without changing the meaning of any scoped expressions.
+ */
+static bool
+expr_safe_to_inline (const expr_t e)
+{
+  switch (e.t) {
+    /* Assignments and named expressions are scoped. */
+  case EXPR_ASSIGN:
+  case EXPR_NAME:
+    return false;
+
+    /* @Offsets are scoped. */
+  case EXPR_ABS_OFFSET:
+  case EXPR_REL_OFFSET:
+  case EXPR_ALIGN_OFFSET:
+    return false;
+
+    /* Everything else should be safe. */
+  case EXPR_NULL:
+  case EXPR_LIST:
+  case EXPR_BYTE:
+  case EXPR_FILE:
+  case EXPR_SCRIPT:
+  case EXPR_STRING:
+  case EXPR_FILL:
+  case EXPR_REPEAT:
+  case EXPR_SLICE:
+    ;
+  }
+
+  return true;
+}
+
+/* Test if a list of expressions is safe to inline in a superior list. */
 static bool
 list_safe_to_inline (const node_ids list)
 {
   size_t i;
 
   for (i = 0; i < list.size; ++i) {
-    const expr_t e = get_node (list.ptr[i]);
-
-    switch (e.t) {
-      /* Assignments and named expressions are scoped. */
-    case EXPR_ASSIGN:
-    case EXPR_NAME:
+    if (!expr_safe_to_inline (get_node (list.ptr[i])))
       return false;
-
-      /* @Offsets are scoped. */
-    case EXPR_ABS_OFFSET:
-    case EXPR_REL_OFFSET:
-    case EXPR_ALIGN_OFFSET:
-      return false;
-
-      /* Everything else should be safe. */
-    case EXPR_NULL:
-    case EXPR_LIST:
-    case EXPR_BYTE:
-    case EXPR_FILE:
-    case EXPR_SCRIPT:
-    case EXPR_STRING:
-    case EXPR_FILL:
-    case EXPR_REPEAT:
-    case EXPR_SLICE:
-      ;
-    }
   }
 
   return true;

tests/Makefile.am
@@ -645,6 +645,7 @@ LIBGUESTFS_TESTS += test-data
 TESTS += \
 	test-data-64b.sh \
 	test-data-7E.sh \
+	test-data-bad.sh \
 	test-data-base64.sh \
 	test-data-extents.sh \
 	test-data-file.sh \
@@ -662,6 +663,7 @@ TESTS += \
 EXTRA_DIST += \
 	test-data-64b.sh \
 	test-data-7E.sh \
+	test-data-bad.sh \
 	test-data-base64.sh \
 	test-data-extents.sh \
 	test-data-file.sh \

tests/test-data-bad.sh
@@ -1,0 +1,161 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2021 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test bad input to the data plugin.
+
+source ./functions.sh
+set -e
+set -x
+
+requires_run
+requires_nbdsh_uri
+
+# Since this test is expected to fail, valgrind will also fail.
+if [ "$NBDKIT_VALGRIND" = "1" ]; then
+    echo "$0: skipping test under valgrind."
+    exit 77
+fi
+
+do_test ()
+{
+    data="$1"
+
+    # This command is expected to fail.
+    if nbdkit -U - -fv -D data.AST=1 data "$data" --run true; then
+        echo "$0: data plugin was expected to fail on bad input: $data"
+        exit 1
+    fi
+}
+
+# Invalid bytes, numbers and words.
+do_test '-1'
+do_test '-'
+do_test '256'
+do_test '0400'
+do_test '0x100'
+do_test '0xfff'
+do_test '0xffffffffffffffff'
+do_test '0xffffffffffffffffffffffffffffffff'
+
+for prefix in le16 be16 le32 be32 le64 be64; do
+    do_test "$prefix"
+    do_test "$prefix:"
+    do_test "$prefix:-1"
+    do_test "$prefix:abc"
+    do_test "$prefix:0xffffffffffffffffffffffffffffffff"
+done
+
+do_test 'le16:0x10000'
+do_test 'be16:0x10000'
+do_test 'le32:0x100000000'
+do_test 'be32:0x100000000'
+do_test 'le64:0x10000000000000000'
+do_test 'be64:0x10000000000000000'
+
+# Invalid barewords and symbols.
+do_test 'a'
+do_test 'x'
+do_test 'be'
+do_test 'le'
+do_test 'be1'
+do_test 'le3'
+do_test '¢'
+#do_test '0x'  # should fail but does not XXX
+do_test '?'
+do_test '\'
+do_test '^'
+do_test '@'
+do_test '@+'
+do_test '@-'
+do_test '@^'
+do_test '<'
+do_test '<('
+do_test '$'
+do_test '*'
+do_test '['
+do_test ':'
+do_test ']'
+
+# Invalid offsets.
+do_test '@-2'
+do_test '@2 @-3'
+do_test '@2 @-2 @-1'
+do_test '@1 @^2 @-3'
+
+# Mismatched parentheses.
+do_test "("
+do_test ")"
+do_test "( ("
+do_test "( ( )"
+do_test ") ( )"
+do_test "( ) )"
+
+# Invalid strings.
+do_test '"'
+do_test '"\'
+do_test '"\\'
+do_test '"\"'
+do_test '"\x"'
+
+# Bad repeats.
+do_test '*0'
+do_test '*1'
+do_test '0*-1'
+do_test '0*'
+do_test '0*x'
+do_test '0**'
+
+# Bad slices.
+do_test '0[2:]'
+do_test '0[:2]'
+do_test '[:]'
+do_test '[0:1]'
+do_test '"111"[4:]'
+do_test '"123"[4:]'
+do_test '"123"[1:0]'
+
+# Bad files.
+if [ ! -r /NOFILE ]; then
+    do_test '</NOFILE'
+fi
+
+# Unknown and out of scope assignments.
+do_test '\a'
+do_test '( 0 -> \a ) \a'
+do_test '\a ( 0 -> \a )'
+do_test '0 -> \a \b'
+do_test '0 -> \a \a \b'
+do_test '( 0 -> \a \a ) \a'
+
+# Unknown variable definition
+unset unknownvar
+do_test '$unknownvar'