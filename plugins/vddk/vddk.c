libguestfs/nbdkit

commit 5bb8f0586e1faabcbf4f43d722a3b3cb5b352e33

vddk: Add logical and physical sector size to -D vddk.diskinfo output
In VDDK >= 7 it is possible to display the logical and physical sector
size in debug output.

This commit also extends the test since this flag was not tested
before.

plugins/vddk/vddk.c
@@ -823,6 +823,12 @@ vddk_get_size (void *handle)
                   info->parentFileNameHint ? : "NULL");
     nbdkit_debug ("disk info: uuid: %s",
                   info->uuid ? : "NULL");
+    if (library_version >= 7) {
+      nbdkit_debug ("disk info: sectory size: "
+                    "logical %" PRIu32 " physical %" PRIu32,
+                    info->logicalSectorSize,
+                    info->physicalSectorSize);
+    }
   }
 
   VDDK_CALL_START (VixDiskLib_FreeInfo, "info")

tests/test-vddk-real.sh
@@ -82,7 +82,8 @@ if grep 'cannot open shared object file' test-vddk-real.log; then
 fi
 
 # Now run nbdkit for the test.
-start_nbdkit -P $pid -U $sock -D vddk.stats=1 vddk libdir="$vddkdir" $vmdk
+start_nbdkit -P $pid -U $sock -D vddk.stats=1 -D vddk.diskinfo=1 \
+             vddk libdir="$vddkdir" $vmdk
 uri="nbd+unix:///?socket=$sock"
 
 # VDDK < 6.0 did not support flush, so disable flush test there.  Also

commit 343dadeb7340d7b8c5730e2bbab33c829b569122

vddk: Fix typo in debug message
Fixes: commit 5bb8f0586e1faabcbf4f43d722a3b3cb5b352e33

plugins/vddk/vddk.c
@@ -824,7 +824,7 @@ vddk_get_size (void *handle)
     nbdkit_debug ("disk info: uuid: %s",
                   info->uuid ? : "NULL");
     if (library_version >= 7) {
-      nbdkit_debug ("disk info: sectory size: "
+      nbdkit_debug ("disk info: sector size: "
                     "logical %" PRIu32 " physical %" PRIu32,
                     info->logicalSectorSize,
                     info->physicalSectorSize);