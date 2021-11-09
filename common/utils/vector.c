libguestfs/nbdkit

commit 12356fa97a840de19bb61e0abedd6e7c7e578e5a

common/urils/vector.c: Optimize vector append
Minimize reallocs by growing the backing array by factor of 1.5.

Testing show that now append() is fast without calling reserve()
upfront, simplifying code using vector.

    NBDKIT_BENCH=1 ./test-vector
    bench_reserve: 1000000 appends in 0.004496 s
    bench_append: 1000000 appends in 0.004180 s

This can make a difference in code appending millions of items.

Ported from libnbd commit 985dfa72ae2e41901f0af21e7205ef85428cd4bd.

common/utils/vector.c
@@ -41,11 +41,21 @@ int
 generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
 {
   void *newptr;
+  size_t reqalloc, newalloc;
 
-  newptr = realloc (v->ptr, (n + v->alloc) * itemsize);
+  reqalloc = v->alloc + n;
+  if (reqalloc < v->alloc)
+    return -1; /* overflow */
+
+  newalloc = (v->alloc * 3 + 1) / 2;
+
+  if (newalloc < reqalloc)
+    newalloc = reqalloc;
+
+  newptr = realloc (v->ptr, newalloc * itemsize);
   if (newptr == NULL)
     return -1;
   v->ptr = newptr;
-  v->alloc += n;
+  v->alloc = newalloc;
   return 0;
 }

commit 0c342208d5424dc85029008ad249bcbdb8299852

common/utils/vector: Better overflow handling
Check newcap * itemsize for overflow prior to calling realloc, so that
we don't accidentally truncate an existing array.  Rewrite the scaling
by 1.5 to be less prone to intermediate overflow.  Set errno to ENOMEM
on all failure paths, rather than leaving it indeterminate on
overflow.  The fix depends on a prerequisite assumption that
v->cap*itemsize has not previously overflowed.

Picked from libnbd commit 884052e0545130a4bfe0ab0499a0025b8ca701fc.
Fixes: 12356fa97a (common/urils/vector.c: Optimize vector append)

common/utils/vector.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2021 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -34,6 +34,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "vector.h"
 
@@ -44,12 +45,14 @@ generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
   size_t reqcap, newcap;
 
   reqcap = v->cap + n;
-  if (reqcap < v->cap)
+  if (reqcap * itemsize < v->cap * itemsize) {
+    errno = ENOMEM;
     return -1; /* overflow */
+  }
 
-  newcap = (v->cap * 3 + 1) / 2;
+  newcap = v->cap + (v->cap + 1) / 2;
 
-  if (newcap < reqcap)
+  if (newcap * itemsize < reqcap * itemsize)
     newcap = reqcap;
 
   newptr = realloc (v->ptr, newcap * itemsize);