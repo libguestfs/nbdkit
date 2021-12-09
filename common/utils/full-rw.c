libguestfs/nbdkit

commit ce0db9d7736dd28dd0f10951ce65853e50b35e41

cache, cow: Use full pread/pwrite operations
Although it probably cannot happen on Linux, POSIX allows pread/pwrite
to return or write fewer bytes than requested.  The cache and cow
filters didn't handle this situation.  Replace the raw
pread(2)/pwrite(2) syscalls with alternate versions which can handle
this.

common/utils/Makefile.am
@@ -40,6 +40,7 @@ libutils_la_SOURCES = \
 	cleanup-nbdkit.c \
 	cleanup.h \
 	environ.c \
+	full-rw.c \
 	quote.c \
 	utils.c \
 	utils.h \

common/utils/full-rw.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2021 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -30,17 +30,52 @@
  * SUCH DAMAGE.
  */
 
-#ifndef NBDKIT_UTILS_H
-#define NBDKIT_UTILS_H
+/* These functions are like pread(2)/pwrite(2) but they always read or
+ * write the full amount, or fail.
+ */
 
-extern void shell_quote (const char *str, FILE *fp);
-extern void uri_quote (const char *str, FILE *fp);
-extern int exit_status_to_nbd_error (int status, const char *cmd);
-extern int set_cloexec (int fd);
-extern int set_nonblock (int fd);
-extern char **copy_environ (char **env, ...) __attribute__((__sentinel__));
-extern char *make_temporary_directory (void);
-extern ssize_t full_pread (int fd, void *buf, size_t count, off_t offset);
-extern ssize_t full_pwrite (int fd, const void *buf, size_t count, off_t offset);
+#include <config.h>
 
-#endif /* NBDKIT_UTILS_H */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+ssize_t
+full_pread (int fd, void *buf, size_t count, off_t offset)
+{
+  ssize_t ret = 0, r;
+
+  while (count > 0) {
+    r = pread (fd, buf, count, offset);
+    if (r == -1) return -1;
+    if (r == 0) {
+      /* Presumably the caller wasn't expecting end-of-file here, so
+       * return an error.
+       */
+      errno = EIO;
+      return -1;
+    }
+    ret += r;
+    offset += r;
+    count -= r;
+  }
+
+  return ret;
+}
+
+ssize_t
+full_pwrite (int fd, const void *buf, size_t count, off_t offset)
+{
+  ssize_t ret = 0, r;
+
+  while (count > 0) {
+    r = pwrite (fd, buf, count, offset);
+    if (r == -1) return -1;
+    ret += r;
+    offset += r;
+    count -= r;
+  }
+
+  return ret;
+}

filters/cache/blk.c
@@ -250,7 +250,7 @@ _blk_read_multiple (nbdkit_next *next,
                       " (offset %" PRIu64 ")",
                       blknum, (uint64_t) offset);
 
-      if (pwrite (fd, block, blksize * runblocks, offset) == -1) {
+      if (full_pwrite (fd, block, blksize * runblocks, offset) == -1) {
         *err = errno;
         nbdkit_error ("pwrite: %m");
         return -1;
@@ -262,7 +262,7 @@ _blk_read_multiple (nbdkit_next *next,
     }
   }
   else {                        /* Read cache. */
-    if (pread (fd, block, blksize * runblocks, offset) == -1) {
+    if (full_pread (fd, block, blksize * runblocks, offset) == -1) {
       *err = errno;
       nbdkit_error ("pread: %m");
       return -1;
@@ -339,7 +339,7 @@ blk_cache (nbdkit_next *next,
       nbdkit_debug ("cache: cache block %" PRIu64 " (offset %" PRIu64 ")",
                     blknum, (uint64_t) offset);
 
-    if (pwrite (fd, block, blksize, offset) == -1) {
+    if (full_pwrite (fd, block, blksize, offset) == -1) {
       *err = errno;
       nbdkit_error ("pwrite: %m");
       return -1;
@@ -380,7 +380,7 @@ blk_writethrough (nbdkit_next *next,
     nbdkit_debug ("cache: writethrough block %" PRIu64 " (offset %" PRIu64 ")",
                   blknum, (uint64_t) offset);
 
-  if (pwrite (fd, block, blksize, offset) == -1) {
+  if (full_pwrite (fd, block, blksize, offset) == -1) {
     *err = errno;
     nbdkit_error ("pwrite: %m");
     return -1;
@@ -414,7 +414,7 @@ blk_write (nbdkit_next *next,
     nbdkit_debug ("cache: writeback block %" PRIu64 " (offset %" PRIu64 ")",
                   blknum, (uint64_t) offset);
 
-  if (pwrite (fd, block, blksize, offset) == -1) {
+  if (full_pwrite (fd, block, blksize, offset) == -1) {
     *err = errno;
     nbdkit_error ("pwrite: %m");
     return -1;

filters/cow/blk.c
@@ -282,7 +282,7 @@ blk_read_multiple (nbdkit_next *next,
     memset (block + n, 0, tail);
   }
   else if (state == BLOCK_ALLOCATED) { /* Read overlay. */
-    if (pread (fd, block, BLKSIZE * runblocks, offset) == -1) {
+    if (full_pread (fd, block, BLKSIZE * runblocks, offset) == -1) {
       *err = errno;
       nbdkit_error ("pread: %m");
       return -1;
@@ -357,7 +357,7 @@ blk_cache (nbdkit_next *next,
   memset (block + n, 0, tail);
 
   if (mode == BLK_CACHE_COW) {
-    if (pwrite (fd, block, BLKSIZE, offset) == -1) {
+    if (full_pwrite (fd, block, BLKSIZE, offset) == -1) {
       *err = errno;
       nbdkit_error ("pwrite: %m");
       return -1;
@@ -376,7 +376,7 @@ blk_write (uint64_t blknum, const uint8_t *block, int *err)
     nbdkit_debug ("cow: blk_write block %" PRIu64 " (offset %" PRIu64 ")",
                   blknum, (uint64_t) offset);
 
-  if (pwrite (fd, block, BLKSIZE, offset) == -1) {
+  if (full_pwrite (fd, block, BLKSIZE, offset) == -1) {
     *err = errno;
     nbdkit_error ("pwrite: %m");
     return -1;

commit aaca7cfaede90c681265f8a413e250accbf1d5f6

windows: Use replacement pread and pwrite functions
Fixes: commit ce0db9d7736dd28dd0f10951ce65853e50b35e41

common/utils/Makefile.am
@@ -53,6 +53,7 @@ libutils_la_SOURCES = \
 libutils_la_CPPFLAGS = \
 	-I$(top_srcdir)/include \
 	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/replacements \
 	$(NULL)
 libutils_la_CFLAGS = \
 	$(WARNINGS_CFLAGS) \

common/utils/full-rw.c
@@ -41,6 +41,9 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include "pread.h"
+#include "pwrite.h"
+
 ssize_t
 full_pread (int fd, void *buf, size_t count, off_t offset)
 {