libguestfs/nbdkit

commit 0b0eece73f04963a66b9adc507e5cbaba608660b

common/utils/vector: Rename `size` to `len`
The field `size` may be confusing with the size of the underlying array.
Rename to `len`, a common term for this concept.

Tested with "make check" as regular user, tests that need root or
external libraries (vddk) are not tested.

Ported from libnbd commit cc0567e9aed7e6b40a44bf8eac0a262ac7314fec.

server/sockets.c
@@ -246,14 +246,14 @@ bind_tcpip_socket (sockets *socks)
 
   freeaddrinfo (ai);
 
-  if (socks->size == 0 && addr_in_use) {
+  if (socks->len == 0 && addr_in_use) {
     fprintf (stderr, "%s: unable to bind to any sockets: %s\n",
              program_name, strerror (EADDRINUSE));
     exit (EXIT_FAILURE);
   }
 
   debug ("bound to IP address %s:%s (%zu socket(s))",
-         ipaddr ? ipaddr : "<any>", port, socks->size);
+         ipaddr ? ipaddr : "<any>", port, socks->len);
 }
 
 void
@@ -443,7 +443,7 @@ accept_connection (int listen_sock)
 static void
 check_sockets_and_quit_fd (const sockets *socks)
 {
-  const size_t nr_socks = socks->size;
+  const size_t nr_socks = socks->len;
   size_t i;
   int r;
 
@@ -552,7 +552,7 @@ accept_incoming_connections (const sockets *socks)
   }
   pthread_mutex_unlock (&count_mutex);
 
-  for (i = 0; i < socks->size; ++i)
+  for (i = 0; i < socks->len; ++i)
     closesocket (socks->ptr[i]);
   free (socks->ptr);
 }

common/allocators/allocator.c
@@ -137,7 +137,7 @@ create_allocator (const char *type, bool debug)
     return NULL;
 
   /* See if we can find the allocator. */
-  for (i = 0; i < allocators.size; ++i) {
+  for (i = 0; i < allocators.len; ++i) {
     if (strncmp (type, allocators.ptr[i]->type, type_len) == 0) {
       ret = allocators.ptr[i]->create (&params);
       break;

common/allocators/malloc.c
@@ -241,7 +241,7 @@ m_alloc_create (const void *paramsv)
   size_t i;
 
   /* Parse the optional mlock=true|false parameter. */
-  for (i = 0; i < params->size; ++i) {
+  for (i = 0; i < params->len; ++i) {
     if (strcmp (params->ptr[i].key, "mlock") == 0) {
       int r = nbdkit_parse_bool (params->ptr[i].value);
       if (r == -1) return NULL;

common/allocators/sparse.c
@@ -150,7 +150,7 @@ sparse_array_free (struct allocator *a)
   size_t i;
 
   if (sa) {
-    for (i = 0; i < sa->l1_dir.size; ++i)
+    for (i = 0; i < sa->l1_dir.len; ++i)
       free_l2_dir (sa->l1_dir.ptr[i].l2_dir);
     free (sa->l1_dir.ptr);
     pthread_mutex_destroy (&sa->lock);
@@ -184,7 +184,7 @@ insert_l1_entry (struct sparse_array *sa, const struct l1_entry *entry)
 {
   size_t i;
 
-  for (i = 0; i < sa->l1_dir.size; ++i) {
+  for (i = 0; i < sa->l1_dir.len; ++i) {
     if (entry->offset < sa->l1_dir.ptr[i].offset) {
       /* Insert new entry before i'th directory entry. */
       if (l1_dir_insert (&sa->l1_dir, *entry, i) == -1) {
@@ -508,7 +508,7 @@ sparse_array_create (const void *paramsv)
   const allocator_parameters *params  = paramsv;
   struct sparse_array *sa;
 
-  if (params->size > 0) {
+  if (params->len > 0) {
     nbdkit_error ("allocator=sparse does not take extra parameters");
     return NULL;
   }

common/allocators/zstd.c
@@ -136,7 +136,7 @@ zstd_array_free (struct allocator *a)
 
     ZSTD_freeCCtx (za->zcctx);
     ZSTD_freeDStream (za->zdstrm);
-    for (i = 0; i < za->l1_dir.size; ++i)
+    for (i = 0; i < za->l1_dir.len; ++i)
       free_l2_dir (za->l1_dir.ptr[i].l2_dir);
     free (za->l1_dir.ptr);
     pthread_mutex_destroy (&za->lock);
@@ -170,7 +170,7 @@ insert_l1_entry (struct zstd_array *za, const struct l1_entry *entry)
 {
   size_t i;
 
-  for (i = 0; i < za->l1_dir.size; ++i) {
+  for (i = 0; i < za->l1_dir.len; ++i) {
     if (entry->offset < za->l1_dir.ptr[i].offset) {
       /* Insert new entry before i'th directory entry. */
       if (l1_dir_insert (&za->l1_dir, *entry, i) == -1) {
@@ -600,7 +600,7 @@ zstd_array_create (const void *paramsv)
   const allocator_parameters *params  = paramsv;
   struct zstd_array *za;
 
-  if (params->size > 0) {
+  if (params->len > 0) {
     nbdkit_error ("allocator=zstd does not take extra parameters");
     return NULL;
   }

common/regions/regions.h
@@ -84,17 +84,17 @@ extern void free_regions (regions *regions)
 static inline size_t __attribute__((__nonnull__ (1)))
 nr_regions (regions *rs)
 {
-  return rs->size;
+  return rs->len;
 }
 
 /* Return the virtual size of the disk. */
 static inline int64_t __attribute__((__nonnull__ (1)))
 virtual_size (regions *rs)
 {
-  if (rs->size == 0)
+  if (rs->len == 0)
     return 0;
   else
-    return rs->ptr[rs->size-1].end + 1;
+    return rs->ptr[rs->len-1].end + 1;
 }
 
 /* Look up the region corresponding to the given offset.  If the

common/utils/environ.c
@@ -82,7 +82,7 @@ copy_environ (char **env, ...)
 
     /* Search for key in the existing environment.  It's O(n^2) ... */
     len = strlen (key);
-    for (i = 0; i < ret.size; ++i) {
+    for (i = 0; i < ret.len; ++i) {
       if (strncmp (key, ret.ptr[i], len) == 0 && ret.ptr[i][len] == '=') {
         /* Replace the existing key. */
         free (ret.ptr[i]);

common/utils/test-vector.c
@@ -73,7 +73,7 @@ test_int64_vector (void)
     assert (v.ptr[i] == i);
 
   int64_vector_remove (&v, 1);
-  assert (v.size == 9);
+  assert (v.len == 9);
   assert (v.ptr[1] == 2);
 
   tmp = 10;

commit 01921503b91e920b6cf5a430dfd398a90ccb79c5

windows: Replace vector .size with .len in a few places
Fixes: commit 0b0eece73f04963a66b9adc507e5cbaba608660b

common/replacements/open_memstream.c
@@ -105,11 +105,11 @@ close_memstream (FILE *fp)
   string_vector content = empty_vector;
   struct file_to_memstream *f2m;
 
-  for (i = 0; i < files.size; ++i) {
+  for (i = 0; i < files.len; ++i) {
     if (files.ptr[i].fp == fp)
       break;
   }
-  assert (i < files.size);
+  assert (i < files.len);
   f2m = &files.ptr[i];
 
   /* Read the file back into memory. */
@@ -139,7 +139,7 @@ close_memstream (FILE *fp)
 
   /* Pass the buffer to the user.  User will free it. */
   *(files.ptr[i].ptr) = content.ptr;
-  *(files.ptr[i].size) = content.size - 1;
+  *(files.ptr[i].size) = content.len - 1;
   file_vector_remove (&files, i);
   return 0;
 }

server/sockets.c
@@ -489,7 +489,7 @@ check_sockets_and_quit_fd (const sockets *socks)
 static void
 check_sockets_and_quit_fd (const sockets *socks)
 {
-  const size_t nr_socks = socks->size;
+  const size_t nr_socks = socks->len;
   size_t i;
   HANDLE h, handles[nr_socks+1];
   DWORD r;