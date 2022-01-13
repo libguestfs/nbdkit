f72d89a870517c52ad0b86d1d72ebd3800ab43a8
Martin Kletzander <mkletzan@redhat.com>
2022-01-13 17:05:59 +0100

    plugins/rust: Run more tests
    There have been additional tests running in GitHub CI and there is no
    reason not to run them outside of CI as well.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- plugins/rust/test.sh
+++ plugins/rust/test.sh
@@ -1,3 +1,20 @@
 #! /bin/sh
 # cargo test wrapper suitable for invocation from automake-generated makefiles
-cargo test && cargo test --release
+
+set -e
+
+export RUSTFLAGS=-Dwarnings
+export RUSTDOCFLAGS=-Dwarnings
+
+cargo test --all-features
+cargo test --all-features --release
+
+if type cargo-doc >/dev/null 2>/dev/null
+then
+   cargo doc --no-deps
+fi
+
+if type cargo-clippy >/dev/null 2>/dev/null
+then
+    cargo clippy --all-features --all-targets -- -D warnings
+fi
