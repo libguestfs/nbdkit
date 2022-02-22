libguestfs/nbdkit

commit acbefbe2210ee45dbd509de4dcb3d447f133fb18

common/utils: Add vector_reserve_page_aligned
Allow vectors to be page aligned (eg. for performance or page
protection reasons).  To create a page aligned vector you have to use
<vector>_reserve_page_aligned instead of plain <vector>_reserve.

Note that <vector>_duplicate creates unaligned duplicates.

common/utils/vector.c
@@ -34,15 +34,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 
 #include "checked-overflow.h"
 #include "vector.h"
 
-int
-generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
+static int
+calculate_capacity (struct generic_vector *v, size_t n, size_t itemsize,
+                    size_t *newcap_r, size_t *newbytes_r)
 {
-  void *newptr;
   size_t reqcap, reqbytes, newcap, newbytes, t;
 
   /* New capacity requested.  We must allocate this minimum (or fail).
@@ -71,10 +73,94 @@ generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
     newbytes = reqbytes;
   }
 
+  *newcap_r = newcap;
+  *newbytes_r = newbytes;
+  return 0;
+}
+
+int
+generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
+{
+  void *newptr;
+  size_t newcap, newbytes;
+
+  if (calculate_capacity (v, n, itemsize, &newcap, &newbytes) == -1)
+    return -1;
+
   newptr = realloc (v->ptr, newbytes);
   if (newptr == NULL)
     return -1;
 
+  v->ptr = newptr;
+  v->cap = newcap;
+  return 0;
+}
+
+/* Always allocates a buffer which is page aligned (both base and size). */
+int
+generic_vector_reserve_page_aligned (struct generic_vector *v,
+                                     size_t n, size_t itemsize)
+{
+#ifdef HAVE_POSIX_MEMALIGN
+  int r;
+#endif
+  void *newptr;
+  size_t newcap, newbytes;
+  long pagesize;
+  size_t extra, extra_items;
+
+  pagesize = sysconf (_SC_PAGE_SIZE);
+  assert (pagesize > 1);
+
+  assert (pagesize % itemsize == 0);
+
+  if (calculate_capacity (v, n, itemsize, &newcap, &newbytes) == -1)
+    return -1;
+
+  /* If the new size (in bytes) is not a full page then we have to
+   * round up to the next page.
+   */
+  extra = newbytes & (pagesize - 1);
+  if (extra > 0) {
+    extra_items = (pagesize - extra) / itemsize;
+
+    if (ADD_OVERFLOW (newcap, extra_items, &newcap) ||
+        ADD_OVERFLOW (newbytes, extra_items * itemsize, &newbytes)) {
+      errno = ENOMEM;
+      return -1;
+    }
+  }
+
+#ifdef HAVE_POSIX_MEMALIGN
+  if ((r = posix_memalign (&newptr, pagesize, newbytes)) != 0) {
+    errno = r;
+    return -1;
+  }
+#elif HAVE_VALLOC
+  newptr = valloc (newbytes);
+  if (newptr == NULL)
+    return -1;
+#else
+#error "this platform does not have posix_memalign or valloc"
+#endif
+
+  /* How much to copy here?
+   *
+   * vector_reserve always makes the buffer larger so we don't have to
+   * deal with the case of a shrinking buffer.
+   *
+   * Like the underlying realloc we do not have to zero the newly
+   * reserved elements.
+   *
+   * However (like realloc) we have to copy the existing elements even
+   * those that are not allocated and only reserved (between 'len' and
+   * 'cap').
+   *
+   * The spec of vector is not clear on the last two points, but
+   * existing code depends on this undocumented behaviour.
+   */
+  memcpy (newptr, v->ptr, v->cap * itemsize);
+  free (v->ptr);
   v->ptr = newptr;
   v->cap = newcap;
   return 0;

configure.ac
@@ -384,7 +384,9 @@ AC_CHECK_FUNCS([\
         pipe \
         pipe2 \
         ppoll \
-        posix_fadvise])
+        posix_fadvise \
+        posix_memalign \
+        valloc])
 
 dnl Check for structs and members.
 AC_CHECK_MEMBERS([struct dirent.d_type], [], [], [[#include <dirent.h>]])

common/utils/vector.h
@@ -101,6 +101,16 @@
                                    sizeof (type));                      \
   }                                                                     \
                                                                         \
+  /* Same as _reserve, but the allocation will be page aligned.  Note   \
+   * that the machine page size must be divisible by sizeof (type).     \
+   */                                                                   \
+  static inline int                                                     \
+  name##_reserve_page_aligned (name *v, size_t n)                       \
+  {                                                                     \
+    return generic_vector_reserve_page_aligned ((struct generic_vector *)v, \
+                                                n, sizeof (type));      \
+  }                                                                     \
+                                                                        \
   /* Insert at i'th element.  i=0 => beginning  i=len => append */      \
   static inline int                                                     \
   name##_insert (name *v, type elem, size_t i)                          \
@@ -196,5 +206,8 @@ struct generic_vector {
 
 extern int generic_vector_reserve (struct generic_vector *v,
                                    size_t n, size_t itemsize);
+
+extern int generic_vector_reserve_page_aligned (struct generic_vector *v,
+                                                size_t n, size_t itemsize);
 
 #endif /* NBDKIT_VECTOR_H */

commit 23de2ee90a705266325cca556c2bac054ea07f8a

windows: Add replacement posix_memalign function
If posix_memalign is missing, use valloc or Windows VirtualAlloc as
replacement.  Replace the open-coded conditionals in vector.

Fixes: commit acbefbe221 ("common/utils: Add vector_reserve_page_aligned")
Fixes: commit 2421748073 ("common/allocators: Always align mlock buffer")

common/replacements/Makefile.am
@@ -53,6 +53,8 @@ EXTRA_DIST = \
 	open_memstream.h \
 	poll.c \
 	poll.h \
+	posix_memalign.c \
+	posix_memalign.h \
 	pread.c \
 	pread.h \
 	pwrite.c \

common/replacements/posix_memalign.c
@@ -1,0 +1,75 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#ifndef HAVE_POSIX_MEMALIGN
+
+#include "posix_memalign.h"
+
+#ifdef HAVE_VALLOC
+
+int
+posix_memalign (void **ptr, size_t alignment, size_t size)
+{
+  *ptr = valloc (size);
+  if (*ptr == NULL)
+    return errno;
+  return 0;
+}
+
+#else /* !HAVE_VALLOC */
+
+#ifdef WIN32
+
+#include <memoryapi.h>
+
+int
+posix_memalign (void **ptr, size_t alignment, size_t size)
+{
+  *ptr = VirtualAlloc (NULL, size, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+  if (*ptr == NULL)
+    return ENOMEM;
+  return 0;
+}
+
+#else /* !WIN32 */
+#error "no replacement posix_memalign() is available on this platform"
+#endif
+
+#endif /* !HAVE_VALLOC */
+
+#endif /* !HAVE_POSIX_MEMALIGN */

common/replacements/posix_memalign.h
@@ -1,0 +1,54 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Replacement for posix_memalign for platforms which lack it.
+ *
+ * NB: This only works for page aligned allocations.  The alignment
+ * parameter is ignored in our replacement functions.
+ */
+
+#ifndef NBDKIT_POSIX_MEMALIGN_H
+#define NBDKIT_POSIX_MEMALIGN_H
+
+#include <config.h>
+
+#ifdef HAVE_POSIX_MEMALIGN
+
+#include <stdlib.h>
+
+#else
+
+extern int posix_memalign (void **ptr, size_t alignment, size_t size);
+
+#endif /* !HAVE_POSIX_MEMALIGN */
+
+#endif /* NBDKIT_POSIX_MEMALIGN_H */

common/utils/vector.c
@@ -39,6 +39,7 @@
 #include <assert.h>
 
 #include "checked-overflow.h"
+#include "posix_memalign.h"
 #include "sysconf.h"
 #include "vector.h"
 
@@ -102,9 +103,7 @@ int
 generic_vector_reserve_page_aligned (struct generic_vector *v,
                                      size_t n, size_t itemsize)
 {
-#ifdef HAVE_POSIX_MEMALIGN
   int r;
-#endif
   void *newptr;
   size_t newcap, newbytes;
   long pagesize;
@@ -132,18 +131,10 @@ generic_vector_reserve_page_aligned (struct generic_vector *v,
     }
   }
 
-#ifdef HAVE_POSIX_MEMALIGN
   if ((r = posix_memalign (&newptr, pagesize, newbytes)) != 0) {
     errno = r;
     return -1;
   }
-#elif HAVE_VALLOC
-  newptr = valloc (newbytes);
-  if (newptr == NULL)
-    return -1;
-#else
-#error "this platform does not have posix_memalign or valloc"
-#endif
 
   /* How much to copy here?
    *

configure.ac
@@ -418,6 +418,7 @@ AC_REPLACE_FUNCS([\
         openlog \
         open_memstream \
         poll \
+        posix_memalign \
         pread \
         pwrite \
         realpath \