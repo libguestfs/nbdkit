libguestfs/nbdkit

commit acbefbe2210ee45dbd509de4dcb3d447f133fb18

common/utils: Add vector_reserve_page_aligned
Allow vectors to be page aligned (eg. for performance or page
protection reasons).  To create a page aligned vector you have to use
<vector>_reserve_page_aligned instead of plain <vector>_reserve.

Note that <vector>_duplicate creates unaligned duplicates.

common/utils/vector.c
@@ -34,15 +34,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 
 #include "checked-overflow.h"
 #include "vector.h"
 
-int
-generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
+static int
+calculate_capacity (struct generic_vector *v, size_t n, size_t itemsize,
+                    size_t *newcap_r, size_t *newbytes_r)
 {
-  void *newptr;
   size_t reqcap, reqbytes, newcap, newbytes, t;
 
   /* New capacity requested.  We must allocate this minimum (or fail).
@@ -71,10 +73,94 @@ generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
     newbytes = reqbytes;
   }
 
+  *newcap_r = newcap;
+  *newbytes_r = newbytes;
+  return 0;
+}
+
+int
+generic_vector_reserve (struct generic_vector *v, size_t n, size_t itemsize)
+{
+  void *newptr;
+  size_t newcap, newbytes;
+
+  if (calculate_capacity (v, n, itemsize, &newcap, &newbytes) == -1)
+    return -1;
+
   newptr = realloc (v->ptr, newbytes);
   if (newptr == NULL)
     return -1;
 
+  v->ptr = newptr;
+  v->cap = newcap;
+  return 0;
+}
+
+/* Always allocates a buffer which is page aligned (both base and size). */
+int
+generic_vector_reserve_page_aligned (struct generic_vector *v,
+                                     size_t n, size_t itemsize)
+{
+#ifdef HAVE_POSIX_MEMALIGN
+  int r;
+#endif
+  void *newptr;
+  size_t newcap, newbytes;
+  long pagesize;
+  size_t extra, extra_items;
+
+  pagesize = sysconf (_SC_PAGE_SIZE);
+  assert (pagesize > 1);
+
+  assert (pagesize % itemsize == 0);
+
+  if (calculate_capacity (v, n, itemsize, &newcap, &newbytes) == -1)
+    return -1;
+
+  /* If the new size (in bytes) is not a full page then we have to
+   * round up to the next page.
+   */
+  extra = newbytes & (pagesize - 1);
+  if (extra > 0) {
+    extra_items = (pagesize - extra) / itemsize;
+
+    if (ADD_OVERFLOW (newcap, extra_items, &newcap) ||
+        ADD_OVERFLOW (newbytes, extra_items * itemsize, &newbytes)) {
+      errno = ENOMEM;
+      return -1;
+    }
+  }
+
+#ifdef HAVE_POSIX_MEMALIGN
+  if ((r = posix_memalign (&newptr, pagesize, newbytes)) != 0) {
+    errno = r;
+    return -1;
+  }
+#elif HAVE_VALLOC
+  newptr = valloc (newbytes);
+  if (newptr == NULL)
+    return -1;
+#else
+#error "this platform does not have posix_memalign or valloc"
+#endif
+
+  /* How much to copy here?
+   *
+   * vector_reserve always makes the buffer larger so we don't have to
+   * deal with the case of a shrinking buffer.
+   *
+   * Like the underlying realloc we do not have to zero the newly
+   * reserved elements.
+   *
+   * However (like realloc) we have to copy the existing elements even
+   * those that are not allocated and only reserved (between 'len' and
+   * 'cap').
+   *
+   * The spec of vector is not clear on the last two points, but
+   * existing code depends on this undocumented behaviour.
+   */
+  memcpy (newptr, v->ptr, v->cap * itemsize);
+  free (v->ptr);
   v->ptr = newptr;
   v->cap = newcap;
   return 0;

configure.ac
@@ -384,7 +384,9 @@ AC_CHECK_FUNCS([\
         pipe \
         pipe2 \
         ppoll \
-        posix_fadvise])
+        posix_fadvise \
+        posix_memalign \
+        valloc])
 
 dnl Check for structs and members.
 AC_CHECK_MEMBERS([struct dirent.d_type], [], [], [[#include <dirent.h>]])

common/utils/vector.h
@@ -101,6 +101,16 @@
                                    sizeof (type));                      \
   }                                                                     \
                                                                         \
+  /* Same as _reserve, but the allocation will be page aligned.  Note   \
+   * that the machine page size must be divisible by sizeof (type).     \
+   */                                                                   \
+  static inline int                                                     \
+  name##_reserve_page_aligned (name *v, size_t n)                       \
+  {                                                                     \
+    return generic_vector_reserve_page_aligned ((struct generic_vector *)v, \
+                                                n, sizeof (type));      \
+  }                                                                     \
+                                                                        \
   /* Insert at i'th element.  i=0 => beginning  i=len => append */      \
   static inline int                                                     \
   name##_insert (name *v, type elem, size_t i)                          \
@@ -196,5 +206,8 @@ struct generic_vector {
 
 extern int generic_vector_reserve (struct generic_vector *v,
                                    size_t n, size_t itemsize);
+
+extern int generic_vector_reserve_page_aligned (struct generic_vector *v,
+                                                size_t n, size_t itemsize);
 
 #endif /* NBDKIT_VECTOR_H */

commit a83a62a53df5d9c77610a4a283ac565e7ddac1ee

windows: Add replacement for sysconf() POSIX function
This is just enough to get the system page size, ie. _SC_PAGESIZE.

Also I renamed _SC_PAGE_SIZE to _SC_PAGESIZE.  POSIX supports both but
it seems from the manual that _SC_PAGESIZE is the canonical one.

Fixes: commit acbefbe221 ("common/utils: Add vector_reserve_page_aligned")
Fixes: commit 2421748073 ("common/allocators: Always align mlock buffer")

Makefile.am
@@ -56,6 +56,7 @@ nbdkit_SOURCES = wrapper.c server/options.h
 nbdkit_CPPFLAGS = \
 	-I$(top_srcdir)/server \
 	-I$(top_srcdir)/common/utils \
+	-I$(top_srcdir)/common/replacements \
 	-Dbuilddir=\"$(abs_top_builddir)\" \
 	-Dsrcdir=\"$(abs_top_srcdir)\" \
 	-DVALGRIND=\"$(VALGRIND)\" \
@@ -63,6 +64,7 @@ nbdkit_CPPFLAGS = \
 nbdkit_CFLAGS = $(WARNINGS_CFLAGS)
 nbdkit_LDADD = \
 	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/common/replacements/libcompat.la \
 	$(NULL)
 EXTRA_nbdkit_DEPENDENCIES = config.status
 endif !ENABLE_LIBFUZZER

common/replacements/Makefile.am
@@ -61,6 +61,8 @@ EXTRA_DIST = \
 	realpath.h \
 	strndup.c \
 	strndup.h \
+	sysconf.c \
+	sysconf.h \
 	syslog.c \
 	syslog.h \
 	vsyslog.c

common/replacements/sysconf.c
@@ -1,0 +1,68 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#ifndef HAVE_SYSCONF
+
+#include "sysconf.h"
+
+#ifdef WIN32
+
+#include <sysinfoapi.h>
+
+/* Replacement sysconf for Win32. */
+
+long
+sysconf (int name)
+{
+  SYSTEM_INFO sysinfo;
+
+  if (name == _SC_PAGESIZE) {
+    GetSystemInfo (&sysinfo);
+    return sysinfo.dwPageSize;
+  }
+  else {
+    errno = EINVAL;
+    return -1;
+  }
+}
+
+#else /* !WIN32 */
+#error "no replacement sysconf() is available on this platform"
+#endif
+
+#endif /* !HAVE_SYSCONF */

common/replacements/sysconf.h
@@ -1,0 +1,56 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Replacement for sysconf for platforms which lack it.  Just enough
+ * to get the system page size.
+ */
+
+#ifndef NBDKIT_SYSCONF_H
+#define NBDKIT_SYSCONF_H
+
+#include <config.h>
+
+#ifdef HAVE_SYSCONF
+
+#include <unistd.h>
+
+#else
+
+#ifndef _SC_PAGESIZE
+#define _SC_PAGESIZE 1
+#endif
+
+extern long sysconf (int name);
+
+#endif /* !HAVE_SYSCONF */
+
+#endif /* NBDKIT_SYSCONF_H */

common/utils/Makefile.am
@@ -99,12 +99,25 @@ TESTS = test-quotes test-vector
 check_PROGRAMS = test-quotes test-vector
 
 test_quotes_SOURCES = test-quotes.c quote.c utils.h
-test_quotes_CPPFLAGS = -I$(srcdir)
+test_quotes_CPPFLAGS = \
+	-I$(srcdir) \
+	-I$(top_srcdir)/common/replacements \
+	$(NULL)
 test_quotes_CFLAGS = $(WARNINGS_CFLAGS)
+test_quotes_LDADD = \
+	$(top_builddir)/common/replacements/libcompat.la \
+	$(NULL)
 
 test_vector_SOURCES = test-vector.c vector.c vector.h bench.h
-test_vector_CPPFLAGS = -I$(srcdir) -I$(top_srcdir)/common/include
+test_vector_CPPFLAGS = \
+	-I$(srcdir) \
+	-I$(top_srcdir)/common/replacements \
+	-I$(top_srcdir)/common/include \
+	$(NULL)
 test_vector_CFLAGS = $(WARNINGS_CFLAGS)
+test_vector_LDADD = \
+	$(top_builddir)/common/replacements/libcompat.la \
+	$(NULL)
 
 bench: test-vector
 	NBDKIT_BENCH=1 ./test-vector

common/utils/vector.c
@@ -39,6 +39,7 @@
 #include <assert.h>
 
 #include "checked-overflow.h"
+#include "sysconf.h"
 #include "vector.h"
 
 static int
@@ -109,7 +110,7 @@ generic_vector_reserve_page_aligned (struct generic_vector *v,
   long pagesize;
   size_t extra, extra_items;
 
-  pagesize = sysconf (_SC_PAGE_SIZE);
+  pagesize = sysconf (_SC_PAGESIZE);
   assert (pagesize > 1);
 
   assert (pagesize % itemsize == 0);

configure.ac
@@ -422,6 +422,7 @@ AC_REPLACE_FUNCS([\
         pwrite \
         realpath \
         strndup \
+        sysconf \
         syslog \
         vsyslog])
 