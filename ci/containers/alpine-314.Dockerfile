libguestfs/nbdkit

commit 200a147d6a99c593204b476d3cbb08cc794d834b

ci: Regenerate CI files using latest libvirt-ci/lcitool
To regenerate these files I used Martin Kletzander's branch of
libvirt-ci which supports nbdkit, rebased on top of libvirt-ci current
head, and ran this command:

$ ../libvirt-ci/lcitool manifest ci/manifest.yml

Then I picked the hunks that I wanted, since some changes will be
needed to libvirt-ci to support the Rawhide nodebug kernels.

See-also: https://listman.redhat.com/archives/libguestfs/2022-March/thread.html#28419
Thanks: Daniel Berrangé

ci/containers/alpine-314.Dockerfile
@@ -37,13 +37,14 @@ RUN apk update && \
         libtorrent \
         libvirt-dev \
         linux-headers \
+        linux-virt \
         lua \
         make \
         musl-utils \
         ocaml \
         openssh-client \
         perl \
-        perl-dev \
+        pkgconf \
         podman \
         py3-flake8 \
         py3-pip \
@@ -69,8 +70,7 @@ RUN apk update && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/g++ && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
-RUN pip3 install \
-         boto3
+RUN pip3 install boto3
 
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"

ci/containers/alpine-edge.Dockerfile
@@ -37,13 +37,14 @@ RUN apk update && \
         libtorrent \
         libvirt-dev \
         linux-headers \
+        linux-virt \
         lua \
         make \
         musl-utils \
         ocaml \
         openssh-client \
         perl \
-        perl-dev \
+        pkgconf \
         podman \
         py3-flake8 \
         py3-pip \
@@ -69,8 +70,7 @@ RUN apk update && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/g++ && \
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
 
-RUN pip3 install \
-         boto3
+RUN pip3 install boto3
 
 ENV LANG "en_US.UTF-8"
 ENV MAKE "/usr/bin/make"

ci/cirrus/freebsd-12.vars
@@ -11,6 +11,6 @@ MAKE='/usr/local/bin/gmake'
 NINJA='/usr/local/bin/ninja'
 PACKAGING_COMMAND='pkg'
 PIP3='/usr/local/bin/pip-3.8'
-PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel pkgconf py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
 PYPI_PKGS=''
 PYTHON='/usr/local/bin/python3'

ci/cirrus/freebsd-13.vars
@@ -11,6 +11,6 @@ MAKE='/usr/local/bin/gmake'
 NINJA='/usr/local/bin/ninja'
 PACKAGING_COMMAND='pkg'
 PIP3='/usr/local/bin/pip-3.8'
-PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel pkgconf py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
 PYPI_PKGS=''
 PYTHON='/usr/local/bin/python3'

ci/cirrus/freebsd-current.vars
@@ -11,6 +11,6 @@ MAKE='/usr/local/bin/gmake'
 NINJA='/usr/local/bin/ninja'
 PACKAGING_COMMAND='pkg'
 PIP3='/usr/local/bin/pip-3.8'
-PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel pkgconf py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
 PYPI_PKGS=''
 PYTHON='/usr/local/bin/python3'

ci/cirrus/macos-11.vars
@@ -11,6 +11,6 @@ MAKE='/usr/local/bin/gmake'
 NINJA='/usr/local/bin/ninja'
 PACKAGING_COMMAND='brew'
 PIP3='/usr/local/bin/pip3'
-PKGS='autoconf automake bash bash-completion ccache cpanminus curl e2fsprogs expect flake8 git glibc-common gnutls golang gzip jq kernel-headers libcom_err libnbd libssh libtool libtorrent libvirt losetup lua make ocaml perl perl5-devel podman python3 python3-boto3 python3-libnbd qemu rust sfdisk socat tcl xorriso xz zlib zstd'
+PKGS='autoconf automake bash bash-completion ccache cpanminus curl e2fsprogs expect flake8 git glibc gnutls golang gzip jq libssh libtool libtorrent libvirt losetup lua make ocaml perl perl5-devel pkg-config podman python3 python3-boto3 python3-libnbd qemu rust sfdisk socat tcl xorriso xz zlib zstd'
 PYPI_PKGS=''
 PYTHON='/usr/local/bin/python3'

ci/containers/almalinux-8.Dockerfile
@@ -35,6 +35,7 @@ RUN dnf update -y && \
         gzip \
         iproute \
         jq \
+        kernel \
         kernel-headers \
         libcom_err-devel \
         libcurl-devel \
@@ -55,6 +56,7 @@ RUN dnf update -y && \
         perl-Pod-Simple \
         perl-devel \
         perl-podlators \
+        pkgconfig \
         podman \
         python3 \
         python3-boto3 \

ci/containers/centos-stream-8.Dockerfile
@@ -35,6 +35,7 @@ RUN dnf update -y && \
         gzip \
         iproute \
         jq \
+        kernel \
         kernel-headers \
         libcom_err-devel \
         libcurl-devel \
@@ -55,6 +56,7 @@ RUN dnf update -y && \
         perl-Pod-Simple \
         perl-devel \
         perl-podlators \
+        pkgconfig \
         podman \
         python3 \
         python3-boto3 \

commit add402933c8c256156fc79df4f40f6a226f5408a

ci: Add perl-dev back to Alpine
Fixes: commit 200a147d6a99c593204b476d3cbb08cc794d834b
Updates: commit c96b6fdab0b0b8b7b3cc7b58b79f11e8504de760

ci/containers/alpine-314.Dockerfile
@@ -44,6 +44,7 @@ RUN apk update && \
         ocaml \
         openssh-client \
         perl \
+        perl-dev \
         pkgconf \
         podman \
         py3-flake8 \

ci/containers/alpine-edge.Dockerfile
@@ -44,6 +44,7 @@ RUN apk update && \
         ocaml \
         openssh-client \
         perl \
+        perl-dev \
         pkgconf \
         podman \
         py3-flake8 \