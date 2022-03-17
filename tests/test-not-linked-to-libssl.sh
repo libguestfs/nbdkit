1193f6c64a8f2ee0eac63848a6d3bffd0ab19387
Richard W.M. Jones <rjones@redhat.com>
2022-03-17 11:51:27 +0000

    tests: Add regression test that nbdkit isn't linked to OpenSSL
    See: https://bugzilla.redhat.com/show_bug.cgi?id=2057490

---
+++ tests/test-not-linked-to-libssl.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2017-2022 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Sometimes gnutls ends up linked to OpenSSL.  This prevents the VDDK
+# plugin (which bundles its own OpenSSL) from loading.
+
+source ./functions.sh
+set -e
+set -x
+
+# Does the nbdkit binary support TLS?
+if ! nbdkit --dump-config | grep -sq tls=yes; then
+    echo "$0: nbdkit built without TLS support"
+    exit 77
+fi
+
+# This will only work for the glibc version, but that's fine.
+requires ldd --version
+
+if ldd ../server/nbdkit | grep libssl ; then
+    echo "$0: nbdkit is linked to OpenSSL, probably via an indirect dependency."
+    echo "This will probably stop nbdkit-vddk-plugin from working, see:"
+    echo "https://bugzilla.redhat.com/show_bug.cgi?id=2057490"
+    exit 1
+fi
