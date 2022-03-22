libguestfs/nbdkit

commit 63d2dd2eb2c9980a07841fe84ec16844085a59c3

vddk: Export block size information for this plugin
VDDK can only handle whole 512 byte sectors, and in addition it
provides information about the underlying logical and physical sector
size of the storage.  Reflect this information over NBD.

In addition, we now return EINVAL for unaligned requests.  This is
what the NBD protocol prefers for block size constraint errors.

Typical output after this change:

$ ./nbdkit vddk -D vddk.diskinfo=1 disk.vmdk --run 'nbdinfo $uri'
protocol: newstyle-fixed without TLS
export="":
	export-size: 1073741824 (1G)
	content: data
	uri: nbd://localhost:10809/
	contexts:
		base:allocation
	is_rotational: false
	is_read_only: false
	can_cache: false
	can_df: true
	can_fast_zero: true
	can_flush: true
	can_fua: true
	can_multi_conn: false
	can_trim: false
	can_zero: true
	block_size_minimum: 512
	block_size_preferred: 4096
	block_size_maximum: 4294967295

plugins/vddk/vddk.c
@@ -50,6 +50,7 @@
 #include <nbdkit-plugin.h>
 
 #include "cleanup.h"
+#include "minmax.h"
 #include "vector.h"
 
 #include "vddk.h"
@@ -831,13 +832,46 @@ static int64_t
 vddk_get_size (void *handle)
 {
   struct vddk_handle *h = handle;
-  uint64_t size;
-  struct command get_size_cmd = { .type = GET_SIZE, .ptr = &size };
+  VixDiskLibInfo *info;
+  int64_t size;
+  struct command info_cmd = { .type = INFO, .ptr = &info };
 
-  if (send_command_and_wait (h, &get_size_cmd) == -1)
+  if (send_command_and_wait (h, &info_cmd) == -1)
     return -1;
 
-  return (int64_t) size;
+  size = info->capacity * (int64_t)VIXDISKLIB_SECTOR_SIZE;
+
+  VDDK_CALL_START (VixDiskLib_FreeInfo, "info")
+    VixDiskLib_FreeInfo (info);
+  VDDK_CALL_END (VixDiskLib_FreeInfo, 0);
+
+  return size;
+}
+
+/* Advertise most efficient block sizes. */
+static int
+vddk_block_size (void *handle,
+                 uint32_t *minimum, uint32_t *preferred, uint32_t *maximum)
+{
+  struct vddk_handle *h = handle;
+  VixDiskLibInfo *info;
+  uint32_t s;
+  struct command info_cmd = { .type = INFO, .ptr = &info };
+
+  if (send_command_and_wait (h, &info_cmd) == -1)
+    return -1;
+
+  /* VDDK can only serve whole 512 byte sectors. */
+  *minimum = VIXDISKLIB_SECTOR_SIZE;
+  s = MAX (info->logicalSectorSize, info->physicalSectorSize);
+  *preferred = MAX (4096, s);
+  *maximum = 0xffffffff;
+
+  VDDK_CALL_START (VixDiskLib_FreeInfo, "info")
+    VixDiskLib_FreeInfo (info);
+  VDDK_CALL_END (VixDiskLib_FreeInfo, 0);
+
+  return 0;
 }
 
 /* The Flush call was added in VDDK 6.0, since we support minimum 6.5
@@ -959,6 +993,7 @@ static struct nbdkit_plugin plugin = {
   .open              = vddk_open,
   .close             = vddk_close,
   .get_size          = vddk_get_size,
+  .block_size        = vddk_block_size,
   .pread             = vddk_pread,
   .pwrite            = vddk_pwrite,
   .can_fua           = vddk_can_fua,

plugins/vddk/vddk.h
@@ -128,7 +128,7 @@ extern int vddk_debug_stats;
   } while (0)
 
 /* Queue of asynchronous commands sent to the background thread. */
-enum command_type { GET_SIZE, READ, WRITE, FLUSH, CAN_EXTENTS, EXTENTS, STOP };
+enum command_type { INFO, READ, WRITE, FLUSH, CAN_EXTENTS, EXTENTS, STOP };
 struct command {
   /* These fields are set by the caller. */
   enum command_type type;       /* command */

plugins/vddk/worker.c
@@ -53,7 +53,7 @@ const char *
 command_type_string (enum command_type type)
 {
   switch (type) {
-  case GET_SIZE:    return "get_size";
+  case INFO:        return "info";
   case READ:        return "read";
   case WRITE:       return "write";
   case FLUSH:       return "flush";
@@ -150,56 +150,48 @@ do_stop (struct command *cmd, struct vddk_handle *h)
   return 0;
 }
 
-/* Get size command. */
+/* Disk info command. */
 static int64_t
-do_get_size (struct command *cmd, struct vddk_handle *h)
+do_info (struct command *cmd, struct vddk_handle *h)
 {
   VixError err;
-  VixDiskLibInfo *info;
-  uint64_t size;
+  VixDiskLibInfo **info = cmd->ptr;
 
-  VDDK_CALL_START (VixDiskLib_GetInfo, "handle, &info")
-    err = VixDiskLib_GetInfo (h->handle, &info);
+  VDDK_CALL_START (VixDiskLib_GetInfo, "handle, info")
+    err = VixDiskLib_GetInfo (h->handle, info);
   VDDK_CALL_END (VixDiskLib_GetInfo, 0);
   if (err != VIX_OK) {
     VDDK_ERROR (err, "VixDiskLib_GetInfo");
     return -1;
   }
 
-  size = info->capacity * (uint64_t)VIXDISKLIB_SECTOR_SIZE;
-
   if (vddk_debug_diskinfo) {
-    nbdkit_debug ("disk info: capacity: %" PRIu64 " sectors "
-                  "(%" PRIi64 " bytes)",
-                  info->capacity, size);
+    nbdkit_debug ("disk info: capacity: %" PRIu64 " sectors",
+                  (*info)->capacity);
     nbdkit_debug ("disk info: biosGeo: C:%" PRIu32 " H:%" PRIu32 " S:%" PRIu32,
-                  info->biosGeo.cylinders,
-                  info->biosGeo.heads,
-                  info->biosGeo.sectors);
+                  (*info)->biosGeo.cylinders,
+                  (*info)->biosGeo.heads,
+                  (*info)->biosGeo.sectors);
     nbdkit_debug ("disk info: physGeo: C:%" PRIu32 " H:%" PRIu32 " S:%" PRIu32,
-                  info->physGeo.cylinders,
-                  info->physGeo.heads,
-                  info->physGeo.sectors);
+                  (*info)->physGeo.cylinders,
+                  (*info)->physGeo.heads,
+                  (*info)->physGeo.sectors);
     nbdkit_debug ("disk info: adapter type: %d",
-                  (int) info->adapterType);
-    nbdkit_debug ("disk info: num links: %d", info->numLinks);
+                  (int) (*info)->adapterType);
+    nbdkit_debug ("disk info: num links: %d", (*info)->numLinks);
     nbdkit_debug ("disk info: parent filename hint: %s",
-                  info->parentFileNameHint ? : "NULL");
+                  (*info)->parentFileNameHint ? : "NULL");
     nbdkit_debug ("disk info: uuid: %s",
-                  info->uuid ? : "NULL");
+                  (*info)->uuid ? : "NULL");
     if (library_version >= 7) {
       nbdkit_debug ("disk info: sector size: "
                     "logical %" PRIu32 " physical %" PRIu32,
-                    info->logicalSectorSize,
-                    info->physicalSectorSize);
+                    (*info)->logicalSectorSize,
+                    (*info)->physicalSectorSize);
     }
   }
 
-  VDDK_CALL_START (VixDiskLib_FreeInfo, "info")
-    VixDiskLib_FreeInfo (info);
-  VDDK_CALL_END (VixDiskLib_FreeInfo, 0);
-
-  return (int64_t) size;
+  return 0;
 }
 
 static int
@@ -213,10 +205,12 @@ do_read (struct command *cmd, struct vddk_handle *h)
   /* Align to sectors. */
   if (!IS_ALIGNED (offset, VIXDISKLIB_SECTOR_SIZE)) {
     nbdkit_error ("%s is not aligned to sectors", "read");
+    errno = EINVAL;
     return -1;
   }
   if (!IS_ALIGNED (count, VIXDISKLIB_SECTOR_SIZE)) {
     nbdkit_error ("%s is not aligned to sectors", "read");
+    errno = EINVAL;
     return -1;
   }
   offset /= VIXDISKLIB_SECTOR_SIZE;
@@ -248,10 +242,12 @@ do_write (struct command *cmd, struct vddk_handle *h)
   /* Align to sectors. */
   if (!IS_ALIGNED (offset, VIXDISKLIB_SECTOR_SIZE)) {
     nbdkit_error ("%s is not aligned to sectors", "write");
+    errno = EINVAL;
     return -1;
   }
   if (!IS_ALIGNED (count, VIXDISKLIB_SECTOR_SIZE)) {
     nbdkit_error ("%s is not aligned to sectors", "write");
+    errno = EINVAL;
     return -1;
   }
   offset /= VIXDISKLIB_SECTOR_SIZE;
@@ -509,16 +505,9 @@ vddk_worker_thread (void *handle)
       stop = true;
       break;
 
-    case GET_SIZE: {
-      int64_t size = do_get_size (cmd, h);
-      if (size == -1)
-        r = -1;
-      else {
-        r = 0;
-        *(uint64_t *)cmd->ptr = size;
-      }
+    case INFO:
+      r = do_info (cmd, h);
       break;
-    }
 
     case READ:
       r = do_read (cmd, h);

commit 31bc5322b179545bef827022e1ae3b7859387b1b

vddk: Don't use uninitialized values when computing preferred block size
Commit 63d2dd2eb2 ("vddk: Export block size information for this
plugin") tried to calculate the preferred block size from the logical
and physical sector size reported by VDDK.  Unfortunately VDDK < 7’s
VixDiskLib_GetInfo API returns a struct which does not contain
these fields at all.  We knew about this already because the
debug code does not print them, but the block size code uses
them regardless of the VDDK version.

The practical result of this error was that sometimes (depending on
existing contents of memory) you would see the error:

nbdkit: vddk[1]: error: plugin must set preferred block size to a power of 2

Fix this by only using the fields when VDDK >= 7, and in earlier
versions assuming VDDK’s normal sector size.

Reported-by: Xiaodai Wang
Fixes: commit 63d2dd2eb2c9980a07841fe84ec16844085a59c3
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2066655

plugins/vddk/vddk.c
@@ -855,6 +855,7 @@ vddk_block_size (void *handle,
 {
   struct vddk_handle *h = handle;
   VixDiskLibInfo *info;
+  uint32_t logicalSectorSize, physicalSectorSize;
   struct command info_cmd = { .type = INFO, .ptr = &info };
 
   if (send_command_and_wait (h, &info_cmd) == -1)
@@ -862,8 +863,20 @@ vddk_block_size (void *handle,
 
   /* VDDK can only serve whole 512 byte sectors. */
   *minimum = VIXDISKLIB_SECTOR_SIZE;
-  *preferred = MAX (MAX (info->logicalSectorSize, info->physicalSectorSize),
-                    4096);
+
+  /* The logicalSectorSize and physicalSectorSize fields are only
+   * present in VDDK >= 7.  In earlier versions they will not be
+   * initialized and contain random values (beyond the end of the
+   * returned structure).  So compute sector sizes with this in mind.
+   */
+  logicalSectorSize = physicalSectorSize = VIXDISKLIB_SECTOR_SIZE;
+  if (library_version >= 7) {
+    logicalSectorSize = info->logicalSectorSize;
+    physicalSectorSize = info->physicalSectorSize;
+  }
+
+  *preferred = MAX (MAX (logicalSectorSize, physicalSectorSize), 4096);
+
   *maximum = 0xffffffff;
 
   VDDK_CALL_START (VixDiskLib_FreeInfo, "info")