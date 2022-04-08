libguestfs/nbdkit

commit 4ede3d7b0778cc1fe661307d2289fadfc18a1af2

python: Avoid memory leak in .list_exports and .extents
PyIter_Next returns a new reference to the tuple, but we never freed
it resulting in a memory leak of these tuples.  This was obvious in
the example code attached to this email which quickly consumed huge
amounts of memory:

https://listman.redhat.com/archives/libguestfs/2022-April/028605.html

Fixes: commit 3c3918e33176cc9b0e76fe48006eb3afdca52897
Fixes: commit cbdcdabbf1d50461ec7d9cc721d36605c391f655

plugins/python/plugin.c
@@ -364,6 +364,7 @@ py_list_exports (int readonly, int is_tls, struct nbdkit_exports *exports)
       py_desc = PyTuple_GetItem (t, 1);
       name = python_to_string (py_name);
       desc = python_to_string (py_desc);
+      Py_DECREF (t);
       if (name == NULL || desc == NULL) {
         nbdkit_error ("list_exports method did not return an iterable of "
                       "string 2-tuples");
@@ -1039,6 +1040,7 @@ py_extents (void *handle, uint32_t count, uint64_t offset,
       extent_offset = PyLong_AsUnsignedLongLong (py_offset);
       extent_length = PyLong_AsUnsignedLongLong (py_length);
       extent_type = PyLong_AsUnsignedLong (py_type);
+      Py_DECREF (t);
       if (check_python_failure ("PyLong") == -1) {
         Py_DECREF (iter);
         Py_DECREF (r);

commit 54663a4ccd52412720939868b7568f54f01a3e74

python: Also plug leak on error case
We plugged the memory leak on the common case of success, but also
need to plug the (smaller) leak if the iterator returns garbage.

Fixes: 4ede3d7b0778cc1fe661307d2289fadfc18a1af2

plugins/python/plugin.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2021 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -358,6 +358,7 @@ py_list_exports (int readonly, int is_tls, struct nbdkit_exports *exports)
                       "2-tuples");
         Py_DECREF (iter);
         Py_DECREF (r);
+        Py_DECREF (t);
         return -1;
       }
       py_name = PyTuple_GetItem (t, 0);
@@ -1032,6 +1033,7 @@ py_extents (void *handle, uint32_t count, uint64_t offset,
         nbdkit_error ("extents method did not return an iterable of 3-tuples");
         Py_DECREF (iter);
         Py_DECREF (r);
+        Py_DECREF (t);
         return -1;
       }
       py_offset = PyTuple_GetItem (t, 0);