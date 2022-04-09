libguestfs/nbdkit

commit dcba0f0b62ed772bf4dd4849043d8f7bec0fd127

curl: Implement header and cookie scripts.
This rather complex feature solves a problem for certain web services
that require a cookie or token for access, especially one which must
be periodically renewed.

For motivation for this feature see the included documentation, and
item (1)(b) here:

https://www.redhat.com/archives/libguestfs/2020-July/msg00069.html

plugins/curl/scripts.c
@@ -1,0 +1,330 @@
+/* nbdkit
+ * Copyright (C) 2014-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Header and cookie scripts. */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <assert.h>
+#include <pthread.h>
+
+#include <curl/curl.h>
+
+#include <nbdkit-plugin.h>
+
+#include "ascii-ctype.h"
+#include "cleanup.h"
+#include "utils.h"
+
+#include "curldefs.h"
+
+/* This lock protects internal state in this file. */
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+/* Last time header-script or cookie-script was run. */
+static time_t header_last = 0;
+static time_t cookie_last = 0;
+static bool header_script_has_run = false;
+static bool cookie_script_has_run = false;
+static unsigned header_iteration = 0;
+static unsigned cookie_iteration = 0;
+
+/* Last set of headers and cookies generated by the scripts. */
+static struct curl_slist *headers_from_script = NULL;
+static char *cookies_from_script = NULL;
+
+void
+scripts_unload (void)
+{
+  curl_slist_free_all (headers_from_script);
+  free (cookies_from_script);
+}
+
+static int run_header_script (struct curl_handle *);
+static int run_cookie_script (struct curl_handle *);
+
+/* This is called from any thread just before we make a curl request.
+ *
+ * Because the thread model is NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS
+ * we can be assured of exclusive access to curl_handle here.
+ */
+int
+do_scripts (struct curl_handle *h)
+{
+  time_t now;
+  struct curl_slist *p;
+
+  /* Return quickly without acquiring the lock if this feature is not
+   * being used.
+   */
+  if (!header_script && !cookie_script)
+    return 0;
+
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+
+  /* Run or re-run header-script if we need to. */
+  if (header_script) {
+    time (&now);
+    if (!header_script_has_run ||
+        (header_script_renew > 0 && now - header_last >= header_script_renew)) {
+      if (run_header_script (h) == -1)
+        return -1;
+      header_last = now;
+      header_script_has_run = true;
+    }
+  }
+
+  /* Run or re-run cookie-script if we need to. */
+  if (cookie_script) {
+    time (&now);
+    if (!cookie_script_has_run ||
+        (cookie_script_renew > 0 && now - cookie_last >= cookie_script_renew)) {
+      if (run_cookie_script (h) == -1)
+        return -1;
+      cookie_last = now;
+      cookie_script_has_run = true;
+    }
+  }
+
+  /* Set headers and cookies in the handle.
+   *
+   * When calling CURLOPT_HTTPHEADER we have to keep the list around
+   * because unfortunately curl doesn't take a copy.  Since we don't
+   * know which other threads might be using it, we must make a copy
+   * of the global list (headers_from_script) per handle
+   * (h->headers_copy).  For CURLOPT_COOKIE, curl internally takes a
+   * copy so we don't need to do this.
+   */
+  if (h->headers_copy) {
+    curl_easy_setopt (h->c, CURLOPT_HTTPHEADER, NULL);
+    curl_slist_free_all (h->headers_copy);
+    h->headers_copy = NULL;
+  }
+  for (p = headers_from_script; p != NULL; p = p->next) {
+    h->headers_copy = curl_slist_append (h->headers_copy, p->data);
+    if (h->headers_copy == NULL) {
+      nbdkit_error ("curl_slist_append: %m");
+      return -1;
+    }
+  }
+  curl_easy_setopt (h->c, CURLOPT_HTTPHEADER, h->headers_copy);
+
+  curl_easy_setopt (h->c, CURLOPT_COOKIE, cookies_from_script);
+
+  return 0;
+}
+
+/* This is called with the lock held when we must run or re-run the
+ * header-script.
+ */
+static int
+run_header_script (struct curl_handle *h)
+{
+  int fd;
+  char tmpfile[] = "/tmp/errorsXXXXXX";
+  FILE *fp;
+  CLEANUP_FREE char *cmd = NULL, *line = NULL;
+  size_t len = 0, linelen = 0, nr_headers = 0;
+
+  assert (header_script != NULL); /* checked by caller */
+
+  /* Reset the list of headers. */
+  curl_slist_free_all (headers_from_script);
+  headers_from_script = NULL;
+
+  /* Create a temporary file for the errors so we can redirect them
+   * into nbdkit_error.
+   */
+  fd = mkstemp (tmpfile);
+  if (fd == -1) {
+    nbdkit_error ("mkstemp");
+    return -1;
+  }
+  close (fd);
+
+  /* Generate the full script with the local $url variable. */
+  fp = open_memstream (&cmd, &len);
+  if (fp == NULL) {
+    nbdkit_error ("open_memstream: %m");
+    return -1;
+  }
+  fprintf (fp, "exec </dev/null\n");    /* Avoid stdin leaking (nbdkit -s). */
+  fprintf (fp, "exec 2>%s\n", tmpfile); /* Catch errors to a temporary file. */
+  fprintf (fp, "url=");                 /* Set the shell variables. */
+  shell_quote (url, fp);
+  putc ('\n', fp);
+  fprintf (fp, "iteration=%u\n", header_iteration++);
+  putc ('\n', fp);
+  fprintf (fp, "%s", header_script);    /* The script or command. */
+  if (fclose (fp) == EOF) {
+    nbdkit_error ("memstream failed");
+    return -1;
+  }
+
+  /* Run the script and read the headers. */
+  nbdkit_debug ("curl: running header-script");
+  fp = popen (cmd, "r");
+  if (fp == NULL) {
+    nbdkit_error ("popen: %m");
+    return -1;
+  }
+  while ((len = getline (&line, &linelen, fp)) != -1) {
+    /* Remove trailing \n and whitespace. */
+    while (len > 0 && ascii_isspace (line[len-1]))
+      line[--len] = '\0';
+    if (len == 0)
+      continue;
+
+    headers_from_script = curl_slist_append (headers_from_script, line);
+    if (headers_from_script == NULL) {
+      nbdkit_error ("curl_slist_append: %m");
+      pclose (fp);
+      return -1;
+    }
+    nr_headers++;
+  }
+
+  /* If the command failed, this should return EOF and the error
+   * message should be in the temporary file (but we only read the
+   * first line).
+   */
+  if (pclose (fp) == EOF) {
+    fp = fopen (tmpfile, "r");
+    if ((len = getline (&line, &linelen, fp)) >= 0) {
+      if (len > 0 && line[len-1] == '\n')
+        line[len-1] = '\0';
+      nbdkit_error ("header-script failed: %s", line);
+    }
+    else
+      nbdkit_error ("header-script failed");
+    return -1;
+  }
+
+  nbdkit_debug ("header-script returned %zu header(s)", nr_headers);
+  return 0;
+}
+
+/* This is called with the lock held when we must run or re-run the
+ * cookie-script.
+ */
+static int
+run_cookie_script (struct curl_handle *h)
+{
+  int fd;
+  char tmpfile[] = "/tmp/errorsXXXXXX";
+  FILE *fp;
+  CLEANUP_FREE char *cmd = NULL, *line = NULL;
+  size_t len = 0, linelen = 0;
+
+  assert (cookie_script != NULL); /* checked by caller */
+
+  /* Reset the cookies. */
+  free (cookies_from_script);
+  cookies_from_script = NULL;
+
+  /* Create a temporary file for the errors so we can redirect them
+   * into nbdkit_error.
+   */
+  fd = mkstemp (tmpfile);
+  if (fd == -1) {
+    nbdkit_error ("mkstemp");
+    return -1;
+  }
+  close (fd);
+
+  /* Generate the full script with the local $url variable. */
+  fp = open_memstream (&cmd, &len);
+  if (fp == NULL) {
+    nbdkit_error ("open_memstream: %m");
+    return -1;
+  }
+  fprintf (fp, "exec </dev/null\n");    /* Avoid stdin leaking (nbdkit -s). */
+  fprintf (fp, "exec 2>%s\n", tmpfile); /* Catch errors to a temporary file. */
+  fprintf (fp, "url=");                 /* Set the shell variable. */
+  shell_quote (url, fp);
+  putc ('\n', fp);
+  fprintf (fp, "iteration=%u\n", cookie_iteration++);
+  putc ('\n', fp);
+  fprintf (fp, "%s", cookie_script);    /* The script or command. */
+  if (fclose (fp) == EOF) {
+    nbdkit_error ("memstream failed");
+    return -1;
+  }
+
+  /* Run the script and read the cookies. */
+  nbdkit_debug ("curl: running cookie-script");
+  fp = popen (cmd, "r");
+  if (fp == NULL) {
+    nbdkit_error ("popen: %m");
+    return -1;
+  }
+  len = getline (&line, &linelen, fp);
+  if (len > 0) {
+    /* Remove trailing \n and whitespace. */
+    while (len > 0 && ascii_isspace (line[len-1]))
+      line[--len] = '\0';
+    if (len > 0) {
+      cookies_from_script = strdup (line);
+      if (cookies_from_script == NULL) {
+        nbdkit_error ("strdup");
+        pclose (fp);
+        return -1;
+      }
+    }
+  }
+
+  /* If the command failed, this should return EOF and the error
+   * message should be in the temporary file (but we only read the
+   * first line).
+   */
+  if (pclose (fp) == EOF) {
+    fp = fopen (tmpfile, "r");
+    if ((len = getline (&line, &linelen, fp)) >= 0) {
+      if (len > 0 && line[len-1] == '\n')
+        line[len-1] = '\0';
+      nbdkit_error ("cookie-script failed: %s", line);
+    }
+    else
+      nbdkit_error ("cookie-script failed");
+    return -1;
+  }
+
+  nbdkit_debug ("cookie-script returned %scookies",
+                cookies_from_script ? "" : "no ");
+  return 0;
+}

tests/Makefile.am
@@ -506,6 +506,10 @@ if HAVE_CURL
 TESTS += test-curl-file.sh
 EXTRA_DIST += test-curl-file.sh
 LIBGUESTFS_TESTS += test-curl
+LIBNBD_TESTS += \
+	test-curl-header-script \
+	test-curl-cookie-script \
+	$(NULL)
 
 test_curl_SOURCES = \
 	test-curl.c \
@@ -528,6 +532,49 @@ test_curl_LDADD = \
 	libtest.la \
 	$(LIBGUESTFS_LIBS) \
 	$(NULL)
+
+test_curl_header_script_SOURCES = \
+	test-curl-header-script.c \
+	web-server.c \
+	web-server.h \
+	$(NULL)
+test_curl_header_script_CPPFLAGS = \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+test_curl_header_script_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(LIBNBD_CFLAGS) \
+	$(PTHREAD_CFLAGS) \
+	$(NULL)
+test_curl_header_script_LDFLAGS = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(PTHREAD_LIBS) \
+	$(NULL)
+test_curl_header_script_LDADD = \
+	$(LIBNBD_LIBS) \
+	$(NULL)
+
+test_curl_cookie_script_SOURCES = \
+	test-curl-cookie-script.c \
+	web-server.c \
+	web-server.h \
+	$(NULL)
+test_curl_cookie_script_CPPFLAGS = \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+test_curl_cookie_script_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(LIBNBD_CFLAGS) \
+	$(PTHREAD_CFLAGS) \
+	$(NULL)
+test_curl_cookie_script_LDFLAGS = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(PTHREAD_LIBS) \
+	$(NULL)
+test_curl_cookie_script_LDADD = \
+	$(LIBNBD_LIBS) \
+	$(NULL)
+
 endif HAVE_CURL
 endif HAVE_MKE2FS_WITH_D
 

plugins/curl/Makefile.am
@@ -38,6 +38,8 @@ if HAVE_CURL
 plugin_LTLIBRARIES = nbdkit-curl-plugin.la
 
 nbdkit_curl_plugin_la_SOURCES = \
+	curldefs.h \
+	scripts.c \
 	curl.c \
 	$(top_srcdir)/include/nbdkit-plugin.h \
 	$(NULL)

.gitignore
@@ -113,6 +113,8 @@ plugins/*/*.3
 /tests/stamp-ssh-user-key
 /tests/test-connect
 /tests/test-curl
+/tests/test-curl-cookie-script
+/tests/test-curl-header-script
 /tests/test-data
 /tests/test-delay
 /tests/test-exit-with-parent

plugins/curl/curl.c
@@ -48,9 +48,11 @@
 
 #include <nbdkit-plugin.h>
 
-#include "cleanup.h"
 #include "ascii-ctype.h"
 #include "ascii-string.h"
+#include "cleanup.h"
+
+#include "curldefs.h"
 
 /* Macro CURL_AT_LEAST_VERSION was added in 2015 (Curl 7.43) so if the
  * macro isn't present then Curl is very old.
@@ -61,24 +63,29 @@
 #endif
 #endif
 
-static const char *url = NULL;  /* required */
+/* Plugin configuration. */
+const char *url = NULL;         /* required */
 
-static const char *cainfo = NULL;
-static const char *capath = NULL;
-static char *cookie = NULL;
-static struct curl_slist *headers = NULL;
-static char *password = NULL;
-static long protocols = CURLPROTO_ALL;
-static const char *proxy = NULL;
-static char *proxy_password = NULL;
-static const char *proxy_user = NULL;
-static bool sslverify = true;
-static bool tcp_keepalive = false;
-static bool tcp_nodelay = true;
-static uint32_t timeout = 0;
-static const char *unix_socket_path = NULL;
-static const char *user = NULL;
-static const char *user_agent = NULL;
+const char *cainfo = NULL;
+const char *capath = NULL;
+char *cookie = NULL;
+const char *cookie_script = NULL;
+unsigned cookie_script_renew = 0;
+struct curl_slist *headers = NULL;
+const char *header_script = NULL;
+unsigned header_script_renew = 0;
+char *password = NULL;
+long protocols = CURLPROTO_ALL;
+const char *proxy = NULL;
+char *proxy_password = NULL;
+const char *proxy_user = NULL;
+bool sslverify = true;
+bool tcp_keepalive = false;
+bool tcp_nodelay = true;
+uint32_t timeout = 0;
+const char *unix_socket_path = NULL;
+const char *user = NULL;
+const char *user_agent = NULL;
 
 /* Use '-D curl.verbose=1' to set. */
 int curl_debug_verbose = 0;
@@ -98,11 +105,12 @@ curl_load (void)
 static void
 curl_unload (void)
 {
-  free (password);
-  free (proxy_password);
   free (cookie);
   if (headers)
     curl_slist_free_all (headers);
+  free (password);
+  free (proxy_password);
+  scripts_unload ();
   curl_global_cleanup ();
 }
 
@@ -202,12 +210,32 @@ curl_config (const char *key, const char *value)
       return -1;
   }
 
+  else if (strcmp (key, "cookie-script") == 0) {
+    cookie_script = value;
+  }
+
+  else if (strcmp (key, "cookie-script-renew") == 0) {
+    if (nbdkit_parse_unsigned ("cookie-script-renew", value,
+                               &cookie_script_renew) == -1)
+      return -1;
+  }
+
   else if (strcmp (key, "header") == 0) {
     headers = curl_slist_append (headers, value);
     if (headers == NULL) {
       nbdkit_error ("curl_slist_append: %m");
       return -1;
     }
+  }
+
+  else if (strcmp (key, "header-script") == 0) {
+    header_script = value;
+  }
+
+  else if (strcmp (key, "header-script-renew") == 0) {
+    if (nbdkit_parse_unsigned ("header-script-renew", value,
+                               &header_script_renew) == -1)
+      return -1;
   }
 
   else if (strcmp (key, "password") == 0) {
@@ -300,6 +328,26 @@ curl_config_complete (void)
     return -1;
   }
 
+  if (headers && header_script) {
+    nbdkit_error ("header and header-script cannot be used at the same time");
+    return -1;
+  }
+
+  if (!header_script && header_script_renew) {
+    nbdkit_error ("header-script-renew cannot be used without header-script");
+    return -1;
+  }
+
+  if (cookie && cookie_script) {
+    nbdkit_error ("cookie and cookie-script cannot be used at the same time");
+    return -1;
+  }
+
+  if (!cookie_script && cookie_script_renew) {
+    nbdkit_error ("cookie-script-renew cannot be used without cookie-script");
+    return -1;
+  }
+
   return 0;
 }
 
@@ -307,7 +355,11 @@ curl_config_complete (void)
   "cainfo=<CAINFO>            Path to Certificate Authority file.\n" \
   "capath=<CAPATH>            Path to directory with CA certificates.\n" \
   "cookie=<COOKIE>            Set HTTP/HTTPS cookies.\n" \
+  "cookie-script=<SCRIPT>     Script to set HTTP/HTTPS cookies.\n" \
+  "cookie-script-renew=<SECS> Time to renew HTTP/HTTPS cookies.\n" \
   "header=<HEADER>            Set HTTP/HTTPS header.\n" \
+  "header-script=<SCRIPT>     Script to set HTTP/HTTPS headers.\n" \
+  "header-script-renew=<SECS> Time to renew HTTP/HTTPS headers.\n" \
   "password=<PASSWORD>        The password for the user account.\n" \
   "protocols=PROTO,PROTO,..   Limit protocols allowed.\n" \
   "proxy=<PROXY>              Set proxy URL.\n" \
@@ -321,18 +373,6 @@ curl_config_complete (void)
   "url=<URL>       (required) The disk image URL to serve.\n" \
   "user=<USER>                The user to log in as.\n" \
   "user-agent=<USER-AGENT>    Send user-agent header for HTTP/HTTPS."
-
-/* The per-connection handle. */
-struct curl_handle {
-  CURL *c;
-  bool accept_range;
-  int64_t exportsize;
-  char errbuf[CURL_ERROR_SIZE];
-  char *write_buf;
-  uint32_t write_count;
-  const char *read_buf;
-  uint32_t read_count;
-};
 
 /* Translate CURLcode to nbdkit_error. */
 #define display_curl_error(h, r, fs, ...)                       \
@@ -450,7 +490,11 @@ curl_open (int readonly)
 
   /* Get the file size and also whether the remote HTTP server
    * supports byte ranges.
+   *
+   * We must run the scripts if necessary and set headers in the
+   * handle.
    */
+  if (do_scripts (h) == -1) goto err;
   h->accept_range = false;
   curl_easy_setopt (h->c, CURLOPT_NOBODY, 1); /* No Body, not nobody! */
   curl_easy_setopt (h->c, CURLOPT_HEADERFUNCTION, header_cb);
@@ -608,6 +652,8 @@ curl_close (void *handle)
   struct curl_handle *h = handle;
 
   curl_easy_cleanup (h->c);
+  if (h->headers_copy)
+    curl_slist_free_all (h->headers_copy);
   free (h);
 }
 
@@ -637,6 +683,9 @@ curl_pread (void *handle, void *buf, uint32_t count, uint64_t offset)
   struct curl_handle *h = handle;
   CURLcode r;
   char range[128];
+
+  /* Run the scripts if necessary and set headers in the handle. */
+  if (do_scripts (h) == -1) return -1;
 
   /* Tell the write_cb where we want the data to be written.  write_cb
    * will update this if the data comes in multiple sections.
@@ -698,6 +747,9 @@ curl_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset)
   struct curl_handle *h = handle;
   CURLcode r;
   char range[128];
+
+  /* Run the scripts if necessary and set headers in the handle. */
+  if (do_scripts (h) == -1) return -1;
 
   /* Tell the read_cb where we want the data to be read from.  read_cb
    * will update this if the data comes in multiple sections.

plugins/curl/curldefs.h
@@ -1,0 +1,76 @@
+/* nbdkit
+ * Copyright (C) 2014-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef NBDKIT_CURLDEFS_H
+#define NBDKIT_CURLDEFS_H
+
+extern const char *url;
+
+extern const char *cainfo;
+extern const char *capath;
+extern char *cookie;
+extern const char *cookie_script;
+extern unsigned cookie_script_renew;
+extern struct curl_slist *headers;
+extern const char *header_script;
+extern unsigned header_script_renew;
+extern char *password;
+extern long protocols;
+extern const char *proxy;
+extern char *proxy_password;
+extern const char *proxy_user;
+extern bool sslverify;
+extern bool tcp_keepalive;
+extern bool tcp_nodelay;
+extern uint32_t timeout;
+extern const char *unix_socket_path;
+extern const char *user;
+extern const char *user_agent;
+
+/* The per-connection handle. */
+struct curl_handle {
+  CURL *c;
+  bool accept_range;
+  int64_t exportsize;
+  char errbuf[CURL_ERROR_SIZE];
+  char *write_buf;
+  uint32_t write_count;
+  const char *read_buf;
+  uint32_t read_count;
+  struct curl_slist *headers_copy;
+};
+
+/* scripts.c */
+extern int do_scripts (struct curl_handle *h);
+extern void scripts_unload (void);
+
+#endif /* NBDKIT_CURLDEFS_H */

commit 83bb70b7644313deee96b6856faee5c7e832a43f

curl: scripts: Multiple fixes
Refactor duplicate code on the error path when an external cookie or
header script fails.  At the same time fix a leak of FILE* on this path.

While fixing the above, I discovered that we didn't return an error
from the do_scripts() function if the external script failed.  This is
because we tested the return value of pclose incorrectly.

Add a test to ensure this is fixed now and does not regress.

Fixes: commit dcba0f0b62ed772bf4dd4849043d8f7bec0fd127

plugins/curl/scripts.c
@@ -82,6 +82,7 @@ scripts_unload (void)
 
 static int run_header_script (struct curl_handle *);
 static int run_cookie_script (struct curl_handle *);
+static void error_from_tmpfile (const char *what, const char *tmpfile);
 
 /* This is called from any thread just before we make a curl request.
  *
@@ -228,19 +229,8 @@ run_header_script (struct curl_handle *h)
     nr_headers++;
   }
 
-  /* If the command failed, this should return EOF and the error
-   * message should be in the temporary file (but we only read the
-   * first line).
-   */
-  if (pclose (fp) == EOF) {
-    fp = fopen (tmpfile, "r");
-    if ((len = getline (&line, &linelen, fp)) >= 0) {
-      if (len > 0 && line[len-1] == '\n')
-        line[len-1] = '\0';
-      nbdkit_error ("header-script failed: %s", line);
-    }
-    else
-      nbdkit_error ("header-script failed");
+  if (pclose (fp) != 0) {
+    error_from_tmpfile ("header-script", tmpfile);
     return -1;
   }
 
@@ -317,25 +307,38 @@ run_cookie_script (struct curl_handle *h)
     }
   }
 
-  /* If the command failed, this should return EOF and the error
-   * message should be in the temporary file (but we only read the
-   * first line).
-   */
-  if (pclose (fp) == EOF) {
-    fp = fopen (tmpfile, "r");
-    if ((len = getline (&line, &linelen, fp)) >= 0) {
-      if (len > 0 && line[len-1] == '\n')
-        line[len-1] = '\0';
-      nbdkit_error ("cookie-script failed: %s", line);
-    }
-    else
-      nbdkit_error ("cookie-script failed");
+  if (pclose (fp) != 0) {
+    error_from_tmpfile ("cookie-script", tmpfile);
     return -1;
   }
 
   nbdkit_debug ("cookie-script returned %scookies",
                 cookies_from_script ? "" : "no ");
   return 0;
+}
+
+/* If the command failed, the error message should be in the temporary
+ * file to which we redirected the script's stderr.  We only read the
+ * first line.
+ */
+static void
+error_from_tmpfile (const char *what, const char *tmpfile)
+{
+  FILE *fp;
+  CLEANUP_FREE char *line = NULL;
+  size_t len, linelen = 0;
+
+  fp = fopen (tmpfile, "r");
+
+  if (fp && (len = getline (&line, &linelen, fp)) >= 0) {
+    if (len > 0 && line[len-1] == '\n')
+      line[len-1] = '\0';
+    nbdkit_error ("%s failed: %s", what, line);
+  }
+  else
+    nbdkit_error ("%s failed", what);
+
+  if (fp) fclose (fp);
 }
 
 #else /* WIN32 */

tests/Makefile.am
@@ -575,8 +575,15 @@ EXTRA_DIST += test-cdi.sh
 # curl plugin test.
 if HAVE_MKE2FS_WITH_D
 if HAVE_CURL
-TESTS += test-curl-file.sh
-EXTRA_DIST += test-curl-file.sh
+TESTS += \
+	test-curl-file.sh \
+	test-curl-header-script-fail.sh \
+	$(NULL)
+EXTRA_DIST += \
+	test-curl-file.sh \
+	test-curl-header-script-fail.script \
+	test-curl-header-script-fail.sh \
+	$(NULL)
 LIBGUESTFS_TESTS += test-curl
 LIBNBD_TESTS += \
 	test-curl-header-script \

tests/test-curl-header-script-fail.script
@@ -1,0 +1,36 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2022 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# See test-curl-header-script-fail.sh
+
+echo TEST MESSAGE TEST MESSAGE >&2
+exit 1

tests/test-curl-header-script-fail.sh
@@ -1,0 +1,57 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright (C) 2018-2022 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test the curl plugin header-script error path.  Although we are
+# using the file plugin (and so no headers would be sent) the header
+# script should still be run once.  We want to check that if it fails
+# then nbdkit correctly logs the error message.
+
+source ./functions.sh
+set -e
+set -x
+
+requires test -f disk
+requires nbdinfo --version
+
+errors="test-curl-header-script-fail.log"
+rm -f $errors
+cleanup_fn rm -f $errors
+
+# This command is expected to fail.
+nbdkit -rfv -U - \
+       curl file:$PWD/disk protocols=file \
+       header-script=$PWD/test-curl-header-script-fail.script \
+       --run 'nbdinfo --no-content $uri' >$errors 2>&1 ||:
+
+cat $errors
+
+grep 'header-script failed: TEST MESSAGE TEST MESSAGE' $errors