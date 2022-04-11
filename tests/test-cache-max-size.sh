73ff1b5c0896e20f68fbd032f8c29030b4c57d40
Richard W.M. Jones <rjones@redhat.com>
2022-04-11 15:03:57 +0100

    tests/test-cache-max-size.sh: Fix tests in locales with different date format
    In pl_PL the date field may be written "DD-MM" instead of "Mon DD"
    (eg. "11-04" instead of "Apr 04").  Thus the output of ls -l will not
    have the expected number of fields.  A simple fix is simply to set
    LANG=C before running the ls command.
    
    I also added the -n flag (numeric UID/GID) since user or group names
    with spaces could also break the output.
    
    Thanks: Ryū Risto
    Fixes: https://gitlab.com/nbdkit/nbdkit/-/issues/4

--- tests/test-cache-max-size.sh
+++ tests/test-cache-max-size.sh
@@ -77,8 +77,8 @@ qemu-io -f raw "nbd+unix://?socket=$sock" \
 # deleted by the filter, and so is only accessible via /proc/$pid/fd.
 # Get the /proc link to the cache file, and the size of it in bytes.
 fddir="/proc/$( cat $d/cache-max-size.pid )/fd"
-ls -l $fddir ||:
-fd="$fddir/$( ls -l $fddir | grep $TMPDIR/ | head -1 |
+LANG=C ls -ln $fddir ||:
+fd="$fddir/$( LANG=C ls -ln $fddir | grep $TMPDIR/ | head -1 |
               tr -s ' ' | cut -d ' ' -f 9 )"
 stat -L $fd
 size=$(( $(stat -L -c '%b * %B' $fd) ))
