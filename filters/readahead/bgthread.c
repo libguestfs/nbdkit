2ff548d66ad3eae87868402ec5b3319edd12090f
Richard W.M. Jones <rjones@redhat.com>
2022-04-16 18:39:13 +0100

    readahead: Rewrite this filter so it prefetches using .cache
    The previous readahead filter did not work well and we have stopped
    using it in virt-v2v.  However the concept is a good one if we can
    make it work.  This commit completely rethinks the filter.
    
    Now, in parallel with the ordinary pread command, we issue a prefetch
    (ie. .cache) to the underlying plugin for the data immediately
    following the pread.  For example a simple sequence of operations:
    
      t=1  pread (offset=0, count=65536)
      t=2  pread (offset=65536, count=65536)
      t=3  pread (offset=131072, count=65536)
    
    would become:
    
      t=1  pread (offset=0, count=65536)     <--\ issued
           cache (offset=65536, count=65536) <--/ in parallel
      t=2  pread (offset=65536, count=65536)
           cache (offset=131072, count=65536)
      t=3  pread (offset=131072, count=65536)
           cache (offset=196608, count=65536)
    
    This requires that the underlying filter(s) and plugin chain can
    actually do something with the .cache request.  If this is not the
    case then the filter does nothing (but it will print a warning).  For
    plugins which don't have native support for prefetching, it is
    sufficient to insert nbdkit-cache-filter after this filter.
    (nbdkit-cow-filter can also be used with cow-on-cache=true, but that
    is more useful for advanced users who are already using the cow
    filter).
    
    The implementation creates a background thread per connection to issue
    the parallel .cache requests.  This is safer than the alternative (one
    background thread in total) since we don't have to deal with the
    problem of cache requests being issued with the wrong export name or
    stale next pointer.  The background thread is controlled by a queue of
    commands, with the only possible commands being "cache" or "quit".
    
    Because the background thread issues parallel requests on the same
    connection, the underlying plugin must support the parallel thread
    model, otherwise we would be violating the plugin's thread model.  It
    may be possible in future to open a new connection from the background
    thread (but with the same exportname), which would lift this
    restriction to at least serialize_requests.  Because of the current
    limitation, nbdkit-curl-plugin cannot use prefetch.

---
+++ filters/readahead/bgthread.c
@@ -0,0 +1,76 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "readahead.h"
+
+#include "cleanup.h"
+
+void *
+readahead_thread (void *vp)
+{
+  struct bgthread_ctrl *ctrl = vp;
+
+  for (;;) {
+    struct command cmd;
+
+    /* Wait until we are sent at least one command. */
+    {
+      ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&ctrl->lock);
+      while (ctrl->cmds.len == 0)
+        pthread_cond_wait (&ctrl->cond, &ctrl->lock);
+      cmd = ctrl->cmds.ptr[0];
+      command_queue_remove (&ctrl->cmds, 0);
+    }
+
+    switch (cmd.type) {
+    case CMD_QUIT:
+      /* Finish processing and exit the thread. */
+      return NULL;
+
+    case CMD_CACHE:
+      /* Issue .cache (readahead) to underlying plugin.  We ignore any
+       * errors because there's no way to communicate that back to the
+       * client, and readahead is only advisory.
+       */
+      cmd.next->cache (cmd.next, cmd.count, cmd.offset, 0, NULL);
+    }
+  }
+}
---
+++ filters/readahead/readahead.h
@@ -0,0 +1,60 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef NBDKIT_READAHEAD_H
+#define NBDKIT_READAHEAD_H
+
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "vector.h"
+
+/* List of commands issued to the background thread. */
+struct command {
+  enum { CMD_QUIT, CMD_CACHE } type;
+  nbdkit_next *next;
+  uint64_t offset;
+  uint32_t count;
+};
+DEFINE_VECTOR_TYPE(command_queue, struct command);
+
+struct bgthread_ctrl {
+  command_queue cmds;           /* Command queue. */
+  pthread_mutex_t lock;         /* Lock for queue. */
+  pthread_cond_t cond;          /* Condition queue size 0 -> 1. */
+};
+
+/* Start background thread (one per connection). */
+extern void *readahead_thread (void *vp);
+
+#endif /* NBDKIT_READAHEAD_H */
