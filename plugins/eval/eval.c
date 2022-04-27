libguestfs/nbdkit

commit f128f98e8892adf013db52c184ec22c796d176f5

eval: Allow user override of 'missing'
A comment in the code mentioned something that didn't actually work,
but which can be useful for user-directed logging of what other
callbacks they might want to implement.  And of course, testsuite
coverage is needed to prevent future breakage (and to ensure via
valgrind that we didn't leak).

Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/eval/eval.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2019 Red Hat Inc.
+ * Copyright (C) 2018-2020 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -72,6 +72,7 @@ static const char *known_methods[] = {
   "flush",
   "get_size",
   "is_rotational",
+  "missing",
   "open",
   "pread",
   "pwrite",
@@ -168,6 +169,12 @@ create_script (const char *method, const char *value)
     return NULL;
   }
 
+  /* Special case for user override of missing */
+  if (missing && strcmp (script, missing) == 0 && unlink (script) == -1) {
+    nbdkit_error ("unlink: %m");
+    return NULL;
+  }
+
   fp = fopen (script, "w");
   if (fp == NULL) {
     nbdkit_error ("fopen: %s: %m", script);
@@ -215,7 +222,7 @@ eval_load (void)
 
   /* To make things easier, create a "missing" script which always
    * exits with code 2.  If a method is missing we call this script
-   * instead.  It could even be overridden by the user.
+   * instead.  It can even be overridden by the user.
    */
   missing = create_script ("missing", "exit 2\n");
   if (!missing)
@@ -254,11 +261,15 @@ static int
 add_method (const char *key, const char *value)
 {
   char *script;
+  char *tmp = missing; /* Needed to allow user override of missing */
 
-  if (get_script (key) != missing) {
+  missing = NULL;
+  if (get_script (key) != NULL) {
+    missing = tmp;
     nbdkit_error ("method %s defined more than once on the command line", key);
     return -1;
   }
+  missing = tmp;
 
   /* Do a bit of checking to make sure the key isn't malicious.  This
    * duplicates work already done by nbdkit, but better safe than

plugins/eval/nbdkit-eval-plugin.pod
@@ -140,6 +140,12 @@ no longer see that key.
 
 All of these parameters are optional.
 
+=item B<missing=>SCRIPT
+
+The parameter C<missing> defines a script that will be called in place
+of any other callback not explicitly provided.  If omitted, this
+defaults to the script "exit 2".
+
 =back
 
 =head1 ENVIRONMENT VARIABLES

tests/test-eval.sh
@@ -42,7 +42,9 @@ cleanup_fn rm -f $files
 nbdkit -U - eval \
        get_size='echo 64M' \
        pread='dd if=/dev/zero count=$3 iflag=count_bytes' \
+       missing='echo "in missing: $@" >> eval.out; exit 2' \
        --run 'qemu-img info $nbd' > eval.out
 
 cat eval.out
 grep '67108864 bytes' eval.out
+grep 'in missing' eval.out

commit c794d6e3ed8163d0d5ec7a359567ba725999580e

eval: Fix memory leak on error path
Found by Coverity:

  Error: RESOURCE_LEAK (CWE-772): [#def20]
  nbdkit-1.30.4/plugins/eval/eval.c:166: alloc_arg: "asprintf" allocates memory that is stored into "script". [Note: The source code implementation of the function has been overridden by a builtin model.]
  nbdkit-1.30.4/plugins/eval/eval.c:172: noescape: Resource "script" is not freed or pointed-to in "strcmp".
  nbdkit-1.30.4/plugins/eval/eval.c:172: noescape: Resource "script" is not freed or pointed-to in "unlink".
  nbdkit-1.30.4/plugins/eval/eval.c:174: leaked_storage: Variable "script" going out of scope leaks the storage it points to.
  #  172|     if (missing && strcmp (script, missing) == 0 && unlink (script) == -1) {
  #  173|       nbdkit_error ("unlink: %m");
  #  174|->     return NULL;
  #  175|     }
  #  176|

Fixes: commit f128f98e8892adf013db52c184ec22c796d176f5

plugins/eval/eval.c
@@ -171,6 +171,7 @@ create_script (const char *method, const char *value)
   /* Special case for user override of missing */
   if (missing && strcmp (script, missing) == 0 && unlink (script) == -1) {
     nbdkit_error ("unlink: %m");
+    free (script);
     return NULL;
   }
 