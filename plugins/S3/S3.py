libguestfs/nbdkit

commit 7edac58a8cebb7d61d889f3d7fcc9305dc055f7f

Don't retrieve entire object just to get its size.

plugins/S3/S3.py
@@ -86,7 +86,7 @@ def open(readonly):
 
 
 def get_size(s3):
-    resp = s3.get_object(Bucket=bucket_name, Key=key_name)
+    resp = s3.head_object(Bucket=bucket_name, Key=key_name)
     size = resp['ResponseMetadata']['HTTPHeaders']['content-length']
     return int(size)
 

commit 144a0c97be19043d4b69c0456c35ffd03318d2ca

S3: Older boto3 didn't have head_object, fall back to get_object
Fixes: commit 7edac58a8cebb7d61d889f3d7fcc9305dc055f7f

plugins/S3/S3.py
@@ -86,7 +86,10 @@ def open(readonly):
 
 
 def get_size(s3):
-    resp = s3.head_object(Bucket=bucket_name, Key=key_name)
+    try:
+        resp = s3.head_object(Bucket=bucket_name, Key=key_name)
+    except AttributeError:
+        resp = s3.get_object(Bucket=bucket_name, Key=key_name)
     size = resp['ResponseMetadata']['HTTPHeaders']['content-length']
     return int(size)
 