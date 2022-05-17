libguestfs/nbdkit

commit 65c20a09ceacb4431986a2982f2c2e746df63fcb

New filter: scan
This filter will simply scan across the disk issuing a series of cache
requests to the underlying plugin.  It is similar in scope and usage
to the new nbdkit-readahead-filter.

filters/scan/bgthread.c
@@ -1,0 +1,131 @@
+/* nbdkit
+ * Copyright (C) 2019-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+
+#include <nbdkit-filter.h>
+
+#include "scan.h"
+
+#include "cleanup.h"
+#include "minmax.h"
+
+static pthread_mutex_t clock_lock;
+static uint64_t clock_ = 0;
+
+static void
+adjust_clock (uint64_t offset)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&clock_lock);
+  if (clock_ < offset)
+    clock_ = offset;
+}
+
+static void
+reset_clock (uint64_t offset)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&clock_lock);
+  clock_ = 0;
+}
+
+static uint64_t
+get_starting_offset (void)
+{
+  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&clock_lock);
+  return scan_clock ? clock_ : 0;
+}
+
+void *
+scan_thread (void *vp)
+{
+  struct bgthread_ctrl *ctrl = vp;
+  uint64_t offset, size;
+  int64_t r;
+
+  assert (ctrl->next != NULL);
+
+  /* Get the size of the underlying plugin.  Exit the thread on error
+   * because there's not much we can do without knowing the size.
+   */
+  r = ctrl->next->get_size (ctrl->next);
+  if (r == -1)
+    return NULL;
+  size = r;
+
+  /* Start scanning. */
+ start:
+  for (offset = get_starting_offset (); offset < size; offset += scan_size) {
+    uint64_t n;
+
+    /* Execute any commands in the queue. */
+    {
+      ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&ctrl->lock);
+      struct command cmd;
+
+      while (ctrl->cmds.len) {
+        cmd = ctrl->cmds.ptr[0];
+        command_queue_remove (&ctrl->cmds, 0);
+
+        switch (cmd.type) {
+        case CMD_QUIT:
+          nbdkit_debug ("scan: exiting background thread on connection close");
+          return NULL;
+
+        case CMD_NOTIFY_PREAD:
+          if (offset < cmd.offset)
+            offset = cmd.offset;
+        }
+      }
+    }
+
+    adjust_clock (offset);
+    if (offset > size)
+      continue;
+
+    /* Issue the next prefetch. */
+    n = MIN (scan_size, size - offset);
+    ctrl->next->cache (ctrl->next, n, offset, 0, NULL);
+  }
+
+  if (scan_forever) {
+    reset_clock (offset);
+    goto start;
+  }
+
+  nbdkit_debug ("scan: finished scanning the plugin");
+  return NULL;
+}

TODO
@@ -167,14 +167,6 @@ Python:
 Suggestions for filters
 -----------------------
 
-* Add scan filter.  This would be placed on top of cache filters and
-  would scan (read) the whole disk in the background, ensuring it is
-  copied into the cache.  Useful if you have a slow plugin, limited
-  size device, and lots of local disk space, especially if you know
-  that the NBD clients will eventually read all of the device.  RWMJ
-  wrote an implementation of this but it doesn't work well without a
-  background thread.
-
 * Add shared filter.  Take advantage of filter context APIs to open a
   single context into the backend shared among multiple client
   connections.  This may even allow a filter to offer a more parallel

configure.ac
@@ -142,6 +142,7 @@ filters="\
         readahead \
         retry \
         retry-request \
+        scan \
         stats \
         swab \
         tar \
@@ -1399,6 +1400,7 @@ AC_CONFIG_FILES([Makefile
                  filters/readahead/Makefile
                  filters/retry/Makefile
                  filters/retry-request/Makefile
+                 filters/scan/Makefile
                  filters/stats/Makefile
                  filters/swab/Makefile
                  filters/tar/Makefile

filters/cache/nbdkit-cache-filter.pod
@@ -28,8 +28,8 @@ loss, as the name suggests).
 
 This filter only caches image contents.  To cache image metadata, use
 L<nbdkit-cacheextents-filter(1)> between this filter and the plugin.
-To accelerate sequential reads, use L<nbdkit-readahead-filter(1)> on
-top of this filter.
+To accelerate sequential reads, use L<nbdkit-readahead-filter(1)> or
+L<nbdkit-scan-filter(1)> on top of this filter.
 
 =head1 PARAMETERS
 

filters/cacheextents/nbdkit-cacheextents-filter.pod
@@ -54,6 +54,7 @@ L<nbdkit(1)>,
 L<nbdkit-cache-filter(1)>,
 L<nbdkit-extentlist-filter(1)>,
 L<nbdkit-readahead-filter(1)>,
+L<nbdkit-scan-filter(1)>,
 L<nbdkit-vddk-plugin(1)>,
 L<nbdkit-filter(3)>,
 L<qemu-img(1)>.

filters/readahead/nbdkit-readahead-filter.pod
@@ -27,6 +27,10 @@ option.
 The filter uses a simple adaptive algorithm which accelerates
 sequential reads and requires no further configuration.
 
+A similar filter is L<nbdkit-scan-filter(1)> which reads ahead over
+the whole disk, useful if you know that the client will be reading
+sequentially across most or all of the disk.
+
 =head2 Limitations
 
 In a number of significant cases this filter will do nothing.  The
@@ -91,6 +95,7 @@ L<nbdkit-cache-filter(1)>,
 L<nbdkit-cow-filter(1)>,
 L<nbdkit-file-plugin(1)>,
 L<nbdkit-retry-filter(1)>,
+L<nbdkit-scan-filter(1)>,
 L<nbdkit-torrent-plugin(1)>,
 L<nbdkit-vddk-plugin(1)>,
 L<nbdkit-filter(3)>,

filters/scan/Makefile.am
@@ -1,0 +1,72 @@
+# nbdkit
+# Copyright (C) 2019-2021 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-scan-filter.pod
+
+filter_LTLIBRARIES = nbdkit-scan-filter.la
+
+nbdkit_scan_filter_la_SOURCES = \
+	scan.c \
+	scan.h \
+	bgthread.c \
+	$(top_srcdir)/include/nbdkit-filter.h \
+	$(NULL)
+
+nbdkit_scan_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdkit_scan_filter_la_CFLAGS = $(WARNINGS_CFLAGS)
+nbdkit_scan_filter_la_LDFLAGS = \
+	-module -avoid-version -shared $(NO_UNDEFINED_ON_WINDOWS) \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms \
+	$(NULL)
+nbdkit_scan_filter_la_LIBADD = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/common/replacements/libcompat.la \
+	$(IMPORT_LIBRARY_ON_WINDOWS) \
+	$(NULL)
+
+if HAVE_POD
+
+man_MANS = nbdkit-scan-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-scan-filter.1: nbdkit-scan-filter.pod \
+		$(top_builddir)/podwrapper.pl
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

filters/scan/nbdkit-scan-filter.pod
@@ -1,0 +1,159 @@
+=head1 NAME
+
+nbdkit-scan-filter - scan disk prefetching data ahead of sequential reads
+
+=head1 SYNOPSIS
+
+ nbdkit --filter=scan PLUGIN [scan-ahead=false] [scan-clock=false]
+                             [scan-forever=true] [scan-size=]NN
+
+ nbdkit --filter=scan --filter=cache PLUGIN
+
+ nbdkit --filter=scan --filter=cow PLUGIN cow-on-cache=true
+
+=head1 DESCRIPTION
+
+C<nbdkit-scan-filter> is a filter that scans the disk prefetching
+data.  It is sometimes useful if you expect that the client will read
+the disk sequentially.
+
+The basic operation of the filter is that when a client connects, the
+filter will start issuing C<.cache> (prefetch) requests to the plugin
+across the whole disk.  Plugins which support this command will
+prefetch the data, making subsequent reads faster.  For plugins which
+do not support this command, you can inject L<nbdkit-cache-filter(1)>
+below (after) this filter, giving approximately the same effect.
+L<nbdkit-cow-filter(1)> can be used instead of nbdkit-cache-filter, if
+you add the C<cow-on-cache=true> option.
+
+Various C<scan-*> parameters can be used to tune scanning, although
+the defaults should be suitable in most cases.
+
+A similar filter is L<nbdkit-readahead-filter(1)>.
+
+=head2 Limitations
+
+In a number of significant cases this filter will do nothing.  The
+filter will print a warning message if this happens.
+
+=over 4
+
+=item Thread model must be parallel *
+
+For example L<nbdkit-curl-plugin(1)> only supports
+C<serialize_requests>, and so this filter cannot perform prefetches in
+parallel with the read requests.
+
+=item Only scans while clients are connected *
+
+The current filter only scans while there is at least one client
+connected.
+
+=item Only scans the default export *
+
+The current filter only scans the default export and ignores all
+clients connecting to the non-default export name.
+
+* We may be able to lift these restrictions in future.
+
+=item Underlying filters or plugin must support C<.cache> (prefetch)
+
+Very many plugins do not have the concept of prefetching and/or
+do not implement the C<.cache> callback, and so there is no
+way for this filter to issue prefetches.
+
+You can usually get around this by adding I<--filter=cache> after this
+filter as explained above.
+
+=item Prefetching the whole disk may load it all into cache
+
+In particular if you use this filter together with
+L<nbdkit-cache-filter(1)> or L<nbdkit-cow-filter(1)>, they will cache
+the whole content of the plugin into a temporary file.  This may be
+many gigabytes of data, consuming all space in F</var/tmp>.  Of course
+this is the whole point of using this filter, but you should be aware
+of it.
+
+If using the cache filter, the total size of the cache can be limited
+(see L<nbdkit-cache-filter(1)/CACHE MAXIMUM SIZE>).
+
+=back
+
+=head1 PARAMETERS
+
+=over 4
+
+=item B<scan-ahead=false>
+
+By default the filter tries to stay ahead of incoming read requests.
+That is to say, it starts prefetching at the beginning of the disk and
+continues incrementally, but if the client issues a read beyond the
+current prefetch point then the filter skips forward and begins
+prefetching after the read.
+
+However if you set this parameter to false, then this behaviour is
+disabled.  The filter simply prefetches sequentially regardless of
+client requests.
+
+=item B<scan-clock=false>
+
+By default, if all clients disconnect and then another client
+connects, prefetching resumes at the same place in the disk.  (Like
+stopping and starting a clock.)
+
+If you set this parameter to false, then the filter starts prefetching
+from the beginning of the disk again.
+
+=item B<scan-forever=true>
+
+By default the filter scans over the disk once and then stops.
+
+If you set this parameter to true, then after the disk has been
+prefetched completely, the filter goes back to the beginning and
+starts over, repeating this for as long as nbdkit is running and there
+are clients connected.
+
+=item B<scan-size=>NN
+
+This parameter controls the prefetch block size.  The default is
+C<2M>.  This must be a power of 2 and most plugins will have their own
+limits on the amount of data they can prefetch in a single request.
+
+=back
+
+=head1 FILES
+
+=over 4
+
+=item F<$filterdir/nbdkit-scan-filter.so>
+
+The filter.
+
+Use C<nbdkit --dump-config> to find the location of C<$filterdir>.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-scan-filter> first appeared in nbdkit 1.32.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-cache-filter(1)>,
+L<nbdkit-cow-filter(1)>,
+L<nbdkit-file-plugin(1)>,
+L<nbdkit-readahead-filter(1)>,
+L<nbdkit-retry-filter(1)>,
+L<nbdkit-torrent-plugin(1)>,
+L<nbdkit-vddk-plugin(1)>,
+L<nbdkit-filter(3)>,
+L<qemu-img(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2019-2022 Red Hat Inc.

commit 953643429b8c57b4dd20a6c0e5b83704ae9a0e88

scan: Fix bound so we don't try to prefetch beyond end of disk
An off-by-one error in the bound could cause the filter to try to
prefetch beyond the end of the underlying plugin.  This would cause
nbdkit to crash with this assertion failure:

nbdkit: backend.c:782: backend_cache: Assertion `backend_valid_range (c, offset, count)' failed.

The sequence of events was:

 - scan filter background thread started

 - client reads to the end of the disk

 - background thread skips ahead to end of disk (offset == size)

 - background thread tries to prefetch from this point

In the final step the calculations caused to the background thread to
prefetch a scan-size block beyond the end of the plugin.

Fixes: commit 65c20a09ceacb4431986a2982f2c2e746df63fcb

filters/scan/bgthread.c
@@ -113,12 +113,12 @@ scan_thread (void *vp)
     }
 
     adjust_clock (offset);
-    if (offset > size)
-      continue;
 
-    /* Issue the next prefetch. */
-    n = MIN (scan_size, size - offset);
-    ctrl->next->cache (ctrl->next, n, offset, 0, NULL);
+    if (offset < size) {
+      /* Issue the next prefetch. */
+      n = MIN (scan_size, size - offset);
+      ctrl->next->cache (ctrl->next, n, offset, 0, NULL);
+    }
   }
 
   if (scan_forever) {