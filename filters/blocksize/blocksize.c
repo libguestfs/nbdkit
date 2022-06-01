libguestfs/nbdkit

commit 1aadbf99710f97f9273f0829e51aa123eaeb228a

blocksize: Allow parallel requests
The only time where we have to be careful is when the client sends
unaligned data; if we add a lock around our use of the bounce buffer,
then we can provide the same parallelism as the underlying plugin for
all other transactions.

When we first added .can_multi_conn, we waffled on what the blocksize
filter should do[1]; blindly slamming to 1 when all requests are
serial would have been correct at the time, but we opted to stay
conservative by instead slamming things to 0 with a promise to revisit
it when changing the thread model.  Now that we are allowing parallel
transactions, blindly slamming to 1 is no longer a valid option, but
slamming to 0 is overkill when we can instead rely on passthrough to
the plugin's .can_multi_conn.

[1] https://listman.redhat.com/archives/libguestfs/2019-January/msg00074.html

filters/blocksize/blocksize.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2021 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -49,22 +49,19 @@
 
 #define BLOCKSIZE_MIN_LIMIT (64U * 1024)
 
-/* As long as we don't have parallel requests, we can reuse a common
- * buffer for alignment purposes; size it to the maximum we allow for
- * minblock. */
+/* In order to handle parallel requests safely, this lock must be held
+ * when using the bounce buffer.
+ */
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+/* A single bounce buffer for alignment purposes, guarded by the lock.
+ * Size it to the maximum we allow for minblock.
+ */
 static char bounce[BLOCKSIZE_MIN_LIMIT];
+
 static unsigned int minblock;
 static unsigned int maxdata;
 static unsigned int maxlen;
-
-/* We are using a common bounce buffer (see above) so we must
- * serialize all requests.
- */
-static int
-blocksize_thread_model (void)
-{
-  return NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS;
-}
 
 static int
 blocksize_parse (const char *name, const char *s, unsigned int *v)
@@ -158,17 +155,6 @@ blocksize_get_size (struct nbdkit_next_ops *next_ops, void *nxdata,
 }
 
 static int
-blocksize_can_multi_conn (struct nbdkit_next_ops *next_ops, void *nxdata,
-                          void *handle)
-{
-  /* Although we are serializing all requests anyway so this likely
-   * doesn't make a difference, return false because the bounce buffer
-   * is not consistent for flush.
-   */
-  return 0;
-}
-
-static int
 blocksize_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
                  void *handle, void *b, uint32_t count, uint64_t offs,
                  uint32_t flags, int *err)
@@ -179,6 +165,7 @@ blocksize_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned head */
   if (offs & (minblock - 1)) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (minblock - 1);
     keep = MIN (minblock - drop, count);
     if (next_ops->pread (nxdata, bounce, minblock, offs - drop, flags,
@@ -202,6 +189,7 @@ blocksize_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned tail */
   if (count) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     if (next_ops->pread (nxdata, bounce, minblock, offs, flags, err) == -1)
       return -1;
     memcpy (buf, bounce, count);
@@ -228,6 +216,7 @@ blocksize_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned head */
   if (offs & (minblock - 1)) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (minblock - 1);
     keep = MIN (minblock - drop, count);
     if (next_ops->pread (nxdata, bounce, minblock, offs - drop, 0, err) == -1)
@@ -253,6 +242,7 @@ blocksize_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned tail */
   if (count) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     if (next_ops->pread (nxdata, bounce, minblock, offs, 0, err) == -1)
       return -1;
     memcpy (bounce, buf, count);
@@ -332,6 +322,7 @@ blocksize_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned head */
   if (offs & (minblock - 1)) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (minblock - 1);
     keep = MIN (minblock - drop, count);
     if (next_ops->pread (nxdata, bounce, minblock, offs - drop, 0, err) == -1)
@@ -355,6 +346,7 @@ blocksize_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
 
   /* Unaligned tail */
   if (count) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
     if (next_ops->pread (nxdata, bounce, minblock, offs, 0, err) == -1)
       return -1;
     memset (bounce, 0, count);
@@ -437,12 +429,10 @@ blocksize_cache (struct nbdkit_next_ops *next_ops, void *nxdata,
 static struct nbdkit_filter filter = {
   .name              = "blocksize",
   .longname          = "nbdkit blocksize filter",
-  .thread_model      = blocksize_thread_model,
   .config            = blocksize_config,
   .config_complete   = blocksize_config_complete,
   .config_help       = blocksize_config_help,
   .get_size          = blocksize_get_size,
-  .can_multi_conn    = blocksize_can_multi_conn,
   .pread             = blocksize_pread,
   .pwrite            = blocksize_pwrite,
   .trim              = blocksize_trim,

commit 539246eabb7511578ca6e4ce4dadefe3dbbbc9bd

blocksize: Avoid losing aligned writes to RMW race
We have a bug where the blocksize filter can lose the effects of an
aligned write that happens in parallel with an overlapping unaligned
write.  In general, a client should be extremely cautious about
overlapping writes; it is not NBD's fault which of the two writes
lands on the disk (or even if both writes land partially, at
block-level granularities on which write landed last).  However, a
client should still be able to assume that even when two writes
partially overlap, the final state of the disk of the non-overlapping
portion will be that of the one write explicitly touching that
portion, rather than stale data from before either write touched that
block.

Fix the bug by switching the blocksize filter locking from a mutex to
a rwlock.  We still need mutual exclusion during all unaligned
operations (whether reading or writing to the plugin), because we
share the same bounce buffer among all such code, which we get via the
mutual exclusion of wrlock.  But we now also add in shared exclusion
for aligned write-like operations (pwrite, trim, zero); these can
operate in parallel with one another, but must not be allowed to fall
in the window between when an overlapping unaligned operation has read
from the plugin but not yet written, so that we do not end up
re-writing stale contents that undo the portion of the aligned write
that was outside the unaligned region [it's ironic that we only need
this protection on write I/O, but get it by using the rdlock feature
of the rwlock].  Read-like operations (pread, extents, cache) don't
need to be protected from RMW operations, because it is already the
user's problem if they execute parallel overlapping reads while
modifying the same portion of the image; and they will still end up
seeing either the state before or after the unaligned write,
indistinguishable from if we had added more locking.

Whether this lock favors rdlock (aka aligned pwrite) or wrlock (aka
unaligned access) is implementation-defined by the pthread
implementation; if we need to be more precise, we'll have to introduce
our own rwlock implementation on top of lower-level primitives
(https://en.wikipedia.org/wiki/Readers%E2%80%93writer_lock mentions
the use of two mutex to favor readers, or a condvar/mutex to favor
writers).  This is also not very fine-grained; it may turn out that we
could get more performance if instead of locking the entire operation
(across all clients, and regardless of whether the offsets overlap),
we instead just lock metadata and then track whether a new operation
overlaps with an existing unaligned operation, or even add in support
for parallel unaligned operations by having more than one bounce
buffer.  But if performance truly matters, you're better off fixing
the client to do aligned access in the first place, rather than
needing the blocksize filter; perhaps by using the blocksize-policy
filter instead.

The next commit will add a matching test case.

Reported-by: Nikolaus Rath <Nikolaus@rath.org>
Fixes: 1aadbf9971 ("blocksize: Allow parallel requests", v1.25.3)

filters/blocksize/blocksize.c
@@ -51,10 +51,15 @@
 
 #define BLOCKSIZE_MIN_LIMIT (64U * 1024)
 
-/* In order to handle parallel requests safely, this lock must be held
- * when using the bounce buffer.
+/* Lock in order to handle overlapping requests safely.
+ *
+ * Grabbed for exclusive access (wrlock) when using the bounce buffer.
+ *
+ * Grabbed for shared access (rdlock) when doing aligned writes.
+ * These can happen in parallel with one another, but must not land in
+ * between the read and write of an unaligned RMW operation.
  */
-static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
 
 /* A single bounce buffer for alignment purposes, guarded by the lock.
  * Size it to the maximum we allow for minblock.
@@ -255,7 +260,7 @@ blocksize_pread (nbdkit_next *next,
 
   /* Unaligned head */
   if (offs & (h->minblock - 1)) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (h->minblock - 1);
     keep = MIN (h->minblock - drop, count);
     if (next->pread (next, bounce, h->minblock, offs - drop, flags, err) == -1)
@@ -278,7 +283,7 @@ blocksize_pread (nbdkit_next *next,
 
   /* Unaligned tail */
   if (count) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     if (next->pread (next, bounce, h->minblock, offs, flags, err) == -1)
       return -1;
     memcpy (buf, bounce, count);
@@ -306,7 +311,7 @@ blocksize_pwrite (nbdkit_next *next,
 
   /* Unaligned head */
   if (offs & (h->minblock - 1)) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (h->minblock - 1);
     keep = MIN (h->minblock - drop, count);
     if (next->pread (next, bounce, h->minblock, offs - drop, 0, err) == -1)
@@ -321,6 +326,7 @@ blocksize_pwrite (nbdkit_next *next,
 
   /* Aligned body */
   while (count >= h->minblock) {
+    ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&lock);
     keep = MIN (h->maxdata, ROUND_DOWN (count, h->minblock));
     if (next->pwrite (next, buf, keep, offs, flags, err) == -1)
       return -1;
@@ -331,7 +337,7 @@ blocksize_pwrite (nbdkit_next *next,
 
   /* Unaligned tail */
   if (count) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     if (next->pread (next, bounce, h->minblock, offs, 0, err) == -1)
       return -1;
     memcpy (bounce, buf, count);
@@ -371,6 +377,7 @@ blocksize_trim (nbdkit_next *next,
 
   /* Aligned body */
   while (count) {
+    ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&lock);
     keep = MIN (h->maxlen, count);
     if (next->trim (next, keep, offs, flags, err) == -1)
       return -1;
@@ -413,7 +420,7 @@ blocksize_zero (nbdkit_next *next,
 
   /* Unaligned head */
   if (offs & (h->minblock - 1)) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     drop = offs & (h->minblock - 1);
     keep = MIN (h->minblock - drop, count);
     if (next->pread (next, bounce, h->minblock, offs - drop, 0, err) == -1)
@@ -428,6 +435,7 @@ blocksize_zero (nbdkit_next *next,
 
   /* Aligned body */
   while (count >= h->minblock) {
+    ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&lock);
     keep = MIN (h->maxlen, ROUND_DOWN (count, h->minblock));
     if (next->zero (next, keep, offs, flags, err) == -1)
       return -1;
@@ -437,7 +445,7 @@ blocksize_zero (nbdkit_next *next,
 
   /* Unaligned tail */
   if (count) {
-    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&lock);
     if (next->pread (next, bounce, h->minblock, offs, 0, err) == -1)
       return -1;
     memset (bounce, 0, count);