libguestfs/nbdkit

commit fd3615548bd8d5f4662ac73ea2de66f2bb078202

filters: Cache and change semantics of can_zero
The tri-state return pattern has proved valuable in other cases where
a backend wants to opt in or out of nbdkit fallbacks.  Using a
tri-state return pattern at the backend level unifies the different
semantics between plugin and filter .can_zero return values, and makes
it possible for plugins to use cached results of .can_zero rather than
calling into the plugin on each .zero request.  As in other recent
patches, it is easy to write an sh script that demonstrates a
resulting speedup from the caching.  All filters are in-tree and do
not have a promise of API compatability, so it's easy to update all of
them (the log filter is okay as-is, the nozero filter is easy to
update, and no other filter overrides .can_zero).  But for backwards
compatibility reasons, we cannot change the semantics of the plugin's
return value for .can_zero while remaining at API version 2; so that
remains a bool, and merely selects between EMULATE or NATIVE at the
backend level.

$ cat script
case "$1" in
    get_size) echo 1m;;
    can_zero) sleep 1;;
    can_write | zero) ;;
    *) exit 2 ;;
esac

Pre-patch:
$ /bin/time -f %e ./nbdkit -U - sh script \
  --run 'qemu-io -f raw -c "w -z 0 512" -c "w -z 512 512" $uri >/dev/null'
3.10

Post-patch:
$ /bin/time -f %e ./nbdkit -U - sh script \
  --run 'qemu-io -f raw -c "w -z 0 512" -c "w -z 512 512" $uri >/dev/null'
1.08

Signed-off-by: Eric Blake <eblake@redhat.com>

docs/nbdkit-filter.pod
@@ -397,16 +397,37 @@ cached value.
 These intercept the corresponding plugin methods, and control feature
 bits advertised to the client.
 
-Of note, the C<.can_zero> callback in the filter controls whether the
-server advertises zero support to the client, which is slightly
-different semantics than the plugin; that is,
-C<next_ops-E<gt>can_zero> always returns true for a plugin, even when
-the plugin's own C<.can_zero> callback returned false, because nbdkit
-implements a fallback to C<.pwrite> at the plugin layer.
+Of note, the semantics of C<.can_zero> callback in the filter are
+slightly different from the plugin, and must be one of three success
+values visible only to filters:
+
+=over 4
+
+=item C<NBDKIT_ZERO_NONE>
+
+Completely suppress advertisement of write zero support (this can only
+be done from filters, not plugins).
+
+=item C<NBDKIT_ZERO_EMULATE>
+
+Inform nbdkit that write zeroes should immediately fall back to
+C<.pwrite> emulation without trying C<.zero> (this value is returned
+by C<next_ops-E<gt>can_zero> if the plugin returned false in its
+C<.can_zero>).
+
+=item C<NBDKIT_ZERO_NATIVE>
+
+Inform nbdkit that write zeroes should attempt to use C<.zero>,
+although it may still fall back to C<.pwrite> emulation for C<ENOTSUP>
+or C<EOPNOTSUPP> failures (this value is returned by
+C<next_ops-E<gt>can_zero> if the plugin returned true in its
+C<.can_zero>).
+
+=back
 
 Remember that most of the feature check functions return merely a
-boolean success value, while C<.can_fua> and C<.can_cache> have three
-success values.
+boolean success value, while C<.can_zero>, C<.can_fua> and
+C<.can_cache> have three success values.
 
 The difference between C<.can_fua> values may affect choices made in
 the filter: when splitting a write request that requested FUA from the
@@ -517,9 +538,10 @@ value to return to the client.
 This intercepts the plugin C<.zero> method and can be used to modify
 zero requests.
 
-This function will not be called if C<.can_zero> returned false; in
-turn, the filter should not call C<next_ops-E<gt>zero> if
-C<next_ops-E<gt>can_zero> did not return true.
+This function will not be called if C<.can_zero> returned
+C<NBDKIT_ZERO_NONE>; in turn, the filter should not call
+C<next_ops-E<gt>zero> if C<next_ops-E<gt>can_zero> returned
+C<NBDKIT_ZERO_NONE>.
 
 On input, the parameter C<flags> may include C<NBDKIT_FLAG_MAY_TRIM>
 unconditionally, and C<NBDKIT_FLAG_FUA> based on the result of

filters/nozero/nozero.c
@@ -99,7 +99,14 @@ nozero_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle,
 static int
 nozero_can_zero (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
 {
-  return zeromode != NONE;
+  switch (zeromode) {
+  case NONE:
+    return NBDKIT_ZERO_NONE;
+  case EMULATE:
+    return NBDKIT_ZERO_EMULATE;
+  default:
+    return next_ops->can_zero (nxdata);
+  }
 }
 
 static int

server/backend.c
@@ -257,14 +257,20 @@ backend_can_trim (struct backend *b, struct connection *conn)
 int
 backend_can_zero (struct backend *b, struct connection *conn)
 {
+  struct b_conn_handle *h = &conn->handles[b->i];
   int r;
 
   debug ("%s: can_zero", b->name);
 
-  r = backend_can_write (b, conn);
-  if (r != 1)
-    return r;
-  return b->can_zero (b, conn);
+  if (h->can_zero == -1) {
+    r = backend_can_write (b, conn);
+    if (r != 1) {
+      h->can_zero = NBDKIT_ZERO_NONE;
+      return r; /* Relies on 0 == NBDKIT_ZERO_NONE */
+    }
+    h->can_zero = b->can_zero (b, conn);
+  }
+  return h->can_zero;
 }
 
 int
@@ -391,6 +397,7 @@ backend_zero (struct backend *b, struct connection *conn,
   int r;
 
   assert (h->can_write == 1);
+  assert (h->can_zero > NBDKIT_ZERO_NONE);
   assert (!(flags & ~(NBDKIT_FLAG_MAY_TRIM | NBDKIT_FLAG_FUA)));
   debug ("%s: zero count=%" PRIu32 " offset=%" PRIu64 " may_trim=%d fua=%d",
          b->name, count, offset, !!(flags & NBDKIT_FLAG_MAY_TRIM),

include/nbdkit-filter.h
@@ -45,6 +45,10 @@ extern "C" {
 
 #define NBDKIT_FILTER_API_VERSION 6 /* Corresponding to v1.16+ */
 
+#define NBDKIT_ZERO_NONE     0
+#define NBDKIT_ZERO_EMULATE  1
+#define NBDKIT_ZERO_NATIVE   2
+
 struct nbdkit_extent {
   uint64_t offset;
   uint64_t length;

server/internal.h
@@ -156,6 +156,7 @@ struct b_conn_handle {
 
   uint64_t exportsize;
   int can_write;
+  int can_zero;
   int can_extents;
   int can_cache;
 };
@@ -178,7 +179,6 @@ struct connection {
   bool can_flush;
   bool is_rotational;
   bool can_trim;
-  bool can_zero;
   bool can_fua;
   bool can_multi_conn;
   bool using_tls;

server/plugins.c
@@ -347,19 +347,23 @@ static int
 plugin_can_zero (struct backend *b, struct connection *conn)
 {
   struct backend_plugin *p = container_of (b, struct backend_plugin, backend);
+  int r;
 
   assert (connection_get_handle (conn, 0));
 
-  /* Note the special case here: the plugin's .can_zero controls only
-   * whether we call .zero; while the backend expects .can_zero to
-   * return whether to advertise zero support.  Since we ALWAYS know
-   * how to fall back to .pwrite in plugin_zero(), we ignore the
-   * difference between the plugin's true or false return, and only
-   * call it to catch a -1 failure during negotiation.  */
-  if (p->plugin.can_zero &&
-      p->plugin.can_zero (connection_get_handle (conn, 0)) == -1)
-    return -1;
-  return 1;
+  /* Note the special case here: the plugin's .can_zero returns a bool
+   * which controls only whether we call .zero; while the backend
+   * expects .can_zero to return a tri-state on level of support.
+   */
+  if (p->plugin.can_zero) {
+    r = p->plugin.can_zero (connection_get_handle (conn, 0));
+    if (r == -1)
+      return -1;
+    return r ? NBDKIT_ZERO_NATIVE : NBDKIT_ZERO_EMULATE;
+  }
+  if (p->plugin.zero || p->plugin._zero_old)
+    return NBDKIT_ZERO_NATIVE;
+  return NBDKIT_ZERO_EMULATE;
 }
 
 static int
@@ -562,7 +566,6 @@ plugin_zero (struct backend *b, struct connection *conn,
   bool fua = flags & NBDKIT_FLAG_FUA;
   bool emulate = false;
   bool need_flush = false;
-  int can_zero = 1; /* TODO cache this per-connection? */
 
   assert (connection_get_handle (conn, 0));
 
@@ -572,12 +575,8 @@ plugin_zero (struct backend *b, struct connection *conn,
   }
   if (!count)
     return 0;
-  if (p->plugin.can_zero) {
-    can_zero = p->plugin.can_zero (connection_get_handle (conn, 0));
-    assert (can_zero != -1);
-  }
 
-  if (can_zero) {
+  if (backend_can_zero (b, conn) == NBDKIT_ZERO_NATIVE) {
     errno = 0;
     if (p->plugin.zero)
       r = p->plugin.zero (connection_get_handle (conn, 0), count, offset,

server/protocol-handshake.c
@@ -64,10 +64,8 @@ protocol_compute_eflags (struct connection *conn, uint16_t *flags)
   fl = backend_can_zero (backend, conn);
   if (fl == -1)
     return -1;
-  if (fl) {
+  if (fl)
     eflags |= NBD_FLAG_SEND_WRITE_ZEROES;
-    conn->can_zero = true;
-  }
 
   fl = backend_can_trim (backend, conn);
   if (fl == -1)

commit 4aabc22f288c6ff8be1e775ec64bd6ba73a24f2f

server: Fix NBDKIT_ZERO_EMULATE from filters
When we turned the result of .can_zero into a tri-state for filters
back in v1.16, the intention was that the backend would emulate by
calling into .pwrite instead of .zero.  The nozero filter already had
an implementation of that algorithm, but it needs to live in backend.c
to be used by all filters, rather than repeatedly recoded in each
filter that wants .zero support by .pwrite emulation.

Discovered because the luks filter asked for .pwrite emulation in
.can_zero without providing a .zero override, resulting in:

$ qemu-img create -f luks --object secret,data=LETMEPASS,id=sec0 -o key-secret=sec0 encrypted.img 100M
Formatting 'encrypted.img', fmt=luks size=104857600 key-secret=sec0
$ rm -f data.img
$ truncate -s 100M data.img
$ nbdkit file encrypted.img --filter=luks passphrase=LETMEPASS --run 'nbdcopy data.img $nbd'
nbdkit: backend.c:718: backend_zero: Assertion `c->can_zero > NBDKIT_ZERO_NONE' failed.
write at offset 0 failed: Transport endpoint is not connected
nbdkit: nbdkit command was killed by signal 6

The ext2 filter has the same problem, except because it does have a
legitimate reason to call next->zero(), it was not hitting the
assertion failure, and so a client callng NBD_CMD_WRITE_ZEROES was
liable to write zeroes into the wrong portion of the underlying
filesystem, causing corruption.

As a result of moving it into the backend, the nozero filter is
simplified, but the corresponding test has to change.  Since
--filter=log does not alter .can_zero, the emulation actually occurs
earlier in the stack (prior to calling into any filter, regardless of
whether log is placed before or after nozero, rather than at the point
of the nozero filter), so there is now no discernable difference
between sock3 and sock4 (we can eliminate the duplicate), and the old
sock5a no longer shows a Zero request.  [Historically, when the nozero
filter was first written, we were relying on qemu to send packets, and
the use of the log filter was essential to ensure that we were getting
the desired NBD_CMD_WRITE_ZEROES across various versions of qemu; but
now that the test uses nbdsh, we know that h.zero() is doing exactly
that, so the reduced length of the testsuite is no longer risky.]

Reported-by: Richard W.M. Jones <rjones@redhat.com>
Fixes: fd361554 ("filters: Cache and change semantics of can_zero", v1.15.1)

docs/nbdkit-filter.pod
@@ -898,8 +898,8 @@ This intercepts the plugin C<.zero> method and can be used to modify
 zero requests.
 
 This function will not be called if C<.can_zero> returned
-C<NBDKIT_ZERO_NONE>; in turn, the filter should not call
-C<next-E<gt>zero> if C<next-E<gt>can_zero> returned
+C<NBDKIT_ZERO_NONE> or C<NBDKIT_ZERO_EMULATE>; in turn, the filter
+should not call C<next-E<gt>zero> if C<next-E<gt>can_zero> returned
 C<NBDKIT_ZERO_NONE>.
 
 On input, the parameter C<flags> may include C<NBDKIT_FLAG_MAY_TRIM>

filters/nozero/nozero.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -48,8 +48,6 @@
 #ifdef IGNORE
 #undef IGNORE
 #endif
-
-#define MAX_WRITE (64 * 1024 * 1024)
 
 static enum ZeroMode {
   NONE,
@@ -159,14 +157,10 @@ nozero_zero (nbdkit_next *next,
              void *handle, uint32_t count, uint64_t offs, uint32_t flags,
              int *err)
 {
-  int writeflags = 0;
-  bool need_flush = false;
-
-  assert (zeromode != NONE);
+  assert (zeromode != NONE && zeromode != EMULATE);
   if (flags & NBDKIT_FLAG_FAST_ZERO) {
     assert (fastzeromode != NOFAST);
-    if (fastzeromode == SLOW ||
-        (fastzeromode == DEFAULT && zeromode == EMULATE)) {
+    if (fastzeromode == SLOW) {
       *err = ENOTSUP;
       return -1;
     }
@@ -177,32 +171,7 @@ nozero_zero (nbdkit_next *next,
   if (zeromode == NOTRIM)
     flags &= ~NBDKIT_FLAG_MAY_TRIM;
 
-  if (zeromode != EMULATE)
-    return next->zero (next, count, offs, flags, err);
-
-  if (flags & NBDKIT_FLAG_FUA) {
-    if (next->can_fua (next) == NBDKIT_FUA_EMULATE)
-      need_flush = true;
-    else
-      writeflags = NBDKIT_FLAG_FUA;
-  }
-
-  while (count) {
-    /* Always contains zeroes, but we can't use const or else gcc 9
-     * will use .rodata instead of .bss and inflate the binary size.
-     */
-    static /* const */ char buffer[MAX_WRITE];
-    uint32_t size = MIN (count, MAX_WRITE);
-
-    if (size == count && need_flush)
-      writeflags = NBDKIT_FLAG_FUA;
-
-    if (next->pwrite (next, buffer, size, offs, writeflags, err) == -1)
-      return -1;
-    offs += size;
-    count -= size;
-  }
-  return 0;
+  return next->zero (next, count, offs, flags, err);
 }
 
 static struct nbdkit_filter filter = {

server/backend.c
@@ -728,7 +728,42 @@ backend_zero (struct context *c,
                   b->name, count, offset,
                   !!(flags & NBDKIT_FLAG_MAY_TRIM), fua, fast);
 
-  r = b->zero (c, count, offset, flags, err);
+  if (c->can_zero == NBDKIT_ZERO_NATIVE)
+    r = b->zero (c, count, offset, flags, err);
+  else { /* NBDKIT_ZERO_EMULATE */
+    int writeflags = 0;
+    bool need_flush = false;
+
+    if (fast) {
+      *err = ENOTSUP;
+      return -1;
+    }
+
+    if (fua) {
+      if (c->can_fua == NBDKIT_FUA_EMULATE)
+        need_flush = true;
+      else
+        writeflags = NBDKIT_FLAG_FUA;
+    }
+
+    while (count) {
+      /* Always contains zeroes, but we can't use const or else gcc 9
+       * will use .rodata instead of .bss and inflate the binary size.
+       */
+      static /* const */ char buffer[MAX_REQUEST_SIZE];
+      uint32_t limit = MIN (count, MAX_REQUEST_SIZE);
+
+      if (limit == count && need_flush)
+        writeflags = NBDKIT_FLAG_FUA;
+
+      if (backend_pwrite (c, buffer, limit, offset, writeflags, err) == -1)
+        return -1;
+      offset += limit;
+      count -= limit;
+    }
+    r = 0;
+  }
+
   if (r == -1) {
     assert (*err);
     if (!fast)

tests/test-nozero.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbdkit
-# Copyright (C) 2018-2020 Red Hat Inc.
+# Copyright (C) 2018-2022 Red Hat Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -36,17 +36,15 @@ set -x
 
 sock2=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
 sock3=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
-sock4=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
-sock5a=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
-sock5b=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
-sock6=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
+sock4a=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
+sock4b=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
+sock5=$(mktemp -u /tmp/nbdkit-test-sock.XXXXXX)
 files="nozero1.img nozero1.log
        nozero2.img nozero2.log $sock2 nozero2.pid
        nozero3.img nozero3.log $sock3 nozero3.pid
-       nozero4.img nozero4.log $sock4 nozero4.pid
-       nozero5.img nozero5a.log nozero5b.log $sock5a $sock5b
-       nozero5a.pid nozero5b.pid
-       nozero6.img nozero6.log $sock6 nozero6.pid"
+       nozero4.img nozero4a.log nozero4b.log $sock4a $sock4b
+       nozero4a.pid nozero4b.pid
+       nozero5.img nozero5.log $sock5 nozero5.pid"
 rm -f $files
 fail=0
 
@@ -60,14 +58,12 @@ cleanup ()
     cat nozero2.log || :
     echo "Log 3 file contents:"
     cat nozero3.log || :
-    echo "Log 4 file contents:"
-    cat nozero4.log || :
-    echo "Log 5a file contents:"
-    cat nozero5a.log || :
-    echo "Log 5b file contents:"
-    cat nozero5b.log || :
-    echo "Log 6 file contents:"
-    cat nozero6.log || :
+    echo "Log 4a file contents:"
+    cat nozero4a.log || :
+    echo "Log 4b file contents:"
+    cat nozero4b.log || :
+    echo "Log 5 file contents:"
+    cat nozero5.log || :
     rm -f $files
 }
 cleanup_fn cleanup
@@ -79,10 +75,9 @@ cp nozero1.img nozero2.img
 cp nozero1.img nozero3.img
 cp nozero1.img nozero4.img
 cp nozero1.img nozero5.img
-cp nozero1.img nozero6.img
 
 # Debug number of blocks and block size in the images.
-for f in {1..6}; do
+for f in {1..5}; do
     stat -c "%n: %b allocated blocks of size %B bytes, total size %s" \
          nozero$f.img
     sizes[$f]=$(stat -c %b nozero$f.img)
@@ -99,24 +94,21 @@ fi
 # Run several parallel nbdkit; to compare the logs and see what changes.
 # 1: unfiltered (above), check that nbdsh sends ZERO request and plugin trims
 # 2: log before filter with zeromode=none (default), to ensure no ZERO request
-# 3: log before filter with zeromode=emulate, to ensure ZERO from client
-# 4: log after filter with zeromode=emulate, to ensure no ZERO to plugin
-# 5a/b: both sides of nbd plugin: even though server side does not advertise
+# 3: log after filter with zeromode=emulate, to ensure no ZERO to plugin
+# 4a/b: both sides of nbd plugin: even though server side does not advertise
 # ZERO, the client side still exposes it, and just skips calling nbd's .zero
-# 6: log after filter with zeromode=notrim, to ensure plugin does not trim
+# 5: log after filter with zeromode=notrim, to ensure plugin does not trim
 start_nbdkit -P nozero2.pid -U $sock2 --filter=log --filter=nozero \
        file logfile=nozero2.log nozero2.img
-start_nbdkit -P nozero3.pid -U $sock3 --filter=log --filter=nozero \
+start_nbdkit -P nozero3.pid -U $sock3 --filter=nozero --filter=log \
        file logfile=nozero3.log nozero3.img zeromode=emulate
-start_nbdkit -P nozero4.pid -U $sock4 --filter=nozero --filter=log \
-       file logfile=nozero4.log nozero4.img zeromode=emulate
-# Start 5b before 5a so that cleanup visits the client before the server
-start_nbdkit -P nozero5b.pid -U $sock5b --filter=log \
-       nbd logfile=nozero5b.log socket=$sock5a
-start_nbdkit -P nozero5a.pid -U $sock5a --filter=log --filter=nozero \
-       file logfile=nozero5a.log nozero5.img
-start_nbdkit -P nozero6.pid -U $sock6 --filter=nozero --filter=log \
-       file logfile=nozero6.log nozero6.img zeromode=notrim
+# Start 4b before 4a so that cleanup visits the client before the server
+start_nbdkit -P nozero4b.pid -U $sock4b --filter=log \
+       nbd logfile=nozero4b.log socket=$sock4a
+start_nbdkit -P nozero4a.pid -U $sock4a --filter=log --filter=nozero \
+       file logfile=nozero4a.log nozero4.img
+start_nbdkit -P nozero5.pid -U $sock5 --filter=nozero --filter=log \
+       file logfile=nozero5.log nozero5.img zeromode=notrim
 
 # Perform the zero write.
 nbdsh -u "nbd+unix://?socket=$sock2" -c '
@@ -124,38 +116,34 @@ assert not h.can_zero()
 h.pwrite (bytearray(1024*1024), 0)
 '
 nbdsh -u "nbd+unix://?socket=$sock3" -c 'h.zero(1024*1024, 0)'
-nbdsh -u "nbd+unix://?socket=$sock4" -c 'h.zero(1024*1024, 0)'
-nbdsh -u "nbd+unix://?socket=$sock5b" -c 'h.zero(1024*1024, 0)'
-nbdsh -u "nbd+unix://?socket=$sock6" -c 'h.zero(1024*1024, 0)'
+nbdsh -u "nbd+unix://?socket=$sock4b" -c 'h.zero(1024*1024, 0)'
+nbdsh -u "nbd+unix://?socket=$sock5" -c 'h.zero(1024*1024, 0)'
 
 # Check for expected ZERO vs. WRITE results
-grep 'connection=1 Zero' nozero1.log
+grep 'connection=1 Zero' nozero1.log || fail=1
 if grep 'connection=1 Zero' nozero2.log; then
     echo "filter should have prevented zero"
     fail=1
 fi
-grep 'connection=1 Zero' nozero3.log
-if grep 'connection=1 Zero' nozero4.log; then
+if grep 'connection=1 Zero' nozero3.log; then
     echo "filter should have converted zero into write"
     fail=1
 fi
-grep 'connection=1 Zero' nozero5b.log
-if grep 'connection=1 Zero' nozero5a.log; then
+if grep 'connection=1 Zero' nozero4a.log; then
     echo "nbdkit should have converted zero into write before nbd plugin"
     fail=1
 fi
-grep 'connection=1 Zero' nozero6.log
+grep 'connection=1 Zero' nozero5.log
 
 # Sanity check on contents - all 5 files should read identically
 cmp nozero1.img nozero2.img
 cmp nozero2.img nozero3.img
 cmp nozero3.img nozero4.img
 cmp nozero4.img nozero5.img
-cmp nozero5.img nozero6.img
 
-# Sanity check on sparseness: images 2-6 should not be sparse (although the
+# Sanity check on sparseness: images 2-5 should not be sparse (although the
 # filesystem may have reserved additional space due to our writes)
-for i in {2..6}; do
+for i in {2..5}; do
     if test "$(stat -c %b nozero$i.img)" -lt "${sizes[$i]}"; then
         echo "nozero$i.img was trimmed by mistake"
         fail=1