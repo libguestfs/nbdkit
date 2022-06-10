libguestfs/nbdkit

commit 8e03612925b2c3ff2f6a41c1d4807451b744ec2b

Add new eval plugin.
This plugin allows you to write custom plugins as shell script
fragments which are ‘eval’d from the command line using /bin/sh.  It
shares most of the code from nbdkit-sh-plugin.

plugins/eval/eval.c
@@ -1,0 +1,423 @@
+/* nbdkit
+ * Copyright (C) 2018-2019 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#define NBDKIT_API_VERSION 2
+
+#include <nbdkit-plugin.h>
+
+#include "cleanup.h"
+
+#include "call.h"
+#include "methods.h"
+
+static char tmpdir[] = "/tmp/nbdkitevalXXXXXX";
+static char *missing;
+
+static const char *known_methods[] = {
+  "cache",
+  "can_cache",
+  "can_extents",
+  "can_fast_zero",
+  "can_flush",
+  "can_fua",
+  "can_multi_conn",
+  "can_trim",
+  "can_write",
+  "can_zero",
+  "close",
+  "config",
+  "config_complete",
+  "dump_plugin",
+  "extents",
+  "flush",
+  "get_size",
+  "is_rotational",
+  "open",
+  "pread",
+  "pwrite",
+  "thread_model",
+  "trim",
+  "unload",
+  "zero",
+  NULL
+};
+
+/* List of method scripts that we have saved.  This is stored in
+ * sorted order of method name.
+ */
+static size_t nr_method_scripts;
+static struct method_script {
+  const char *method;
+  char *script;
+} *method_scripts;
+
+static int
+compare_script (const void *methodvp, const void *entryvp)
+{
+  const char *method = methodvp;
+  const struct method_script *entry = entryvp;
+  return strcmp (method, entry->method);
+}
+
+static int
+insert_method_script (const char *method, char *script)
+{
+  struct method_script *newp;
+  size_t i;
+  int r;
+
+  newp = realloc (method_scripts,
+                  sizeof (struct method_script) * (nr_method_scripts + 1));
+  if (newp == NULL) {
+    nbdkit_error ("insert_method_script: realloc: %m");
+    return -1;
+  }
+  nr_method_scripts++;
+  method_scripts = newp;
+
+  for (i = 0; i < nr_method_scripts-1; ++i) {
+    r = compare_script (method, &method_scripts[i]);
+    /* This shouldn't happen.  insert_method_script() must not be
+     * called if the method has already been added.  Call get_script()
+     * first to check.
+     */
+    assert (r != 0);
+    if (r < 0) {
+      /* Insert before this element. */
+      memmove (&method_scripts[i+1], &method_scripts[i],
+               sizeof (struct method_script) * (nr_method_scripts-i-1));
+      method_scripts[i].method = method;
+      method_scripts[i].script = script;
+      return 0;
+    }
+  }
+
+  /* Insert at end of list. */
+  method_scripts[i].method = method;
+  method_scripts[i].script = script;
+  return 0;
+}
+
+/* This is called back by methods.c to get the current script name. */
+const char *
+get_script (const char *method)
+{
+  struct method_script *p;
+
+  p = bsearch (method,
+               method_scripts,
+               nr_method_scripts, sizeof (struct method_script),
+               compare_script);
+  if (p)
+    return p->script;
+  else
+    return missing;
+}
+
+/* Save a script into tmpdir.  Return its full path (must be freed by
+ * the caller).
+ */
+static char *
+create_script (const char *method, const char *value)
+{
+  FILE *fp;
+  char *script;
+
+  if (asprintf (&script, "%s/%s", tmpdir, method) == -1) {
+    nbdkit_error ("asprintf: %m");
+    return NULL;
+  }
+
+  fp = fopen (script, "w");
+  if (fp == NULL) {
+    nbdkit_error ("fopen: %s: %m", script);
+    free (script);
+    return NULL;
+  }
+  if (fwrite (value, strlen (value), 1, fp) != 1) {
+    nbdkit_error ("fwrite: %s: %m", script);
+    fclose (fp);
+    free (script);
+    return NULL;
+  }
+
+  if (fchmod (fileno (fp), 0500) == -1) {
+    nbdkit_error ("fchmod: %s: %m", script);
+    fclose (fp);
+    free (script);
+    return NULL;
+  }
+
+  if (fclose (fp) == EOF) {
+    nbdkit_error ("fclose: %s: %m", script);
+    free (script);
+    return NULL;
+  }
+
+  return script;
+}
+
+static void
+eval_load (void)
+{
+  /* Create the temporary directory for the shell script to use. */
+  if (mkdtemp (tmpdir) == NULL) {
+    nbdkit_error ("mkdtemp: /tmp: %m");
+    exit (EXIT_FAILURE);
+  }
+  /* Set $tmpdir for the script. */
+  if (setenv ("tmpdir", tmpdir, 1) == -1) {
+    nbdkit_error ("setenv: tmpdir=%s: %m", tmpdir);
+    exit (EXIT_FAILURE);
+  }
+
+  nbdkit_debug ("eval: load: tmpdir: %s", tmpdir);
+
+  /* To make things easier, create a "missing" script which always
+   * exits with code 2.  If a method is missing we call this script
+   * instead.  It could even be overridden by the user.
+   */
+  missing = create_script ("missing", "exit 2\n");
+  if (!missing)
+    exit (EXIT_FAILURE);
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-result"
+static void
+eval_unload (void)
+{
+  const char *method = "unload";
+  const char *script = get_script (method);
+  CLEANUP_FREE char *cmd = NULL;
+  size_t i;
+
+  /* Run the unload method.  Ignore all errors. */
+  if (script) {
+    const char *args[] = { script, method, NULL };
+
+    call (args);
+  }
+
+  /* Delete the temporary directory.  Ignore all errors. */
+  if (asprintf (&cmd, "rm -rf %s", tmpdir) >= 0)
+    system (cmd);
+
+  for (i = 0; i < nr_method_scripts; ++i)
+    free (method_scripts[i].script);
+  free (method_scripts);
+  free (missing);
+}
+#pragma GCC diagnostic pop
+
+static int
+add_method (const char *key, const char *value)
+{
+  char *script;
+
+  if (get_script (key) != missing) {
+    nbdkit_error ("method %s defined more than once on the command line", key);
+    return -1;
+  }
+
+  /* Do a bit of checking to make sure the key isn't malicious.  This
+   * duplicates work already done by nbdkit, but better safe than
+   * sorry.
+   */
+  if (strchr (key, '.') || strchr (key, '/')) {
+    nbdkit_error ("method name %s is invalid", key);
+    return -1;
+  }
+
+  /* Copy the value into a script in tmpdir. */
+  script = create_script (key, value);
+  if (!script)
+    return -1;
+
+  /* After this, the script variable will be stored in the global
+   * array and freed on unload.
+   */
+  return insert_method_script (key, script);
+}
+
+static int
+eval_config (const char *key, const char *value)
+{
+  size_t i;
+
+  /* Try to determine if this is a method or a user parameter. */
+  for (i = 0; known_methods[i] != NULL; ++i) {
+    if (strcmp (key, known_methods[i]) == 0)
+      return add_method (key, value);
+  }
+
+  /* User parameter, so call config. */
+  const char *method = "config";
+  const char *script = get_script (method);
+  const char *args[] = { script, method, key, value, NULL };
+
+  switch (call (args)) {
+  case OK:
+    return 0;
+
+  case MISSING:
+    /* Emulate what core nbdkit does if a config callback is NULL. */
+    nbdkit_error ("%s: this plugin does not need command line configuration",
+                  script);
+    return -1;
+
+  case ERROR:
+    return -1;
+
+  case RET_FALSE:
+    nbdkit_error ("%s: %s method returned unexpected code (3/false)",
+                  script, "config");
+    errno = EIO;
+    return -1;
+
+  default: abort ();
+  }
+}
+
+static int
+create_can_wrapper (const char *test_method, const char *can_method)
+{
+  char *can_script;
+
+  if (get_script (test_method) != missing &&
+      get_script (can_method) == missing) {
+    can_script = create_script (can_method, "exit 0\n");
+    if (!can_script)
+      return -1;
+    return insert_method_script (can_method, can_script);
+  }
+
+  return 0;
+}
+
+static int
+eval_config_complete (void)
+{
+  const char *method = "config_complete";
+  const char *script = get_script (method);
+  const char *args[] = { script, method, NULL };
+
+  /* Synthesize can_* scripts as the core nbdkit server would for C
+   * plugins.
+   */
+  if (create_can_wrapper ("pwrite",  "can_write") == -1 ||
+      create_can_wrapper ("flush",   "can_flush") == -1 ||
+      create_can_wrapper ("trim",    "can_trim") == -1 ||
+      create_can_wrapper ("zero",    "can_zero") == -1 ||
+      create_can_wrapper ("extents", "can_flush") == -1)
+    return -1;
+
+  /* Call config_complete. */
+  switch (call (args)) {
+  case OK:
+  case MISSING:
+    return 0;
+
+  case ERROR:
+    return -1;
+
+  case RET_FALSE:
+    nbdkit_error ("%s: %s method returned unexpected code (3/false)",
+                  script, method);
+    errno = EIO;
+    return -1;
+
+  default: abort ();
+  }
+
+  return 0;
+}
+
+#define eval_config_help \
+  "get_size=' SCRIPT '\n" \
+  "pread=' SCRIPT '\n" \
+  "[etc]"
+
+/* See also the comments in call.c:call3() */
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
+static struct nbdkit_plugin plugin = {
+  .name              = "eval",
+  .version           = PACKAGE_VERSION,
+  .load              = eval_load,
+  .unload            = eval_unload,
+
+  .dump_plugin       = sh_dump_plugin,
+
+  .config            = eval_config,
+  .config_complete   = eval_config_complete,
+  .config_help       = eval_config_help,
+  .thread_model      = sh_thread_model,
+
+  .open              = sh_open,
+  .close             = sh_close,
+
+  .get_size          = sh_get_size,
+  .can_write         = sh_can_write,
+  .can_flush         = sh_can_flush,
+  .is_rotational     = sh_is_rotational,
+  .can_trim          = sh_can_trim,
+  .can_zero          = sh_can_zero,
+  .can_extents       = sh_can_extents,
+  .can_fua           = sh_can_fua,
+  .can_multi_conn    = sh_can_multi_conn,
+  .can_cache         = sh_can_cache,
+  .can_fast_zero     = sh_can_fast_zero,
+
+  .pread             = sh_pread,
+  .pwrite            = sh_pwrite,
+  .flush             = sh_flush,
+  .trim              = sh_trim,
+  .zero              = sh_zero,
+  .extents           = sh_extents,
+  .cache             = sh_cache,
+
+  .errno_is_preserved = 1,
+};
+
+NBDKIT_REGISTER_PLUGIN(plugin)

.gitignore
@@ -14,6 +14,7 @@
 *.trs
 
 .deps
+.dirstamp
 .gdb_history
 .libs
 Makefile

configure.ac
@@ -41,7 +41,7 @@ AC_USE_SYSTEM_EXTENSIONS
 AC_SYS_LARGEFILE
 
 dnl NB: Do not [quote] this parameter.
-AM_INIT_AUTOMAKE(foreign)
+AM_INIT_AUTOMAKE(foreign subdir-objects)
 AC_PROG_LIBTOOL
 LT_INIT
 
@@ -861,6 +861,7 @@ lang_plugins="\
 non_lang_plugins="\
         curl \
         data \
+        eval \
         example1 \
         example2 \
         example3 \
@@ -938,6 +939,7 @@ AC_CONFIG_FILES([Makefile
                  plugins/Makefile
                  plugins/curl/Makefile
                  plugins/data/Makefile
+                 plugins/eval/Makefile
                  plugins/example1/Makefile
                  plugins/example2/Makefile
                  plugins/example3/Makefile

plugins/eval/Makefile.am
@@ -1,5 +1,5 @@
 # nbdkit
-# Copyright (C) 2018 Red Hat Inc.
+# Copyright (C) 2017-2019 Red Hat Inc.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -31,43 +31,42 @@
 
 include $(top_srcdir)/common-rules.mk
 
-EXTRA_DIST = \
-	nbdkit-sh-plugin.pod \
-	assemble.sh \
-	example.sh \
-	$(NULL)
+EXTRA_DIST = nbdkit-eval-plugin.pod
 
-plugin_LTLIBRARIES = nbdkit-sh-plugin.la
+plugin_LTLIBRARIES = nbdkit-eval-plugin.la
 
-nbdkit_sh_plugin_la_SOURCES = \
-	call.c \
-	call.h \
-	methods.c \
-	methods.h \
-	sh.c \
+# This plugin shares most of the same sources as nbdkit-sh-plugin.
+
+nbdkit_eval_plugin_la_SOURCES = \
+	$(srcdir)/../sh/call.c \
+	$(srcdir)/../sh/call.h \
+	$(srcdir)/../sh/methods.c \
+	$(srcdir)/../sh/methods.h \
+	eval.c \
 	$(top_srcdir)/include/nbdkit-plugin.h \
 	$(NULL)
-
-nbdkit_sh_plugin_la_CPPFLAGS = \
+nbdkit_eval_plugin_la_CPPFLAGS = \
+	-I$(top_srcdir)/plugins/sh \
 	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include \
 	-I$(top_srcdir)/common/utils \
 	$(NULL)
-nbdkit_sh_plugin_la_CFLAGS = $(WARNINGS_CFLAGS)
-nbdkit_sh_plugin_la_LIBADD = \
+nbdkit_eval_plugin_la_CFLAGS = $(WARNINGS_CFLAGS)
+nbdkit_eval_plugin_la_LIBADD = \
 	$(top_builddir)/common/utils/libutils.la \
 	$(NULL)
-nbdkit_sh_plugin_la_LDFLAGS = \
+nbdkit_eval_plugin_la_LDFLAGS = \
 	-module -avoid-version -shared \
 	-Wl,--version-script=$(top_srcdir)/plugins/plugins.syms \
 	$(NULL)
 
 if HAVE_POD
 
-man_MANS = nbdkit-sh-plugin.3
+man_MANS = nbdkit-eval-plugin.1
 CLEANFILES += $(man_MANS)
 
-nbdkit-sh-plugin.3: nbdkit-sh-plugin.pod
-	$(PODWRAPPER) --section=3 --man $@ \
+nbdkit-eval-plugin.1: nbdkit-eval-plugin.pod
+	$(PODWRAPPER) --section=1 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 

plugins/eval/nbdkit-eval-plugin.pod
@@ -1,0 +1,182 @@
+=head1 NAME
+
+nbdkit-eval-plugin - write a shell script plugin on the command line
+
+=head1 SYNOPSIS
+
+ nbdkit eval get_size='SCRIPT' pread='SCRIPT' pwrite='SCRIPT' [...]
+
+=head1 DESCRIPTION
+
+C<nbdkit-eval-plugin> is an L<nbdkit(1)> plugin which allows you to
+write custom plugins as shell scripts snippets ‘eval’d on the command
+line.
+
+A common alternative to this plugin is L<nbdkit-sh-plugin(1)>.  Both
+plugins share the same source code and work in almost the same way.
+You should read L<nbdkit-sh-plugin(1)> first.  It is easier to
+describe the differences between the two plugins and look at the
+examples below.
+
+=over 4
+
+=item *
+
+L<nbdkit-sh-plugin(1)> plugins are written as a single script in a
+separate file.  Eval plugins are shell script fragments written on the
+nbdkit command line — there is no separate script file.
+
+=item *
+
+L<nbdkit-sh-plugin(1)> has no way to know if a method is missing or
+not and so each C<can_*> method (eg. C<can_write>) must be written
+explicitly.  In eval plugins you have the option of omitting C<can_*>
+methods if the associated callback (eg. C<pwrite>) is defined.  In
+this way eval plugins work more like regular nbdkit plugins.
+
+=item *
+
+Eval plugins can only use F</bin/sh> to run the script snippets, but
+L<nbdkit-sh-plugin(1)> (in spite of the name) can run any executable.
+
+=item *
+
+There is no C<load> method (although there is an C<unload> method and
+all other methods are identical).
+
+=back
+
+=head1 EXAMPLES
+
+Create a 64M read-only disk of zeroes:
+
+ nbdkit eval get_size=' echo 64M ' \
+                pread=' dd if=/dev/zero count=$3 iflag=count_bytes '
+
+The following command is the eval plugin equivalent of
+L<nbdkit-file-plugin(1)> (except not as fast and missing many
+features):
+
+ nbdkit eval \
+   config='ln -sf "$(realpath "$3")" $tmpdir/file' \
+   get_size='stat -Lc %s $tmpdir/file' \
+   pread='dd if=$tmpdir/file skip=$4 count=$3 iflag=count_bytes,skip_bytes' \
+   pwrite='dd of=$tmpdir/file seek=$4 conv=notrunc oflag=seek_bytes' \
+   file=disk.img
+
+=head1 PARAMETERS
+
+=over 4
+
+=item B<cache=>SCRIPT
+
+=item B<can_cache=>SCRIPT
+
+=item B<can_extents=>SCRIPT
+
+=item B<can_fast_zero=>SCRIPT
+
+=item B<can_flush=>SCRIPT
+
+=item B<can_fua=>SCRIPT
+
+=item B<can_multi_conn=>SCRIPT
+
+=item B<can_trim=>SCRIPT
+
+=item B<can_write=>SCRIPT
+
+=item B<can_zero=>SCRIPT
+
+=item B<close=>SCRIPT
+
+=item B<config=>SCRIPT
+
+=item B<config_complete=>SCRIPT
+
+=item B<dump_plugin=>SCRIPT
+
+=item B<extents=>SCRIPT
+
+=item B<flush=>SCRIPT
+
+=item B<get_size=>SCRIPT
+
+=item B<is_rotational=>SCRIPT
+
+=item B<open=>SCRIPT
+
+=item B<pread=>SCRIPT
+
+=item B<pwrite=>SCRIPT
+
+=item B<thread_model=>SCRIPT
+
+=item B<trim=>SCRIPT
+
+=item B<unload=>SCRIPT
+
+=item B<zero=>SCRIPT
+
+Define the script associated with each method.  C<SCRIPT> is a
+fragment of shell script which is executed when nbdkit wants to invoke
+the associated method.
+
+If you are typing these commands at the shell, be careful about
+quoting.  Normally you will need to enclose C<SCRIPT> in C<'...'>
+(single quotes) to prevent it from being modified by your shell.
+
+The script fragment behaves the same way as the corresponding method
+in L<nbdkit-sh-plugin(1)>.  In particular, parameters are identical,
+C<$tmpdir> is present and used in the same way, the exit code must be
+one of the valid exit codes described in that manual page, and error
+handling works the same way too.
+
+All of these parameters are optional.
+
+=back
+
+=head1 ENVIRONMENT VARIABLES
+
+=over 4
+
+=item C<tmpdir>
+
+This is defined to the name of a temporary directory which can be used
+by the script snippets.  It is deleted when nbdkit exits.
+
+=back
+
+=head1 FILES
+
+=over 4
+
+=item F</bin/sh>
+
+Shell script fragments are executed using F</bin/sh>.
+
+=item F<$plugindir/nbdkit-eval-plugin.so>
+
+The plugin.
+
+Use C<nbdkit --dump-config> to find the location of C<$plugindir>.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-eval-plugin> first appeared in nbdkit 1.18.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<nbdkit-sh-plugin(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2019 Red Hat Inc.

plugins/sh/eval.sh
@@ -1,61 +1,0 @@
-#!/usr/bin/env bash
-#
-# Example plugin for nbdkit which lets you write a shell script plugin
-# entirely on the nbdkit command line.
-#
-# This example can be freely used for any purpose.
-#
-# Run it from the build directory like this:
-#
-#   ./nbdkit sh ./plugins/sh/eval.sh \
-#     get_size='echo 64M' \
-#     pread='dd if=/dev/zero count=$3 iflag=count_bytes' \
-#     pwrite='echo ENOSPC Out of space >&2; exit 1'
-#
-# which creates a 64M disk of zeroes that gives an ENOSPC error when
-# written to.
-
-# Check we're being run from nbdkit.
-if [ ! -d $tmpdir ] || [ "x$1" = "x" ]; then
-    echo "$0: this script must be run from nbdkit" >&2
-    echo "Use ‘nbdkit sh $0’" >&2
-    exit 1
-fi
-
-function create_can_wrapper
-{
-    if [ -f $tmpdir/$1 ] && [ ! -f $tmpdir/$2 ]; then
-        echo 'exit 0' > $tmpdir/$2
-        chmod +x $tmpdir/$2
-    fi
-}
-
-case "$1" in
-    config)
-        # Save each parameter to a separate file under $tmpdir.
-        # Security is a thing.  We're relying on the fact that
-        # parameter keys in nbdkit are only allowed to use a limited
-        # range of characters.
-        echo "$3" > $tmpdir/$2
-        chmod +x $tmpdir/$2
-        ;;
-    config_complete)
-        # For convenience create can_* wrappers corresponding to
-        # certain scripts, but only if they don't exist already.
-        create_can_wrapper pwrite  can_write
-        create_can_wrapper flush   can_flush
-        create_can_wrapper trim    can_trim
-        create_can_wrapper zero    can_zero
-        create_can_wrapper extents can_extents
-        ;;
-
-    *)
-        # If the script corresponding to this method exists, run it,
-        # otherwise exit with 2 (missing method).
-        if [ -x $tmpdir/$1 ]; then
-            exec $tmpdir/$1 "$@"
-        else
-            exit 2
-        fi
-        ;;
-esac

plugins/sh/nbdkit-sh-plugin.pod
@@ -15,10 +15,14 @@ nbdkit-sh-plugin - nbdkit shell, script or executable plugin
 C<nbdkit-sh-plugin> allows you to write plugins for L<nbdkit(1)> using
 arbitrary scripting languages, including shells like L<bash(1)>,
 L<dash(1)>, L<csh(1)>, L<zsh(1)> etc., other scripting environments,
-or any executable.  Note if you want to use an established scripting
-language like Perl or Python, then nbdkit has specific plugins to
-handle those languages and those will be more efficient (see
-L<nbdkit(1)> for a complete list).
+or any executable.
+
+Note if you want to use an established scripting language like Perl or
+Python, then nbdkit has specific plugins to handle those languages and
+those will be more efficient (see L<nbdkit(1)> for a complete list).
+
+To use shell script fragments from the nbdkit command line (rather
+than a separate script) see L<nbdkit-eval-plugin(1)>.
 
 =head2 If you have been given an nbdkit sh plugin
 
@@ -454,7 +458,8 @@ C<nbdkit-sh-plugin> first appeared in nbdkit 1.8.
 =head1 SEE ALSO
 
 L<nbdkit(1)>,
-L<nbdkit-plugin(3)>.
+L<nbdkit-plugin(3)>,
+L<nbdkit-eval-plugin(1)>.
 
 =head1 AUTHORS
 

tests/Makefile.am
@@ -65,6 +65,7 @@ EXTRA_DIST = \
 	test-error10.sh \
 	test-error100.sh \
 	test-error-triggered.sh \
+	test-eval.sh \
 	test-export-name.sh \
 	test-file-extents.sh \
 	test-floppy.sh \
@@ -488,6 +489,9 @@ test_data_SOURCES = test-data.c test.h
 test_data_CPPFLAGS = -I$(top_srcdir)/common/include
 test_data_CFLAGS = $(WARNINGS_CFLAGS) $(LIBGUESTFS_CFLAGS)
 test_data_LDADD = libtest.la $(LIBGUESTFS_LIBS)
+
+# eval plugin test.
+TESTS += test-eval.sh
 
 # ext2 plugin test.
 if HAVE_EXT2

commit b4b8bd78ee66e5e1bc3d9b5464b10be0719445f1

eval: Provide default for .can_cache
While the sh plugin truly cannot provide a default for any of the
.can_* callbacks, the eval plugin can do it for most of them (.can_fua
and .can_multi_conn being notable exceptions).  However, we missed
providing a sane default for .can_cache.

Fixes: 8e036129 ("Add new eval plugin.", v1.17.1)

plugins/eval/eval.c
@@ -321,13 +321,14 @@ eval_config (const char *key, const char *value)
 }
 
 static int
-create_can_wrapper (const char *test_method, const char *can_method)
+create_can_wrapper (const char *test_method, const char *can_method,
+                    const char *content)
 {
   char *can_script;
 
   if (get_script (test_method) != missing &&
       get_script (can_method) == missing) {
-    can_script = create_script (can_method, "exit 0");
+    can_script = create_script (can_method, content);
     if (!can_script)
       return -1;
     return insert_method_script (can_method, can_script);
@@ -346,11 +347,12 @@ eval_config_complete (void)
   /* Synthesize can_* scripts as the core nbdkit server would for C
    * plugins.
    */
-  if (create_can_wrapper ("pwrite",  "can_write") == -1 ||
-      create_can_wrapper ("flush",   "can_flush") == -1 ||
-      create_can_wrapper ("trim",    "can_trim") == -1 ||
-      create_can_wrapper ("zero",    "can_zero") == -1 ||
-      create_can_wrapper ("extents", "can_extents") == -1)
+  if (create_can_wrapper ("pwrite",  "can_write",   "exit 0") == -1 ||
+      create_can_wrapper ("flush",   "can_flush",   "exit 0") == -1 ||
+      create_can_wrapper ("trim",    "can_trim",    "exit 0") == -1 ||
+      create_can_wrapper ("zero",    "can_zero",    "exit 0") == -1 ||
+      create_can_wrapper ("extents", "can_extents", "exit 0") == -1 ||
+      create_can_wrapper ("cache",   "can_cache",   "echo native") == -1)
     return -1;
 
   /* Call config_complete. */

tests/test-eval-cache.sh
@@ -30,7 +30,7 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-# Check that NBDKIT_CACHE_EMULATE works on large requests.
+# Check various .cache scenarios using eval
 
 source ./functions.sh
 set -e
@@ -82,3 +82,9 @@ nbdkit -U - -v eval \
       fi
       dd if=/dev/zero count=$3 iflag=count_bytes
     ' --run 'nbdsh -u "$uri" -c "$script"'
+
+# This plugin provides .cache but not .can_cache; eval should synthesize one.
+nbdkit -U - -v eval \
+    get_size='echo 1M' cache='exit 0' pread='echo EIO >&2; exit 1' \
+    --run 'nbdsh -u "$uri" -c "assert h.can_cache()" \
+      -c "h.cache(1024*1024, 0)"'