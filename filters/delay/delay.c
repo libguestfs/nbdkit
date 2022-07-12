libguestfs/nbdkit

commit 58187831a4346b44e398f105163abac8f3dfb7f0

delay: Improve parsing and representation of delay times
We used sscanf to parse the delay settings for both NNms and NN
(seconds).  In the first case we need to use sscanf otherwise the code
is awkward.  In the second case using sscanf meant that bogus suffices
were not ignored, eg:

  $ nbdkit null --filter=delay delay-open=10SECS --run 'nbdinfo $uri'

Use nbdkit_parse_unsigned instead.  This now results in an error:

  nbdkit: error: delay-open: could not parse number: "10SECS": trailing garbage

Also, previously we stored delay times as an int.  This makes no sense
since they cannot be negative, so use an unsigned instead.

Reported-by: Ming Xie

filters/delay/delay.c
@@ -42,25 +42,28 @@
 
 #include <nbdkit-filter.h>
 
-static int delay_read_ms = 0;   /* read delay (milliseconds) */
-static int delay_write_ms = 0;  /* write delay (milliseconds) */
-static int delay_zero_ms = 0;   /* zero delay (milliseconds) */
-static int delay_trim_ms = 0;   /* trim delay (milliseconds) */
-static int delay_extents_ms = 0;/* extents delay (milliseconds) */
-static int delay_cache_ms = 0;  /* cache delay (milliseconds) */
+static unsigned delay_read_ms = 0;   /* read delay (milliseconds) */
+static unsigned delay_write_ms = 0;  /* write delay (milliseconds) */
+static unsigned delay_zero_ms = 0;   /* zero delay (milliseconds) */
+static unsigned delay_trim_ms = 0;   /* trim delay (milliseconds) */
+static unsigned delay_extents_ms = 0;/* extents delay (milliseconds) */
+static unsigned delay_cache_ms = 0;  /* cache delay (milliseconds) */
+static unsigned delay_open_ms = 0;   /* open delay (milliseconds) */
+static unsigned delay_close_ms = 0;  /* close delay (milliseconds) */
+
 static int delay_fast_zero = 1; /* whether delaying zero includes fast zero */
-static int delay_open_ms = 0;   /* open delay (milliseconds) */
-static int delay_close_ms = 0;  /* close delay (milliseconds) */
 
 static int
-parse_delay (const char *key, const char *value)
+parse_delay (const char *key, const char *value, unsigned *r)
 {
   size_t len = strlen (value);
-  int r;
 
   if (len > 2 && strcmp (&value[len-2], "ms") == 0) {
-    if (sscanf (value, "%d", &r) == 1 && r >= 0)
-      return r;
+    /* We have to use sscanf here instead of nbdkit_parse_unsigned
+     * because that function will reject the "ms" suffix.
+     */
+    if (sscanf (value, "%u", r) == 1)
+      return 0;
     else {
       nbdkit_error ("cannot parse %s in milliseconds parameter: %s",
                     key, value);
@@ -68,24 +71,19 @@ parse_delay (const char *key, const char *value)
     }
   }
   else {
-    if (sscanf (value, "%d", &r) == 1 && r >= 0) {
-      if (r * 1000LL > INT_MAX) {
-        nbdkit_error ("seconds parameter %s is too large: %s",
-                      key, value);
-        return -1;
-      }
-      return r * 1000;
-    }
-    else {
-      nbdkit_error ("cannot parse %s in seconds parameter: %s",
-                    key, value);
+    if (nbdkit_parse_unsigned (key, value, r) == -1)
+      return -1;
+    if (*r * 1000U > UINT_MAX) {
+      nbdkit_error ("seconds parameter %s is too large: %s", key, value);
       return -1;
     }
+    *r *= 1000;
+    return 0;
   }
 }
 
 static int
-delay (int ms, int *err)
+delay (unsigned ms, int *err)
 {
   if (ms > 0 && nbdkit_nanosleep (ms / 1000, (ms % 1000) * 1000000) == -1) {
     *err = errno;
@@ -150,14 +148,12 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
   if (strcmp (key, "rdelay") == 0 ||
       strcmp (key, "delay-read") == 0 ||
       strcmp (key, "delay-reads") == 0) {
-    delay_read_ms = parse_delay (key, value);
-    if (delay_read_ms == -1)
+    if (parse_delay (key, value, &delay_read_ms) == -1)
       return -1;
     return 0;
   }
   else if (strcmp (key, "wdelay") == 0) {
-    delay_write_ms = parse_delay (key, value);
-    if (delay_write_ms == -1)
+    if (parse_delay (key, value, &delay_write_ms) == -1)
       return -1;
     /* Historically wdelay set all write-related delays. */
     delay_zero_ms = delay_trim_ms = delay_write_ms;
@@ -165,15 +161,13 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
   }
   else if (strcmp (key, "delay-write") == 0 ||
            strcmp (key, "delay-writes") == 0) {
-    delay_write_ms = parse_delay (key, value);
-    if (delay_write_ms == -1)
+    if (parse_delay (key, value, &delay_write_ms) == -1)
       return -1;
     return 0;
   }
   else if (strcmp (key, "delay-zero") == 0 ||
            strcmp (key, "delay-zeroes") == 0) {
-    delay_zero_ms = parse_delay (key, value);
-    if (delay_zero_ms == -1)
+    if (parse_delay (key, value, &delay_zero_ms) == -1)
       return -1;
     return 0;
   }
@@ -181,21 +175,18 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
            strcmp (key, "delay-trims") == 0 ||
            strcmp (key, "delay-discard") == 0 ||
            strcmp (key, "delay-discards") == 0) {
-    delay_trim_ms = parse_delay (key, value);
-    if (delay_trim_ms == -1)
+    if (parse_delay (key, value, &delay_trim_ms) == -1)
       return -1;
     return 0;
   }
   else if (strcmp (key, "delay-extent") == 0 ||
            strcmp (key, "delay-extents") == 0) {
-    delay_extents_ms = parse_delay (key, value);
-    if (delay_extents_ms == -1)
+    if (parse_delay (key, value, &delay_extents_ms) == -1)
       return -1;
     return 0;
   }
   else if (strcmp (key, "delay-cache") == 0) {
-    delay_cache_ms = parse_delay (key, value);
-    if (delay_cache_ms == -1)
+    if (parse_delay (key, value, &delay_cache_ms) == -1)
       return -1;
     return 0;
   }
@@ -206,14 +197,12 @@ delay_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
     return 0;
   }
   else if (strcmp (key, "delay-open") == 0) {
-    delay_open_ms = parse_delay (key, value);
-    if (delay_open_ms == -1)
+    if (parse_delay (key, value, &delay_open_ms) == -1)
       return -1;
     return 0;
   }
   else if (strcmp (key, "delay-close") == 0) {
-    delay_close_ms = parse_delay (key, value);
-    if (delay_close_ms == -1)
+    if (parse_delay (key, value, &delay_close_ms) == -1)
       return -1;
     return 0;
   }

commit 06bab8763e8ca2cc8bb6782c556bbdd4adcfbac6

delay: Fix bounds check when parsing delay parameters
Previously the check always evaluated to false.  This was because
'*r', '1000U' and UINT_MAX are all unsigned, so the multiplication
would simply wrap around.  Casting one parameter to be 64 bit should
cause everything to be promoted, fixing the check.

This bug was accidentally introduced in commit 58187831a4 ("delay:
Improve parsing and representation of delay times").  Prior to this
commit the code used signed ints and 1000LL.

Found by Coverity:

  Error: CONSTANT_EXPRESSION_RESULT (CWE-569): [#def46]
  nbdkit-1.30.7/filters/delay/delay.c:77: result_independent_of_operands: "*r * 1000U > 4294967295U /* 2147483647 * 2U + 1U */" is always false regardless of the values of its operands. This occurs as the logical operand of "if".
  #   75|       if (nbdkit_parse_unsigned (key, value, r) == -1)
  #   76|         return -1;
  #   77|->     if (*r * 1000U > UINT_MAX) {
  #   78|         nbdkit_error ("seconds parameter %s is too large: %s", key, value);
  #   79|         return -1;

Fixes: commit 58187831a4346b44e398f105163abac8f3dfb7f0

filters/delay/delay.c
@@ -74,7 +74,7 @@ parse_delay (const char *key, const char *value, unsigned *r)
   else {
     if (nbdkit_parse_unsigned (key, value, r) == -1)
       return -1;
-    if (*r * 1000U > UINT_MAX) {
+    if (*r * UINT64_C(1000) > UINT_MAX) {
       nbdkit_error ("seconds parameter %s is too large: %s", key, value);
       return -1;
     }