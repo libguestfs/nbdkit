libguestfs/nbdkit

commit 4871e496b95403679e044c69a12bf28bc05b72c5

server: Add and use nbdkit_nanosleep
There are a couple of problems with filters trying to sleep. First,
when it is time to shut down nbdkit, we wait until all pending
transactions have had a chance to wind down.  But consider what
happens if one or more of those pending transactions are blocked in a
sleep.  POSIX says nanosleep is interrupted with EINTR if that thread
handles a signal, but wiring up signal masks just to ensure a specific
thread will get the signal is not pretty, and that means the thread
processing SIGINT is NOT the thread blocked in nanosleep.  Couple that
with the fact that if multiple threads are sleeping, only one thread
needs to process the signal, so the rest continue to sleep.  Thus,
even though we know the user wants nbdkit to quit, we are blocked
waiting for a potentially long sleep to complete.  This problem can be
solved by realizing we already have a pipe-to-self to learn about a
quit request or the end of a particular connection, and check for
activities on those pipes in parallel with a timeout through pselect
or ppoll to break our wait as soon as we know there is no reason to
continue on with the transaction.

Second, if we ever add support for a signal that is not fatal to
nbdkit (such as SIGHUP triggering a config reload), but don't mask
which thread can process that signal, then if a thread servicing
nanosleep gets the signal the sleep will terminate early.  But in that
case, we want execution to continue on.  This problem can be solved by
either setting a signal mask so as to avoid processing our non-fatal
signal in the same thread as any filter/plugin processing, or by
paying attention to EINTR results and restarting a sleep.  And rather
than making each sleeping client reimplement that logic, we might as
well stick it in on our common helper.

Thus, this patch introduces nbdkit_nanosleep, and implements it under
the hood as a ppoll that redirects signals but wakes up as needed.

server/public.c
@@ -47,6 +47,8 @@
 #include <limits.h>
 #include <termios.h>
 #include <errno.h>
+#include <poll.h>
+#include <signal.h>
 
 #include "get-current-dir-name.h"
 
@@ -296,4 +298,74 @@ nbdkit_realpath (const char *path)
   }
 
   return ret;
+}
+
+
+int
+nbdkit_nanosleep (unsigned sec, unsigned nsec)
+{
+#if defined HAVE_PPOLL && defined POLLRDHUP
+  /* End the sleep early if any of these happen:
+   * - nbdkit has received a signal to shut down the server
+   * - the current connection is multi-threaded and another thread detects
+   *   NBD_CMD_DISC or a problem with the connection
+   * - the input socket detects POLLRDHUP/POLLHUP/POLLERR
+   */
+  struct timespec ts;
+  struct connection *conn = threadlocal_get_conn ();
+  struct pollfd fds[] = {
+    [0].fd = quit_fd,
+    [0].events = POLLIN,
+    [1].fd = conn ? conn->status_pipe[0] : -1,
+    [1].events = POLLIN,
+    [2].fd = conn ? conn->sockin : -1,
+    [2].events = POLLRDHUP,
+  };
+  sigset_t all;
+
+  if (sec >= INT_MAX - nsec / 1000000000) {
+    nbdkit_error ("sleep request is too long");
+    errno = EINVAL;
+    return -1;
+  }
+  ts.tv_sec = sec + nsec / 1000000000;
+  ts.tv_nsec = nsec % 1000000000;
+
+  /* Block all signals to this thread during the poll, so we don't
+   * have to worry about EINTR
+   */
+  if (sigfillset(&all))
+    abort ();
+  switch (ppoll (fds, sizeof fds / sizeof fds[0], &ts, &all)) {
+  case -1:
+    assert (errno != EINTR);
+    nbdkit_error ("poll: %m");
+    return -1;
+  case 0:
+    return 0;
+  }
+
+  /* We don't have to read the pipe-to-self; if poll returned an
+   * event, we know the connection should be shutting down.
+   */
+  assert (quit ||
+          (conn && conn->nworkers > 0 && connection_get_status (conn) < 1) ||
+          (conn && (fds[2].revents & (POLLRDHUP | POLLHUP | POLLERR))));
+  nbdkit_error ("aborting sleep to shut down");
+  errno = ESHUTDOWN;
+  return -1;
+#else
+# error "Please port this to your platform"
+  /* Porting ideas, in order of preference:
+   * - POSIX requires pselect; it's a bit clunkier to set up than poll,
+   *   but the same ability to atomically mask all signals and operate
+   *   on struct timespec makes it similar to the preferred ppoll interface
+   * - calculate an end time target, then use poll in a loop on EINTR with
+   *   a recalculation of the timeout to still reach the end time (masking
+   *   signals in that case is not safe, as it is a non-atomic race)
+   */
+  nbdkit_error ("nbdkit_nanosleep not yet ported to systems without ppoll");
+  errno = ENOSYS;
+  return -1;
+#endif
 }

configure.ac
@@ -198,6 +198,7 @@ AC_CHECK_FUNCS([\
 	get_current_dir_name \
 	mkostemp \
 	pipe2 \
+	ppoll \
 	posix_fadvise])
 
 dnl Check whether printf("%m") works

docs/nbdkit-plugin.pod
@@ -342,6 +342,20 @@ and returns C<NULL>.
 
 The returned string must be freed by the caller.
 
+=head2 C<nbdkit_nanosleep>
+
+ int nbdkit_nanosleep (unsigned sec, unsigned nsec);
+
+The utility function C<nbdkit_nanosleep> suspends the current thread,
+and returns 0 if it slept at least as many seconds and nanoseconds as
+requested, or -1 after calling C<nbdkit_error> if there is no point in
+continuing the current command.  Attempts to sleep more than
+C<INT_MAX> seconds are treated as an error.
+
+Plugins should prefer this function instead of sleeping using system
+calls like L<sleep(3)> or L<nanosleep(3)>, since it allows nbdkit to
+shut down cleanly without delay.
+
 =head2 umask
 
 All plugins will see a L<umask(2)> of C<0022>.

filters/delay/delay.c
@@ -37,7 +37,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <time.h>
 
 #include <nbdkit-filter.h>
 
@@ -83,16 +82,9 @@ parse_delay (const char *key, const char *value)
 static int
 delay (int ms, int *err)
 {
-  if (ms > 0) {
-    const struct timespec ts = {
-      .tv_sec = ms / 1000,
-      .tv_nsec = (ms % 1000) * 1000000,
-    };
-    if (nanosleep (&ts, NULL) == -1) {
-      nbdkit_error ("nanosleep: %m");
-      *err = errno;
-      return -1;
-    }
+  if (ms > 0 && nbdkit_nanosleep (ms / 1000, (ms % 1000) * 1000000) == -1) {
+    *err = errno;
+    return -1;
   }
   return 0;
 }

filters/rate/rate.c
@@ -262,12 +262,10 @@ maybe_sleep (struct bucket *bucket, pthread_mutex_t *lock, uint32_t count,
       bits = bucket_run (bucket, bits, &ts);
     }
 
-    if (bits > 0)
-      if (nanosleep (&ts, NULL) == -1) {
-        nbdkit_error ("nanosleep: %m");
-        *err = errno;
-        return -1;
-      }
+    if (bits > 0 && nbdkit_nanosleep (ts.tv_sec, ts.tv_nsec) == -1) {
+      *err = errno;
+      return -1;
+    }
   }
   return 0;
 }

include/nbdkit-common.h
@@ -82,6 +82,7 @@ extern int64_t nbdkit_parse_size (const char *str);
 extern int nbdkit_parse_bool (const char *str);
 extern int nbdkit_read_password (const char *value, char **password);
 extern char *nbdkit_realpath (const char *path);
+extern int nbdkit_nanosleep (unsigned sec, unsigned nsec);
 
 struct nbdkit_extents;
 extern int nbdkit_add_extent (struct nbdkit_extents *,

server/nbdkit.syms
@@ -46,6 +46,7 @@
     nbdkit_extents_free;
     nbdkit_extents_new;
     nbdkit_get_extent;
+    nbdkit_nanosleep;
     nbdkit_parse_bool;
     nbdkit_parse_size;
     nbdkit_read_password;

server/test-public.c
@@ -42,12 +42,26 @@
 
 static bool error_flagged;
 
-/* Stub for linking against minimal source files, and for proving that
+/* Stubs for linking against minimal source files, and for proving that
  * an error message is issued when expected.  */
 void
 nbdkit_error (const char *fs, ...)
 {
   error_flagged = true;
+}
+
+volatile int quit;
+int quit_fd = -1;
+
+struct connection *
+threadlocal_get_conn (void)
+{
+  abort ();
+}
+
+int connection_get_status (struct connection *conn)
+{
+  abort ();
 }
 
 static bool
@@ -215,6 +229,8 @@ main (int argc, char *argv[])
   bool pass = true;
   pass &= test_nbdkit_parse_size ();
   pass &= test_nbdkit_read_password ();
-  /* XXX add tests for nbdkit_absolute_path */
+  /* nbdkit_absolute_path and nbdkit_nanosleep not unit-tested here, but
+   * get plenty of coverage in the main testsuite.
+   */
   return pass ? EXIT_SUCCESS : EXIT_FAILURE;
 }

commit 8a95b92dbeefe50b6a88ad81c254997191507bb6

server: Avoid Coverity warning on volatile variable
Coverity warns that reading a volatile variable inside an assert() may
trigger a side-effect, and lose the effect when compiling with
-DNDEBUG.  In this particular case, our 'quit' variable must be marked
volatile, but reading it does not trigger a side effect, so the
Coverity warning is a false positive.  Still, we can shut it up by
hoisting the read outside the assesrt.

Fixes: 4871e496 ("Server: Add and use nbdkit_nanosleep", v1.13.9)

server/public.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2021 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -725,11 +725,12 @@ nbdkit_nanosleep (unsigned sec, unsigned nsec)
   /* We don't have to read the pipe-to-self; if poll returned an
    * event, we know the connection should be shutting down.
    */
-  assert (quit ||
+  bool has_quit = quit;
+  assert (has_quit ||
           (conn && conn->nworkers > 0 && connection_get_status () < 1) ||
           (conn && (fds[2].revents & (POLLRDHUP | POLLHUP | POLLERR |
                                       POLLNVAL))));
-  if (quit)
+  if (has_quit)
     nbdkit_error ("aborting sleep because of server shut down");
   else
     nbdkit_error ("aborting sleep because of connection close or error");