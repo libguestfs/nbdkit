libguestfs/nbdkit

commit ef13f0072b8d6a5c13406809c5a110b384e5ea9b

plugins/S3.py: correctly handle listing contents of empty buckets.

plugins/S3/S3.py
@@ -472,6 +472,8 @@ class Server:
 
         while True:
             resp = self.s3.list_objects_v2(**args)
+            if not 'Contents' in resp:
+                return
             for el in resp['Contents']:
                 yield el['Key']
             if not resp['IsTruncated']:

commit f216554ad06c7f0af4ada1fd1e5b5f57e361a5bd

S3: Fix "E713 test for membership should be 'not in'"
Fixes: commit ef13f0072b8d6a5c13406809c5a110b384e5ea9b
Fixes: https://gitlab.com/nbdkit/nbdkit/-/merge_requests/14

plugins/S3/S3.py
@@ -472,7 +472,7 @@ class Server:
 
         while True:
             resp = self.s3.list_objects_v2(**args)
-            if not 'Contents' in resp:
+            if 'Contents' not in resp:
                 return
             for el in resp['Contents']:
                 yield el['Key']