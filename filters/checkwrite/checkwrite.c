libguestfs/nbdkit

commit 2d0954dd6431cfd8d82e873a5b99285b0fddbeb8

New filter: checkwrite
This filter checks that writes to the underlying plugin match the
contents of the plugin.  If not it injects EIO into the responses.

filters/checkwrite/checkwrite.c
@@ -1,0 +1,261 @@
+/* nbdkit
+ * Copyright (C) 2019-2020 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <nbdkit-filter.h>
+
+#include "cleanup.h"
+#include "iszero.h"
+#include "minmax.h"
+
+static void *
+checkwrite_open (nbdkit_next_open *next, nbdkit_backend *nxdata,
+                 int readonly, const char *exportname, int is_tls)
+{
+  /* Ignore readonly flag passed in, open the plugin readonly. */
+  if (next (nxdata, 1, exportname) == -1)
+    return NULL;
+  return NBDKIT_HANDLE_NOT_NEEDED;
+}
+
+/* Whatever the underlying plugin can or can't do, we can do all the
+ * write-like operations.
+ */
+static int
+checkwrite_can_write (struct nbdkit_next_ops *next_ops, void *nxdata,
+                      void *handle)
+{
+  return 1;
+}
+
+static int
+checkwrite_can_flush (struct nbdkit_next_ops *next_ops, void *nxdata,
+                      void *handle)
+{
+  return 1;
+}
+
+static int
+checkwrite_can_fua (struct nbdkit_next_ops *next_ops, void *nxdata,
+                    void *handle)
+{
+  return NBDKIT_FUA_NATIVE;
+}
+
+static int
+checkwrite_can_trim (struct nbdkit_next_ops *next_ops, void *nxdata,
+                     void *handle)
+{
+  return 1;
+}
+
+static int
+checkwrite_can_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
+                     void *handle)
+{
+  return NBDKIT_ZERO_NATIVE;
+}
+
+static inline int
+data_does_not_match (int *err)
+{
+  *err = EIO;
+  nbdkit_error ("data written does not match expected");
+  return -1;
+}
+
+/* Provide write-like operations which perform the additional checks. */
+static int
+checkwrite_pwrite (struct nbdkit_next_ops *next_ops, void *nxdata,
+                   void *handle,
+                   const void *buf, uint32_t count, uint64_t offset,
+                   uint32_t flags, int *err)
+{
+  CLEANUP_FREE char *expected;
+
+  expected = malloc (count);
+  if (expected == NULL) {
+    *err = errno;
+    nbdkit_error ("malloc: %m");
+    return -1;
+  }
+
+  /* Read underlying plugin data into the buffer. */
+  if (next_ops->pread (nxdata, expected, count, offset, 0, err) == -1)
+    return -1;
+
+  /* If data written doesn't match data expected, inject EIO. */
+  if (memcmp (buf, expected, count) != 0)
+    return data_does_not_match (err);
+
+  return 0;
+}
+
+static int
+checkwrite_flush (struct nbdkit_next_ops *next_ops, void *nxdata,
+                  void *handle, uint32_t flags, int *err)
+{
+  /* Does nothing, we just have to support it. */
+  return 0;
+}
+
+#define MAX_REQUEST_SIZE (64 * 1024 * 1024) /* XXX */
+
+/* Trim and zero are effectively the same operation for this plugin.
+ * We have to check that the underlying plugin contains all zeroes.
+ *
+ * Note we don't check that the extents exactly match since a valid
+ * copying operation is to either add sparseness (qemu-img convert -S)
+ * or create a fully allocated target (nbdcopy --allocated).
+ */
+static int
+checkwrite_trim_zero (struct nbdkit_next_ops *next_ops, void *nxdata,
+                      void *handle, uint32_t count, uint64_t offset,
+                      uint32_t flags, int *err)
+{
+  /* If the plugin supports extents, speed this up by using them. */
+  if (next_ops->can_extents (nxdata)) {
+    while (count > 0) {
+      struct nbdkit_extents *exts;
+      size_t i, n;
+
+      exts = nbdkit_extents_new (offset, offset + count);
+      if (exts == NULL)
+        return -1;
+      if (next_ops->extents (nxdata, count, offset, 0, exts, err) == -1)
+        return -1;
+
+      /* Ignore any extents or partial extents which are outside the
+       * offset/count that we are looking at.  The plugin is required
+       * to return at least one relevant extent so we can assume this
+       * loop will make forward progress.
+       */
+      n = nbdkit_extents_count (exts);
+      for (i = 0; i < n && count > 0; ++i) {
+        uint64_t next_extent_offset;
+        struct nbdkit_extent e;
+
+        e = nbdkit_get_extent (exts, i);
+
+        if (e.offset + e.length <= offset)
+          continue;
+        if (e.offset > offset)
+          break;
+
+        next_extent_offset = e.offset + e.length;
+
+        /* Anything that reads back as zero is good. */
+        if ((e.type & NBDKIT_EXTENT_ZERO) != 0) {
+          const uint64_t zerolen = MIN (count, next_extent_offset - offset);
+
+          offset += zerolen;
+          count -= zerolen;
+          continue;
+        }
+
+        /* Otherwise we have to read the underlying data and check. */
+        while (count > 0) {
+          const size_t buflen =
+            MIN3 (MAX_REQUEST_SIZE, count, next_extent_offset - offset);
+          CLEANUP_FREE char *buf = malloc (buflen);
+          if (buf == NULL) {
+            *err = errno;
+            nbdkit_error ("malloc: %m");
+            return -1;
+          }
+
+          if (next_ops->pread (nxdata, buf, buflen, offset, 0, err) == -1)
+            return -1;
+          if (! is_zero (buf, buflen))
+            return data_does_not_match (err);
+
+          count -= buflen;
+          offset += buflen;
+        }
+      } /* for extent */
+
+      nbdkit_extents_free (exts);
+    } /* while (count > 0) */
+  }
+
+  /* Otherwise the plugin does not support extents, so do this the
+   * slow way.
+   */
+  else {
+    CLEANUP_FREE char *buf;
+
+    buf = malloc (MIN (MAX_REQUEST_SIZE, count));
+    if (buf == NULL) {
+      *err = errno;
+      nbdkit_error ("malloc: %m");
+      return -1;
+    }
+
+    while (count > 0) {
+      uint32_t n = MIN (MAX_REQUEST_SIZE, count);
+
+      if (next_ops->pread (nxdata, buf, n, offset, 0, err) == -1)
+        return -1;
+      if (! is_zero (buf, n))
+        return data_does_not_match (err);
+      count -= n;
+      offset += n;
+    }
+  }
+
+  return 0;
+}
+
+static struct nbdkit_filter filter = {
+  .name              = "checkwrite",
+  .longname          = "nbdkit checkwrite filter",
+
+  .open              = checkwrite_open,
+  .can_write         = checkwrite_can_write,
+  .can_flush         = checkwrite_can_flush,
+  .can_fua           = checkwrite_can_fua,
+  .can_trim          = checkwrite_can_trim,
+  .can_zero          = checkwrite_can_zero,
+
+  .pwrite            = checkwrite_pwrite,
+  .flush             = checkwrite_flush,
+  .trim              = checkwrite_trim_zero,
+  .zero              = checkwrite_trim_zero,
+};
+
+NBDKIT_REGISTER_FILTER(filter)

TODO
@@ -209,11 +209,6 @@ Suggestions for filters
 * "bandwidth quota" filter which would close a connection after it
   exceeded a certain amount of bandwidth up or down.
 
-* Filter which checks if write match underlying plugin.  Currently
-  random and sparse-random plugins behave such that writes are checked
-  against what should be read.  For testing it would be a good idea to
-  make this functionality generically available through a filter.
-
 * nbdkit-cache-filter should handle ENOSPC errors automatically by
   reclaiming blocks from the cache
 

common/include/minmax.h
@@ -76,4 +76,8 @@
 
 #endif
 
+/* Helpful to have MIN3, MAX3 and maybe more in future. */
+#define MIN3(x0, x1, x2) (MIN (MIN ((x0), (x1)), (x2)))
+#define MAX3(x0, x1, x2) (MAX (MAX ((x0), (x1)), (x2)))
+
 #endif /* NBDKIT_MINMAX_H */

configure.ac
@@ -101,6 +101,7 @@ filters="\
         blocksize \
         cache \
         cacheextents \
+        checkwrite \
         cow \
         ddrescue \
         delay \
@@ -1228,6 +1229,7 @@ AC_CONFIG_FILES([Makefile
                  filters/blocksize/Makefile
                  filters/cache/Makefile
                  filters/cacheextents/Makefile
+                 filters/checkwrite/Makefile
                  filters/cow/Makefile
                  filters/ddrescue/Makefile
                  filters/delay/Makefile

filters/checkwrite/Makefile.am
@@ -1,0 +1,68 @@
+# nbdkit
+# Copyright (C) 2019-2020 Red Hat Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-checkwrite-filter.pod
+
+filter_LTLIBRARIES = nbdkit-checkwrite-filter.la
+
+nbdkit_checkwrite_filter_la_SOURCES = \
+	checkwrite.c \
+	$(top_srcdir)/include/nbdkit-filter.h \
+	$(NULL)
+
+nbdkit_checkwrite_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdkit_checkwrite_filter_la_CFLAGS = $(WARNINGS_CFLAGS)
+nbdkit_checkwrite_filter_la_LDFLAGS = \
+	-module -avoid-version -shared $(NO_UNDEFINED_ON_WINDOWS) \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms \
+	$(NULL)
+nbdkit_checkwrite_filter_la_LIBADD = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(IMPORT_LIBRARY_ON_WINDOWS) \
+	$(NULL)
+
+if HAVE_POD
+
+man_MANS = nbdkit-checkwrite-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-checkwrite-filter.1: nbdkit-checkwrite-filter.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD

filters/checkwrite/nbdkit-checkwrite-filter.pod
@@ -1,0 +1,79 @@
+=head1 NAME
+
+nbdkit-checkwrite-filter - check writes match contents of plugin
+
+=head1 SYNOPSIS
+
+ nbdkit --filter=checkwrite PLUGIN
+
+=head1 DESCRIPTION
+
+C<nbdkit-checkwrite-filter> is an nbdkit filter that checks writes to
+the underlying plugin match the content of the plugin.  If a client
+tries to write non-matching data to the plugin then the filter injects
+an EIO Input/output error.  All write-like operations are covered by
+this filter, such as trimming and zeroing.
+
+The underlying plugin is opened read-only and is never written to.
+
+=head2 Using this filter to test copying tools
+
+This filter is usually used for testing other NBD tools.
+
+You can check that a copying tool is copying data correctly by
+creating an nbdkit instance containing some test data, overlaying this
+filter, and copying from and to nbdkit at the same time:
+
+ nbdkit -U - --filter=checkwrite data "@32768 1" \
+        --run 'nbdcopy "$uri" "$uri"'
+
+If the copying program is buggy then you will see EIO errors and (if
+the copying program also handles errors correctly) it should exit with
+an error.
+
+=head2 Other plugins for testing NBD tools
+
+If you are doing this kind of testing you will also want to look at
+L<nbdkit-random-plugin(1)> and L<nbdkit-sparse-random-plugin(1)>.
+These plugins already have this behaviour built in to them (because it
+is more efficient to build it into the plugin) — you do B<not> need to
+use this filter with those plugins.  This filter is useful for testing
+with arbitrary plugins.
+
+=head1 PARAMETERS
+
+There are no parameters specific to this filter.  Parameters are
+passed through to the underlying plugin.
+
+=head1 FILES
+
+=over 4
+
+=item F<$filterdir/nbdkit-checkwrite-filter.so>
+
+The filter.
+
+Use C<nbdkit --dump-config> to find the location of C<$filterdir>.
+
+=back
+
+=head1 VERSION
+
+C<nbdkit-checkwrite-filter> first appeared in nbdkit 1.24.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-random-plugin(1)>,
+L<nbdkit-sparse-random-plugin(1)>,
+L<nbdkit-filter(3)>,
+L<nbdkit-plugin(3)>,
+L<nbdcopy(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2020 Red Hat Inc.

plugins/random/nbdkit-random-plugin.pod
@@ -30,6 +30,7 @@ same:
  nbdkit -U - random size=100M --run 'nbdcopy "$uri" "$uri"'
 
 C<qemu-img convert> could be used in place of nbdcopy.
+See also L<nbdkit-checkwrite-filter(1)>.
 
 =head1 PARAMETERS
 

plugins/sparse-random/nbdkit-sparse-random-plugin.pod
@@ -38,6 +38,7 @@ use a single instance of this plugin for both read and write:
  nbdkit -U - sparse-random size=1T --run 'nbdcopy "$uri" "$uri"'
 
 C<qemu-img convert> could be used in place of nbdcopy.
+See also L<nbdkit-checkwrite-filter(1)>.
 
 =head1 PARAMETERS
 

commit 16a13fa9e1f3c4cbc0fae89e364f832e1a41fb40

checkwrite: Fix bounds error
In the rare case where a client tries to trim or zero an underlying
plugin which has two or more extents in the region being zeroed, and
the first of those extents was marked as data (but contained zeroes),
the filter would get "stuck" on the first extent.  It would try to
read the first extent up to the end, and finally issue a zero length
read to the underlying plugin which caused an assertion failure:

nbdkit: backend.c:624: backend_pread: Assertion `backend_valid_range (c, offset, count)' failed.

In practice this only happened on ppc64le when testing the checkwrite
filter on top of the file plugin.  This is likely because of the
larger filesystem extent granularity making it more likely to hit this
corner case.  However eventually I came up with a synthetic test which
would hit this problem on any architecture (see link).

The fix is to check if we're at the end of the current extent and
break out of the "stuck" loop before issuing the zero-sized read, and
move to the next extent in the list.

The bug has been present since the filter was introduced in
December 2020 / nbdkit 1.24.

Fixes: commit 2d0954dd6431cfd8d82e873a5b99285b0fddbeb8
Link: https://listman.redhat.com/archives/libguestfs/2022-July/029496.html

filters/checkwrite/checkwrite.c
@@ -192,7 +192,7 @@ checkwrite_trim_zero (nbdkit_next *next,
         *err = ENOTSUP;
         return -1;
       }
-      while (count > 0) {
+      while (offset < next_extent_offset && count > 0) {
         size_t buflen = MIN (MAX_REQUEST_SIZE, count);
         buflen = MIN (buflen, next_extent_offset - offset);
 