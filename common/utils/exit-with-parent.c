libguestfs/nbdkit

commit 82ee45027bb9e5e8f7e4aa547bf1742466966812

server: Simplify --exit-with-parent implementation for macOS
Use a thread instead of a monitoring process.  Get rid of the whole
CoreFoundation business, it turns out its not necessary and we can
just use "raw" kevents (this code would probably work on FreeBSD too).

Updates: commit 40f87267e031f3f41db1b3a37d2a6519918cbac8

common/utils/exit-with-parent.c
@@ -83,127 +83,78 @@ can_exit_with_parent (void)
 
 #elif defined(__APPLE__)
 
-/* For macOS.
- *
- * Adapted from:
- * https://developer.apple.com/documentation/corefoundation/cffiledescriptor-ru3
- * and https://stackoverflow.com/a/6484903 (note this example is
- * wrong).
- */
+/* For macOS. */
 
-#include <CoreFoundation/CoreFoundation.h>
 #include <unistd.h>
-#include <signal.h>
 #include <sys/event.h>
+#include <pthread.h>
 
 #include "nbdkit-plugin.h"
 
-static pid_t nbdkit_pid = 0;
-
-static void
-parent_died (CFFileDescriptorRef fdref, CFOptionFlags callBackTypes,
-             void *info)
+static void *
+exit_with_parent_loop (void *vppid)
 {
-  if (nbdkit_pid > 0) {
+  pid_t ppid = * (pid_t *) vppid;
+  int fd;
+  struct kevent kev, res[1];
+  int r;
+
+  free (vppid);
+
+  /* Register the kevent to wait for ppid to exit. */
+  fd = kqueue ();
+  if (fd == -1) {
+    nbdkit_error ("exit_with_parent_loop: kqueue: %m");
+    return NULL;
+  }
+  EV_SET (&kev, ppid, EVFILT_PROC, EV_ADD|EV_ENABLE, NOTE_EXIT, 0, NULL);
+  if (kevent (fd, &kev, 1, NULL, 0, NULL) == -1) {
+    nbdkit_error ("exit_with_parent_loop: kevent: %m");
+    close (fd);
+    return NULL;
+  }
+
+  /* Wait for the kevent to happen. */
+  r = kevent (fd, 0, 0, res, 1, NULL);
+  if (r == 1 && res[0].ident == ppid) {
+    /* Exit the whole process when the parent dies. */
     nbdkit_debug ("macOS: --exit-with-parent: "
-                  "kill nbdkit (pid %d) because parent process died",
-                  nbdkit_pid);
-    kill (nbdkit_pid, SIGTERM);
-    nbdkit_pid = 0;
+                  "exit because parent process died");
     exit (EXIT_SUCCESS);
   }
-}
 
-static void
-do_monitor (pid_t pid)
-{
-  int fd;
-  struct kevent kev;
-  CFFileDescriptorRef fdref;
-  CFRunLoopSourceRef source;
-
-  fd = kqueue ();
-  if (fd == -1)
-    _exit (EXIT_FAILURE);
-  EV_SET (&kev, pid, EVFILT_PROC, EV_ADD|EV_ENABLE, NOTE_EXIT, 0, NULL);
-  if (kevent (fd, &kev, 1, NULL, 0, NULL) == -1)
-    _exit (EXIT_FAILURE);
-
-  fdref = CFFileDescriptorCreate (kCFAllocatorDefault, fd, true,
-                                  parent_died, NULL);
-  if (fdref == NULL)
-    _exit (EXIT_FAILURE);
-  CFFileDescriptorEnableCallBacks (fdref, kCFFileDescriptorReadCallBack);
-  source =
-    CFFileDescriptorCreateRunLoopSource (kCFAllocatorDefault, fdref, 0);
-  if (source == NULL)
-    _exit (EXIT_FAILURE);
-  CFRunLoopAddSource (CFRunLoopGetMain(), source, kCFRunLoopDefaultMode);
-  CFRelease (source);
-}
-
-static void
-exit_monitor_process (CFFileDescriptorRef fdref, CFOptionFlags callBackTypes,
-                      void *info)
-{
-  nbdkit_debug ("macOS: --exit-with-parent: "
-                "monitor exiting because nbdkit exited");
-  exit (EXIT_SUCCESS);
-}
-
-static void
-do_exit_for_nbdkit (pid_t pid)
-{
-  int fd;
-  struct kevent kev;
-  CFFileDescriptorRef fdref;
-  CFRunLoopSourceRef source;
-
-  fd = kqueue ();
-  if (fd == -1)
-    _exit (EXIT_FAILURE);
-  EV_SET (&kev, pid, EVFILT_PROC, EV_ADD|EV_ENABLE, NOTE_EXIT, 0, NULL);
-  if (kevent (fd, &kev, 1, NULL, 0, NULL) == -1)
-    _exit (EXIT_FAILURE);
-
-  fdref = CFFileDescriptorCreate (kCFAllocatorDefault, fd, true,
-                                  exit_monitor_process, NULL);
-  if (fdref == NULL)
-    _exit (EXIT_FAILURE);
-  CFFileDescriptorEnableCallBacks (fdref, kCFFileDescriptorReadCallBack);
-  source =
-    CFFileDescriptorCreateRunLoopSource (kCFAllocatorDefault, fdref, 0);
-  if (source == NULL)
-    _exit (EXIT_FAILURE);
-  CFRunLoopAddSource (CFRunLoopGetMain(), source, kCFRunLoopDefaultMode);
-  CFRelease (source);
+  return NULL;
 }
 
 int
 set_exit_with_parent (void)
 {
   pid_t ppid = getppid ();
-  pid_t monitor_pid;
+  pid_t *ppid_data;
+  int r;
+  pthread_attr_t attrs;
+  pthread_t exit_with_parent_thread;
 
   nbdkit_debug ("macOS: --exit-with-parent: "
                 "registering exit with parent for ppid %d",
                 (int) ppid);
-  nbdkit_pid = getpid ();
 
-  /* We have to run a main loop (ie a new process) to get similar
+  /* We have to run a main loop (ie a new thread) to get similar
    * behaviour to --exit-with-parent on other platforms.
    *
-   * nbdkit_pid = nbdkit's PID
    * ppid = parent of nbdkit that we are monitoring
-   * monitor_pid = monitoring PID
    */
-  monitor_pid = fork ();
-  if (monitor_pid == 0) {       /* Child (monitoring process) */
-    do_monitor (ppid);          /* Monitor this parent PID. */
-    do_exit_for_nbdkit (nbdkit_pid); /* Just exit if nbdkit exits. */
-    CFRunLoopRun ();
-
-    _exit (EXIT_SUCCESS);
+  ppid_data = malloc (sizeof ppid);
+  if (ppid_data == NULL)
+    return -1;
+  *ppid_data = ppid;
+  pthread_attr_init (&attrs);
+  pthread_attr_setdetachstate (&attrs, PTHREAD_CREATE_DETACHED);
+  r = pthread_create (&exit_with_parent_thread, NULL,
+                      exit_with_parent_loop, ppid_data);
+  if (r != 0) {
+    errno = r;
+    return -1;
   }
 
   return 0;

configure.ac
@@ -551,18 +551,6 @@ LIBS="$DL_LIBS $LIBS"
 AC_CHECK_FUNCS([dladdr])
 LIBS="$old_LIBS"
 
-dnl Is this macOS (darwin)?
-dnl macOS requires that the server is linked to CoreFoundation.
-AC_MSG_CHECKING([if the target is macOS])
-AS_CASE([$host_os],
-  [darwin*], [
-      MACOS_LDFLAGS="-Wl,-framework,CoreFoundation"
-      AC_SUBST([MACOS_LDFLAGS])
-      AC_MSG_RESULT([yes])
-  ],
-  [AC_MSG_RESULT([no])]
-)
-
 dnl Is this Windows?
 AC_MSG_CHECKING([if the target is Windows])
 AS_CASE([$host_os],

server/Makefile.am
@@ -105,7 +105,6 @@ nbdkit_LDADD = \
 nbdkit_LDFLAGS = \
 	$(PTHREAD_LIBS) \
 	$(DL_LDFLAGS) \
-	$(MACOS_LDFLAGS) \
 	$(NULL)
 EXTRA_nbdkit_DEPENDENCIES = nbdkit.syms
 if USE_LINKER_SCRIPT

commit 5764fa6f4a2b1408e4e62c39b2413e55ef77ab36

common/utils/exit-with-parent.c: Use nbdkit_shutdown instead of exit
Using nbdkit_shutdown we can ensure that unload handlers in the plugin
and filters run, rather than abruptly exiting.

Fixes: commit 82ee45027bb9e5e8f7e4aa547bf1742466966812

common/utils/exit-with-parent.c
@@ -120,10 +120,10 @@ exit_with_parent_loop (void *vp)
   /* Wait for the kevent to happen. */
   r = kevent (fd, 0, 0, res, 1, NULL);
   if (r == 1 && res[0].ident == ppid) {
-    /* Exit the whole process when the parent dies. */
+    /* Shut down the whole process when the parent dies. */
     nbdkit_debug ("macOS: --exit-with-parent: "
                   "exit because parent process died");
-    exit (EXIT_SUCCESS);
+    nbdkit_shutdown ();
   }
 
   return NULL;