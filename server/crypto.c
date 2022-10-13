libguestfs/nbdkit

commit bd9a52e058309da7e148a3d29dc056d4ad6e1f0f

server/crypto: Don't need to explicitly malloc the session pointer.
gnutls_session_t is a pointer (not a larger object), so it doesn't
have to be explicitly allocated.  In addition we can store the pointer
directly in the ‘void *’ of the connection (instead of storing a
pointer to a pointer).  The old code wasn't wrong but did an
unnecessary and complex extra allocation.

server/crypto.c
@@ -314,7 +314,7 @@ crypto_free (void)
 static int
 crypto_recv (struct connection *conn, void *vbuf, size_t len)
 {
-  gnutls_session_t *session = conn->crypto_session;
+  gnutls_session_t session = conn->crypto_session;
   char *buf = vbuf;
   ssize_t r;
   bool first_read = true;
@@ -322,7 +322,7 @@ crypto_recv (struct connection *conn, void *vbuf, size_t len)
   assert (session != NULL);
 
   while (len > 0) {
-    r = gnutls_record_recv (*session, buf, len);
+    r = gnutls_record_recv (session, buf, len);
     if (r < 0) {
       if (r == GNUTLS_E_INTERRUPTED || r == GNUTLS_E_AGAIN)
         continue;
@@ -351,14 +351,14 @@ crypto_recv (struct connection *conn, void *vbuf, size_t len)
 static int
 crypto_send (struct connection *conn, const void *vbuf, size_t len)
 {
-  gnutls_session_t *session = conn->crypto_session;
+  gnutls_session_t session = conn->crypto_session;
   const char *buf = vbuf;
   ssize_t r;
 
   assert (session != NULL);
 
   while (len > 0) {
-    r = gnutls_record_send (*session, buf, len);
+    r = gnutls_record_send (session, buf, len);
     if (r < 0) {
       if (r == GNUTLS_E_INTERRUPTED || r == GNUTLS_E_AGAIN)
         continue;
@@ -377,22 +377,21 @@ crypto_send (struct connection *conn, const void *vbuf, size_t len)
 static void
 crypto_close (struct connection *conn)
 {
-  gnutls_session_t *session = conn->crypto_session;
+  gnutls_session_t session = conn->crypto_session;
   int sockin, sockout;
 
   assert (session != NULL);
 
-  gnutls_transport_get_int2 (*session, &sockin, &sockout);
+  gnutls_transport_get_int2 (session, &sockin, &sockout);
 
-  gnutls_bye (*session, GNUTLS_SHUT_RDWR);
+  gnutls_bye (session, GNUTLS_SHUT_RDWR);
 
   if (sockin >= 0)
     close (sockin);
   if (sockout >= 0 && sockin != sockout)
     close (sockout);
 
-  gnutls_deinit (*session);
-  free (session);
+  gnutls_deinit (session);
   conn->crypto_session = NULL;
 }
 
@@ -403,18 +402,12 @@ crypto_close (struct connection *conn)
 int
 crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
 {
-  gnutls_session_t *session;
+  gnutls_session_t session;
   CLEANUP_FREE char *priority = NULL;
   int err;
 
   /* Create the GnuTLS session. */
-  session = malloc (sizeof *session);
-  if (session == NULL) {
-    nbdkit_error ("malloc: %m");
-    return -1;
-  }
-
-  err = gnutls_init (session, GNUTLS_SERVER);
+  err = gnutls_init (&session, GNUTLS_SERVER);
   if (err < 0) {
     nbdkit_error ("gnutls_init: %s", gnutls_strerror (err));
     free (session);
@@ -424,7 +417,7 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
   switch (crypto_auth) {
   case CRYPTO_AUTH_CERTIFICATES:
     /* Associate the session with the server credentials (key, cert). */
-    err = gnutls_credentials_set (*session, GNUTLS_CRD_CERTIFICATE,
+    err = gnutls_credentials_set (session, GNUTLS_CRD_CERTIFICATE,
                                   x509_creds);
     if (err < 0) {
       nbdkit_error ("gnutls_credentials_set: %s", gnutls_strerror (err));
@@ -437,8 +430,8 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
      */
     if (tls_verify_peer) {
 #ifdef HAVE_GNUTLS_SESSION_SET_VERIFY_CERT
-      gnutls_certificate_server_set_request (*session, GNUTLS_CERT_REQUEST);
-      gnutls_session_set_verify_cert (*session, NULL, 0);
+      gnutls_certificate_server_set_request (session, GNUTLS_CERT_REQUEST);
+      gnutls_session_set_verify_cert (session, NULL, 0);
 #else
       nbdkit_error ("--tls-verify-peer: "
                     "GnuTLS >= 3.4.6 is required for this feature");
@@ -455,7 +448,7 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
 
   case CRYPTO_AUTH_PSK:
     /* Associate the session with the server PSK credentials. */
-    err = gnutls_credentials_set (*session, GNUTLS_CRD_PSK, psk_creds);
+    err = gnutls_credentials_set (session, GNUTLS_CRD_PSK, psk_creds);
     if (err < 0) {
       nbdkit_error ("gnutls_credentials_set: %s", gnutls_strerror (err));
       goto error;
@@ -473,7 +466,7 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
   }
 
   assert (priority != NULL);
-  err = gnutls_priority_set_direct (*session, priority, NULL);
+  err = gnutls_priority_set_direct (session, priority, NULL);
   if (err < 0) {
     nbdkit_error ("failed to set TLS session priority to %s: %s",
                   priority, gnutls_strerror (err));
@@ -481,15 +474,15 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
   }
 
   /* Set up GnuTLS so it reads and writes on the raw sockets. */
-  gnutls_transport_set_int2 (*session, sockin, sockout);
+  gnutls_transport_set_int2 (session, sockin, sockout);
 
   /* Perform the handshake. */
   debug ("starting TLS handshake");
-  gnutls_handshake_set_timeout (*session,
+  gnutls_handshake_set_timeout (session,
                                 GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
 
   do {
-    err = gnutls_handshake (*session);
+    err = gnutls_handshake (session);
   } while (err < 0 && gnutls_error_is_fatal (err) == 0);
   if (err < 0) {
     gnutls_handshake_description_t in, out;
@@ -498,8 +491,8 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
      * handshake protocol it failed.  You have to look up these codes in
      * <gnutls/gnutls.h>.
      */
-    in = gnutls_handshake_get_last_in (*session);
-    out = gnutls_handshake_get_last_out (*session);
+    in = gnutls_handshake_get_last_in (session);
+    out = gnutls_handshake_get_last_out (session);
     nbdkit_error ("gnutls_handshake: %s (%d/%d)",
                   gnutls_strerror (err), (int) in, (int) out);
     goto error;
@@ -516,8 +509,7 @@ crypto_negotiate_tls (struct connection *conn, int sockin, int sockout)
   return 0;
 
  error:
-  gnutls_deinit (*session);
-  free (session);
+  gnutls_deinit (session);
   return -1;
 }
 

commit 40faf3dfb20c06b9c5faa0a122607e3ae7c6202a

server: Don't free(gnutls_session_t) on gnutls_init error
This is a blatant bug, where our refactoring now causes us to try and
use free(gnutls_session_t) on an opaque type if gnutls_init() fails,
even though gnutls says that only gnutls_deinit() is safe to use at
that point.  Missed when refactoring the code, where we had instead
been using gnutls_session_t* at which point free(session) was correct.

Sadly, as of gnutls 3.7.8 we CANNOT call gnutls_deinit(session) after
gnutls_init(&session, ...) fails, because the gnutls code has an error
path (FAIL_IF_LIB_ERROR) where *session is left uninitialized, yet
does not document a sane value for us to pre-store into the opaque
gnutls_session_t variable.  I'm filing that as a bug with the gnutls
folks.

At any rate, it so happens that gnutls_init() uses calloc() to
populate gnutls_session_t (so the opaque type is indeed a pointer, and
one that can be free()d); but even so, that is liable to leak any
secondary pointers stored within the calloc'd storage if the failure
is partway through gnutls_init() - so I'm not sure why Coverity didn't
flag it.  If gnutls were to mark their function with __attribute__
((malloc(gnutls_deinit, 1))), gcc -fanalyzer would be able to flag us
as using the wrong deallocator function.

Fixes: bd9a52e0 ("server/crypto: Don't need to explicitly malloc the session pointer.", v1.13.4)

server/crypto.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2017 Red Hat Inc.
+ * Copyright (C) 2017-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -648,7 +648,6 @@ crypto_negotiate_tls (int sockin, int sockout)
   err = gnutls_init (&session, GNUTLS_SERVER);
   if (err < 0) {
     nbdkit_error ("gnutls_init: %s", gnutls_strerror (err));
-    free (session);
     return -1;
   }
 