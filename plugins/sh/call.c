libguestfs/nbdkit

commit 53672b859f2f9aad0e0f4f05f6513f17dc3722b9

sh: Refactor handle_script_error()
Instead of having every client perform its own switch statement (which
does not fare well when adding even more status codes to normalize in
the next patch), collect all the common aspects of this into the
utility function handle_script_error().  Also, based on the way ebuf
is populated, it always has an allocated pointer because we added a
NUL terminator for convenience, letting us get rid of an awkward
mid-function label as a goto target.

Extracted from <20221109204353.1342583-3-eblake@redhat.com>
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

plugins/sh/call.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2020 Red Hat Inc.
+ * Copyright (C) 2018-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -397,17 +397,28 @@ call3 (const char *wbuf, size_t wbuflen, /* sent to stdin (can be NULL) */
   return ret;
 }
 
-static void
-handle_script_error (const char *argv0, string *ebuf)
+/* Normalize return codes and parse error string. */
+static exit_code
+handle_script_error (const char *argv0, string *ebuf, exit_code code)
 {
   int err;
   size_t skip = 0;
   char *p;
 
-  if (ebuf->len == 0) {
+  switch (code) {
+  case OK:
+  case MISSING:
+  case RET_FALSE:
+    /* Script successful. */
+    return code;
+
+  case ERROR:
+  default:
     err = EIO;
-    goto no_error_message;
+    break;
   }
+
+  assert (ebuf->ptr); /* Even if empty, ebuf was NUL-terminated in call3 */
 
   /* Recognize the errno values that match NBD protocol errors */
   if (ascii_strncasecmp (ebuf->ptr, "EPERM", 5) == 0) {
@@ -463,11 +474,7 @@ handle_script_error (const char *argv0, string *ebuf)
     err = EFBIG;
     skip = 5;
   }
-  /* Default to EIO. */
-  else {
-    err = EIO;
-    skip = 0;
-  }
+  /* Otherwise, use value of err populated in switch above */
 
   if (skip && ebuf->ptr[skip]) {
     if (!ascii_isspace (ebuf->ptr[skip])) {
@@ -495,13 +502,13 @@ handle_script_error (const char *argv0, string *ebuf)
     nbdkit_error ("%s: %s", argv0, &ebuf->ptr[skip]);
   }
   else {
-  no_error_message:
     nbdkit_error ("%s: script exited with error, "
                   "but did not print an error message on stderr", argv0);
   }
 
   /* Set errno. */
   errno = err;
+  return ERROR;
 }
 
 /* Call the script with parameters.  Don't write to stdin or read from
@@ -516,19 +523,7 @@ call (const char **argv)
   CLEANUP_FREE_STRING string ebuf = empty_vector;
 
   r = call3 (NULL, 0, &rbuf, &ebuf, argv);
-  switch (r) {
-  case OK:
-  case MISSING:
-  case RET_FALSE:
-    /* Script successful. */
-    return r;
-
-  case ERROR:
-  default:
-    /* Error case. */
-    handle_script_error (argv[0], &ebuf);
-    return ERROR;
-  }
+  return handle_script_error (argv[0], &ebuf, r);
 }
 
 /* Call the script with parameters.  Read from stdout and return the
@@ -541,20 +536,10 @@ call_read (string *rbuf, const char **argv)
   CLEANUP_FREE_STRING string ebuf = empty_vector;
 
   r = call3 (NULL, 0, rbuf, &ebuf, argv);
-  switch (r) {
-  case OK:
-  case MISSING:
-  case RET_FALSE:
-    /* Script successful. */
-    return r;
-
-  case ERROR:
-  default:
-    /* Error case. */
+  r = handle_script_error (argv[0], &ebuf, r);
+  if (r == ERROR)
     string_reset (rbuf);
-    handle_script_error (argv[0], &ebuf);
-    return ERROR;
-  }
+  return r;
 }
 
 /* Call the script with parameters.  Write to stdin of the script.
@@ -568,17 +553,5 @@ call_write (const char *wbuf, size_t wbuflen, const char **argv)
   CLEANUP_FREE_STRING string ebuf = empty_vector;
 
   r = call3 (wbuf, wbuflen, &rbuf, &ebuf, argv);
-  switch (r) {
-  case OK:
-  case MISSING:
-  case RET_FALSE:
-    /* Script successful. */
-    return r;
-
-  case ERROR:
-  default:
-    /* Error case. */
-    handle_script_error (argv[0], &ebuf);
-    return ERROR;
-  }
+  return handle_script_error (argv[0], &ebuf, r);
 }

commit e0e2392313b19a94e2dc4bd5d48ce0e37bd54963

eval: Fix assertion error when ebuf->ptr is not allocated
The following test case:

  $ export NBDKIT_VALGRIND=1
  $ ./nbdkit eval get_size=' echo $((16*1024*1024)) ' \
                  pwrite=' echo ENOMEM >&2 ; exit 1 ' \
             --run 'nbdsh -u $uri -c "h.pwrite(bytearray(h.get_size()), 0)"'

results in this error followed by an assertion that kills nbdkit:

  nbdkit: eval[1]: error: /tmp/nbdkitne1mVI/pwrite: write to script failed because of a broken pipe: this can happen if the script exits without consuming stdin, which usually indicates a bug in the script
  nbdkit: call.c:443: handle_script_error: Assertion `ebuf->ptr' failed.

(Note I don't think valgrind is relevant to the error, it simply slows
down nbdkit enough to reveal a race condition.)

The pwrite script is wrong as described in the error message.  However
that shouldn't cause nbdkit to crash.

The problem was removing the conditional:

  -  if (ebuf->len == 0) {

in commit 53672b859f ("sh: Refactor handle_script_error()").  In fact
ebuf->ptr is not guaranteed to be allocated after call3() returns,
especially on some error paths like the one above.

I fixed this by simply making ebuf->ptr always allocated in the
handle_script_error function.

Fixes: commit 53672b859f ("sh: Refactor handle_script_error()")

plugins/sh/call.c
@@ -405,6 +405,19 @@ handle_script_error (const char *argv0, string *ebuf, exit_code code)
   size_t skip = 0;
   char *p;
 
+  /* ebuf->ptr might be NULL on some return paths from call3().  To
+   * make the following code easier, allocate it and reserve one byte.
+   * Note that ebuf->len is still 0 after this.
+   */
+  if (ebuf->len == 0) {
+    if (string_reserve (ebuf, 1) == -1) {
+      nbdkit_error ("realloc: %m");
+      err = EIO;
+      return ERROR;
+    }
+    ebuf->ptr[ebuf->len] = '\0';
+  }
+
   switch (code) {
   case OK:
   case MISSING:
@@ -439,8 +452,6 @@ handle_script_error (const char *argv0, string *ebuf, exit_code code)
     err = ESHUTDOWN;
     break;
   }
-
-  assert (ebuf->ptr); /* Even if empty, ebuf was NUL-terminated in call3 */
 
   /* Recognize the errno values that match NBD protocol errors */
   if (ascii_strncasecmp (ebuf->ptr, "EPERM", 5) == 0) {