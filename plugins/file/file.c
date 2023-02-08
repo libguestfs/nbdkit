libguestfs/nbdkit

commit 96512bb0907706478ab43758cfea4aa021c3d918

file: Mention new options in --help
It's always worth documenting what options we accept.

Fixes: aa5a2183a
Signed-off-by: Eric Blake <eblake@redhat.com>

plugins/file/file.c
@@ -220,6 +220,8 @@ file_config_complete (void)
 #define file_config_help \
   "[file=]<FILENAME>     The filename to serve.\n" \
   "directory=<DIRNAME>   A directory containing files to serve.\n" \
+  "cache=<MODE>          Set use of caching (default, none).\n" \
+  "fadise=<LEVEL>        Set fadvise hint (normal, random, sequential).\n" \
 
 /* Print some extra information about how the plugin was compiled. */
 static void

commit 4788adc6a9e583297e6777c5314f805c76129659

file: Fix typo in --help output
Fixes: commit 96512bb0907706478ab43758cfea4aa021c3d918

plugins/file/file.c
@@ -364,7 +364,7 @@ file_config_complete (void)
   "[file=]<FILENAME>     The filename to serve.\n" \
   "dir=<DIRNAME>         A directory containing files to serve.\n" \
   "cache=<MODE>          Set use of caching (default, none).\n" \
-  "fadise=<LEVEL>        Set fadvise hint (normal, random, sequential).\n" \
+  "fadvise=<LEVEL>       Set fadvise hint (normal, random, sequential).\n" \
 
 /* Print some extra information about how the plugin was compiled. */
 static void