libguestfs/nbdkit

commit b5a66e03c59f99e11a33f879b015f1e1d5362df3

ondemand: Add wait=true flag and resolve rare race in the test
tests/test-ondemand.sh failed rarely (especially when nbdkit was
running slowly because of system load).

This happened because "guestfish --ro" opens two NBD connections, the
first when using qemu-img to create the protective overlay, and the
second when using qemu to connect for real.  Since the NBD protocol
does not have a way for a client to request read-only access, both
connections are read-write, and so if there was any overlap then the
second connection would be rejected.  The symptom was the following
line in the test log:

nbdkit: ondemand[9]: error: default: filesystem is locked by another client

Resolving this is not totally trivial.  Of course we could hide the
problem by using "guestfish --rw" (which doesn't create an overlay and
therefore only connects once).  But the error could still happen in
other contexts.

I chose to add a wait=true flag to the ondemand plugin which turns
F_OFD_SETLK into F_OFD_SETLKW, thus waiting for the first connection
to disconnect, hopefully only very briefly.

I did not make this the default since normally it's desirable to
reject multiple connections to the same export quickly, as it
indicates a user error.

Hopefully this mainly happens when writing test scripts and not when
using the ondemand plugin for real.

This only fully solves the problem for the non-valgrind case.  Under
valgrind things are a bit stranger.  For some reason valgrind appears
to hold open the connection even after qemu-img has cleanly exited.
This may be a bug or peculiarity of valgrind.

Under lsof I observed this:

memcheck-   61428          rjones    6u  unix 0x00000000b894e68a      0t0 2164851673 /tmp/nbdkit-test-sock.VIuyOU type=STREAM (LISTEN)
memcheck-   61428          rjones    7u  unix 0x0000000032821f69      0t0 2164879120 /tmp/nbdkit-test-sock.VIuyOU type=STREAM (CONNECTED)

plugins/ondemand/ondemand.c
@@ -63,6 +63,7 @@
 static char *dir;                   /* dir parameter */
 static DIR *exportsdir;             /* opened exports dir */
 static int64_t requested_size = -1; /* size parameter on the command line */
+static int waitlock;                /* wait if locked */
 
 /* Shell variables. */
 static struct var {
@@ -104,6 +105,12 @@ ondemand_config (const char *key, const char *value)
   else if (strcmp (key, "dir") == 0) {
     dir = nbdkit_realpath (value);
     if (dir == NULL)
+      return -1;
+  }
+
+  else if (strcmp (key, "wait") == 0) {
+    waitlock = nbdkit_parse_bool (value);
+    if (waitlock == -1)
       return -1;
   }
 
@@ -356,6 +363,7 @@ ondemand_open (int readonly)
   struct stat statbuf;
 #ifdef F_OFD_SETLK
   struct flock lock;
+  int cmd;
 #endif
 
   h = malloc (sizeof *h);
@@ -415,9 +423,7 @@ ondemand_open (int readonly)
     }
   }
 
-  /* Lock the file to prevent filesystem corruption.  It's safe for
-   * all clients to be reading.  If a client wants to write it must
-   * have exclusive access.
+  /* Lock the file to prevent filesystem corruption.
    *
    * This uses a currently Linux-specific extension.  It requires
    * Linux >= 3.15 (released in 2014, later backported to RHEL 7).
@@ -425,6 +431,11 @@ ondemand_open (int readonly)
    */
 #ifdef F_OFD_SETLK
   memset (&lock, 0, sizeof lock);
+  /* While we do check the readonly flag here, it's not very useful
+   * because NBD clients cannot specify that they want to open a
+   * connection readonly, and using the -r command line flag is not
+   * very useful with this plugin.
+   */
   if (readonly)
     lock.l_type = F_RDLCK;
   else
@@ -432,7 +443,11 @@ ondemand_open (int readonly)
   lock.l_whence = SEEK_SET;
   lock.l_start = 0;
   lock.l_len = 0;
-  if (fcntl (h->fd, F_OFD_SETLK, &lock) == -1) {
+ again:
+  cmd = waitlock ? F_OFD_SETLKW : F_OFD_SETLK;
+  if (fcntl (h->fd, cmd, &lock) == -1) {
+    if (errno == EINTR && cmd == F_OFD_SETLKW)
+      goto again;
     if (errno == EACCES || errno == EAGAIN) {
       nbdkit_error ("%s: filesystem is locked by another client",
                     h->exportname);

plugins/ondemand/nbdkit-ondemand-plugin.pod
@@ -4,7 +4,7 @@ nbdkit-ondemand-plugin - create filesystems on demand
 
 =head1 SYNOPSIS
 
- nbdkit ondemand dir=EXPORTSDIR [size=]SIZE
+ nbdkit ondemand dir=EXPORTSDIR [size=]SIZE [wait=true]
                  { [type=ext4|xfs|vfat|...] [label=LABEL]
                    | command=COMMAND [VAR=VALUE ...] }
 
@@ -150,6 +150,21 @@ See L<nbdkit(1)/Magic parameters>.
 Select the filesystem type.  The default is C<ext4>.  Most
 non-networked, non-cluster filesystem types supported by the
 L<mkfs(8)> command can be used here.
+
+=item B<wait=true>
+
+If set, if two clients try to connect at the same time to the same
+export then the second client will wait for the first to disconnect.
+The default behaviour is to reject the second client with the error
+message:
+
+ filesystem is locked by another client
+
+This setting is sometimes useful if you are making repeated
+connections and at the network level the first connection does not
+fully disconnect before the next connection starts.  This can also
+happen as a side-effect of using C<guestfish --ro> which opens two NBD
+connections in quick succession.
 
 =back
 

tests/test-ondemand.sh
@@ -47,7 +47,8 @@ rm -f $files
 cleanup_fn rm -f $files
 
 # Run nbdkit.
-start_nbdkit -P ondemand.pid -U $sock --log=stderr ondemand dir=$dir size=100M
+start_nbdkit -P ondemand.pid -U $sock --log=stderr \
+             ondemand dir=$dir size=100M wait=true
 
 # Simply querying an export will create the filesystem.
 qemu-img info nbd:unix:$sock
@@ -70,6 +71,11 @@ fi
 guestfish --format=raw -a "nbd://?socket=$sock" -m /dev/sda <<EOF
   write /test.txt "hello"
 EOF
+
+# This part of the test fails under valgrind for unclear reasons which
+# appear to be a bug in valgrind.
+if [ "x$NBDKIT_VALGRIND" = "x1" ]; then exit 0; fi
+
 guestfish --ro --format=raw -a "nbd://?socket=$sock" -m /dev/sda <<EOF
   cat /test.txt
 EOF

commit 197175d5c604603828b2f38708e0b231372e37fe

ondemand: Document wait=true in --help output
Fixes: commit b5a66e03c59f99e11a33f879b015f1e1d5362df3

plugins/ondemand/ondemand.c
@@ -179,6 +179,7 @@ ondemand_get_ready (void)
   "size=<SIZE>      (required) Virtual filesystem size.\n" \
   "label=<LABEL>               The filesystem label.\n" \
   "type=ext4|...               The filesystem type.\n" \
+  "wait=true                   Wait instead of rejecting second client.\n" \
   "command=<COMMAND>           Alternate command instead of mkfs."
 
 /* Because we rewind the exportsdir handle, we need a lock to protect