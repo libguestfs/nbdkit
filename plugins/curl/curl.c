libguestfs/nbdkit

commit f692e1001db73b9aca29c521268812de73bdd0f2

nbdkit/curl: add support for ssl configuration
Some clients may want to enforce specific TLS/SSL version and ciphers, what is not
possible at the moment.

To make it possible, curl plugin options are added:
- ssl-version, and
- ssl-cipher-list.

Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

plugins/curl/curl.c
@@ -83,6 +83,8 @@ const char *proxy = NULL;
 char *proxy_password = NULL;
 const char *proxy_user = NULL;
 bool sslverify = true;
+const char *ssl_version = NULL;
+const char *ssl_cipher_list = NULL;
 bool tcp_keepalive = false;
 bool tcp_nodelay = true;
 uint32_t timeout = 0;
@@ -301,6 +303,12 @@ curl_config (const char *key, const char *value)
     sslverify = r;
   }
 
+  else if (strcmp (key, "ssl-version") == 0)
+    ssl_version = value;
+
+  else if (strcmp (key, "ssl-cipher-list") == 0)
+    ssl_cipher_list = value;
+
   else if (strcmp (key, "tcp-keepalive") == 0) {
     r = nbdkit_parse_bool (value);
     if (r == -1)
@@ -403,6 +411,8 @@ curl_config_complete (void)
   "proxy-user=<USER>          The proxy user.\n" \
   "timeout=<TIMEOUT>          Set the timeout for requests (seconds).\n" \
   "sslverify=false            Do not verify SSL certificate of remote host.\n" \
+  "ssl-version=<VERSION>      Specify preferred TLS/SSL version.\n " \
+  "ssl-cipher-list=C1:C2:..   Specify TLS/SSL cipher suites to be used.\n" \
   "tcp-keepalive=true         Enable TCP keepalives.\n" \
   "tcp-nodelay=false          Disable Nagle’s algorithm.\n" \
   "unix-socket-path=<PATH>    Open Unix domain socket instead of TCP/IP.\n" \
@@ -520,6 +530,30 @@ curl_open (int readonly)
     curl_easy_setopt (h->c, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt (h->c, CURLOPT_SSL_VERIFYHOST, 0L);
   }
+  if (ssl_version) {
+    if (strcmp (ssl_version, "tlsv1") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
+    else if (strcmp (ssl_version, "sslv2") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_SSLv2);
+    else if (strcmp (ssl_version, "sslv3") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_SSLv3);
+    else if (strcmp (ssl_version, "tlsv1.0") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_0);
+    else if (strcmp (ssl_version, "tlsv1.1") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_1);
+    else if (strcmp (ssl_version, "tlsv1.2") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+    else if (strcmp (ssl_version, "tlsv1.3") == 0)
+      curl_easy_setopt (h->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
+    else {
+      display_curl_error (h, r, "curl_easy_setopt: CURLOPT_SSLVERSION [%s]",
+			  ssl_version);
+      goto err;
+    }
+
+  }
+  if (ssl_cipher_list)
+    curl_easy_setopt (h->c, CURLOPT_SSL_CIPHER_LIST, ssl_cipher_list);
   if (tcp_keepalive)
     curl_easy_setopt (h->c, CURLOPT_TCP_KEEPALIVE, 1L);
   if (!tcp_nodelay)

plugins/curl/nbdkit-curl-plugin.pod
@@ -242,6 +242,25 @@ Set the proxy username and password.
 
 Don't verify the SSL certificate of the remote host.
 
+=item B<ssl-cipher-list=>CIPHER[:CIPHER...]
+
+=item B<ssl-version=tlsv1>
+
+=item B<ssl-version=sslv2>
+
+=item B<ssl-version=sslv3>
+
+=item B<ssl-version=tlsv1.0>
+
+=item B<ssl-version=tlsv1.1>
+
+=item B<ssl-version=tlsv1.2>
+
+=item B<ssl-version=tlsv1.3>
+
+Set the SSL ciphers and TLS version.  For further information see
+L<CURLOPT_SSL_CIPHER_LIST(3)> and L<CURLOPT_SSLVERSION(3)>.
+
 =item B<tcp-keepalive=true>
 
 (nbdkit E<ge> 1.20)
@@ -465,6 +484,8 @@ L<CURLOPT_COOKIEJAR(3)>,
 L<CURLOPT_FOLLOWLOCATION(3)>,
 L<CURLOPT_HTTPHEADER(3)>,
 L<CURLOPT_PROXY(3)>,
+L<CURLOPT_SSL_CIPHER_LIST(3)>,
+L<CURLOPT_SSLVERSION(3)>,
 L<CURLOPT_TCP_KEEPALIVE(3)>,
 L<CURLOPT_TCP_NODELAY(3)>,
 L<CURLOPT_URL(3)>,

commit 36a05015853744be1d9f6f5403f7aa03099ae920

curl: Fix ssl-version so it checks value during plugin configuration
Fix this parameter so that instead of storing the string and resolving
it to a CURL_SSLVERSION_* value when creating the handle, we do it
when configuring the plugin.  This will ensure that errors are
detected early (the plugin will not start up), rather than having the
plugin start up and then fail on first connection.

Before this change:

  $ nbdkit -f curl https://foo ssl-version=blah

(no error until you connect first time)

After this change:

  $ nbdkit -f curl https://foo ssl-version=blah
  nbdkit: error: unknown ssl-version: blah

Fixes: commit f692e1001db73b9aca29c521268812de73bdd0f2

plugins/curl/curl.c
@@ -78,7 +78,7 @@ char *proxy_password = NULL;
 const char *proxy_user = NULL;
 bool sslverify = true;
 const char *ssl_cipher_list = NULL;
-const char *ssl_version = NULL;
+long ssl_version = CURL_SSLVERSION_DEFAULT;
 const char *tls13_ciphers = NULL;
 bool tcp_keepalive = false;
 bool tcp_nodelay = true;
@@ -315,8 +315,28 @@ curl_config (const char *key, const char *value)
     sslverify = r;
   }
 
-  else if (strcmp (key, "ssl-version") == 0)
-    ssl_version = value;
+  else if (strcmp (key, "ssl-version") == 0) {
+    if (strcmp (value, "default") == 0)
+      ssl_version = CURL_SSLVERSION_DEFAULT;
+    else if (strcmp (value, "tlsv1") == 0)
+      ssl_version = CURL_SSLVERSION_TLSv1;
+    else if (strcmp (value, "sslv2") == 0)
+      ssl_version = CURL_SSLVERSION_SSLv2;
+    else if (strcmp (value, "sslv3") == 0)
+      ssl_version = CURL_SSLVERSION_SSLv3;
+    else if (strcmp (value, "tlsv1.0") == 0)
+      ssl_version = CURL_SSLVERSION_TLSv1_0;
+    else if (strcmp (value, "tlsv1.1") == 0)
+      ssl_version = CURL_SSLVERSION_TLSv1_1;
+    else if (strcmp (value, "tlsv1.2") == 0)
+      ssl_version = CURL_SSLVERSION_TLSv1_2;
+    else if (strcmp (value, "tlsv1.3") == 0)
+      ssl_version = CURL_SSLVERSION_TLSv1_3;
+    else {
+      nbdkit_error ("unknown ssl-version: %s", value);
+      return -1;
+    }
+  }
 
   else if (strcmp (key, "ssl-cipher-list") == 0)
     ssl_cipher_list = value;

plugins/curl/curldefs.h
@@ -71,7 +71,7 @@ extern char *proxy_password;
 extern const char *proxy_user;
 extern bool sslverify;
 extern const char *ssl_cipher_list;
-extern const char *ssl_version;
+extern long ssl_version;
 extern const char *tls13_ciphers;
 extern bool tcp_keepalive;
 extern bool tcp_nodelay;

plugins/curl/nbdkit-curl-plugin.pod
@@ -259,6 +259,8 @@ Don't verify the SSL certificate of the remote host.
 
 =item B<ssl-cipher-list=>CIPHER[:CIPHER...]
 
+=item B<ssl-version=default>
+
 =item B<ssl-version=tlsv1>
 
 =item B<ssl-version=sslv2>

plugins/curl/pool.c
@@ -275,28 +275,8 @@ allocate_handle (void)
     curl_easy_setopt (ch->c, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt (ch->c, CURLOPT_SSL_VERIFYHOST, 0L);
   }
-  if (ssl_version) {
-    if (strcmp (ssl_version, "tlsv1") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
-    else if (strcmp (ssl_version, "sslv2") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_SSLv2);
-    else if (strcmp (ssl_version, "sslv3") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_SSLv3);
-    else if (strcmp (ssl_version, "tlsv1.0") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_0);
-    else if (strcmp (ssl_version, "tlsv1.1") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_1);
-    else if (strcmp (ssl_version, "tlsv1.2") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-    else if (strcmp (ssl_version, "tlsv1.3") == 0)
-      curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
-    else {
-      display_curl_error (ch, r, "curl_easy_setopt: CURLOPT_SSLVERSION [%s]",
-			  ssl_version);
-      goto err;
-    }
-
-  }
+  if (ssl_version != CURL_SSLVERSION_DEFAULT)
+    curl_easy_setopt (ch->c, CURLOPT_SSLVERSION, (long) ssl_version);
   if (ssl_cipher_list)
     curl_easy_setopt (ch->c, CURLOPT_SSL_CIPHER_LIST, ssl_cipher_list);
   if (tls13_ciphers) {